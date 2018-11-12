37e8f4c850723b11e41a3794d60cd9dcfc3fe948
cynepCTAPuk <CTAPuk@gmail.com>
2018-11-12 21:35:09 +0300

    MethodCall

--- src/javaRush/level02/lecture07/MethodCall.java
+++ src/javaRush/level02/lecture07/MethodCall.java
@@ -3,6 +3,9 @@ package javaRush.level02.lecture07;
 public class MethodCall {
   public static void main(String[] args) {
     print4("I like to move it, move it.");
+    int a = 5, b = 7;
+    int m = min(a, b);
+    System.out.println("Minimum is " + m);
   }
 
   public static void print4(String s) {
@@ -10,5 +13,12 @@ public class MethodCall {
     System.out.println(s);
     System.out.println(s);
     System.out.println(s);
+  }
+
+  public static int min(int c, int d) {
+    int m2;
+    if (c < d) m2 = c;
+    else m2 = d;
+    return m2;
   }
 }
