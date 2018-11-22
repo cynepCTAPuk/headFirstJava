58e85a52d02792fd4f93cc9823aedfc9c7459476
cynepCTAPuk <CTAPuk@gmail.com>
2018-11-22 22:06:24 +0300

    ---

--- algorithms/lafore/chap02/OrderedArray/orderedArray.java
+++ algorithms/lafore/chap02/OrderedArray/orderedArray.java
@@ -1,3 +1,5 @@
+package lafore.chap02.OrderedArray;
+
 // orderedArray.java
 // demonstrates ordered array class
 // to run this program: C>java OrderedApp
@@ -21,32 +23,25 @@ class OrdArray {
   public int find(long searchKey) {
     int lowerBound = 0;
     int upperBound = nElems - 1;
-    int curIn;
 
     while (true) {
-      curIn = (lowerBound + upperBound) / 2;
-      if (a[curIn] == searchKey)
-        return curIn;              // found it
-      else if (lowerBound > upperBound)
-        return nElems;             // can't search it
-      else                          // divide range
-      {
-        if (a[curIn] < searchKey)
-          lowerBound = curIn + 1; // it's in upper half
-        else
-          upperBound = curIn - 1; // it's in lower half
-      }  // end else divide range
-    }  // end while
-  }  // end search()
+      int curIn = (lowerBound + upperBound) / 2;
+      if (a[curIn] == searchKey) return curIn;              // found it
+      else {
+        if (lowerBound > upperBound) return nElems;         // can't search it
+        else {
+          if (a[curIn] < searchKey) lowerBound = curIn + 1; // it's in upper half
+          else upperBound = curIn - 1;                      // it's in lower half
+        } // end else divide range
+      }
+    } // end while
+  } // end search()
 
   //-----------------------------------------------------------
   public void insert(long value) {
     int j;
-    for (j = 0; j < nElems; j++)        // search where it goes
-      if (a[j] > value)            // (linear search)
-        break;
-    for (int k = nElems; k > j; k--)    // move bigger ones up
-      a[k] = a[k - 1];
+    for (j = 0; j < nElems; j++) if (a[j] > value) break;   // linear search & search where it goes
+    for (int k = nElems; k > j; k--) a[k] = a[k - 1];       // move bigger ones up
     a[j] = value;                  // insert it
     nElems++;                      // increment size
   }  // end insert()
@@ -54,13 +49,10 @@ class OrdArray {
   //-----------------------------------------------------------
   public boolean delete(long value) {
     int j = find(value);
-    if (j == nElems)                  // can't search it
-      return false;
-    else                           // found it
-    {
-      for (int k = j; k < nElems; k++) // move bigger ones down
-        a[k] = a[k + 1];
-      nElems--;                   // decrement size
+    if (j == nElems) return false; // can't search it
+    else {
+      for (int k = j; k < nElems; k++) a[k] = a[k + 1];     // move bigger ones down
+      nElems--;                                             // decrement size
       return true;
     }
   }  // end delete()
@@ -68,19 +60,17 @@ class OrdArray {
   //-----------------------------------------------------------
   public void display() {
     for (int j = 0; j < nElems; j++) System.out.print(a[j] + " ");
-    System.out.println("");
-  }
-  //-----------------------------------------------------------
+    System.out.println();
+  } // end display()
 }  // end class OrdArray
 
 ////////////////////////////////////////////////////////////////
 class OrderedApp {
   public static void main(String[] args) {
-    int maxSize = 100;             // array size
-    OrdArray arr;                  // reference to array
-    arr = new OrdArray(maxSize);   // create the array
+    int maxSize = 100;                    // array size
+    OrdArray arr = new OrdArray(maxSize); // reference to array &create the array
 
-    arr.insert(77);                // insert 10 items
+    arr.insert(77);                 // insert 10 items
     arr.insert(99);
     arr.insert(44);
     arr.insert(55);
@@ -91,16 +81,14 @@ class OrderedApp {
     arr.insert(66);
     arr.insert(33);
 
-    int searchKey = 55;            // search for item
+    int searchKey = 55;                   // search for item
     if (arr.find(searchKey) != arr.size()) System.out.println("Found " + searchKey);
     else System.out.println("Can't search " + searchKey);
 
-    arr.display();                 // display items
-
-    arr.delete(00);                // delete 3 items
+    arr.display();                        // display items
+    arr.delete(00);                 // delete 3 items
     arr.delete(55);
     arr.delete(99);
-
-    arr.display();                 // display items again
-  }  // end main()
-}  // end class OrderedApp
+    arr.display();                        // display items again
+  } // end main()
+} // end class OrderedApp
