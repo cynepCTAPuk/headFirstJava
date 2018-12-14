4ef0ead0fb8b5c9f8d2ac6d91ad87b4cbd65618d
cynepCTAPuk <CTAPuk@gmail.com>
2018-12-14 17:43:26 +0300

    heap review

--- algorithms/lafore/chap12/heap/heap.java
+++ algorithms/lafore/chap12/heap/heap.java
@@ -59,8 +59,7 @@ class Heap {
         int parent = (index - 1) / 2;
         Node bottom = heapArray[index];
 
-        while (index > 0 &&
-                heapArray[parent].getKey() < bottom.getKey()) {
+        while (index > 0 && heapArray[parent].getKey() < bottom.getKey()) {
             heapArray[index] = heapArray[parent];  // move it down
             index = parent;
             parent = (parent - 1) / 2;
@@ -80,16 +79,14 @@ class Heap {
     public void trickleDown(int index) {
         int largerChild;
         Node top = heapArray[index];       // save root
-        while (index < currentSize / 2)       // while node has at
-        {                               //    least one child,
+        while (index < currentSize / 2) { // while node has at least one child,
             int leftChild = 2 * index + 1;
             int rightChild = leftChild + 1;
             // search larger child
-            if (rightChild < currentSize &&  // (rightChild exists?)
-                    heapArray[leftChild].getKey() <
-                            heapArray[rightChild].getKey())
+            if (rightChild < currentSize &&         // (rightChild exists?)
+                    heapArray[leftChild].getKey() < heapArray[rightChild].getKey()) {
                 largerChild = rightChild;
-            else
+            } else
                 largerChild = leftChild;
             // top >= largerChild?
             if (top.getKey() >= heapArray[largerChild].getKey())
@@ -108,21 +105,22 @@ class Heap {
         int oldValue = heapArray[index].getKey(); // remember old
         heapArray[index].setKey(newValue);  // change to new
 
-        if (oldValue < newValue)             // if raised,
-            trickleUp(index);                // trickle it up
-        else                                // if lowered,
-            trickleDown(index);              // trickle it down
+        if (oldValue < newValue) {          // if raised,
+            trickleUp(index);               // trickle it up
+        } else                              // if lowered,
+            trickleDown(index);             // trickle it down
         return true;
     }  // end change()
 
     // -------------------------------------------------------------
     public void displayHeap() {
         System.out.print("heapArray: ");    // array format
-        for (int m = 0; m < currentSize; m++)
-            if (heapArray[m] != null)
+        for (int m = 0; m < currentSize; m++) {
+            if (heapArray[m] != null) {
                 System.out.print(heapArray[m].getKey() + " ");
-            else
-                System.out.print("-- ");
+            } else System.out.print("-- ");
+        }
+
         System.out.println();
         // heap format
         int nBlanks = 32;
@@ -132,26 +130,26 @@ class Heap {
         String dots = "...............................";
         System.out.println(dots + dots);      // dotted top line
 
-        while (currentSize > 0)              // for each heap item
-        {
-            if (column == 0)                  // first item in row?
-                for (int k = 0; k < nBlanks; k++)  // preceding blanks
+        while (currentSize > 0) {      // for each heap item
+            if (column == 0) {         // first item in row?
+                for (int k = 0; k < nBlanks; k++) {    // preceding blanks
                     System.out.print(' ');
-            // display item
-            System.out.print(heapArray[j].getKey());
+                }
+            }
 
-            if (++j == currentSize)           // done?
-                break;
+            System.out.print(heapArray[j].getKey()); // display item
 
-            if (++column == itemsPerRow)        // end of row?
-            {
+            if (++j == currentSize) break;    // done?
+
+            if (++column == itemsPerRow) {   // end of row?
                 nBlanks /= 2;                 // half the blanks
                 itemsPerRow *= 2;             // twice the items
                 column = 0;                   // start over on
                 System.out.println();         //    new row
             } else                             // next item on row
-                for (int k = 0; k < nBlanks * 2 - 2; k++)
+                for (int k = 0; k < nBlanks * 2 - 2; k++) {
                     System.out.print(' ');     // interim blanks
+                }
         }  // end for
         System.out.println("\n" + dots + dots); // dotted bottom line
     }  // end displayHeap()
@@ -176,10 +174,10 @@ class HeapApp {
         theHeap.insert(10);
         theHeap.insert(90);
 
-        while (true)                   // until [Ctrl]-[C]
-        {
+        boolean enter = true;
+        while (enter) {          // until [Ctrl]-[C]
             System.out.print("Enter first letter of ");
-            System.out.print("show, insert, remove, change: ");
+            System.out.print("show, insert, remove, change or x for EXIT: ");
             int choice = getChar();
             switch (choice) {
                 case 's':                        // show
@@ -189,14 +187,12 @@ class HeapApp {
                     System.out.print("Enter value to insert: ");
                     value = getInt();
                     success = theHeap.insert(value);
-                    if (!success)
-                        System.out.println("Can't insert; heap full");
+                    if (!success) System.out.println("Can't insert; heap full");
                     break;
                 case 'r':                        // remove
-                    if (!theHeap.isEmpty())
+                    if (!theHeap.isEmpty()) {
                         theHeap.remove();
-                    else
-                        System.out.println("Can't remove; heap empty");
+                    } else System.out.println("Can't remove; heap empty");
                     break;
                 case 'c':                        // change
                     System.out.print("Enter current index of item: ");
@@ -204,8 +200,11 @@ class HeapApp {
                     System.out.print("Enter new key: ");
                     value2 = getInt();
                     success = theHeap.change(value, value2);
-                    if (!success)
-                        System.out.println("Invalid index");
+                    if (!success) System.out.println("Invalid index");
+                    break;
+                case 'x':
+                    enter = false;
+                    System.out.println("Bye!");
                     break;
                 default:
                     System.out.println("Invalid entry\n");
