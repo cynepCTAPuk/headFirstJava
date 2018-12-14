b56c4392747baaaf7862c73c94cd0a8643e22f73
cynepCTAPuk <CTAPuk@gmail.com>
2018-12-14 20:36:12 +0300

    ---

--- algorithms/sortInt/Selection.java
+++ algorithms/sortInt/Selection.java
@@ -1,32 +1,36 @@
 package sortInt;
 
-import tools.Utils;
+import tools.*;
 
-import java.util.Arrays;
+import java.util.*;
 
 public class Selection {
-  public static void sort(int[] array) {
-    int left = 0, right = array.length - 1;
+    public static void sort(int[] array) {
+        int left = 0, right = array.length - 1;
+        int n = 0;
+        while (left < right) {
+            int min = left;
+            System.out.format("%-2d = %s%n",n++, Arrays.toString(array));
 
-    while (left < right) {
-      int min = left;
-      for (int i = left; i < right; i++) {
-        if (array[min] > array[i+1]) {
-          min = i + 1;
+            for (int i = left; i < right; i++) {
+                if (array[min] > array[i + 1]) {
+                    min = i + 1;
+                    System.out.format("\tmin = %d",array[min]);
+                }
+            }
+            System.out.println();
+            int temp = array[left];
+            array[left] = array[min];
+            array[min] = temp;
+            left++;
         }
-      }
-      int temp = array[left];
-      array[left] = array[min];
-      array[min] = temp;
-      left++;
     }
-  }
 
-  public static void main(String[] args) {
-    int[] array = new int[50];
-    Utils.fillRandom100(array);
-    System.out.println(Arrays.toString(array));
-    Selection.sort(array);
-    System.out.println(Arrays.toString(array));
-  }
+    public static void main(String[] args) {
+        int[] array = new int[20];
+        Utils.fillRandom100(array);
+//    System.out.println(Arrays.toString(array));
+        Selection.sort(array);
+        System.out.println(Arrays.toString(array));
+    }
 }
