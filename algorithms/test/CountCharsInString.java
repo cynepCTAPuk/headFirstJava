c400e5aac117af54af86b1abbff9d3a7973abca0
cynepCTAPuk <CTAPuk@gmail.com>
2018-12-17 20:38:03 +0300

    CountCharsInString

--- algorithms/test/CountCharsInString.java
+++ algorithms/test/CountCharsInString.java
@@ -2,7 +2,7 @@ package test;
 
 import java.util.*;
 
-public class ElementsTest {
+public class CountCharsInString {
 
     public static void main(String[] args) {
         String string = "Hello my little friends!";
