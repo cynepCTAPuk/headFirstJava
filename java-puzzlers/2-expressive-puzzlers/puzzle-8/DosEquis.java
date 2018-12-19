7062e0ae894009ec32284d7ae3daab2c58c48c3e
cynepCTAPuk <CTAPuk@gmail.com>
2018-12-19 18:22:56 +0300

    DosEquis

--- java-puzzlers/2-expressive-puzzlers/puzzle-8/DosEquis.java
+++ java-puzzlers/2-expressive-puzzlers/puzzle-8/DosEquis.java
@@ -2,7 +2,8 @@ public class DosEquis {
     public static void main(String[] args) {
         char x = 'X';
         int i = 0;
+
         System.out.print(true  ? x : 0);
-        System.out.print(false ? i : x); 
+        System.out.print(false ? i : x);
     }
 }
