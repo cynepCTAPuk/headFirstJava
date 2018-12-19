3f28fcf74cff3a65b1cf065bc60de95c71ccde71
cynepCTAPuk <CTAPuk@gmail.com>
2018-12-19 18:28:09 +0300

    Tweedledee

--- java-puzzlers/2-expressive-puzzlers/puzzle-10/Tweedledee.java
+++ java-puzzlers/2-expressive-puzzlers/puzzle-10/Tweedledee.java
@@ -1,15 +1,15 @@
 public class Tweedledee {
     public static void main(String[] args) {
         // Put your declarations for x and i here
-        double x = 1; // vk
+        int x = 1; // vk
         char i = 'a'; // vk
 
         System.out.format("char i = 'a';\t-> %3d%n", (int)i);
 
         x = x + i;  // Must be LEGAL
-        System.out.format("x = x + i;\t\t-> %3.0f%n", x);
+        System.out.format("x = x + i;\t\t-> %3d%n", x);
 
         x += i;     // Must be ILLEGAL
-        System.out.format("x += i;\t\t\t-> %3.0f%n", x);
+        System.out.format("x += i;\t\t\t-> %3d%n", x);
     }
 }
