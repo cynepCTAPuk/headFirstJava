eaa250439e181ca833f077abf1bd6704d55002c9
cynepCTAPuk <CTAPuk@gmail.com>
2018-12-19 19:34:25 +0300

    Me replaceAll

--- java-puzzlers/3-puzzlers-with-character/puzzle-20/com/javapuzzlers/Me.java
+++ java-puzzlers/3-puzzlers-with-character/puzzle-20/com/javapuzzlers/Me.java
@@ -3,6 +3,9 @@ package com.javapuzzlers;
 public class Me {
    public static void main(String[] args) {
       System.out.println(
-         Me.class.getName().replaceAll(".", "/") + ".class");
+              Me.class.getName().replaceAll(".", "/") + ".class");
+      System.out.println(Me.class.getName());
+
+      System.out.println("Never Say Never Again".replaceAll("e","^"));
    }
 }
