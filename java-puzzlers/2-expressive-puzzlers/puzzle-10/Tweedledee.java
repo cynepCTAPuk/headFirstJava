3f40e04404e39baaf3a8473b633b55e0b14a5f50
cynepCTAPuk <CTAPuk@gmail.com>
2018-12-19 17:41:16 +0300

    ---

--- java-puzzlers/2-expressive-puzzlers/puzzle-10/Tweedledee.java
+++ java-puzzlers/2-expressive-puzzlers/puzzle-10/Tweedledee.java
@@ -4,7 +4,12 @@ public class Tweedledee {
         double x = 1; // vk
         char i = 'a'; // vk
 
+        System.out.format("char i = 'a';\t-> %3d%n", (int)i);
+
         x = x + i;  // Must be LEGAL
+        System.out.format("x = x + i;\t\t-> %3.0f%n", x);
+
         x += i;     // Must be ILLEGAL
+        System.out.format("x += i;\t\t\t-> %3.0f%n", x);
     }
 }
--- java-puzzlers/2-expressive-puzzlers/puzzle-7/CleverSwap.java
+++ java-puzzlers/2-expressive-puzzlers/puzzle-7/CleverSwap.java
@@ -1,8 +1,20 @@
 public class CleverSwap {
     public static void main(String[] args) {
         int x = 1984;
+        System.out.format("%14s%n", Integer.toBinaryString(x));
         int y = 2001;
-        x ^= y ^= x ^= y;
-        System.out.println("x = " + x + "; y = " + y);
+        System.out.format("%14s%n", Integer.toBinaryString(y));
+        System.out.println();
+//        int a = x ^= y;
+//        int b = x ^= y ^= x;
+//        int c = x ^= y ^= x ^= y;
+//        System.out.format("%14s %d%n", Integer.toBinaryString(a), a);
+//        System.out.format("%14s %d%n", Integer.toBinaryString(b), b);
+//        System.out.format("%14s %d%n", Integer.toBinaryString(c), c);
+        System.out.format("%14s%n", Integer.toBinaryString(x ^= y));
+        System.out.format("%14s%n", Integer.toBinaryString(y ^= x ^= y));
+        System.out.format("%14s%n", Integer.toBinaryString(x ^= y ^= x ^= y));
+//        x ^= y ^= x ^= y;
+        System.out.println("x = " + x + ";\ny = " + y);
     }
 }
