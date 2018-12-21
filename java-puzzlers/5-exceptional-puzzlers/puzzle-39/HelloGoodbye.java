2b2b9d8f12bafa0429dbbdaa1232a10cfddcc74b
cynepCTAPuk <CTAPuk@gmail.com>
2018-12-21 20:09:26 +0300

    HelloGoodbye

--- java-puzzlers/5-exceptional-puzzlers/puzzle-39/HelloGoodbye.java
+++ java-puzzlers/5-exceptional-puzzlers/puzzle-39/HelloGoodbye.java
@@ -2,9 +2,9 @@ public class HelloGoodbye {
     public static void main(String[] args) {
         try {
             System.out.println("Hello world");
-            System.exit(0);
+//            System.exit(0); // vk
         } finally {
             System.out.println("Goodbye world");
         }
-    } 
+    }
 }
