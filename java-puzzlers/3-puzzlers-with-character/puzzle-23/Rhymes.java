7f566d2c42cab1cc1042763808a0aea4ef3413d4
cynepCTAPuk <CTAPuk@gmail.com>
2018-12-21 14:54:04 +0300

    Rhymes

--- java-puzzlers/3-puzzlers-with-character/puzzle-23/Rhymes.java
+++ java-puzzlers/3-puzzlers-with-character/puzzle-23/Rhymes.java
@@ -5,14 +5,21 @@ public class Rhymes {
 
     public static void main(String[] args) {
         StringBuffer word = null;
-        switch(rnd.nextInt(2)) {
-            case 1:  word = new StringBuffer('P');
-            case 2:  word = new StringBuffer('G');
-            default: word = new StringBuffer('M');
+        int rand = rnd.nextInt(2);
+        System.out.println(rnd.nextInt(2));
+
+        switch (rand) {
+            case 1:
+                word = new StringBuffer('P');
+            case 2:
+                word = new StringBuffer('G');
+            default:
+                word = new StringBuffer('M');
         }
         word.append('a');
         word.append('i');
         word.append('n');
         System.out.println(word);
+        System.out.println(word.capacity());
     }
 }
