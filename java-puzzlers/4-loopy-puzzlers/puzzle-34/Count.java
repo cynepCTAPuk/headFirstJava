2a12e3463dfa0ab27f1dcdcc45b4b3528af1ea81
cynepCTAPuk <CTAPuk@gmail.com>
2018-12-21 18:30:47 +0300

    Count

--- java-puzzlers/4-loopy-puzzlers/puzzle-34/Count.java
+++ java-puzzlers/4-loopy-puzzlers/puzzle-34/Count.java
@@ -1,6 +1,6 @@
 public class Count {
     public static void main(String[] args) {
-        final int START = 2000000000;
+        final int START = 2_000_000_000;
         int count = 0;
         for (float f = START; f < START + 50; f++)
             count++;
