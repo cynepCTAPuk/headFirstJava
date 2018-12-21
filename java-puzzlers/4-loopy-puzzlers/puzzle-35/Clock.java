bd631350fe4cdd60a446cc451d4757f01526a3a7
cynepCTAPuk <CTAPuk@gmail.com>
2018-12-21 17:24:54 +0300

    Clock

--- java-puzzlers/4-loopy-puzzlers/puzzle-35/Clock.java
+++ java-puzzlers/4-loopy-puzzlers/puzzle-35/Clock.java
@@ -1,9 +1,14 @@
 public class Clock {
     public static void main(String[] args) {
         int minutes = 0;
-        for (int ms = 0; ms < 60*60*1000; ms++)
-            if (ms % 60*1000 == 0)
+        for (int ms = 0; ms < 60 * 60 * 1000; ms++) {
+
+            if (ms % (60 * 1000) == 0) {
+                System.out.print(ms + " ");
                 minutes++;
+            }
+        }
+        System.out.println();
         System.out.println(minutes);
     }
 }
