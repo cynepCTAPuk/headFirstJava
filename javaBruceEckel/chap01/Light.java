a11b80747455fdb9c0df028d94e94b6329b23fab
cynepCTAPuk <CTAPuk@gmail.com>
2018-12-24 18:30:33 +0300

    ---

--- javaBruceEckel/chap01/Light.java
+++ javaBruceEckel/chap01/Light.java
@@ -1,42 +1,38 @@
 package chap01;
 
 public class Light {
-    private boolean status = false;
-    private int bright = 0;
+    private boolean status;
+    private int brightness;
 
-    public boolean getStatus() {
-        return status;
-    }
-
-    public void setStatus(boolean status) {
-        this.status = status;
-    }
-
-    public int getBright() {
-        return bright;
-    }
-
-    public void setBright(int bright) {
-        this.bright = bright;
+    public Light() {
+        brightness = 0;
+        status = false;
     }
 
     public void on() {
+        brightness = 50;
         status = true;
     }
 
     public void of() {
+        brightness = 0;
         status = false;
     }
 
-    public void brghten() {
-        if (bright < 100) {
-            bright++;
-            status = true;
-        }
+    public int brighten() {
+        if (status && brightness < 100) brightness++;
+        return brightness;
     }
 
-    public void dim() {
-        if (bright > 0) bright--;
-        if (bright == 0) status = false;
+    public int dim() {
+        if (status && brightness > 0) brightness--;
+        if (brightness == 0) of();
+        return brightness;
+    }
+
+    @Override
+    public String toString() {
+        return String.format("Light{ status= %5s, " +
+                "brightness=%3d}", status, brightness);
     }
 }
--- javaBruceEckel/chap01/LightTest.java
+++ javaBruceEckel/chap01/LightTest.java
@@ -3,7 +3,12 @@ package chap01;
 public class LightTest {
     public static void main(String[] args) {
         Light light = new Light();
-        light.getStatus();
+//        light.on();
+        System.out.println(light);
+        light.brighten();
+        System.out.println(light);
+        light.of();
+        System.out.println(light);
     }
 
 }
