d93f83a410961e10df28b460b20616ea3ccfab60
cynepCTAPuk <CTAPuk@gmail.com>
2018-12-24 20:04:21 +0300

    ---

--- javaBruceEckel/chap01/Shape.java
+++ javaBruceEckel/chap01/Shape.java
@@ -2,20 +2,21 @@ package chap01;
 
 public class Shape {
     void draw() {
-
+        System.out.println("Draw");
     }
     void erase() {
-
+        System.out.println("Erase");
     }
     void move() {
-
+        System.out.println("Move");
     }
 
     void getColor() {
-
+        System.out.println("Get color");
     }
 
     void setColor() {
-
+        System.out.println("Set color");
     }
+
 }
---
+++ javaBruceEckel/chap01/ShapeTest.java
@@ -0,0 +1,7 @@
+package chap01;
+
+public class ShapeTest {
+    public static void main(String[] args) {
+        
+    }
+}
