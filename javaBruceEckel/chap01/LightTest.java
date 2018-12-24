1c52bcc9b3c3a4924888f40059877fb82a3d3a7e
cynepCTAPuk <CTAPuk@gmail.com>
2018-12-24 18:35:25 +0300

    LightTest

--- javaBruceEckel/chap01/LightTest.java
+++ javaBruceEckel/chap01/LightTest.java
@@ -3,9 +3,11 @@ package chap01;
 public class LightTest {
     public static void main(String[] args) {
         Light light = new Light();
-//        light.on();
+        light.on();
         System.out.println(light);
         light.brighten();
+        System.out.println(light);
+        for (int i = 0; i < 51; i++) light.dim();
         System.out.println(light);
         light.of();
         System.out.println(light);
