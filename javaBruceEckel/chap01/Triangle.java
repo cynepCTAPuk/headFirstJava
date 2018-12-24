ebdcb074786a05ab2ab4005a2283dd2b675596b6
cynepCTAPuk <CTAPuk@gmail.com>
2018-12-24 20:21:35 +0300

    Triangle

--- javaBruceEckel/chap01/Triangle.java
+++ javaBruceEckel/chap01/Triangle.java
@@ -3,14 +3,14 @@ package chap01;
 class Triangle extends Shape {
     @Override
     void draw() {
-//        super.draw();
-        System.out.println("Draw triangle");
+        super.draw();
+//        System.out.println("Draw triangle");
     }
 
     @Override
     void erase() {
         super.erase();
-        System.out.println("Erase triangle");
+//        System.out.println("Erase triangle");
     }
 
     void flipVertical() {
