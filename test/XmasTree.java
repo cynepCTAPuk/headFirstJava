0295cc9de2d268490b32e1f1b65c40374ec99052
cynepCTAPuk <CTAPuk@gmail.com>
2018-12-25 17:27:46 +0300

    XmasTree

--- test/XmasTree.java
+++ test/XmasTree.java
@@ -1,22 +1,37 @@
 public class XmasTree {
     public static void main(String[] args) {
+        xmasTree1();
+    }
+
+    static void xmasTree1() {
         int i, j, k, r;
         int m = 1;
-        int height = 2, width = 10;
-        int space = width * 4;
+        int height = 3, width = 6;
+        int space = width * 5;
 
         for (r = 1; r <= height; r++) {
-            for (i = m; i <= width; i++) {
+            for (i = m; i <= width; i = i + 1) {
                 for (j = space; j >= i; j--) System.out.print(" ");
                 for (k = 1; k <= i; k++) System.out.print("# ");
                 System.out.println();
             }
-            m += width/2;
+            m += width / 2;
             width += 6;
         }
-        for (i = 1; i <= 7; i++) {
+        int branch = 4;
+        for (i = 1; i <= branch; i++) {
             for (j = space - 3; j >= 1; j--) System.out.print(" ");
             for (k = 1; k <= 4; k++) System.out.print("| ");
+            System.out.println();
+        }
+    }
+
+    static void xmasTree2() {
+        int i, j, k;
+        int width = 10;
+        for (i = 0; i < width; i++) {
+            for (j = 0; j < 10 - i; j++) System.out.print(" ");
+            for (k = 0; k < (2 * i + 1); k++) System.out.print("*");
             System.out.println();
         }
     }
