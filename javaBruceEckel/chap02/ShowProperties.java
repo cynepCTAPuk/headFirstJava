d865f729754e2a5f583e953bc1f48068fd000e79
cynepCTAPuk <CTAPuk@gmail.com>
2018-12-26 17:17:39 +0300

    ShowProperties

--- javaBruceEckel/chap02/ShowProperties.java
+++ javaBruceEckel/chap02/ShowProperties.java
@@ -3,7 +3,9 @@ package chap02;
 public class ShowProperties {
     public static void main(String[] args) {
         System.getProperties().list(System.out);
+        System.out.println("-".repeat(66));
         System.out.println(System.getProperty("user.name"));
+        System.out.println("-".repeat(66));
         System.out.println(System.getProperty("java.library.path"));
     }
 }
