a602fbf85e5906f96f94fd30144737e43a50d275
cynepCTAPuk <CTAPuk@gmail.com>
2018-12-26 14:21:41 +0300

    SimpleObject

--- javaBruceEckel/chap02/SimpleObject.java
+++ javaBruceEckel/chap02/SimpleObject.java
@@ -1,4 +1,33 @@
 package chap02;
 
 public class SimpleObject {
+    boolean aBoolean;
+    char aChar;
+    byte aByte;
+    short aShort;
+    int anInt;
+    long aLong;
+    float aFloat;
+    double aDouble;
+    SimpleObject simpleObject;
+
+    @Override
+    public String toString() {
+        return "SimpleObject{" +
+                "aBoolean=" + aBoolean +
+                ", aChar=" + aChar +
+                ", aByte=" + aByte +
+                ", aShort=" + aShort +
+                ", anInt=" + anInt +
+                ", aLong=" + aLong +
+                ", aFloat=" + aFloat +
+                ", aDouble=" + aDouble +
+                ", simpleObject=" + simpleObject +
+                '}';
+    }
+
+    public static void main(String[] args) {
+        SimpleObject simpleObject = new SimpleObject();
+        System.out.println(simpleObject);
+    }
 }
