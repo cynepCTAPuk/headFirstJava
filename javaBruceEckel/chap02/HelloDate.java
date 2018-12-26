bac5efd9c8ac781d7465407f4099119c6db98eba
cynepCTAPuk <CTAPuk@gmail.com>
2018-12-26 18:43:19 +0300

    HelloDate

--- javaBruceEckel/chap02/HelloDate.java
+++ javaBruceEckel/chap02/HelloDate.java
@@ -1,8 +1,22 @@
 package chap02;
 
+//: object/HelloDate.java
+
 import java.util.*;
 
+/**
+ * The first Thinking in Java example program.
+ * Displays a string and today’s date.
+ *
+ * @author Bruce Eckel
+ * @author www.MindView.net
+ * @version 4.0
+ */
 public class HelloDate {
+    /** Entry point to class & application.
+     * @param args array of string arguments
+     * @throws exceptions No exceptions thrown
+     */
     public static void main(String[] args) {
         System.out.print("Hello, it's ");
         System.out.println(new Date());
