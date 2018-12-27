f97d586edb61f5da071a5e6c1046442f8e3ea0db
cynepCTAPuk <CTAPuk@gmail.com>
2018-12-27 15:32:24 +0300

    auto-increment & auto-decrement

--- javaBruceEckel/chap03/AutoInc.java
+++ javaBruceEckel/chap03/AutoInc.java
@@ -7,9 +7,11 @@ public class AutoInc {
         int i = 1;
         System.out.println("i : " + i);
         System.out.println("++i : " + ++i); // Pre-increment
+        System.out.println("i : " + i);
         System.out.println("i++ : " + i++); // Post-increment
         System.out.println("i : " + i);
         System.out.println("--i : " + --i); // Pre-decrement
+        System.out.println("i : " + i);
         System.out.println("i-- : " + i--); // Post-decrement
         System.out.println("i : " + i);
     }
