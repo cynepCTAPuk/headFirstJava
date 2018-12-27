124ed7161e7bb20aac989f17adbc092f8cf802f3
cynepCTAPuk <CTAPuk@gmail.com>
2018-12-27 15:20:04 +0300

    RandomSeed test

--- javaBruceEckel/chap03/RandomSeed.java
+++ javaBruceEckel/chap03/RandomSeed.java
@@ -4,7 +4,8 @@ import java.util.*;
 
 public class RandomSeed {
     public static void main(String[] args) {
-        Random random = new Random(47);
+        long seed = 47;
+        Random random = new Random(seed);
         for (int i = 0; i < 150; i++) {
             if (i % 30 == 0) System.out.println();
             System.out.print(random.nextInt(89) + 10 + " ");
