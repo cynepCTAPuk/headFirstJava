599564d826a1eaa1ee8e8a1385b5ec5d73866b02
cynepCTAPuk <CTAPuk@gmail.com>
2018-12-28 20:18:36 +0300

    Exponents

--- javaBruceEckel/chap03/Exponents.java
+++ javaBruceEckel/chap03/Exponents.java
@@ -6,11 +6,23 @@ public class Exponents {
     public static void main(String[] args) {
 // Uppercase and lowercase ‘e’ are the same:
         float expFloat = 1.39e-43f;
-        expFloat = 1.39E-43f;
         System.out.println(expFloat);
+        System.out.format("expFloat 1.39E-43f = %,g%n", expFloat);
         double expDouble1 = 47e47d; // ‘d’ is optional
         double expDouble2 = 47e47; // Automatically double
-        System.out.println(expDouble1 + " " + expDouble2);
+        System.out.println(expDouble1);
+        System.out.format(
+                "expDouble1 47e47d = %,g%n" +
+                        "expDouble2 47e47  = %,g%n",
+                expDouble1, expDouble2);
+        float bigFloat = 0xfffffff;
+        System.out.format("bigFloat = %f %g%n", bigFloat, bigFloat);
+        double bigDouble = 0xffffffff;
+        System.out.format("bigDouble = %f %g%n", bigDouble, bigDouble);
+        System.out.println("Float.MAX_VALUE:  " + Float.MAX_VALUE);
+        System.out.println("Float.MIN_VALUE:  " + Float.MIN_VALUE);
+        System.out.println("Double.MAX_VALUE: " + Double.MAX_VALUE);
+        System.out.println("Double.MIN_VALUE: " + Double.MIN_VALUE);
     }
 } /* Output:
 1.39E-43
