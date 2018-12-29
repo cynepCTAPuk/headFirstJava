8d2d162b30c392de09ff163a4e4b5e703cfff21b
cynepCTAPuk <CTAPuk@gmail.com>
2018-12-29 18:49:42 +0300

    Exercise 13

---
+++ javaBruceEckel/chap03/Exercise13.java
@@ -0,0 +1,21 @@
+package chap03;
+
+public class Exercise13 {
+    public static void main(String[] args) {
+//        int i = (1 << 31);
+//        int i = 0x10_00_00_00;
+//        int i = 020_00_00_00_00;
+//        int i = 268_435_456;
+//        int i = ((int) Math.pow(2, 31) << 1);
+        char i = 'a';
+        String integString = Integer.toBinaryString(i);
+        String octalString = Integer.toOctalString(i);
+        String hexalString = Integer.toHexString(i);
+        System.out.println(integString + " " + integString.length() + " " + (int)i);
+        System.out.println(octalString + " " + octalString.length() + " " + i);
+        System.out.println(hexalString + " " + hexalString.length() + " " + i);
+        for (int j = 0; j < 32; j++) {
+            System.out.format("%10s", i >> j);
+        }
+    }
+}
