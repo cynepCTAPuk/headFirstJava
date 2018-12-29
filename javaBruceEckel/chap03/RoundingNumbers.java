43a02e81ab3f84ff5ab42e5297b425b6d52d8b2b
cynepCTAPuk <CTAPuk@gmail.com>
2018-12-29 19:35:38 +0300

    RoundingNumbers

--- javaBruceEckel/chap03/RoundingNumbers.java
+++ javaBruceEckel/chap03/RoundingNumbers.java
@@ -1,4 +1,19 @@
 package chap03;
 
+//: operators/RoundingNumbers.java
+// Rounding floats and doubles.
 public class RoundingNumbers {
-}
+    public static void main(String[] args) {
+        double above = 0.7, below = 0.4;
+        float fabove = 0.7f, fbelow = 0.4f;
+        System.out.println("Math.round(above): " + Math.round(above));
+        System.out.println("Math.round(below): " + Math.round(below));
+        System.out.println("Math.round(fabove): " + Math.round(fabove));
+        System.out.println("Math.round(fbelow): " + Math.round(fbelow));
+    }
+} /* Output:
+Math.round(above): 1
+Math.round(below): 0
+Math.round(fabove): 1
+Math.round(fbelow): 0
+*///:~
