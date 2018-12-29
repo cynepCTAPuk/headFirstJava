3f9c0067b3188c4353a14cf571d796d15b1339f2
cynepCTAPuk <CTAPuk@gmail.com>
2018-12-29 18:53:20 +0300

    Exercise 11

--- javaBruceEckel/chap03/Exercise11.java
+++ javaBruceEckel/chap03/Exercise11.java
@@ -14,7 +14,7 @@ public class Exercise11 {
         System.out.println(octalString + " " + octalString.length() + " " + i);
         System.out.println(hexalString + " " + hexalString.length() + " " + i);
         for (int j = 0; j < 32; j++) {
-            System.out.format("%10s",i>>>j);
+            System.out.format("%,20d",i>>>j);
         }
     }
 }
