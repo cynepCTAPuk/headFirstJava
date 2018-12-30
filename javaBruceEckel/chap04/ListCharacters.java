d19bde74825cccc6101b899e66e004fb236c0700
cynepCTAPuk <CTAPuk@gmail.com>
2018-12-30 17:56:13 +0300

    ListCharacters

--- javaBruceEckel/chap04/ListCharacters.java
+++ javaBruceEckel/chap04/ListCharacters.java
@@ -5,9 +5,23 @@ package chap04;
 // all the lowercase ASCII letters.
 public class ListCharacters {
     public static void main(String[] args) {
-        for (char c = 0; c < 128; c++)
-            if (Character.isLowerCase(c))
-                System.out.println("value: " + (int) c + " character: " + c);
+        int i = 0, j = 0, k = 0;
+/*
+        for (char c = 0; c < 128; c++) {
+            if (Character.isLowerCase(c)) System.out.print((int) c + ":" + c + " ");
+            if (++i % 10 == 0) System.out.println();
+        }
+*/
+/*
+        for (char c = 0; c < 128; c++) {
+            if (Character.isLetter(c)) System.out.print((int) c + ":" + c + " ");
+            if (++j % 10 == 0) System.out.println();
+        }
+*/
+        for (char c = 0; c < 128; c++) {
+            if (Character.isUpperCase(c)) System.out.print((int) c + ":" + c + " ");
+            if (++k % 10 == 0) System.out.println();
+        }
     }
 } /* Output:
 value: 97 character: a
