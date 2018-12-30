9767015c4b684049338f183699288fe66c936673
cynepCTAPuk <CTAPuk@gmail.com>
2018-12-30 17:40:38 +0300

    WhileTest

--- javaBruceEckel/chap04/WhileTest.java
+++ javaBruceEckel/chap04/WhileTest.java
@@ -1,4 +1,19 @@
 package chap04;
 
+//: control/WhileTest.java
+// Demonstrates the while loop.
 public class WhileTest {
-}
+    static boolean condition() {
+        boolean result = Math.random() < 0.99;
+        System.out.print(result + ", ");
+        return result;
+    }
+
+    public static void main(String[] args) {
+        int i = 0;
+        while (condition()) {
+            System.out.println("Inside ‘while’ " + (++i));
+        }
+        System.out.println("Exited ‘while’");
+    }
+} /* (Execute to see output) *///:~
