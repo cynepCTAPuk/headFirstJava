353c0f77a67600aed41b2aec628e3e4e56775ee4
cynepCTAPuk <CTAPuk@gmail.com>
2018-12-30 21:06:52 +0300

    Exercise 4

--- javaBruceEckel/chap04/Exercise4.java
+++ javaBruceEckel/chap04/Exercise4.java
@@ -2,9 +2,10 @@ package chap04;
 
 public class Exercise4 {
     public static void main(String[] args) {
-        int max = 1_000_000;
+        int max = 1_000;
         int count;
-        int qty = 0;
+        int qty = 1;
+        System.out.format("%,d ", 2);
         for (int i = 3; i < max; i += 2) {
             count = 0;
             for (int j = 2; j < (i / 2); j++) {
