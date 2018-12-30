cc0f1e808fff28cacbc11762f02a8b4748db3e53
cynepCTAPuk <CTAPuk@gmail.com>
2018-12-30 18:04:16 +0300

    Exercise 1

---
+++ javaBruceEckel/chap04/Exercise1.java
@@ -0,0 +1,10 @@
+package chap04;
+
+public class Exercise1 {
+    public static void main(String[] args) {
+        int j=0;
+        for (int i = 0; i < 100; i++) {
+            System.out.print(++j+" ");
+        }
+    }
+}
