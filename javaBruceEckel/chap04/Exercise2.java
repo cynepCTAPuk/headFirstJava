cf492578cbccafaf8114bf2552f1459d0f32acf2
cynepCTAPuk <CTAPuk@gmail.com>
2018-12-30 18:16:19 +0300

    Exercise 2

--- javaBruceEckel/chap04/Exercise2.java
+++ javaBruceEckel/chap04/Exercise2.java
@@ -1,4 +1,23 @@
 package chap04;
 
+import java.util.*;
+
 public class Exercise2 {
+    public static void main(String[] args) {
+        Random random = new Random();
+        Random random1 = new Random();
+        int one = random.nextInt(10);
+        int two;
+        System.out.println(one);
+        for (int i = 0; i < 25; i++) {
+            two = random1.nextInt(10);
+            if (two > one) {
+                System.out.print(two + "> ");
+            } else {
+                if (two == one) {
+                    System.out.print(two + "= ");
+                } else System.out.print(two + "< ");
+            }
+        }
+    }
 }
