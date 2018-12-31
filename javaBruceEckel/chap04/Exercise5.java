715acfb108a4e4d6e01644921b093401b339c63b
cynepCTAPuk <CTAPuk@gmail.com>
2018-12-31 03:04:59 +0300

    Exercise5

--- javaBruceEckel/chap04/Exercise5.java
+++ javaBruceEckel/chap04/Exercise5.java
@@ -2,7 +2,7 @@ package chap04;
 
 public class Exercise5 {
     public static void main(String[] args) {
-        int number = pow(2, 0) - 6;
+        int number = pow(2, 0);
         for (int i = 0; i < 11; i++) {
             System.out.format("%32s ", toBinary(number));
             System.out.format("%32s %4d\n", Integer.toBinaryString(number), number);
@@ -10,17 +10,33 @@ public class Exercise5 {
         }
     }
 
+    private static String oddeven(int temp, String binary) {
+        return temp % 2 == 0 ? 0 + binary : 1 + binary;
+    }
+
     static String toBinary(int number) {
         String binary = "";
         int temp = number;
         if (temp == 0) binary += 0;
         while (temp > 0) {
+            binary = (temp % 2) + binary;
+            temp = temp / 2;
+        }
+/*
+        while (temp > 0) {
+            binary = oddeven(temp, binary);
+            temp = temp / 2;
+        }
+*/
+/*
+        while (temp > 0) {  // it works!
             if (temp % 2 == 0) binary = 0 + binary;
             else binary = 1 + binary;
             temp = temp / 2;
         }
+*/
 /*
-        while (temp < 0) {
+        while (temp < 0) { // should be done!!!
             if (temp % 2 == 0) binary = 0 + binary;
             else binary = 1 + binary;
             temp = temp / 2;
