e735daa8f73a78bf75ec05221c7b75a5cf6c943e
cynepCTAPuk <CTAPuk@gmail.com>
2018-12-31 12:50:17 +0300

    BinaryString

--- javaBruceEckel/chap04/BinaryString.java
+++ javaBruceEckel/chap04/BinaryString.java
@@ -1,23 +1,41 @@
 package chap04;
 
 public class BinaryString {
+    static final char[] digits = {
+            '0', '1', '2', '3', '4', '5',
+            '6', '7', '8', '9', 'a', 'b',
+            'c', 'd', 'e', 'f', 'g', 'h',
+            'i', 'j', 'k', 'l', 'm', 'n',
+            'o', 'p', 'q', 'r', 's', 't',
+            'u', 'v', 'w', 'x', 'y', 'z'
+    };
+
     public static void main(String[] args) {
-        int number = pow(2, 0)-6;
+        int number = pow(2, 0);
         for (int i = 0; i < 11; i++) {
             System.out.format("%32s ", toBinary(number));
-            System.out.format("%32s %4d\n", Integer.toBinaryString(number), number);
+//            System.out.format("%32s %4d\n", Integer.toBinaryString(number), number);
+            System.out.println(" " + number);
             number++;
+
         }
     }
 
-    static String toBinary(int number) {
-        String binary = "";
-        if (number == 0) binary += 0;
-        while (number > 0) {
-            binary = (number % 2) + binary;
-            number = number / 2;
-        }
-        return binary;
+    public static String toBinary(int i) {
+        return toUnsignedString(i, 1);
+    }
+
+    private static String toUnsignedString(int number, int shift) {
+        char[] buf = new char[32];
+        int charPos = 32;
+        int radix = 1 << shift;
+        int mask = radix - 1;
+        do {
+            buf[--charPos] = digits[number & mask];
+            number >>>= shift;
+        } while (number != 0);
+
+        return new String(buf, charPos, (32 - charPos));
     }
 
     static int pow(int base, int exp) {
