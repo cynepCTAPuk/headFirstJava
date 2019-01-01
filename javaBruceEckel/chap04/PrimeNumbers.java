1c1b36109f6aa7e43d3d6c48ecca990778ec844c
cynepCTAPuk <CTAPuk@gmail.com>
2019-01-01 21:13:08 +0300

    PrimeNumbers

--- javaBruceEckel/chap04/PrimeNumbers.java
+++ javaBruceEckel/chap04/PrimeNumbers.java
@@ -4,7 +4,7 @@ public class PrimeNumbers {
     static int[] primeNumbers;
 
     public static void main(String[] args) {
-        int n = 1_000_000;
+        int n = 1_000;
         long t0 = System.nanoTime();
         printPrimeNumbers(n);
         long t1 = System.nanoTime();
@@ -22,32 +22,19 @@ public class PrimeNumbers {
         k = 0;
 
         for (n = 2; n < numbers; n++) { // Проверяем на простоту все числа
-
             for (i = 0; i < k; i++) { // Делим на все уже найденные простые числа
-                if ((n % primeNumbers[i]) == 0) { // Если поделилось - не простое
-                    break;
-                }
+                // Если поделилось - не простое
+                if ((n % primeNumbers[i]) == 0) break;
             }
             /* Если предыдущий цикл нормально закончился - значит, ни на что
              * не поделилось - простое - добавляем в массив */
-            if (i == k) {
-                primeNumbers[k++] = n;
-            }
+            if (i == k) primeNumbers[k++] = n;
         }
         /* Выводим все найденные числа */
-//        System.out.printf("Простые числа, не превосходящие %,d\n",numbers);
-        for (i = 0; i < k; i++)
-            System.out.printf("%,d ", primeNumbers[i]);
+        System.out.printf("Простые числа, не превосходящие %,d\n", numbers);
+        for (i = 0; i < k; i++) System.out.printf("%,d ", primeNumbers[i]);
         System.out.printf("\nКол-во простых чисел: %d\n", k);
 
         return k;
     } /* End of 'printPrimeNumbers' function */
-
-    static void printArray(int[] array, int k) {
-        /* Выводим все найденные числа */
-//        System.out.printf("Простые числа, не превосходящие %,d\n",numbers);
-        for (int i = 0; i < k; i++)
-            System.out.printf("%,d ", primeNumbers[i]);
-        System.out.printf("\nКол-во простых чисел: %d", k);
-    }
 }
