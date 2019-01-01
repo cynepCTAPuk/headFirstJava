7b5336a9595a923e7b1744c021c11e4fd43d1cae
cynepCTAPuk <CTAPuk@gmail.com>
2019-01-01 21:27:24 +0300

    ForEachString

---
+++ javaBruceEckel/chap04/ForEachString.java
@@ -0,0 +1,11 @@
+package chap04;
+
+//: control/ForEachString.java
+public class ForEachString {
+    public static void main(String[] args) {
+        String string = "An African Swallow";
+        for(char c : string.toCharArray() ) System.out.print(c + " ");
+    }
+} /* Output:
+A n A f r i c a n S w a l l o w
+*///:~
