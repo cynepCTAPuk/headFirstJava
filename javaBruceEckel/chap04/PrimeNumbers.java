fd1c53f2075324f5f94dde9a44a5726e16a53f00
cynepCTAPuk <CTAPuk@gmail.com>
2019-01-01 18:24:46 +0300

    PrimeNumbers

--- javaBruceEckel/chap04/PrimeNumbers.java
+++ javaBruceEckel/chap04/PrimeNumbers.java
@@ -1,24 +1,53 @@
 package chap04;
 
 public class PrimeNumbers {
+    static int[] primeNumbers;
+
     public static void main(String[] args) {
-        int max = 1_000;
-        int count;
-        int qty = 1;
-        System.out.format("%,d ", 2);
-        for (int i = 3; i < max; i += 2) {
-            count = 0;
-            for (int j = 2; j < (i / 2); j++) {
-                if (i % j == 0) {
-                    count++;
+        int n = 1_000;
+        long t0 = System.nanoTime();
+        printPrimeNumbers(n);
+        long t1 = System.nanoTime();
+        System.out.println(t1-t0);
+    }
+
+    static int printPrimeNumbers(int numbers) {   // Основная функция
+        // Массив найденных простых чисел
+        int primes = numbers / 2;
+        primeNumbers = new int[primes];
+
+        int i; // номер проверяемого числа - делителя
+        int k; // количество найденных простых чисел
+        int n; // очередное проверяемое число
+        k = 0;
+
+        for (n = 2; n < numbers; n++) { // Проверяем на простоту все числа
+
+            for (i = 0; i < k; i++) { // Делим на все уже найденные простые числа
+                if ((n % primeNumbers[i]) == 0) { // Если поделилось - не простое
                     break;
                 }
             }
-            if (count == 0) {
-                System.out.format("%,d ", i);
-                qty++;
+            /* Если предыдущий цикл нормально закончился - значит, ни на что
+             * не поделилось - простое - добавляем в массив */
+            if (i == k) {
+                primeNumbers[k++] = n;
             }
         }
-        System.out.println("\nPrime numbers quantity " + qty);
+        /* Выводим все найденные числа */
+//        System.out.printf("Простые числа, не превосходящие %,d\n",numbers);
+        for (i = 0; i < k; i++)
+            System.out.printf("%,d ", primeNumbers[i]);
+        System.out.printf("\nКол-во простых чисел: %d", k);
+
+        return k;
+    } /* End of 'printPrimeNumbers' function */
+
+    static void printArray(int[] array, int k) {
+        /* Выводим все найденные числа */
+//        System.out.printf("Простые числа, не превосходящие %,d\n",numbers);
+        for (int i = 0; i < k; i++)
+            System.out.printf("%,d ", primeNumbers[i]);
+        System.out.printf("\nКол-во простых чисел: %d", k);
     }
 }
