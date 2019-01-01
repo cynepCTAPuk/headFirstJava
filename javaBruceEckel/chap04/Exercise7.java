7a4139228b05e43f874721954b37bf9a214ff9af
cynepCTAPuk <CTAPuk@gmail.com>
2019-01-02 00:30:43 +0300

    Exercise7

--- javaBruceEckel/chap04/Exercise7.java
+++ javaBruceEckel/chap04/Exercise7.java
@@ -2,9 +2,11 @@ package chap04;
 
 public class Exercise7 {
     public static void main(String[] args) {
-        int j=0;
+        int j = 0;
         for (int i = 0; i < 100; i++) {
-            System.out.print(++j+" ");
+            if (i == 99) break;
+//            if (i == 99) return;
+            System.out.print(++j + " ");
         }
     }
 }
