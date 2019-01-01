40306133eb048115bfe898721acc3b4ba47fae15
cynepCTAPuk <CTAPuk@gmail.com>
2019-01-02 00:16:36 +0300

    ForEachInt

--- javaBruceEckel/chap04/ForEachInt.java
+++ javaBruceEckel/chap04/ForEachInt.java
@@ -1,5 +1,7 @@
 package chap04;
 
+import static chap04.Range.range;
+
 //: control/ForEachInt.java
 public class ForEachInt {
     public static void main(String[] args) {
@@ -11,21 +13,6 @@ public class ForEachInt {
 
         for (int i : range(5, 20, 3)) System.out.print(i + " ");   // 5..20 step 3
         System.out.println();
-    }
-
-    static int[] range(int end) {
-        return range(0, end, 1);
-    }
-
-    static int[] range(int start, int end) {
-        return range(start, end, 1);
-    }
-
-    static int[] range(int start, int end, int step) {
-        int length = (end - start) / step;
-        int[] array = new int[length];
-        for (int i = 0; i < array.length; i++) array[i] = (start + (step * i));
-        return array;
     }
 } /* Output:
 0 1 2 3 4 5 6 7 8 9
