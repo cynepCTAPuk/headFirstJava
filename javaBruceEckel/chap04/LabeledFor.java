7a84c57cd040a90973a9954a9f325f81606e3807
cynepCTAPuk <CTAPuk@gmail.com>
2019-01-02 00:51:41 +0300

    LabeledFor

--- javaBruceEckel/chap04/LabeledFor.java
+++ javaBruceEckel/chap04/LabeledFor.java
@@ -1,39 +1,41 @@
 package chap04;
 
-import static util.Print.print;
+import static util.Print.*;
 
 //: control/LabeledFor.java
 // For loops with "labeled break" and "labeled continue."
 public class LabeledFor {
     public static void main(String[] args) {
         int i = 0;
-        outer: // Can’t have statements here
-        for(; true ;) { // infinite loop
-            inner: // Can’t have statements here
-            for(; i < 10; i++) {
+        outer:
+        // Can’t have statements here
+        for (; true; ) {                     // infinite loop
+            inner:
+            // Can’t have statements here
+            for (; i < 10; i++) {
                 print("i = " + i);
-                if(i == 2) {
+                if (i == 2) {
                     print("continue");
                     continue;
                 }
-                if(i == 3) {
+                if (i == 3) {
                     print("break");
-                    i++; // Otherwise i never
-// gets incremented.
+                    i++;                    // Otherwise i never
+                    // gets incremented.
                     break;
                 }
-                if(i == 7) {
+                if (i == 7) {
                     print("continue outer");
-                    i++; // Otherwise i never
-// gets incremented.
+                    i++;                    // Otherwise i never
+                    // gets incremented.
                     continue outer;
                 }
-                if(i == 8) {
+                if (i == 8) {
                     print("break outer");
                     break outer;
                 }
-                for(int k = 0; k < 5; k++) {
-                    if(k == 3) {
+                for (int k = 0; k < 5; k++) {
+                    if (k == 3) {
                         print("continue inner");
                         continue inner;
                     }
