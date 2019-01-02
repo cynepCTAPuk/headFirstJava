b28b7867d45e26e6f81efd906c8e58371637e541
cynepCTAPuk <CTAPuk@gmail.com>
2019-01-02 17:20:15 +0300

    VowelsAndConsonants

--- javaBruceEckel/chap04/VowelsAndConsonants.java
+++ javaBruceEckel/chap04/VowelsAndConsonants.java
@@ -2,25 +2,31 @@ package chap04;
 
 //: control/VowelsAndConsonants.java
 // Demonstrates the switch statement.
+
 import java.util.*;
+
 import static util.Print.*;
+
 public class VowelsAndConsonants {
     public static void main(String[] args) {
         Random rand = new Random(47);
-        for(int i = 0; i < 100; i++) {
-            int c = rand.nextInt(26) + ‘a’;
-            printnb((char)c + ", " + c + ": ");
-            switch(c) {
-case ‘a’:
-case ‘e’:
-case ‘i’:
-case ‘o’:
-case ‘u’: print("vowel");
-                break;
-case ‘y’:
-case ‘w’: print("Sometimes a vowel");
-                break;
-                default: print("consonant");
+        for (int i = 0; i < 100; i++) {
+            int c = rand.nextInt(26) + 'a';
+            printnb((char) c + ", " + c + ": ");
+            switch (c) {
+                case 'a':
+                case 'e':
+                case 'i':
+                case 'o':
+                case 'u':
+                    print("vowel");
+                    break;
+                case 'y':
+                case 'w':
+                    print("Sometimes a vowel");
+                    break;
+                default:
+                    print("consonant");
             }
         }
     }
