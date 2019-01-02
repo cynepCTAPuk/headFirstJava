2a0d38a214ef2f9d8d9930201c730d86833e975d
cynepCTAPuk <CTAPuk@gmail.com>
2019-01-02 18:28:47 +0300

    Exercise 9a

---
+++ javaBruceEckel/chap04/Exercise9a.java
@@ -0,0 +1,19 @@
+package chap04;
+
+public class Exercise9a {
+    public static void main(String[] args) {
+        int n = 20;
+        int prev = 1;
+        int curr = 1;
+        int next;
+        System.out.print(prev + " " + curr + " ");
+        for (int i = 2; i < n; i++) {
+            next = prev + curr;
+            System.out.print(next);
+            System.out.print(" ");
+            prev = curr;
+            curr = next;
+
+        }
+    }
+}
