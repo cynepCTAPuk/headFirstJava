4a3297721db01dcaf05ed1d0d4a678a4ed210377
cynepCTAPuk <CTAPuk@gmail.com>
2019-01-02 17:54:13 +0300

    Exercise 8

--- javaBruceEckel/chap04/Exercise8.java
+++ javaBruceEckel/chap04/Exercise8.java
@@ -1,4 +1,25 @@
 package chap04;
 
 public class Exercise8 {
+    public static void main(String[] args) {
+        for (int i = 0; i < 5; i++) {
+            switch (i) {
+                case (0):
+                    System.out.format("%d",i);
+//                    break;
+                case (1):
+                    System.out.format("%d",i);
+//                    break;
+                case (2):
+                    System.out.format("%d",i);
+//                    break;
+                case (3):
+                    System.out.format("%d",i);
+//                    break;
+                default:
+                    System.out.format("%d",i);
+            }
+            System.out.println();
+        }
+    }
 }
