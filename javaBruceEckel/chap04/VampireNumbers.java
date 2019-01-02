0ec02ddb80448adaf325f56a2e0481d964fde919
cynepCTAPuk <CTAPuk@gmail.com>
2019-01-03 02:22:51 +0300

    VampireNumbers

--- javaBruceEckel/chap04/VampireNumbers.java
+++ javaBruceEckel/chap04/VampireNumbers.java
@@ -3,39 +3,52 @@ package chap04;
 public class VampireNumbers {
 
     public static void main(String[] args) {
-        int qty = 0;
-        int start = 10;
+        int begin = 10;
         int[] numbers = new int[90];
-        for (int i = 0; i < 90; i++) numbers[i] = start++;
+        for (int i = 0; i < 90; i++) numbers[i] = begin++;
 
-        for (int i = 1_000; i < 2_000; i++) {
-            if (i % 100 == 0) continue;
-            String primary = new String("");
-            primary = String.valueOf(i);
-
+        int qty = 0;
+        int start = 1_000;
+        String primary;
+        for (int i = 0; i < 9_000; i++) {
+            start++;
+            if (start % 100 == 0) continue;
+            primary = String.valueOf(start);
             for (int j = 0; j < 90; j++) {
                 String divider1 = String.valueOf(numbers[j]);
-                if (!contain(primary, divider1)) continue;
+                if (contain(primary, divider1) == null) continue;
                 else {
                     for (int k = 0; k < 90; k++) {
                         String divider2 = String.valueOf(numbers[k]);
-                        if (!contain(primary, divider2)) continue;
-                        else {
-                            if (i == (numbers[j] * numbers[k])) {
+                        if (contain(contain(primary, divider1), divider2) == null) {
+                            continue;
+                        } else {
+                            if (start == (numbers[j] * numbers[k])) {
                                 System.out.format("%,d = %,d*%,d %s\t|\t"
-                                        , i, numbers[j], numbers[k], primary);
+                                        , start, numbers[j], numbers[k], primary);
                                 qty++;
-                                if(qty%3==0) System.out.println();
+                                if (qty % 3 == 0) System.out.println();
                             }
                         }
                     }
                 }
             }
-//            System.out.println();
         }
         System.out.println("\nКол-во чисел = " + qty);
     }
 
+    static String contain(String base, String part) {
+        char[] chars = part.toCharArray();
+        int i;
+        for (i = 0; i < chars.length; i++) {
+            if (!base.contains(String.valueOf(chars[i]))) break;
+            else base = base.replace(chars[i], 'x');
+        }
+        if (i == part.length()) return base;
+        else return null;
+    }
+
+/*
     static boolean contain(String base, String part) {
         char[] chars = part.toCharArray();
         int i;
@@ -46,16 +59,9 @@ public class VampireNumbers {
             }
         }
         if (i == part.length()) {
-//            System.out.print(base + " ");
             return true;
         } else return false;
     }
 
-    static String replace(String base, String part) {
-        char[] chars = part.toCharArray();
-        for (int i = 0; i < chars.length; i++) {
-            base = base.replace(chars[i], 'x');
-        }
-        return base;
-    }
+*/
 }
