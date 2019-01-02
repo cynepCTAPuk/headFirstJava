5f56300e1ccce687a20c796fa38ffd672f94bdfe
cynepCTAPuk <CTAPuk@gmail.com>
2019-01-02 23:47:22 +0300

    VampireNumbers

--- javaBruceEckel/chap04/VampireNumbers.java
+++ javaBruceEckel/chap04/VampireNumbers.java
@@ -1,37 +1,46 @@
 package chap04;
 
 public class VampireNumbers {
-    static String primary;
 
     public static void main(String[] args) {
         int qty = 0;
         int start = 10;
         int[] numbers = new int[90];
-
         for (int i = 0; i < 90; i++) numbers[i] = start++;
 
-        for (int i = 1_000; i < 1_010; i++) {
+        for (int i = 1_000; i < 1_100; i++) {
             if (i % 100 == 0) continue;
-            primary = String.valueOf(i);
-            char[] charsPrimary = primary.toCharArray();
+            String primary = String.valueOf(i);
 
             for (int j = 0; j < 90; j++) {
                 String divider1 = String.valueOf(numbers[j]);
-                char[] charsDivider1 = divider1.toCharArray();
+                if (!contain(primary, divider1)) continue;
+                else {
+                    for (int k = 0; k < 90; k++) {
+                        String divider2 = String.valueOf(numbers[k]);
+                        char[] charsDivider2 = divider2.toCharArray();
 
-
-                for (int k = 0; k < 90; k++) {
-                    String divider2 = String.valueOf(numbers[k]);
-                    char[] charsDivider2 = divider2.toCharArray();
-
-                    if (i == (numbers[j] * numbers[k])) {
-                        System.out.format("%,d = %,d * %,d\n", i, numbers[j], numbers[k]);
-                        qty++;
+                        if (i == (numbers[j] * numbers[k])) {
+                            System.out.format("%,d = %,d * %,d %s\n"
+                                    , i, numbers[j], numbers[k], primary);
+                            qty++;
+                        }
                     }
                 }
             }
 //            System.out.println();
         }
         System.out.println("\nКол-во чисел = " + qty);
+    }
+
+    static boolean contain(String base, String part) {
+        char[] chars = part.toCharArray();
+        int i;
+        for (i = 0; i < chars.length; i++) {
+            if (!base.contains(String.valueOf(chars[i]))) break;
+            base = base.replace(chars[i], 'x');
+        }
+        if (i == part.length()) return true;
+        else return false;
     }
 }
