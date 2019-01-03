f3b5a76bc3f1c15a41179f33c883466a180b4824
cynepCTAPuk <CTAPuk@gmail.com>
2019-01-03 20:25:14 +0300

    PrimitiveOverloading

--- javaBruceEckel/chap05/PrimitiveOverloading.java
+++ javaBruceEckel/chap05/PrimitiveOverloading.java
@@ -30,83 +30,63 @@ public class PrimitiveOverloading {
     void f2(byte x) {
         printnb("f2(byte) ");
     }
-
     void f2(short x) {
         printnb("f2(short) ");
     }
-
     void f2(int x) {
         printnb("f2(int) ");
     }
-
     void f2(long x) {
         printnb("f2(long) ");
     }
-
     void f2(float x) {
         printnb("f2(float) ");
     }
-
     void f2(double x) {
         printnb("f2(double) ");
     }
-
     void f3(short x) {
         printnb("f3(short) ");
     }
-
     void f3(int x) {
         printnb("f3(int) ");
     }
-
     void f3(long x) {
         printnb("f3(long) ");
     }
-
     void f3(float x) {
         printnb("f3(float) ");
     }
-
     void f3(double x) {
         printnb("f3(double) ");
     }
-
     void f4(int x) {
         printnb("f4(int) ");
     }
-
     void f4(long x) {
         printnb("f4(long) ");
     }
-
     void f4(float x) {
         printnb("f4(float) ");
     }
-
     void f4(double x) {
         printnb("f4(double) ");
     }
-
     void f5(long x) {
         printnb("f5(long) ");
     }
-
     void f5(double x) {
         printnb("f5(double) ");
     }
-
     void f6(float x) {
         printnb("f6(float) ");
     }
-
     void f6(double x) {
         printnb("f6(double) ");
     }
-
     void f7(double x) {
         printnb("f7(double) ");
     }
-
     void testConstVal() {
         printnb("5: ");
         f1(5);
@@ -118,7 +98,6 @@ public class PrimitiveOverloading {
         f7(5);
         print();
     }
-
     void testChar() {
         char x = 'x';
         printnb("char: ");
@@ -131,7 +110,6 @@ public class PrimitiveOverloading {
         f7(x);
         print();
     }
-
     void testByte() {
         byte x = 0;
         printnb("byte: ");
@@ -144,7 +122,6 @@ public class PrimitiveOverloading {
         f7(x);
         print();
     }
-
     void testShort() {
         short x = 0;
         printnb("short: ");
@@ -157,7 +134,6 @@ public class PrimitiveOverloading {
         f7(x);
         print();
     }
-
     void testInt() {
         int x = 0;
         printnb("int: ");
@@ -170,7 +146,6 @@ public class PrimitiveOverloading {
         f7(x);
         print();
     }
-
     void testLong() {
         long x = 0;
         printnb("long: ");
@@ -183,7 +158,6 @@ public class PrimitiveOverloading {
         f7(x);
         print();
     }
-
     void testFloat() {
         float x = 0;
         printnb("float: ");
@@ -196,7 +170,6 @@ public class PrimitiveOverloading {
         f7(x);
         print();
     }
-
     void testDouble() {
         double x = 0;
         printnb("double: ");
