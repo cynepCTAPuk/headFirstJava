bf44ec09ae2049f806c6df0a4271c28d945df720
cynepCTAPuk <CTAPuk@gmail.com>
2019-01-03 03:38:06 +0300

    Exercise 2

--- javaBruceEckel/chap05/Exercise2.java
+++ javaBruceEckel/chap05/Exercise2.java
@@ -1,9 +1,22 @@
 package chap05;
 
-public class Exercise1 {
+public class Exercise2 {
     public static void main(String[] args) {
-        Obj obj = new Obj();
-        System.out.println(obj.str);
+        Obje obje1 = new Obje();
+        Obje obje2 = new Obje("New");
+        System.out.println(obje1.str);
+        System.out.println(obje2.str);
+    }
+}
+
+class Obje {
+    String str = "Old";
+
+    Obje() {
+    }
+
+    Obje(String str) {
+        this.str = str;
     }
 }
 
