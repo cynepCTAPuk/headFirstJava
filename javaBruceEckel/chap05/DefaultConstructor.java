3a1fff4a26c53d1e961697ce7922299809700da1
cynepCTAPuk <CTAPuk@gmail.com>
2019-01-03 20:32:22 +0300

    DefaultConstructor

--- javaBruceEckel/chap05/DefaultConstructor.java
+++ javaBruceEckel/chap05/DefaultConstructor.java
@@ -1,4 +1,11 @@
 package chap05;
 
-public class DefaultConstructor {
+//: initialization/DefaultConstructor.java
+class Bird {
 }
+
+public class DefaultConstructor {
+    public static void main(String[] args) {
+        Bird b = new Bird(); // Default!
+    }
+} ///:~
