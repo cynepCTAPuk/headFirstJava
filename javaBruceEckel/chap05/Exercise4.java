fa749cc4f28ca34327e822febb8c38c2118867e4
cynepCTAPuk <CTAPuk@gmail.com>
2019-01-03 20:40:01 +0300

    Exercise 4

--- javaBruceEckel/chap05/Exercise4.java
+++ javaBruceEckel/chap05/Exercise4.java
@@ -2,12 +2,12 @@ package chap05;
 
 public class Exercise4 {
     public static void main(String[] args) {
-        Obj4 obj4 = new Obj4();
+        Obj4 obj4 = new Obj4("1");
     }
 }
 class Obj4 {
-    Obj4() {
-        System.out.println("new Obj3");
+    Obj4(String string) {
+        System.out.println("new Obj4 "+ string);
     }
 }
 
