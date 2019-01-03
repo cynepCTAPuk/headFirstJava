7bbf63b4207402acf88736352b8dfc5ae220ea5f
cynepCTAPuk <CTAPuk@gmail.com>
2019-01-03 03:08:02 +0300

    VampireNumbers

--- javaBruceEckel/chap04/VampireNumbers.java
+++ javaBruceEckel/chap04/VampireNumbers.java
@@ -1,35 +1,40 @@
 package chap04;
 
+import static java.lang.Math.pow;
+
 public class VampireNumbers {
 
     public static void main(String[] args) {
-        int begin = 10;
-        int[] numbers = new int[90];
-        for (int i = 0; i < 90; i++) numbers[i] = begin++;
+        int dimension = 3;
+        int begin = (int) pow(10, dimension - 1);
+        int end = begin * 10 - begin;
+        int[] numbers = new int[end];
 
+        for (int i = 0; i < end; i++) numbers[i] = begin++;
         int qty = 0;
-        int start = 1_000;
-        String primary;
-        for (int i = 0; i < 9_000; i++) {
+        int start = (int) pow(10, 2 * dimension - 1);
+        int finish = start * 10 - start;
+
+        for (int i = 0; i < finish; i++) {
             start++;
             if (start % 100 == 0) continue;
-            primary = String.valueOf(start);
-            for (int j = 0; j < 90; j++) {
+
+            String primary = String.valueOf(start);
+
+            for (int j = 0; j < end; j++) {
                 String divider1 = String.valueOf(numbers[j]);
-                if (contain(primary, divider1) == null) continue;
-                else {
-                    for (int k = 0; k < 90; k++) {
-                        String divider2 = String.valueOf(numbers[k]);
-                        if (contain(contain(primary, divider1), divider2) == null) {
-                            continue;
-                        } else {
-                            if (start == (numbers[j] * numbers[k])) {
-                                System.out.format("%,d = %,d*%,d %s\t|\t"
-                                        , start, numbers[j], numbers[k], primary);
-                                qty++;
-                                if (qty % 3 == 0) System.out.println();
-                            }
-                        }
+                String primary1 = contain(primary, divider1);
+                if (primary1 == null) continue;
+
+                for (int k = 0; k < end; k++) {
+                    String divider2 = String.valueOf(numbers[k]);
+                    String primary2 = contain(primary1, divider2);
+                    if (primary2 == null) continue;
+
+                    if (start == (numbers[j] * numbers[k])) {
+                        System.out.format("%,d = %,d*%,d\t", start, numbers[j], numbers[k]);
+                        qty++;
+                        if (qty % 2 == 0) System.out.println();
                     }
                 }
             }
@@ -42,26 +47,9 @@ public class VampireNumbers {
         int i;
         for (i = 0; i < chars.length; i++) {
             if (!base.contains(String.valueOf(chars[i]))) break;
-            else base = base.replace(chars[i], 'x');
+            else base = base.replaceFirst(String.valueOf(chars[i]), "x");
         }
         if (i == part.length()) return base;
         else return null;
     }
-
-/*
-    static boolean contain(String base, String part) {
-        char[] chars = part.toCharArray();
-        int i;
-        for (i = 0; i < chars.length; i++) {
-            if (!base.contains(String.valueOf(chars[i]))) break;
-            else {
-                base = base.replace(chars[i], 'x');
-            }
-        }
-        if (i == part.length()) {
-            return true;
-        } else return false;
-    }
-
-*/
 }
