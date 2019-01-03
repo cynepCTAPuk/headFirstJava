18e2850add4f7fd5dd67f689c470adea7fe81398
cynepCTAPuk <CTAPuk@gmail.com>
2019-01-03 21:31:15 +0300

    Exercise 8

---
+++ javaBruceEckel/chap05/Exercese8.java
@@ -0,0 +1,19 @@
+package chap05;
+
+public class Exercese8 {
+    public static void main(String[] args) {
+        
+    }
+}
+
+
+class Obj8 {
+    void first() {
+        second();
+        this.second();
+    }
+
+    void second() {
+
+    }
+}
