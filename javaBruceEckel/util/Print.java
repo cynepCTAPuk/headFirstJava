b396d1fe56324b1a6c205b23102931c901abbfe9
cynepCTAPuk <CTAPuk@gmail.com>
2019-01-03 18:53:54 +0300

    Print.java add new overload method

--- javaBruceEckel/util/Print.java
+++ javaBruceEckel/util/Print.java
@@ -5,6 +5,9 @@ public class Print {
     public static void print(String string) {
         System.out.println(string);
     }
+    public static void print() {
+        System.out.println();
+    }
     public static void printnb(String string) {
         System.out.print(string);
     }
