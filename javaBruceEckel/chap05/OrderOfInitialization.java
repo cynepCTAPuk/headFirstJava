435d6f9303c6089ebdbbc800ea6e913becad1a89
cynepCTAPuk <CTAPuk@gmail.com>
2019-01-05 00:06:12 +0300

    OrderOfInitialization

--- javaBruceEckel/chap05/OrderOfInitialization.java
+++ javaBruceEckel/chap05/OrderOfInitialization.java
@@ -7,10 +7,14 @@ import static util.Print.*;
 
 // When the constructor is called to create a
 // Window object, you’ll see a message:
-class Window { Window(int marker) { print("Window(" + marker + ")");}
+class Window {
+    Window(int marker) {
+        print("Window(" + marker + ")");
+    }
 }
 
-class House { Window w1 = new Window(1); // Before constructor
+class House {
+    Window w1 = new Window(1); // Before constructor
 
     House() {   // Show that we’re in the constructor:
         print("House()");
@@ -19,7 +23,8 @@ class House { Window w1 = new Window(1); // Before constructor
 
     Window w2 = new Window(2); // After constructor
 
-    void f() { print("f()");
+    void f() {
+        print("f()");
     }
 
     Window w3 = new Window(3); // At end
