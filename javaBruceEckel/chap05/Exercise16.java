640df878fa67f7f81fa16770975d4ca759d76b77
cynepCTAPuk <CTAPuk@gmail.com>
2019-01-05 21:52:09 +0300

    Exercise 16

---
+++ javaBruceEckel/chap05/Exercise16.java
@@ -0,0 +1,9 @@
+package chap05;
+
+public class Exercise16 {
+    public static void main(String[] args) {
+        String[] strings;
+        strings = new String[]{"abc", "def", "ghk"};
+        System.out.println(strings);
+    }
+}
