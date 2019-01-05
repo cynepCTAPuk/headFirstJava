6101124662fd637e635876b99dc099fb6f510c04
cynepCTAPuk <CTAPuk@gmail.com>
2019-01-05 21:48:28 +0300

    DynamicArray

--- javaBruceEckel/chap05/DynamicArray.java
+++ javaBruceEckel/chap05/DynamicArray.java
@@ -1,4 +1,17 @@
 package chap05;
 
+//: initialization/DynamicArray.java
+// Array initialization.
 public class DynamicArray {
+    public static void main(String[] args) {
+        Other.main(new String[]{ "fiddle", "de", "dum" });
+    }
 }
+class Other {
+    public static void main(String[] args) {
+        for(String s : args)
+            System.out.print(s + " ");
+    }
+} /* Output:
+fiddle de dum
+*///:~
