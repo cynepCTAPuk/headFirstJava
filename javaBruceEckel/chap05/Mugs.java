63585c998349c1a5418c1d05d0d6fcb9eabcd5c7
cynepCTAPuk <CTAPuk@gmail.com>
2019-01-05 20:45:03 +0300

    Mugs

--- javaBruceEckel/chap05/Mugs.java
+++ javaBruceEckel/chap05/Mugs.java
@@ -2,35 +2,42 @@ package chap05;
 
 //: initialization/Mugs.java
 // Java "Instance Initialization."
-import static util.Print.print;
+
+import static util.Print.*;
 
 class Mug {
     Mug(int marker) {
         print("Mug(" + marker + ")");
     }
+
     void f(int marker) {
         print("f(" + marker + ")");
     }
 }
+
 public class Mugs {
     Mug mug1;
     Mug mug2;
+
     {
         mug1 = new Mug(1);
         mug2 = new Mug(2);
         print("mug1 & mug2 initialized");
     }
+
     Mugs() {
         print("Mugs()");
     }
+
     Mugs(int i) {
-        print("Mugs(int)");
+        print("Mugs(" + i +")");
     }
+
     public static void main(String[] args) {
         print("Inside main()");
         new Mugs();
         print("new Mugs() completed");
-        new Mugs(1);
+        new Mugs(111);
         print("new Mugs(1) completed");
     }
 } /* Output:
