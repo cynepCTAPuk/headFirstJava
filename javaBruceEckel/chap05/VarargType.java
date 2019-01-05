509c63345752d47fcdffccef5410c0841016c585
cynepCTAPuk <CTAPuk@gmail.com>
2019-01-05 22:24:07 +0300

    VarargType

--- javaBruceEckel/chap05/VarargType.java
+++ javaBruceEckel/chap05/VarargType.java
@@ -6,14 +6,16 @@ public class VarargType {
         System.out.print(args.getClass());
         System.out.println(" length " + args.length);
     }
+
     static void g(int... args) {
         System.out.print(args.getClass());
         System.out.println(" length " + args.length);
     }
+
     public static void main(String[] args) {
-        f('a');
+        f('a', 'b');
         f();
-        g(1);
+        g(1, 2);
         g();
         System.out.println("int[]: " + new int[0].getClass());
     }
