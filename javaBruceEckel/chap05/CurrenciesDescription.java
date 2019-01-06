dc3b57f862ac4b0fb366d72284b6c4f6a9aff867
cynepCTAPuk <CTAPuk@gmail.com>
2019-01-06 19:49:34 +0300

    CurrenciesTest

---
+++ javaBruceEckel/chap05/CurrenciesDescription.java
@@ -0,0 +1,14 @@
+package chap05;
+
+public class CurrenciesDescription {
+    Currencies money;
+
+    public CurrenciesDescription(Currencies money) {
+        this.money = money;
+    }
+
+    private void describe(){
+
+    }
+
+}
--- javaBruceEckel/chap05/CurrenciesTest.java
+++ javaBruceEckel/chap05/CurrenciesTest.java
@@ -1,5 +1,9 @@
 package chap05;
 
 public class CurrenciesTest {
-    
+    public static void main(String[] args) {
+        for (Currencies c : Currencies.values()) {
+            System.out.println(c.ordinal() + " " + c);
+        }
+    }
 }
