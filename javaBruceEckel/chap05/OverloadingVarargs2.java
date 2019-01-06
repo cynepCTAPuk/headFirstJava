c28fb6d382779db654be23184f070df6175ee6ea
cynepCTAPuk <CTAPuk@gmail.com>
2019-01-06 19:17:16 +0300

    OverloadingVarargs2

--- javaBruceEckel/chap05/OverloadingVarargs2.java
+++ javaBruceEckel/chap05/OverloadingVarargs2.java
@@ -1,12 +1,12 @@
 package chap05;
 
 //: initialization/OverloadingVarargs2.java
-// {CompileTimeError} (Won’t compile)
+// {CompileTimeError} (Won’t compile) 
 public class OverloadingVarargs2 {
     static void f(float i, Character... args) {
         System.out.println("first");
     }
-    static void f(Character... args) {
+    static void f(char c, Character... args) {
         System.out.print("second");
     }
     public static void main(String[] args) {
