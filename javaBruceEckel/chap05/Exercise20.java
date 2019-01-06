340f032f96daabac76035ce897e63332f3d71451
cynepCTAPuk <CTAPuk@gmail.com>
2019-01-06 19:24:47 +0300

    Exercise 20

--- javaBruceEckel/chap05/Exercise20.java
+++ javaBruceEckel/chap05/Exercise20.java
@@ -1,7 +1,9 @@
 package chap05;
 
+import java.util.*;
+
 public class Exercise20 {
     public static void main(String... args) {
-        System.out.println(args);
+        System.out.println(Arrays.toString(args));
     }
 }
