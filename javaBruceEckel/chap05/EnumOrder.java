67a99d466dfde87ab577153fa8afaec90d393134
cynepCTAPuk <CTAPuk@gmail.com>
2019-01-06 19:30:59 +0300

    EnumOrder

--- javaBruceEckel/chap05/EnumOrder.java
+++ javaBruceEckel/chap05/EnumOrder.java
@@ -1,4 +1,19 @@
 package chap05;
 
+import java.util.*;
+
+//: initialization/EnumOrder.java
 public class EnumOrder {
-}
+    public static void main(String[] args) {
+        for(Spiciness s : Spiciness.values())
+            System.out.println(s + ", ordinal " + s.ordinal());
+
+        System.out.println(Arrays.toString(Spiciness.values()));
+    }
+} /* Output:
+NOT, ordinal 0
+MILD, ordinal 1
+MEDIUM, ordinal 2
+HOT, ordinal 3
+FLAMING, ordinal 4
+*///:~
