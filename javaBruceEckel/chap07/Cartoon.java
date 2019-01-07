1f1974e72a306776d79a6cdecb62c9d4241534c3
cynepCTAPuk <CTAPuk@gmail.com>
2019-01-07 19:59:33 +0300

    Cartoon

--- javaBruceEckel/chap07/Cartoon.java
+++ javaBruceEckel/chap07/Cartoon.java
@@ -11,7 +11,7 @@ class Drawing extends Art {
     Drawing() { print("Drawing constructor"); }
 }
 public class Cartoon extends Drawing {
-    public Cartoon() { print("Cartoon constructor"); }
+//    public Cartoon() { print("Cartoon constructor"); }
     public static void main(String[] args) {
         Cartoon x = new Cartoon();
     }
