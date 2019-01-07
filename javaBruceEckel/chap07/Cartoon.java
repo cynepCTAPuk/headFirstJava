13a50a05a4562fac6c4eeb029256965b153a6f88
cynepCTAPuk <CTAPuk@gmail.com>
2019-01-07 19:56:18 +0300

    Cartoon

--- javaBruceEckel/chap07/Cartoon.java
+++ javaBruceEckel/chap07/Cartoon.java
@@ -1,4 +1,22 @@
 package chap07;
 
-public class Cartoon {
+//: reusing/Cartoon.java
+// Constructor calls during inheritance.
+import static util.Print.print;
+
+class Art {
+    Art() { print("Art constructor"); }
 }
+class Drawing extends Art {
+    Drawing() { print("Drawing constructor"); }
+}
+public class Cartoon extends Drawing {
+    public Cartoon() { print("Cartoon constructor"); }
+    public static void main(String[] args) {
+        Cartoon x = new Cartoon();
+    }
+} /* Output:
+Art constructor
+Drawing constructor
+Cartoon constructor
+*///:~
