8be22bbda23627f0ac15c1add20ee48cbc3014d0
cynepCTAPuk <CTAPuk@gmail.com>
2019-01-07 18:45:01 +0300

    Dinner

--- javaBruceEckel/chap06/Dinner.java
+++ javaBruceEckel/chap06/Dinner.java
@@ -1,4 +1,15 @@
 package chap06;
 
+//: access/Dinner.java
+// Uses the library.
+
+import chap06.dessert.*;
+
 public class Dinner {
-}
+    public static void main(String[] args) {
+        Cookie x = new Cookie();
+//        x.bite(); // Can’t access
+    }
+} /* Output:
+Cookie constructor
+*///:~
