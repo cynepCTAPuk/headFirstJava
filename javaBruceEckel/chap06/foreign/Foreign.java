90f9b99da240fe19c092efdef6305ff3f6faa39c
cynepCTAPuk <CTAPuk@gmail.com>
2019-01-07 19:14:19 +0300

    Foreign

--- javaBruceEckel/chap06/foreign/Foreign.java
+++ javaBruceEckel/chap06/foreign/Foreign.java
@@ -1,4 +1,10 @@
 package chap06.foreign;
 
+// access/foreign/Foreign.java
+import chap06.local;
+
 public class Foreign {
+    public static void main(String[] args) {
+        PackagedClass pc = new PackagedClass();
+    }
 }
