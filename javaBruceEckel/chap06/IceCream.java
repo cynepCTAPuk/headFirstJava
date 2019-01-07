00f981d8279ba85222aa2b28303614218306224e
cynepCTAPuk <CTAPuk@gmail.com>
2019-01-07 18:50:39 +0300

    IceCream

--- javaBruceEckel/chap06/IceCream.java
+++ javaBruceEckel/chap06/IceCream.java
@@ -1,4 +1,17 @@
 package chap06;
 
-public class IceCream {
+//: access/IceCream.java
+// Demonstrates "private" keyword.
+class Sundae {
+    private Sundae() {}
+    static Sundae makeASundae() {
+        return new Sundae();
+    }
 }
+public class IceCream {
+    public static void main(String[] args) {
+// Sundae x = new Sundae();
+        Sundae x = Sundae.makeASundae();
+        System.out.println(x);
+    }
+} ///:~
