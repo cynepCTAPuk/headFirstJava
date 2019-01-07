119348dbb70d0ca48df5d6e1b5f1f56f2da6248b
cynepCTAPuk <CTAPuk@gmail.com>
2019-01-07 18:21:18 +0300

    SingleImport

--- javaBruceEckel/chap06/SingleImport.java
+++ javaBruceEckel/chap06/SingleImport.java
@@ -1,4 +1,10 @@
 package chap06;
 
+//: access/SingleImport.java
+import java.util.ArrayList;
 public class SingleImport {
-}
+    public static void main(String[] args) {
+        ArrayList list = new java.util.ArrayList();
+        System.out.println(list.isEmpty());
+    }
+} ///:~
