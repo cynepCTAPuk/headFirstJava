dbf8f402ce28d1a7ebe908aedef65a6855e07071
cynepCTAPuk <CTAPuk@gmail.com>
2019-01-07 20:05:20 +0300

    Exercise 5

---
+++ javaBruceEckel/chap07/Exercise5.java
@@ -0,0 +1,22 @@
+package chap07;
+
+public class Exercise5 {
+    public static void main(String[] args) {
+        C c = new C();
+        System.out.println(c);
+    }
+}
+
+class A {
+    A() { System.out.println("A"); }
+}
+
+class B {
+    B() { System.out.println("B"); }
+}
+
+class C extends A {
+    B b = new B();
+    C() { System.out.println("C"); }
+}
+
