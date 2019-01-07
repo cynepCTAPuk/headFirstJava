7854c6b842b93c1884dd6e818b34a4be5349bb1e
cynepCTAPuk <CTAPuk@gmail.com>
2019-01-07 18:38:45 +0300

    Range

--- javaBruceEckel/util/Range.java
+++ javaBruceEckel/util/Range.java
@@ -2,18 +2,21 @@ package util;
 
 //: control/ForEachInt.java
 public class Range {
+    // Produce a sequence [0..n)
     public static int[] range(int end) {
         return range(0, end, 1);
     }
 
+    // Produce a sequence [start..end)
     public static int[] range(int start, int end) {
         return range(start, end, 1);
     }
 
+    // Produce a sequence [start..end) incrementing by step
     public static int[] range(int start, int end, int step) {
         int size = (end - start) / step;
         int[] array = new int[size];
-        for (int i = 0; i < array.length; i++) array[i] = (start + (step * i));
+        for (int i = 0; i < size; i++) array[i] = (start + (step * i));
         return array;
     }
 }
