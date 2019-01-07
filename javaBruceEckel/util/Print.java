1b7a558f339e6267a4635857a26f7dd298157c8e
cynepCTAPuk <CTAPuk@gmail.com>
2019-01-07 18:33:22 +0300

    Print

--- javaBruceEckel/util/Print.java
+++ javaBruceEckel/util/Print.java
@@ -1,14 +1,18 @@
 package util;
+import java.io.*;
 
 //: control/ForEachInt.java
 public class Print {
-    public static void print(String string) {
-        System.out.println(string);
-    }
-    public static void print() {
-        System.out.println();
-    }
-    public static void printnb(String string) {
-        System.out.print(string);
-    }
-}
+    // Print with a newline:
+    public static void print(Object obj) {System.out.println(obj);}
+
+    // Print a newline by itself:
+    public static void print() {System.out.println();}
+
+    // Print with no line break:
+    public static void printnb(Object obj) {System.out.print(obj);}
+
+    // The new Java SE5 printf() (from C):
+    public static PrintStream printf(String format, Object... args) {
+        return System.out.printf(format, args);}
+} ///:~
