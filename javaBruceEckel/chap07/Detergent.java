61d6902c2f2ae6057d0cbf99ebeb3e2d60b9bfd1
cynepCTAPuk <CTAPuk@gmail.com>
2019-01-07 19:46:20 +0300

    Detergent

--- javaBruceEckel/chap07/Detergent.java
+++ javaBruceEckel/chap07/Detergent.java
@@ -1,4 +1,42 @@
 package chap07;
 
-public class Detergent {
+//: reusing/Detergent.java
+// Inheritance syntax & properties.
+import static util.Print.print;
+
+class Cleanser {
+    private String s = "Cleanser";
+    public void append(String a) { s += a; }
+    public void dilute() { append(" dilute()"); }
+    public void apply() { append(" apply()"); }
+    public void scrub() { append(" scrub()"); }
+    public String toString() { return s; }
+    public static void main(String[] args) {
+        Cleanser x = new Cleanser();
+        x.dilute(); x.apply(); x.scrub();
+        print(x);
+    }
 }
+public class Detergent extends Cleanser {
+    public void scrub() {   // Change a method:
+        append(" Detergent.scrub()");
+        super.scrub(); // Call base-class version
+    }
+    // Add methods to the interface:
+    public void foam() { append(" foam()"); }
+    // Test the new class:
+    public static void main(String[] args) {
+        Detergent x = new Detergent();
+        x.dilute();
+        x.apply();
+        x.scrub();
+        x.foam();
+        print(x);
+        print("Testing base class:");
+        Cleanser.main(args);
+    }
+} /* Output:
+Cleanser dilute() apply() Detergent.scrub() scrub() foam()
+Testing base class:
+Cleanser dilute() apply() scrub()
+*///:~
