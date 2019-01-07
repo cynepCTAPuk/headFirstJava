125c7289afda7537e7acbe9fd2cb997af81497d1
cynepCTAPuk <CTAPuk@gmail.com>
2019-01-07 18:26:37 +0300

    List

---
+++ javaBruceEckel/util/List.java
@@ -0,0 +1,10 @@
+package util;
+
+//: net/mindview/simple/List.java
+// Creating a package.
+//package net.mindview.simple;
+public class List {
+    public List() {
+        System.out.println("net.mindview.simple.List");
+    }
+} ///:~
