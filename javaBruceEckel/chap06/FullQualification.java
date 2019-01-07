ce3ec19398c98878f6353e7ed08ee1a15cb6f44b
cynepCTAPuk <CTAPuk@gmail.com>
2019-01-07 18:17:24 +0300

    FullQualification

--- javaBruceEckel/chap06/FullQualification.java
+++ javaBruceEckel/chap06/FullQualification.java
@@ -1,5 +1,8 @@
 package chap06;
 
+//: access/FullQualification.java
 public class FullQualification {
-    
-}
+    public static void main(String[] args) {
+        java.util.ArrayList list = new java.util.ArrayList();
+    }
+} ///:~
