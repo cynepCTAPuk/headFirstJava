50ac51db5ccaac65f13c8131bd8ba30efbf098a2
cynepCTAPuk <CTAPuk@gmail.com>
2019-01-07 18:42:48 +0300

    Cookie

--- javaBruceEckel/chap06/dessert/Cookie.java
+++ javaBruceEckel/chap06/dessert/Cookie.java
@@ -1,4 +1,10 @@
 package chap06.dessert;
 
+//: access/dessert/Cookie.java
+// Creates a library.
 public class Cookie {
-}
+    public Cookie() {
+        System.out.println("Cookie constructor");
+    }
+    void bite() { System.out.println("bite"); }
+} ///:~
