0db908f554f41a4b88532cfece7cb5282b161db9
cynepCTAPuk <CTAPuk@gmail.com>
2019-01-07 19:05:54 +0300

    Lunch

--- javaBruceEckel/chap06/Lunch.java
+++ javaBruceEckel/chap06/Lunch.java
@@ -10,8 +10,7 @@ class Soup1 {
 }
 class Soup2 {
     private Soup2() {}
-    // (2) Create a static object and return a reference
-// upon request.(The "Singleton" pattern):
+    // (2) Create a static object and return a reference upon request.(The "Singleton" pattern):
     private static Soup2 ps1 = new Soup2();
     public static Soup2 access() {return ps1;}
     public void f() {}
