ec86be7e9a438e0d1415ee5b6a0d699b7e0ceec5
cynepCTAPuk <CTAPuk@gmail.com>
2019-01-08 15:32:38 +0300

    PlaceSetting

--- javaBruceEckel/chap07/PlaceSetting.java
+++ javaBruceEckel/chap07/PlaceSetting.java
@@ -2,53 +2,62 @@ package chap07;
 
 //: reusing/PlaceSetting.java
 // Combining composition & inheritance.
-import static util.Print.print;
+
+import static util.Print.*;
 
 class Plate {
     Plate(int i) {
         print("Plate constructor");
     }
 }
+
 class DinnerPlate extends Plate {
     DinnerPlate(int i) {
         super(i);
         print("DinnerPlate constructor");
     }
 }
+
 class Utensil {
     Utensil(int i) {
         print("Utensil constructor");
     }
 }
+
 class Spoon extends Utensil {
     Spoon(int i) {
         super(i);
         print("Spoon constructor");
     }
 }
+
 class Fork extends Utensil {
     Fork(int i) {
         super(i);
         print("Fork constructor");
     }
 }
+
 class Knife extends Utensil {
     Knife(int i) {
         super(i);
         print("Knife constructor");
     }
 }
+
 // A cultural way of doing something:
 class Custom {
     Custom(int i) {
         print("Custom constructor");
     }
 }
+
 public class PlaceSetting extends Custom {
     private Spoon sp;
     private Fork frk;
     private Knife kn;
     private DinnerPlate pl;
+
     public PlaceSetting(int i) {
         super(i + 1);
         sp = new Spoon(i + 2);
@@ -57,6 +66,7 @@ public class PlaceSetting extends Custom {
         pl = new DinnerPlate(i + 5);
         print("PlaceSetting constructor");
     }
+
     public static void main(String[] args) {
         PlaceSetting x = new PlaceSetting(9);
     }
