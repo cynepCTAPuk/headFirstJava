08ae7ba8557870ec5ed797a0bab908568d23909c
cynepCTAPuk <CTAPuk@gmail.com>
2019-01-09 17:15:09 +0300

    Triangle

--- javaBruceEckel/chap08/shape/Triangle.java
+++ javaBruceEckel/chap08/shape/Triangle.java
@@ -1,4 +1,9 @@
 package chap08.shape;
 
-public class Triangle {
-}
+import static util.Print.print;
+
+//: polymorphism/shape/Triangle.java
+public class Triangle extends Shape {
+    public void draw() { print("Triangle.draw()"); }
+    public void erase() { print("Triangle.erase()"); }
+} ///:~
