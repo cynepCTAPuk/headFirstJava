4bcabc9495a7512f1571d35225b1682bb402b624
cynepCTAPuk <CTAPuk@gmail.com>
2019-01-09 17:10:06 +0300

    Exercise 1

--- javaBruceEckel/chap08/Exercise1.java
+++ javaBruceEckel/chap08/Exercise1.java
@@ -6,7 +6,7 @@ public class Exercise1 {
     static void ride(Cycle c) { c.ride(); }
     public static void main(String[] args) {
         Cycle cycle = new Cycle();
-        Unicycle unicycle = new Unicycle();
+        Cycle unicycle = new Unicycle();
         Bicycle bicycle = new Bicycle();
         Tricycle tricycle = new Tricycle();
         ride(cycle);
