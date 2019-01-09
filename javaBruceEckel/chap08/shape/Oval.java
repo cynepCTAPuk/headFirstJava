37894a5b3b3a90a8a792eb5e50276e9bd7276880
cynepCTAPuk <CTAPuk@gmail.com>
2019-01-09 17:39:42 +0300

    Oval

--- javaBruceEckel/chap08/shape/Oval.java
+++ javaBruceEckel/chap08/shape/Oval.java
@@ -2,9 +2,9 @@ package chap08.shape;
 
 import static util.Print.*;
 
-//: polymorphism/shape/Circle.java
+//: polymorphism/shape/Oval.java
 public class Oval extends Shape {
-    @Override public void draw() { print("Circle.draw()"); }
-    @Override public void erase() { print("Circle.erase()"); }
-    @Override public void fill() { print("Circle.fill"); }
+    @Override public void draw() { print("Oval.draw()"); }
+    @Override public void erase() { print("Oval.erase()"); }
+    @Override public void fill() { print("Oval.fill"); }
 } ///:~
