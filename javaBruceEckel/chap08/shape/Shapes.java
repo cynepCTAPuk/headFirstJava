5e0d90732517a7e9914743572c536c5e02085b36
cynepCTAPuk <CTAPuk@gmail.com>
2019-01-09 17:23:49 +0300

    Shapes

--- javaBruceEckel/chap08/shape/Shapes.java
+++ javaBruceEckel/chap08/shape/Shapes.java
@@ -5,11 +5,11 @@ package chap08.shape;
 public class Shapes {
     private static RandomShapeGenerator gen = new RandomShapeGenerator();
     public static void main(String[] args) {
-        Shape[] s = new Shape[9];
-// Fill up the array with shapes:
-        for(int i = 0; i < s.length; i++) s[i] = gen.next();
-// Make polymorphic method calls:
-        for(Shape shp : s) shp.draw();
+        Shape[] shapes = new Shape[9];
+        // Fill up the array with shapes:
+        for(int i = 0; i < shapes.length; i++) shapes[i] = gen.next();
+        // Make polymorphic method calls:
+        for(Shape shp : shapes) shp.draw();
     }
 } /* Output:
 Triangle.draw()
