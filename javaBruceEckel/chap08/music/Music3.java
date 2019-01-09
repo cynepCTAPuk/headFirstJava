dbf5423dcde305ea528b4b8569d8aa9248f2c5c8
cynepCTAPuk <CTAPuk@gmail.com>
2019-01-09 18:19:48 +0300

    Music3

--- javaBruceEckel/chap08/music/Music3.java
+++ javaBruceEckel/chap08/music/Music3.java
@@ -1,4 +1,31 @@
 package chap08.music;
 
 public class Music3 {
-}
+    // Doesn’t care about type, so new types
+// added to the system still work right:
+    public static void tune(Instrument i) {
+// ...
+        i.play(Note.MIDDLE_C);
+    }
+    public static void tuneAll(Instrument[] e) {
+        for(Instrument i : e)
+            tune(i);
+    }
+    public static void main(String[] args) {
+// Upcasting during addition to the array:
+        Instrument[] orchestra = {
+                new Wind(),
+                new Percussion(),
+                new Stringed(),
+                new Brass(),
+                new Woodwind()
+        };
+        tuneAll(orchestra);
+    }
+} /* Output:
+Wind.play() MIDDLE_C
+Percussion.play() MIDDLE_C
+Stringed.play() MIDDLE_C
+Brass.play() MIDDLE_C
+Woodwind.play() MIDDLE_C
+*///:~
