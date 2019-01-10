a3f02d358ee59fe483ac9ac710a68c5dac626b39
cynepCTAPuk <CTAPuk@gmail.com>
2019-01-10 18:45:08 +0300

    Apply

--- javaBruceEckel/chap09/Apply.java
+++ javaBruceEckel/chap09/Apply.java
@@ -9,25 +9,23 @@ class Processor {
     Object process(Object input) { return input; }
 }
 class Upcase extends Processor {
-    String process(Object input) { // Covariant return
-        return ((String)input).toUpperCase();
-    }
+    // Covariant return
+    String process(Object input) {  return ((String)input).toUpperCase(); }
 }
 class Downcase extends Processor {
     String process(Object input) { return ((String)input).toLowerCase(); }
 }
 class Splitter extends Processor {
-    String process(Object input) {
-// The split() argument divides a String into pieces:
-        return Arrays.toString(((String)input).split(" "));
-    }
+    // The split() argument divides a String into pieces:
+    String process(Object input) { return Arrays.toString(((String)input).split(" ")); }
 }
 public class Apply {
+    public static String s = "Disagreement with beliefs is by definition incorrect";
     public static void process(Processor p, Object s) {
         print("Using Processor " + p.name());
         print(p.process(s));
     }
-    public static String s = "Disagreement with beliefs is by definition incorrect";
+
     public static void main(String[] args) {
         process(new Upcase(), s);
         process(new Downcase(), s);
