dd2b86a36aadd31f269f9610d727997fa6ba4994
cynepCTAPuk <CTAPuk@gmail.com>
2019-01-10 15:07:29 +0300

    Sandwich

--- javaBruceEckel/chap08/Sandwich.java
+++ javaBruceEckel/chap08/Sandwich.java
@@ -16,8 +16,12 @@ class Cheese {
 class Lettuce {
     Lettuce() { print("Lettuce()"); }
 }
+class Pickle {
+    Pickle() { print("Pickle()"); }
+}
 class Lunch extends Meal {
     Lunch() { print("Lunch()"); }
+
 }
 class PortableLunch extends Lunch {
     PortableLunch() { print("PortableLunch()");}
@@ -26,7 +30,9 @@ public class Sandwich extends PortableLunch {
     private Bread b = new Bread();
     private Cheese c = new Cheese();
     private Lettuce l = new Lettuce();
+    private Pickle p = new Pickle();
     public Sandwich() { print("Sandwich()"); }
+
     public static void main(String[] args) {
         new Sandwich();
     }
