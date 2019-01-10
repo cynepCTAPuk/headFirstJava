a239ced04102f4d47ae3b3e7da61898eb6e7d260
cynepCTAPuk <CTAPuk@gmail.com>
2019-01-10 19:21:17 +0300

    interfaceprocessor

---
+++ javaBruceEckel/chap09/interfaceprocessor/Apply.java
@@ -0,0 +1,11 @@
+package chap09.interfaceprocessor;
+
+//: interfaces/interfaceprocessor/Apply.java
+import static util.Print.print;
+
+public class Apply {
+    public static void process(Processor p, Object s) {
+        print("Using Processor " + p.name());
+        print(p.process(s));
+    }
+} ///:~
---
+++ javaBruceEckel/chap09/interfaceprocessor/Processor.java
@@ -0,0 +1,7 @@
+package chap09.interfaceprocessor;
+
+//: interfaces/interfaceprocessor/Processor.java
+public interface Processor {
+    String name();
+    Object process(Object input);
+} ///:~
