ccbaa1013f256d96b53899be44f8a138ba902cdd
cynepCTAPuk <CTAPuk@gmail.com>
2019-01-10 16:35:29 +0300

    ReferenceCounting

--- javaBruceEckel/chap08/ReferenceCounting.java
+++ javaBruceEckel/chap08/ReferenceCounting.java
@@ -31,9 +31,8 @@ class Composing {
 public class ReferenceCounting {
     public static void main(String[] args) {
         Shared shared = new Shared();
-        Composing[] composing = { new Composing(shared),
-                new Composing(shared), new Composing(shared),
-                new Composing(shared), new Composing(shared) };
+        Composing[] composing = {  new Composing(shared), new Composing(shared),
+                new Composing(shared), new Composing(shared), new Composing(shared) };
         for(Composing c : composing) c.dispose();
     }
 } /* Output:
