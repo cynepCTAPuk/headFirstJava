502ed1ca91054ce87de806ef155088b230f43617
cynepCTAPuk <CTAPuk@gmail.com>
2019-01-10 20:10:53 +0300

    Adventure

--- javaBruceEckel/chap09/Adventure.java
+++ javaBruceEckel/chap09/Adventure.java
@@ -1,29 +1,23 @@
 package chap09;
 
+import static util.Print.print;
+
 //: interfaces/Adventure.java
 // Multiple interfaces.
-interface CanFight {
-    void fight();
-}
-interface CanSwim {
-    void swim();
-}
-interface CanFly {
-    void fly();
-}
-class ActionCharacter {
-    public void fight() {}
-}
-class Hero extends ActionCharacter
-        implements CanFight, CanSwim, CanFly {
-    public void swim() {}
-    public void fly() {}
+interface CanFight { void fight(); }
+interface CanSwim { void swim(); }
+interface CanFly { void fly(); }
+class ActionCharacter { public void fight() {print("fight");} }
+class Hero extends ActionCharacter implements CanFight, CanSwim, CanFly {
+    public void swim() {print("swim");}
+    public void fly() {print("fly");}
 }
 public class Adventure {
     public static void t(CanFight x) { x.fight(); }
     public static void u(CanSwim x) { x.swim(); }
     public static void v(CanFly x) { x.fly(); }
     public static void w(ActionCharacter x) { x.fight(); }
+
     public static void main(String[] args) {
         Hero h = new Hero();
         t(h); // Treat it as a CanFight
