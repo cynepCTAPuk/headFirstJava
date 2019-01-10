a3b5fd9c3a5678cac7b8cebd1be402c0c8d8e320
cynepCTAPuk <CTAPuk@gmail.com>
2019-01-10 18:30:27 +0300

    Music

--- javaBruceEckel/chap09/music/Stringed.java
+++ javaBruceEckel/chap09/music/Stringed.java
@@ -2,7 +2,7 @@ package chap09.music;
 
 import static util.Print.*;
 
-class Stringed implements Instrument {
+class Stringed extends Base implements Instrument {
     public void play(Note n) { print("Stringed.play() " + n); }
     public String toString() { return "Stringed"; }
     public void adjust() { print("Stringed.adjust()");}
--- javaBruceEckel/chap09/music/Wind.java
+++ javaBruceEckel/chap09/music/Wind.java
@@ -2,11 +2,7 @@ package chap09.music;
 
 import static util.Print.*;
 
-//: polymorphism/music/Wind.java
-// Wind objects are instruments
-// because they have the same interface:
 public class Wind implements Instrument {
-    // Redefine interface method:
     public void play(Note n) { System.out.println("Wind.play() " + n); }
     public String toString() { return "Wind"; }
     public void adjust() { print("Wind.adjust()");}
