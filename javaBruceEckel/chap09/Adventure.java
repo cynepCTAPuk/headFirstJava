4bb6d9797806e5d6861f86c407b980bcb0f6bf4d
cynepCTAPuk <CTAPuk@gmail.com>
2019-01-10 20:30:29 +0300

    Adventure

--- javaBruceEckel/chap09/Adventure.java
+++ javaBruceEckel/chap09/Adventure.java
@@ -7,22 +7,27 @@ import static util.Print.print;
 interface CanFight { void fight(); }
 interface CanSwim { void swim(); }
 interface CanFly { void fly(); }
+interface CanClimb { void climb(); }
+
 class ActionCharacter { public void fight() {print("fight");} }
-class Hero extends ActionCharacter implements CanFight, CanSwim, CanFly {
+class Hero extends ActionCharacter implements CanFight, CanSwim, CanFly, CanClimb {
     public void swim() {print("swim");}
     public void fly() {print("fly");}
+    public void climb() {print("climb");}
 }
 public class Adventure {
     public static void t(CanFight x) { x.fight(); }
     public static void u(CanSwim x) { x.swim(); }
     public static void v(CanFly x) { x.fly(); }
-    public static void w(ActionCharacter x) { x.fight(); }
+    public static void w(CanClimb x) { x.climb(); }
+    public static void x(ActionCharacter x) { x.fight(); }
 
     public static void main(String[] args) {
         Hero h = new Hero();
         t(h); // Treat it as a CanFight
         u(h); // Treat it as a CanSwim
         v(h); // Treat it as a CanFly
-        w(h); // Treat it as an ActionCharacter
+        w(h);
+        x(h); // Treat it as an ActionCharacter
     }
 } ///:~
---
+++ javaBruceEckel/chap09/First.java
@@ -0,0 +1,17 @@
+package chap09;
+
+public interface First {
+    void first();
+}
+
+interface Second extends First {
+    void second();
+}
+
+interface Third extends First {
+    void third();
+}
+
+interface Fourth extends Second, Third {
+    void fourth();
+}
