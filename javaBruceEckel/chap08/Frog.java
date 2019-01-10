70f26cd99b77fb8064a89f5240cca225b4ea1606
cynepCTAPuk <CTAPuk@gmail.com>
2019-01-10 15:53:02 +0300

    Frog

--- javaBruceEckel/chap08/Frog.java
+++ javaBruceEckel/chap08/Frog.java
@@ -1,18 +1,17 @@
 package chap08;
 
-import static util.Print.print;
 
 //: polymorphism/Frog.java
 // Cleanup and inheritance.
+import static util.Print.print;
+
 class Characteristic {
     private String s;
     Characteristic(String s) {
         this.s = s;
         print("Creating Characteristic " + s);
     }
-    protected void dispose() {
-        print("disposing Characteristic " + s);
-    }
+    protected void dispose() { print("disposing Characteristic " + s); }
 }
 class Description {
     private String s;
@@ -20,18 +19,12 @@ class Description {
         this.s = s;
         print("Creating Description " + s);
     }
-    protected void dispose() {
-        print("disposing Description " + s);
-    }
+    protected void dispose() { print("disposing Description " + s); }
 }
 class LivingCreature {
-    private Characteristic p =
-            new Characteristic("is alive");
-    private Description t =
-            new Description("Basic Living Creature");
-    LivingCreature() {
-        print("LivingCreature()");
-    }
+    private Characteristic p = new Characteristic("is alive");
+    private Description t = new Description("Basic Living Creature");
+    LivingCreature() { print("LivingCreature()"); }
     protected void dispose() {
         print("LivingCreature dispose");
         t.dispose();
@@ -39,10 +32,8 @@ class LivingCreature {
     }
 }
 class Animal extends LivingCreature {
-    private Characteristic p =
-            new Characteristic("has heart");
-    private Description t =
-            new Description("Animal not Vegetable");
+    private Characteristic p = new Characteristic("has heart");
+    private Description t = new Description("Animal not Vegetable");
     Animal() { print("Animal()"); }
     protected void dispose() {
         print("Animal dispose");
@@ -52,13 +43,9 @@ class Animal extends LivingCreature {
     }
 }
 class Amphibian extends Animal {
-    private Characteristic p =
-            new Characteristic("can live in water");
-    private Description t =
-            new Description("Both water and land");
-    Amphibian() {
-        print("Amphibian()");
-    }
+    private Characteristic p = new Characteristic("can live in water");
+    private Description t = new Description("Both water and land");
+    Amphibian() { print("Amphibian()"); }
     protected void dispose() {
         print("Amphibian dispose");
         t.dispose();
