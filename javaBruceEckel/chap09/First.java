6ad463bfb40e3ba975718d11c42e3e7d114be56d
cynepCTAPuk <CTAPuk@gmail.com>
2019-01-10 20:32:28 +0300

    Inherit interfaces

--- javaBruceEckel/chap09/First.java
+++ javaBruceEckel/chap09/First.java
@@ -1,6 +1,6 @@
 package chap09;
 
-public interface First {
+interface First {
     void first();
 }
 
@@ -15,3 +15,4 @@ interface Third extends First {
 interface Fourth extends Second, Third {
     void fourth();
 }
+
