7d87b3d35017cfb12db79086f393a9a350836a57
cynepCTAPuk <CTAPuk@gmail.com>
2019-01-11 19:02:20 +0300

    Sequence

--- javaBruceEckel/chap10/Sequence.java
+++ javaBruceEckel/chap10/Sequence.java
@@ -11,15 +11,15 @@ public class Sequence {
     private Object[] items;
     private int next = 0;
     public Sequence(int size) { items = new Object[size]; }
-    public void add(Object x) {
-        if(next < items.length) items[next++] = x;
-    }
+    public void add(Object x) { if(next < items.length) items[next++] = x; }
+
     private class SequenceSelector implements Selector {
         private int i = 0;
         public Object current() { return items[i]; }
         public boolean end() { return i == items.length; }
         public void next() { if(i < items.length) i++; }
     }
+
     public Selector selector() { return new SequenceSelector(); }
 
     public static void main(String[] args) {
