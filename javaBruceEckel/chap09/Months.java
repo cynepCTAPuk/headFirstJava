5e04516355e52bc74a5581d718379822fcf5eaaa
cynepCTAPuk <CTAPuk@gmail.com>
2019-01-11 17:23:31 +0300

    Months

--- javaBruceEckel/chap09/Months.java
+++ javaBruceEckel/chap09/Months.java
@@ -8,4 +8,8 @@ public interface Months {
             APRIL = 4, MAY = 5, JUNE = 6, JULY = 7,
             AUGUST = 8, SEPTEMBER = 9, OCTOBER = 10,
             NOVEMBER = 11, DECEMBER = 12;
+
+    static void main(String[] args) {
+        System.out.println(JANUARY);
+    }
 } ///:~
