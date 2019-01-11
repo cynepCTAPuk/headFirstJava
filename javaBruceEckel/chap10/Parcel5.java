49e86d7418d2b4b9fa3b258e513ba7a03d5d0f08
cynepCTAPuk <CTAPuk@gmail.com>
2019-01-11 23:53:41 +0300

    Parcel5

--- javaBruceEckel/chap10/Parcel5.java
+++ javaBruceEckel/chap10/Parcel5.java
@@ -6,9 +6,7 @@ public class Parcel5 {
     public Destination destination(String s) {
         class PDestination implements Destination {
             private String label;
-            private PDestination(String whereTo) {
-                label = whereTo;
-            }
+            private PDestination(String whereTo) { label = whereTo; }
             public String readLabel() { return label; }
         }
         return new PDestination(s);
