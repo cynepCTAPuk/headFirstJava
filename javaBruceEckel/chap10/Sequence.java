ce64a26587d7b6e7dd202fa729e113939fa30c75
cynepCTAPuk <CTAPuk@gmail.com>
2019-01-11 18:53:16 +0300

    Sequence

--- javaBruceEckel/chap10/Sequence.java
+++ javaBruceEckel/chap10/Sequence.java
@@ -21,6 +21,7 @@ public class Sequence {
         public void next() { if(i < items.length) i++; }
     }
     public Selector selector() { return new SequenceSelector(); }
+
     public static void main(String[] args) {
         Sequence sequence = new Sequence(10);
         for(int i = 0; i < 10; i++) sequence.add(Integer.toString(i));
