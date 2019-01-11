3c8afbe67912a58a101501d2e9e63d7b9aae6145
cynepCTAPuk <CTAPuk@gmail.com>
2019-01-11 21:17:17 +0300

    TestParcel

--- javaBruceEckel/chap10/TestParcel.java
+++ javaBruceEckel/chap10/TestParcel.java
@@ -6,20 +6,17 @@ class Parcel4 {
         private int i = 11;
         public int value() { return i; }
     }
+
     protected class PDestination implements Destination {
         private String label;
-        private PDestination(String whereTo) {
-            label = whereTo;
-        }
+        private PDestination(String whereTo) { label = whereTo; }
         public String readLabel() { return label; }
     }
-    public Destination destination(String s) {
-        return new PDestination(s);
-    }
-    public Contents contents() {
-        return new PContents();
-    }
+
+    public Destination destination(String s) { return new PDestination(s); }
+    public Contents contents() { return new PContents(); }
 }
+
 public class TestParcel {
     public static void main(String[] args) {
         Parcel4 p = new Parcel4();
