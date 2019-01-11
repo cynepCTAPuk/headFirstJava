ed3b7f416e4d85a0f9451d40995c58a4d68c8ae5
cynepCTAPuk <CTAPuk@gmail.com>
2019-01-11 20:15:38 +0300

    Sequence

--- javaBruceEckel/chap10/Sequence.java
+++ javaBruceEckel/chap10/Sequence.java
@@ -32,7 +32,7 @@ public class Sequence {
             if(selector.end()) System.out.println();
         }
         System.out.println(sequence);
-        System.out.println();
+        System.out.println(selector);
     }
 } /* Output:
 0 1 2 3 4 5 6 7 8 9
