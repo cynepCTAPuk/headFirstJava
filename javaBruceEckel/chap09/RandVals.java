a92bacece004cf19b693654c93d0f63f28a20e35
cynepCTAPuk <CTAPuk@gmail.com>
2019-01-11 17:33:30 +0300

    RandVals

--- javaBruceEckel/chap09/RandVals.java
+++ javaBruceEckel/chap09/RandVals.java
@@ -1,4 +1,24 @@
 package chap09;
 
-public class RandVals {
-}
+//: interfaces/RandVals.java
+// Initializing interface fields with
+// non-constant initializers.
+import java.util.*;
+
+import static util.Print.print;
+import static util.Print.printf;
+
+public interface RandVals {
+    Random RAND = new Random(47);
+    int RANDOM_INT = RAND.nextInt(10);
+    long RANDOM_LONG = RAND.nextLong() * 10;
+    float RANDOM_FLOAT = RAND.nextLong() * 10;
+    double RANDOM_DOUBLE = RAND.nextDouble() * 10;
+
+    static void main(String[] args) {
+        print(RANDOM_INT);
+        printf("%,d\n",RANDOM_LONG);
+        print(RANDOM_FLOAT);
+        print(RANDOM_DOUBLE);
+    }
+} ///:~
