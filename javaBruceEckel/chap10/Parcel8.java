9abf87fae40692304569976f7f7e1703f9cd70ff
cynepCTAPuk <CTAPuk@gmail.com>
2019-01-12 13:53:04 +0300

    Parcel8

--- javaBruceEckel/chap10/Parcel8.java
+++ javaBruceEckel/chap10/Parcel8.java
@@ -7,9 +7,7 @@ public class Parcel8 {
     public Wrapping wrapping(int x) {
         // Base constructor call:
         return new Wrapping(x) { // Pass constructor argument.
-            public int value() {
-                return super.value() * 47;
-            }
+            public int value() { return super.value() * 47; }
         }; // Semicolon required
     }
     public static void main(String[] args) {
