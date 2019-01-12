fd3cf194678c8181661ffe2d685d538f47cd65c7
cynepCTAPuk <CTAPuk@gmail.com>
2019-01-12 13:51:11 +0300

    Wrapping

--- javaBruceEckel/chap10/Wrapping.java
+++ javaBruceEckel/chap10/Wrapping.java
@@ -1,4 +1,8 @@
 package chap10;
 
+//: innerclasses/Wrapping.java
 public class Wrapping {
-}
+    private int i;
+    public Wrapping(int x) { i = x; }
+    public int value() { return i; }
+} ///:~
