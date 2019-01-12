35a8697f859f77782918118f1d46d8ba4434595f
cynepCTAPuk <CTAPuk@gmail.com>
2019-01-12 18:04:27 +0300

    InheritInner

--- javaBruceEckel/chap10/InheritInner.java
+++ javaBruceEckel/chap10/InheritInner.java
@@ -3,15 +3,18 @@ package chap10;
 //: innerclasses/InheritInner.java
 // Inheriting an inner class.
 class WithInner {
-    class Inner {}
+    class Inner {Inner(){
+        System.out.println("WithInner.Inner");
+    }}
 }
 public class InheritInner extends WithInner.Inner {
     //! InheritInner() {} // Won’t compile
     InheritInner(WithInner wi) { wi.super(); }
 
     public static void main(String[] args) {
-        WithInner wi = new WithInner();
-        InheritInner ii = new InheritInner(wi);
+//        WithInner wi = new WithInner();
+//        InheritInner ii = new InheritInner(wi);
+        InheritInner ii = new InheritInner(new WithInner());
         System.out.println(ii);
     }
 } ///:~
