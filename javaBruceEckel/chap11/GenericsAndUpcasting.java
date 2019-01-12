e74543f1f04c7bd4ca4d36cafbd1a487c6e0b3b2
cynepCTAPuk <CTAPuk@gmail.com>
2019-01-12 18:50:51 +0300

    GenericsAndUpcasting

--- javaBruceEckel/chap11/GenericsAndUpcasting.java
+++ javaBruceEckel/chap11/GenericsAndUpcasting.java
@@ -10,10 +10,11 @@ public class GenericsAndUpcasting {
     public static void main(String[] args) {
         ArrayList<Apple> apples = new ArrayList<Apple>();
         apples.add(new GrannySmith());
+        apples.add(new Gala());
+        apples.add(new GrannySmith());
         apples.add(new Fuji());
         apples.add(new Braeburn());
-        for(Apple c : apples)
-            System.out.println(c);
+        for(Apple c : apples) System.out.println(c);
     }
 } /* Output: (Sample)
 GrannySmith@7d772e
