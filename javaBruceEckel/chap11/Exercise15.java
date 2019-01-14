cfd030173f300f54a4694fc725a88aa59c97d9a5
cynepCTAPuk <CTAPuk@gmail.com>
2019-01-14 21:29:01 +0300

    Exercise 15

--- javaBruceEckel/chap11/Exercise15.java
+++ javaBruceEckel/chap11/Exercise15.java
@@ -3,10 +3,13 @@ package chap11;
 public class Exercise15 {
     public static void main(String[] args) {
         String s = "+U+n+c—+e+r+t—+a-+i-+n+t+y—+ -+r+u—+l+e+s—";
-        char[] c = s.toCharArray();
         Stack<Character> stack = new Stack<>();
-        for (int i = 0; i < s.length(); i++) stack.push(s.charAt(i));
-        for (int i = 0; i < s.length(); i++) System.out.println(stack.pop());
+        for (int j = 0; j < s.length(); j++) {
+            if (s.charAt(j) == '+') stack.push(s.charAt(j + 1));
+            if (s.charAt(j) == '-') System.out.print(stack.pop());
+        }
+        System.out.println();
+        System.out.println(stack.toString());
     }
 }
 
