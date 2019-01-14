aa3f67a9badbf65b6f44d1ff38d9426d0dff3d4f
cynepCTAPuk <CTAPuk@gmail.com>
2019-01-14 19:53:52 +0300

    Stack

--- javaBruceEckel/chap11/Stack.java
+++ javaBruceEckel/chap11/Stack.java
@@ -1,4 +1,13 @@
 package chap11;
 
-public class Stack {
-}
+//: net/mindview/util/Stack.java
+// Making a stack from a LinkedList.
+import java.util.LinkedList;
+public class Stack<T> {
+    private LinkedList<T> storage = new LinkedList<T>();
+    public void push(T v) { storage.addFirst(v); }
+    public T peek() { return storage.getFirst(); }
+    public T pop() { return storage.removeFirst(); }
+    public boolean empty() { return storage.isEmpty(); }
+    public String toString() { return storage.toString(); }
+} ///:~
