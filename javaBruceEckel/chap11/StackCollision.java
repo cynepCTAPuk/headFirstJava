3e2019ead7791e3769625ff1504b454d684beab7
cynepCTAPuk <CTAPuk@gmail.com>
2019-01-14 20:28:36 +0300

    StackCollision

--- javaBruceEckel/chap11/StackCollision.java
+++ javaBruceEckel/chap11/StackCollision.java
@@ -1,4 +1,18 @@
 package chap11;
 
+//: holding/StackCollision.java
 public class StackCollision {
-}
+    public static void main(String[] args) {
+        chap11.Stack<String> stack = new chap11.Stack<>();
+        for(String s : "My dog has fleas".split(" ")) stack.push(s);
+        while(!stack.empty()) System.out.print(stack.pop() + " ");
+        System.out.println();
+
+        java.util.Stack<String> stack2 = new java.util.Stack<>();
+        for(String s : "My dog has fleas".split(" ")) stack2.push(s);
+        while(!stack2.empty()) System.out.print(stack2.pop() + " ");
+    }
+} /* Output:
+fleas has dog My
+fleas has dog My
+*///:~
