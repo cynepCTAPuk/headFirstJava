3bafeefa1394a2d7317e6b2be8caf9e708749abf
cynepCTAPuk <CTAPuk@gmail.com>
2019-01-15 16:06:53 +0300

    SetVowels

--- javaBruceEckel/chap11/SetVowels.java
+++ javaBruceEckel/chap11/SetVowels.java
@@ -2,10 +2,10 @@ package chap11;
 
 import java.util.*;
 
-public class Exercise16 {
+public class SetVowels {
     public static void main(String[] args) {
-        String vowels = "a e i o u y";
-        Set<String> setVowels = new HashSet<>();
+        String vowels = "a e i o u y A E I O U Y";
+        Set<String> setVowels = new TreeSet<>();
         setVowels.addAll(Arrays.asList(vowels.split(" ")));
         System.out.println(setVowels);
     }
