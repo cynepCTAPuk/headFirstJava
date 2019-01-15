1ff7cb0ff7d9cc20d4404389f4ddd486272bf742
cynepCTAPuk <CTAPuk@gmail.com>
2019-01-15 15:38:19 +0300

    UniqueWords

--- javaBruceEckel/chap11/UniqueWords.java
+++ javaBruceEckel/chap11/UniqueWords.java
@@ -6,7 +6,7 @@ import util.*;
 public class UniqueWords {
     public static void main(String[] args) {
         Set<String> words = new TreeSet<String>(
-                new TextFile("SetOperations.java", "\\W+"));
+                new TextFile("chap11/SetOperations.java", "\\W+"));
         System.out.println(words);
     }
 } /* Output:
