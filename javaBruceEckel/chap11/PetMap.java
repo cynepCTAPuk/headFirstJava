4ac0fb1c2b203bb7560d279723359fcb9a63b3fd
cynepCTAPuk <CTAPuk@gmail.com>
2019-01-15 17:27:02 +0300

    PetMap

--- javaBruceEckel/chap11/PetMap.java
+++ javaBruceEckel/chap11/PetMap.java
@@ -6,15 +6,17 @@ import java.util.*;
 import static util.Print.*;
 public class PetMap {
     public static void main(String[] args) {
-        Map<String,Pet> petMap = new HashMap<String,Pet>();
+        Map<String,Pet> petMap = new HashMap<>();
         petMap.put("My Cat", new Cat("Molly"));
         petMap.put("My Dog", new Dog("Ginger"));
         petMap.put("My Hamster", new Hamster("Bosco"));
         print(petMap);
+
         Pet dog = petMap.get("My Dog");
         print(dog);
         print(petMap.containsKey("My Dog"));
         print(petMap.containsValue(dog));
+        print(petMap.values());
     }
 } /* Output:
 {My Cat=Cat Molly, My Hamster=Hamster Bosco, My Dog=Dog Ginger}
