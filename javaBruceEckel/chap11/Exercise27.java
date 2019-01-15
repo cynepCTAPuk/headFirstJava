3a83dc00f3a043cb651c9cf9ed6be7e321bcbf5c
cynepCTAPuk <CTAPuk@gmail.com>
2019-01-15 18:26:55 +0300

    Exercise 27

---
+++ javaBruceEckel/chap11/Exercise27.java
@@ -0,0 +1,10 @@
+package chap11;
+
+import java.util.*;
+
+public class Exercise27 {
+    public static void main(String[] args) {
+        Queue<Command> queue = new LinkedList<>();
+        queue.offer(new Command("first"));
+    }
+}
