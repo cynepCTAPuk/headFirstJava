db2300b723e67f1eed8a2de608c5e92962cd177f
cynepCTAPuk <CTAPuk@gmail.com>
2019-01-15 17:37:20 +0300

    MapOfList

--- javaBruceEckel/chap11/MapOfList.java
+++ javaBruceEckel/chap11/MapOfList.java
@@ -5,24 +5,30 @@ import java.util.*;
 import static util.Print.*;
 
 public class MapOfList {
-    public static Map<Person, List<? extends Pet>>
-            petPeople = new HashMap<>();
+    public static Map<Person, List<? extends Pet>> petPeople = new HashMap<>();
 
     static {
         petPeople.put(new Person("Dawn"),
-                Arrays.asList(new Cymric("Molly"), new Mutt("Spot")));
+                Arrays.asList(
+                        new Cymric("Molly"),
+                        new Mutt("Spot")));
         petPeople.put(new Person("Kate"),
-                Arrays.asList(new Cat("Shackleton"),
-                        new Cat("Elsie May"), new Dog("Margrett")));
+                Arrays.asList(
+                        new Cat("Shackleton"),
+                        new Cat("Elsie May"),
+                        new Dog("Margrett")));
         petPeople.put(new Person("Marilyn"),
                 Arrays.asList(
                         new Pug("Louie aka Louis Snorkelstein Dupree"),
                         new Cat("Stanford aka Stinky el Negro"),
                         new Cat("Pinkola")));
         petPeople.put(new Person("Luke"),
-                Arrays.asList(new Rat("Fuzzy"), new Rat("Fizzy")));
+                Arrays.asList(
+                        new Rat("Fuzzy"),
+                        new Rat("Fizzy")));
         petPeople.put(new Person("Isaac"),
-                Arrays.asList(new Rat("Freckly")));
+                Arrays.asList(
+                        new Rat("Freckly")));
     }
 
     public static void main(String[] args) {
