8c27ec325e785cd41a11cdaacdd934c3d171a2f1
cynepCTAPuk <CTAPuk@gmail.com>
2019-01-16 15:39:13 +0300

    ModifyingArraysAsList

--- javaBruceEckel/chap08/shape/ModifyingArraysAsList.java
+++ javaBruceEckel/chap08/shape/ModifyingArraysAsList.java
@@ -7,12 +7,16 @@ public class ModifyingArraysAsList {
         Integer[] ia = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
 
         List<Integer> list1 = new ArrayList<>(Arrays.asList(ia));
+        System.out.println("List<Integer> list1 = new ArrayList<>(Arrays.asList(ia))");
         System.out.println("list1 Before shuffling: " + list1);
         Collections.shuffle(list1, rand);
         System.out.println("list1 After shuffling: " + list1);
         System.out.println("array: " + Arrays.toString(ia));
 
+        System.out.println();
+
         List<Integer> list2 = Arrays.asList(ia);
+        System.out.println("List<Integer> list2 = Arrays.asList(ia)");
         System.out.println("list2 Before shuffling: " + list2);
         Collections.shuffle(list2, rand);
         System.out.println("list2 After shuffling: " + list2);
