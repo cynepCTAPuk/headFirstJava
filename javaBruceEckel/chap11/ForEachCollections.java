503f2d7ea0677b2bd8afd19d41841cccd4514e41
cynepCTAPuk <CTAPuk@gmail.com>
2019-01-16 13:28:46 +0300

    ForEachCollections

--- javaBruceEckel/chap11/ForEachCollections.java
+++ javaBruceEckel/chap11/ForEachCollections.java
@@ -1,4 +1,13 @@
+//: holding/ForEachCollections.java
+// All collections work with foreach.
 package chap11;
-
+import java.util.*;
 public class ForEachCollections {
-}
+    public static void main(String[] args) {
+        Collection<String> cs = new LinkedList<String>();
+        Collections.addAll(cs, "Take the long way home".split(" "));
+        for(String s : cs) System.out.print("‘" + s + "‘ ");
+    }
+} /* Output:
+‘Take’ ‘the’ ‘long’ ‘way’ ‘home’
+*///:~
