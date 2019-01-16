29d8b60f51a45d1e3d1062c7296ed8bf6e800edc
cynepCTAPuk <CTAPuk@gmail.com>
2019-01-16 13:46:08 +0300

    EnvironmentVariables

--- javaBruceEckel/chap11/EnvironmentVariables.java
+++ javaBruceEckel/chap11/EnvironmentVariables.java
@@ -1,4 +1,11 @@
 package chap11;
-
+//: holding/EnvironmentVariables.java
+import java.util.*;
 public class EnvironmentVariables {
-}
+    public static void main(String[] args) {
+        for(Map.Entry entry: System.getenv().entrySet()) {
+            System.out.println(entry.getKey() + ": " +
+                    entry.getValue());
+        }
+    }
+} /* (Execute to see output) *///:~
