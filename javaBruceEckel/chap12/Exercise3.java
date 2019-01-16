d35c9377de53198fba48d8629750336bf98b9900
cynepCTAPuk <CTAPuk@gmail.com>
2019-01-16 16:37:47 +0300

    Exercise 3

--- javaBruceEckel/chap12/Exercise3.java
+++ javaBruceEckel/chap12/Exercise3.java
@@ -3,6 +3,11 @@ package chap12;
 public class Exercise3 {
     public static void main(String[] args) {
         int[] ints = {1, 2, 3, 4};
-        System.out.println( ints[4]);
+        try {
+            System.out.println(ints[5]);
+        } catch (ArrayIndexOutOfBoundsException e) {
+            System.err.println("index");
+            e.printStackTrace(System.out);
+        }
     }
 }
