a76c65072da9d1007e3b596ab79cbedd7adfa9c5
cynepCTAPuk <CTAPuk@gmail.com>
2019-01-16 14:22:39 +0300

    Shapes

--- javaBruceEckel/chap08/shape/Shapes.java
+++ javaBruceEckel/chap08/shape/Shapes.java
@@ -9,10 +9,7 @@ public class Shapes {
         // Fill up the array with shapes:
         for(int i = 0; i < shapes.length; i++) shapes[i] = shape.next();
         // Make polymorphic method calls:
-        for(Shape shp : shapes) {
-            shp.draw();
-//            shp.fill();
-        }
+        for(Shape shp : shapes) { shp.draw(); }
     }
 } /* Output:
 Triangle.draw()
