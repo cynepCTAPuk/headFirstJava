96b54572ebf883a0b0f2b8b784fef22b69ca682e
cynepCTAPuk <CTAPuk@gmail.com>
2019-01-17 13:38:29 +0300

    OnOffSwitch

--- javaBruceEckel/chap12/OnOffSwitch.java
+++ javaBruceEckel/chap12/OnOffSwitch.java
@@ -3,12 +3,11 @@ package chap12;
 // Why use finally?
 public class OnOffSwitch {
     private static Switch sw = new Switch();
-    public static void f()
-            throws OnOffException1,OnOffException2 {}
+    public static void f() throws OnOffException1,OnOffException2 {}
     public static void main(String[] args) {
         try {
             sw.on();
-// Code that can throw exceptions...
+            // Code that can throw exceptions...
             f();
             sw.off();
         } catch(OnOffException1 e) {
