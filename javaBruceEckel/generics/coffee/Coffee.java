7af446b2032d8d10404f8893268b445e7208d58f
cynepCTAPuk <CTAPuk@gmail.com>
2019-01-17 19:16:38 +0300

    Coffee

--- javaBruceEckel/generics/coffee/Coffee.java
+++ javaBruceEckel/generics/coffee/Coffee.java
@@ -1,1 +1,9 @@
+//: generics/coffee/Coffee.java
 package generics.coffee;
+public class Coffee {
+    private static long counter = 0;
+    private final long id = counter++;
+    public String toString() {
+        return getClass().getSimpleName() + " " + id;
+    }
+} ///:~
