2ac6ea1b2809d753754f5da3f418f74c09c18784
cynepCTAPuk <CTAPuk@gmail.com>
2019-01-17 18:14:13 +0300

    MainException

--- javaBruceEckel/chap12/MainException.java
+++ javaBruceEckel/chap12/MainException.java
@@ -1,4 +1,13 @@
+//: exceptions/MainException.java
 package chap12;
-
+import java.io.*;
 public class MainException {
-}
+    // Pass all exceptions to the console:
+    public static void main(String[] args) throws Exception {
+        // Open the file:
+        FileInputStream file = new FileInputStream("chap12/MainException.java");
+        // Use the file ...
+        // Close the file:
+        file.close();
+    }
+} ///:~
