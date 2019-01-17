9cca5a19e821a6dc8554a6bf1d92014e7554179e
cynepCTAPuk <CTAPuk@gmail.com>
2019-01-17 19:34:31 +0300

    InfiniteRecursion

--- javaBruceEckel/strings/InfiniteRecursion.java
+++ javaBruceEckel/strings/InfiniteRecursion.java
@@ -1,4 +1,16 @@
+//: strings/InfiniteRecursion.java
+// Accidental recursion.
+// {RunByHand}
 package strings;
-
+import java.util.*;
 public class InfiniteRecursion {
-}
+    public String toString() {
+        return " InfiniteRecursion address: " + this + "\n";
+    }
+
+    public static void main(String[] args) {
+        List<InfiniteRecursion> v = new ArrayList<>();
+        for(int i = 0; i < 10; i++) v.add(new InfiniteRecursion());
+        System.out.println(v);
+    }
+} ///:~
