6092a4713dd7b783750e4391967de1bb14993b00
cynepCTAPuk <CTAPuk@gmail.com>
2019-01-17 13:13:59 +0300

    NeverCaught

--- javaBruceEckel/chap12/NeverCaught.java
+++ javaBruceEckel/chap12/NeverCaught.java
@@ -1,4 +1,15 @@
 package chap12;
-
+//: exceptions/NeverCaught.java
+// Ignoring RuntimeExceptions.
+// {ThrowsException}
 public class NeverCaught {
-}
+    static void f() {
+        throw new RuntimeException("From f()");
+    }
+    static void g() {
+        f();
+    }
+    public static void main(String[] args) {
+        g();
+    }
+} ///:~
