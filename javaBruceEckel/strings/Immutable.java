21c4b9b84e84e196b882df48bc219683b6d832d3
cynepCTAPuk <CTAPuk@gmail.com>
2019-01-17 18:35:25 +0300

    Immutable

--- javaBruceEckel/strings/Immutable.java
+++ javaBruceEckel/strings/Immutable.java
@@ -2,8 +2,10 @@
 package strings;
 import static util.Print.*;
 public class Immutable {
-    public static String upcase(String s) { return s.toUpperCase(); }
-    
+    public static String upcase(String s) {
+        return s.toUpperCase();
+    }
+
     public static void main(String[] args) {
         String q = "howdy";
         print(q); // howdy
