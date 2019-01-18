46b68ca9d70589cb9936d0796602b4778f9e9cf2
cynepCTAPuk <CTAPuk@gmail.com>
2019-01-18 16:41:45 +0300

    DatabaseException

--- javaBruceEckel/chap13_strings/DatabaseException.java
+++ javaBruceEckel/chap13_strings/DatabaseException.java
@@ -1,4 +1,16 @@
+//: strings/DatabaseException.java
 package chap13_strings;
-
-public class DatabaseException {
-}
+public class DatabaseException extends Exception {
+    public DatabaseException(int transactionID, int queryID, String message) {
+        super(String.format("(t%d, q%d) %s", transactionID, queryID, message));
+    }
+    public static void main(String[] args) {
+        try {
+            throw new DatabaseException(3, 7, "Write failed");
+        } catch(Exception e) {
+            System.out.println(e);
+        }
+    }
+} /* Output:
+DatabaseException: (t3, q7) Write failed
+*///:~
