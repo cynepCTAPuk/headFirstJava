eab64d26fe65c8d167828b6284b84cba26727cca
cynepCTAPuk <CTAPuk@gmail.com>
2019-01-18 19:30:23 +0300

    Exercise 9

---
+++ javaBruceEckel/chap13_strings/Exercise9.java
@@ -0,0 +1,12 @@
+//: strings/Splitting.java
+package chap13_strings;
+
+public class Exercise9 {
+    public static String knights =
+            "Then, when you have found the shrubbery, you must " +
+                    "cut down the mightiest tree in the forest... with... a herring!";
+
+    public static void main(String[] args) {
+        System.out.println(knights.replaceAll("[a|e|i|o|u|y|A|E|I|O|U|Y]","*"));
+    }
+}
