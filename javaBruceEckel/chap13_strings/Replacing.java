76f8f96d903d234c8959385628c1063aee6be6ee
cynepCTAPuk <CTAPuk@gmail.com>
2019-01-18 17:44:27 +0300

    Replacing

--- javaBruceEckel/chap13_strings/Replacing.java
+++ javaBruceEckel/chap13_strings/Replacing.java
@@ -4,6 +4,7 @@ import static util.Print.*;
 public class Replacing {
     static String s = Splitting.knights;
     public static void main(String[] args) {
+        print(s.replaceFirst("f\\w+", "located"));
         print(s.replaceAll("shrubbery|tree|herring","banana"));
     }
 } /* Output:
