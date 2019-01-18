e5ea882ef694ec013de6c8760160ab85efff7e32
cynepCTAPuk <CTAPuk@gmail.com>
2019-01-18 17:10:52 +0300

    Exercise 6

--- javaBruceEckel/chap13_strings/Exercise6.java
+++ javaBruceEckel/chap13_strings/Exercise6.java
@@ -16,11 +16,11 @@ public class Exercise6 {
     @Override
     public String toString() {
 //        return "Exercise6{" + "i=" + i + ", l=" + l + ", f=" + f + ", d=" + d + '}';
-        return String.format("Exercise6{i=%d, l=%l, f=%f, d=%f}", i, l, f, d);
+        return String.format("Exercise6{i=%,d, l=%,d, f=%.2f, d=%.2f}", i, l, f, d);
     }
 
     public static void main(String[] args) {
-        Exercise6 ex = new Exercise6(1, 2l, 3.00f, 4.00);
+        Exercise6 ex = new Exercise6(1000, 2000l, 3.00f, 4.00);
         System.out.println(ex);
     }
 }
