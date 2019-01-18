7e834aaddbf85167af7d7bfe92cb733923ecb6f9
cynepCTAPuk <CTAPuk@gmail.com>
2019-01-18 17:32:57 +0300

    Splitting

--- javaBruceEckel/chap13_strings/Splitting.java
+++ javaBruceEckel/chap13_strings/Splitting.java
@@ -4,8 +4,7 @@ import java.util.*;
 public class Splitting {
     public static String knights =
             "Then, when you have found the shrubbery, you must " +
-                    "cut down the mightiest tree in the forest... " +
-                    "with... a herring!";
+                    "cut down the mightiest tree in the forest... with... a herring!";
     public static void split(String regex) {
         System.out.println( Arrays.toString(knights.split(regex)));
     }
