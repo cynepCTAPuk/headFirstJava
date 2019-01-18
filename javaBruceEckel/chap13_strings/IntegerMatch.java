ff2e62902f329a7f46a1c6c765dedbce011ea029
cynepCTAPuk <CTAPuk@gmail.com>
2019-01-18 17:20:18 +0300

    IntegerMatch

--- javaBruceEckel/chap13_strings/IntegerMatch.java
+++ javaBruceEckel/chap13_strings/IntegerMatch.java
@@ -1,4 +1,17 @@
+//: strings/IntegerMatch.java
 package chap13_strings;
-
 public class IntegerMatch {
-}
+    public static void main(String[] args) {
+        System.out.println("-?\\d+");
+        System.out.println("-1234".matches("-?\\d+"));
+        System.out.println("5678".matches("-?\\d+"));
+        System.out.println("+911".matches("-?\\d+"));
+        System.out.println("+911".matches("(-|\\+)?\\d+"));
+        System.out.println("+911".matches("([-+])?\\d+")); // new
+    }
+} /* Output:
+true
+true
+false
+true
+*///:~
