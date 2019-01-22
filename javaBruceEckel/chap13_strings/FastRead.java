9f6008eece0a4bbb9c4009f28b5a0252b9e71a64
cynepCTAPuk <CTAPuk@gmail.com>
2019-01-22 18:52:53 +0300

    FastRead

---
+++ javaBruceEckel/chap13_strings/FastRead.java
@@ -0,0 +1,39 @@
+package chap13_strings;
+
+import java.io.*;
+import java.util.*;
+
+public class FastRead {
+    public static void main(String[] args) {
+        FastScanner scanner = new FastScanner(System.in);
+        int n = scanner.nextInt();
+    }
+
+    static class FastScanner {
+        BufferedReader br;
+        StringTokenizer st;
+
+        FastScanner(InputStream stream) {
+            try {
+                br = new BufferedReader(new InputStreamReader(stream));
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+
+        String next() {
+            while (st == null || !st.hasMoreTokens()) {
+                try {
+                    st = new StringTokenizer(br.readLine());
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+            return st.nextToken();
+        }
+
+        int nextInt() {
+            return Integer.parseInt(next());
+        }
+    }
+}
