bb67682161352648c3909c0816be3f475b8bba2a
cynepCTAPuk <CTAPuk@gmail.com>
2019-01-22 21:46:32 +0300

    ThreatAnalyzer

--- javaBruceEckel/chap13_strings/ThreatAnalyzer.java
+++ javaBruceEckel/chap13_strings/ThreatAnalyzer.java
@@ -15,9 +15,9 @@ public class ThreatAnalyzer {
         String pattern = "(\\d+[.]\\d+[.]\\d+[.]\\d+)@" + "(\\d{2}/\\d{2}/\\d{4})";
         while(scanner.hasNext(pattern)) {
             scanner.next(pattern);
-            MatchResult match = scanner.match();
-            String ip = match.group(1);
-            String date = match.group(2);
+            MatchResult matcher = scanner.match();
+            String ip = matcher.group(1);
+            String date = matcher.group(2);
             System.out.format("Threat on %s from %s\n", date,ip);
         }
     }
