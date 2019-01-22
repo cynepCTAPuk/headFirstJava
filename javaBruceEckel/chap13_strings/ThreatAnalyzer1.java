3a9358115ae7ce721c63528bb1553c425364964a
cynepCTAPuk <CTAPuk@gmail.com>
2019-01-23 00:39:23 +0300

    ThreatAnalyzer1

--- javaBruceEckel/chap13_strings/ThreatAnalyzer1.java
+++ javaBruceEckel/chap13_strings/ThreatAnalyzer1.java
@@ -1,19 +1,14 @@
 //: strings/ThreatAnalyzer.java
 package chap13_strings;
 
+import java.io.File;
+import java.io.FileNotFoundException;
 import java.util.*;
 import java.util.regex.*;
 
 public class ThreatAnalyzer1 {
-    static String threatData =
-            "58.27.82.161@02/10/2005\n" +
-                    "204.45.234.40@02/11/2005\n" +
-                    "58.27.82.161@02/11/2005\n" +
-                    "58.27.82.161@02/12/2005\n" +
-                    "58.27.82.161@02/12/2005\n" +
-                    "[Next log section with different data format]";
-    public static void main(String[] args) {
-        Scanner scanner = new Scanner("C:/000/threadData.txt");
+    public static void main(String[] args) throws FileNotFoundException {
+        Scanner scanner = new Scanner(new File("/000/threadData.txt"));
         String pattern = "(\\d+[.]\\d+[.]\\d+[.]\\d+)@" + "(\\d{2}/\\d{2}/\\d{4})";
         while(scanner.hasNext(pattern)) {
             scanner.next(pattern);
