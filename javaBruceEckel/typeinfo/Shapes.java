e1b4ba42069743d525b6273af8f9643efc7ca469
cynepCTAPuk <CTAPuk@gmail.com>
2019-01-23 18:30:06 +0300

    Shapes

--- javaBruceEckel/typeinfo/Shapes.java
+++ javaBruceEckel/typeinfo/Shapes.java
@@ -1,32 +1,26 @@
 //: typeinfo/Shapes.java
 package typeinfo;
 import java.util.*;
-
 import static util.Print.print;
-
 abstract class Shape {
     void draw() { print(this + ".draw()"); }
     abstract public String toString();
 }
-class Circle extends Shape {
-    public String toString() { return "Circle"; }
-}
-class Square extends Shape {
-    public String toString() { return "Square"; }
-}
-class Triangle extends Shape {
-    public String toString() { return "Triangle"; }
-}
-class Rhomboid extends Shape {
-    public String toString() { return "Rhomboid"; }
-}
+class Circle extends Shape { public String toString() { return "Circle"; }}
+class Square extends Shape { public String toString() { return "Square"; }}
+class Triangle extends Shape { public String toString() { return "Triangle"; }}
+class Rhomboid extends Shape { public String toString() { return "Rhomboid"; }}
 public class Shapes {
     public static void main(String[] args) {
         List<Shape> shapeList = Arrays.asList(
                 new Circle(), new Square(), new Triangle(), new Rhomboid()
         );
-        for(Shape shape : shapeList) shape.draw();
-        for(Shape shape : shapeList) print(shape.getClass().getSimpleName());
+//        for(Shape shape : shapeList) shape.draw();
+//        for(Shape shape : shapeList) print(shape.getClass().getSimpleName());
+        Rhomboid r = new Rhomboid();
+        print((Shape)r);
+//        print((Circle)r);
+// incompatible types:typeinfo.Rhomboid cannot be converted to typeinfo.Circel
     }
 } /* Output:
 Circle.draw()
