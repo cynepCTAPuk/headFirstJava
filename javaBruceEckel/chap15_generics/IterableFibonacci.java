3e937a9d01f84a7839333a27dd9aa2290fa17db1
cynepCTAPuk <CTAPuk@gmail.com>
2019-01-24 21:16:33 +0300

    IterableFibonacci

--- javaBruceEckel/chap15_generics/IterableFibonacci.java
+++ javaBruceEckel/chap15_generics/IterableFibonacci.java
@@ -6,15 +6,14 @@ public class IterableFibonacci extends Fibonacci implements Iterable<Integer> {
     private int n;
     public IterableFibonacci(int count) { n = count; }
     public Iterator<Integer> iterator() {
-        return new Iterator<Integer>() {
+        return new Iterator<>() {
             public boolean hasNext() { return n > 0; }
             public Integer next() {
                 n--;
                 return IterableFibonacci.this.next();
             }
-            public void remove() { // Not implemented
-                throw new UnsupportedOperationException();
-            }
+            // Not implemented
+            public void remove() { throw new UnsupportedOperationException(); }
         };
     }
     public static void main(String[] args) {
