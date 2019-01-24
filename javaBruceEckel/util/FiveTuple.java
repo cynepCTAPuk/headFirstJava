8d871aaa57d215d138db3bb17e43364cc5de5d5c
cynepCTAPuk <CTAPuk@gmail.com>
2019-01-24 19:05:42 +0300

    FiveTuple

--- javaBruceEckel/util/FiveTuple.java
+++ javaBruceEckel/util/FiveTuple.java
@@ -7,7 +7,6 @@ public class FiveTuple<A,B,C,D,E>  extends FourTuple<A,B,C,D> {
         fifth = e;
     }
     public String toString() {
-        return "(" + first + ", " + second + ", " +
-                third + ", " + fourth + ", " + fifth + ")";
+        return "(" + first + ", " + second + ", " + third + ", " + fourth + ", " + fifth + ")";
     }
 } ///:~
