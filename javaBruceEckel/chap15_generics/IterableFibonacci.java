69be6a0682bc2c2c1ea9a0cfab9c2af626856a6f
cynepCTAPuk <CTAPuk@gmail.com>
2019-01-24 21:28:43 +0300

    IterableFibonacci

--- javaBruceEckel/chap15_generics/IterableFibonacci.java
+++ javaBruceEckel/chap15_generics/IterableFibonacci.java
@@ -19,6 +19,10 @@ public class IterableFibonacci extends Fibonacci implements Iterable<Integer> {
     }
     public static void main(String[] args) {
         for(int i : new IterableFibonacci(18)) System.out.print(i + " ");
+        System.out.println();
+        
+        IterableFibonacci ifi = new IterableFibonacci(18);
+        while(ifi.iterator().hasNext()) System.out.print(ifi.iterator().next() + " ");
     }
 } /* Output:
 1 1 2 3 5 8 13 21 34 55 89 144 233 377 610 987 1597 2584
