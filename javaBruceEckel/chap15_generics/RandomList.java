2510a6b7a5d2a82f83cf05a2ec77d5ce1feb24a9
cynepCTAPuk <CTAPuk@gmail.com>
2019-01-24 21:02:57 +0300

    RandomList

--- javaBruceEckel/chap15_generics/RandomList.java
+++ javaBruceEckel/chap15_generics/RandomList.java
@@ -6,9 +6,7 @@ public class RandomList<T> {
     private ArrayList<T> storage = new ArrayList<>();
     private Random rand = new Random(47);
 
-    public void add(T item) {
-        storage.add(item);
-    }
+    public void add(T item) { storage.add(item); }
 
     public T select() {
         return storage.get(rand.nextInt(storage.size()));
@@ -16,9 +14,21 @@ public class RandomList<T> {
 
     public static void main(String[] args) {
         RandomList<String> rls = new RandomList<>();
-        String string = "The quick brown fox jumped over the lazy brown dog";
-        for (String s : string.split(" ")) rls.add(s);
+        String ss = "The quick brown fox jumped over the lazy brown dog";
+        for (String s : ss.split(" ")) rls.add(s);
         for (int i = 0; i < 17; i++) System.out.print(rls.select() + " ");
+
+        RandomList<Integer> rli = new RandomList<>();
+        Integer[] ints = {1, 2, 3, 10, 20, 30};
+        for (Integer i : ints) rli.add(i);
+        for (int i = 0; i < 17; i++) System.out.print(rli.select() + " ");
+
+        RandomList<Boolean> rlb = new RandomList<>();
+        rlb.add(false);
+        rlb.add(true);
+        for (int i = 0; i < 17; i++) System.out.print(rlb.select() + " ");
+
+
     }
 } /* Output:
 brown over fox quick quick dog brown The brown lazy brown
