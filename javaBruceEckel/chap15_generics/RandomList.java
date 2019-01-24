549ab73fbac9407eb400918fd3eb2280b6cdbed2
cynepCTAPuk <CTAPuk@gmail.com>
2019-01-24 19:41:51 +0300

    RandomList

--- javaBruceEckel/chap15_generics/RandomList.java
+++ javaBruceEckel/chap15_generics/RandomList.java
@@ -1,4 +1,17 @@
 package chap15_generics;
-
-public class RandomList {
-}
+//: generics/RandomList.java
+import java.util.*;
+public class RandomList<T> {
+    private ArrayList<T> storage = new ArrayList<>();
+    private Random rand = new Random(47);
+    public void add(T item) { storage.add(item); }
+    public T select() { return storage.get(rand.nextInt(storage.size())); }
+    public static void main(String[] args) {
+        RandomList<String> rs = new RandomList<>();
+        String string = "The quick brown fox jumped over the lazy brown dog";
+        for(String s: string.split(" ")) rs.add(s);
+        for(int i = 0; i < 11; i++) System.out.print(rs.select() + " ");
+    }
+} /* Output:
+brown over fox quick quick dog brown The brown lazy brown
+*///:~
