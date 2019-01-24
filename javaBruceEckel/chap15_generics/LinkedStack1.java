0f16456fe77b7f958216c5c8e83a507d1ceb52ba
cynepCTAPuk <CTAPuk@gmail.com>
2019-01-24 19:33:18 +0300

    LinkedStack

--- javaBruceEckel/chap15_generics/LinkedStack1.java
+++ javaBruceEckel/chap15_generics/LinkedStack1.java
@@ -1,21 +1,21 @@
 //: generics/LinkedStack.java
 // A stack implemented with an internal linked structure.
 package chap15_generics;
-public class LinkedStack<T> {
-    private static class Node<U> {
-        U item;
-        Node<U> next;
+public class LinkedStack1<T> {
+    private static class Node {
+        T item;
+        Node next;
         Node() { item = null; next = null; }
-        Node(U item, Node<U> next) {
+        Node(T item, Node next) {
             this.item = item;
             this.next = next;
         }
         boolean end() { return item == null && next == null; }
     }
 
-    private Node<T> top = new Node<>(); // End sentinel
+    private Node top = new Node(); // End sentinel
 
-    public void push(T item) { top = new Node<>(item, top); }
+    public void push(T item) { top = new Node(item, top); }
     public T pop() {
         T result = top.item;
         if(!top.end()) top = top.next;
@@ -23,7 +23,7 @@ public class LinkedStack<T> {
     }
 
     public static void main(String[] args) {
-        LinkedStack<String> lss = new LinkedStack<>();
+        LinkedStack1<String> lss = new LinkedStack1<>();
         for(String s : "Phasers on stun!".split(" ")) lss.push(s);
         String s;
         while((s = lss.pop()) != null) System.out.println(s);
