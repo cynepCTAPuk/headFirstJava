aa16e7382374411caa6bc1d604e18221fed81af5
cynepCTAPuk <CTAPuk@gmail.com>
2019-01-24 22:43:09 +0300

    GeneratorsEx13

--- javaBruceEckel/chap15_generics/GeneratorsEx13.java
+++ javaBruceEckel/chap15_generics/GeneratorsEx13.java
@@ -1,22 +1,28 @@
 // A utility to use with Generators.
 package chap15_generics;
-
 import generics.coffee.*;
 import util.*;
-
 import java.util.*;
 
 public class GeneratorsEx13 {
-    public static <T> Collection<T> fill(Collection<T> coll, Generator<T> gen, int n) {
+    public static <T> List<T> fill(List<T> coll, Generator<T> gen, int n) {
+        for (int i = 0; i < n; i++) coll.add(gen.next());
+        return coll;
+    }
+    public static <T> Queue<T> fill(LinkedList<T> coll, Generator<T> gen, int n) {
+        for (int i = 0; i < n; i++) coll.add(gen.next());
+        return coll;
+    }
+    public static <T> Set<T> fill(HashSet<T> coll, Generator<T> gen, int n) {
         for (int i = 0; i < n; i++) coll.add(gen.next());
         return coll;
     }
 
     public static void main(String[] args) {
-        Collection<Coffee> coffee = fill(new ArrayList<>(), new CoffeeGenerator(), 3);
+        Collection<Coffee> coffee = fill(new ArrayList<>(), new CoffeeGenerator(), 4);
         for (Coffee c : coffee) System.out.println(c);
 
-        Collection<Integer> fnumbers = fill(new ArrayList<>(), new Fibonacci(), 12);
+        Collection<Integer> fnumbers = fill(new LinkedList<>(), new Fibonacci(), 12);
         for (int i : fnumbers) System.out.print(i + ", ");
     }
 } /* Output:
