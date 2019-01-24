984230067f7f6bb6c63eef0d147a698d1693614d
cynepCTAPuk <CTAPuk@gmail.com>
2019-01-24 18:23:34 +0300

    Holder1

--- javaBruceEckel/chap15_generics/Holder1.java
+++ javaBruceEckel/chap15_generics/Holder1.java
@@ -1,4 +1,8 @@
 package chap15_generics;
-
+//: generics/Holder1.java
+class Automobile {}
 public class Holder1 {
-}
+    private Automobile a;
+    public Holder1(Automobile a) { this.a = a; }
+    Automobile get() { return a; }
+} ///:~
