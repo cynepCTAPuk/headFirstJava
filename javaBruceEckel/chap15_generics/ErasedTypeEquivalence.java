77804afd775a0ddcf8c372e825cbbcb1e95d1cf4
cynepCTAPuk <CTAPuk@gmail.com>
2019-01-25 19:21:43 +0300

    ErasedTypeEquivalence

--- javaBruceEckel/chap15_generics/ErasedTypeEquivalence.java
+++ javaBruceEckel/chap15_generics/ErasedTypeEquivalence.java
@@ -6,6 +6,7 @@ public class ErasedTypeEquivalence {
         Class c1 = new ArrayList<String>().getClass();
         Class c2 = new ArrayList<Integer>().getClass();
         System.out.println(c1 == c2);
+        System.out.println(c1);
     }
 } /* Output:
 true
