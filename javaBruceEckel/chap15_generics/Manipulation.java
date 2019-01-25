bf60ef0f1e3c7b02e9e6849107d247c93d81880b
cynepCTAPuk <CTAPuk@gmail.com>
2019-01-25 19:39:30 +0300

    Manipulation

--- javaBruceEckel/chap15_generics/Manipulation.java
+++ javaBruceEckel/chap15_generics/Manipulation.java
@@ -1,16 +1,31 @@
-package chap15_generics;
 //: generics/Manipulation.java
 // {CompileTimeError} (Won’t compile)
+package chap15_generics;
 class Manipulator<T> {
     private T obj;
     public Manipulator(T x) { obj = x; }
-    // Error: cannot find symbol: method f():
-//    public void manipulate() { obj.f(); }
+//    public void manipulate() { obj.f(); } // Error: cannot find symbol: method f():
+}
+class Manipulator2<T extends HasF> {
+    private T obj;
+    public Manipulator2(T x) { obj = x; }
+    public void manipulate() { obj.f(); }
+}
+class Manipulator3 {
+    private HasF obj;
+    public Manipulator3(HasF x) { obj = x; }
+    public void manipulate() { obj.f(); }
 }
 public class Manipulation {
     public static void main(String[] args) {
         HasF hf = new HasF();
-        Manipulator<HasF> manipulator = new Manipulator<HasF>(hf);
+        Manipulator<HasF> manipulator = new Manipulator<>(hf);
 //        manipulator.manipulate();
+
+        Manipulator2<HasF> manipulator2 = new Manipulator2<>(hf);
+        manipulator2.manipulate();
+
+        Manipulator3 manipulator3 = new Manipulator3(hf);
+        manipulator3.manipulate();
     }
 } ///:~
