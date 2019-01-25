6ae7c34491085a5a18d64ae40a5b5c78ca74a218
cynepCTAPuk <CTAPuk@gmail.com>
2019-01-25 19:29:22 +0300

    Manipulation

--- javaBruceEckel/chap15_generics/Manipulation.java
+++ javaBruceEckel/chap15_generics/Manipulation.java
@@ -1,4 +1,13 @@
 package chap15_generics;
-
-public class Manipulation {
+//: generics/Manipulation.java
+// {CompileTimeError} (Won’t compile)
+class Manipulator<T> {
+    private T obj;
+    public Manipulator(T x) { obj = x; }
+    // Error: cannot find symbol: method f():
+    public void manipulate() { obj.f(); }
 }
+public class Manipulation {
+    public static void main(String[] args) {
+        HasF hf = new HasF();
+        
