1fd4f27c9ed9f4a6fb6fd4fcc783d7395e01752b
cynepCTAPuk <CTAPuk@gmail.com>
2019-01-27 17:59:22 +0300

    ListMaker

--- javaBruceEckel/generics/ListMaker.java
+++ javaBruceEckel/generics/ListMaker.java
@@ -1,4 +1,13 @@
 package generics;
-
-public class ListMaker {
-}
+//: generics/ListMaker.java
+import java.util.*;
+public class ListMaker<T> {
+    List<T> create() { return new ArrayList<>(); }
+    public static void main(String[] args) {
+        ListMaker<String> stringMaker= new ListMaker<>();
+        List<String> stringList = stringMaker.create();
+        stringList.add("ArrayList");
+        stringList.add("ArrayList");
+        System.out.println(stringList);
+    }
+} ///:~
