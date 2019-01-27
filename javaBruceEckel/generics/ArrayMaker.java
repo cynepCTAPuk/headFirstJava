97bcf66ec798f028b1331309639af9e8c05e7607
cynepCTAPuk <CTAPuk@gmail.com>
2019-01-27 14:36:02 +0300

    ArrayMaker

--- javaBruceEckel/generics/ArrayMaker.java
+++ javaBruceEckel/generics/ArrayMaker.java
@@ -5,7 +5,7 @@ import java.util.*;
 public class ArrayMaker<T> {
     private Class<T> kind;
     public ArrayMaker(Class<T> kind) { this.kind = kind; }
-    @SuppressWarnings("unchecked")
+//    @SuppressWarnings("unchecked")
     T[] create(int size) { return (T[])Array.newInstance(kind, size); }
 
     public static void main(String[] args) {
