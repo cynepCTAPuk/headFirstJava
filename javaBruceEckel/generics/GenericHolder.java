8b8f08592ee96b2b43e73ca419bb822fda4aa1af
cynepCTAPuk <CTAPuk@gmail.com>
2019-01-27 18:13:18 +0300

    //: generics/GenericHolder.java

--- javaBruceEckel/generics/GenericHolder.java
+++ javaBruceEckel/generics/GenericHolder.java
@@ -1,4 +1,12 @@
 package generics;
-
-public class GenericHolder {
-}
+//: generics/GenericHolder.java
+public class GenericHolder<T> {
+    private T obj;
+    public void set(T obj) { this.obj = obj; }
+    public T get() { return obj; }
+    public static void main(String[] args) {
+        GenericHolder<String> holder = new GenericHolder<>();
+        holder.set("Item");
+        String s = holder.get();
+    }
+} ///:~
