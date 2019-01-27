049688a949e034bba2374700d1ba95fc409ef5aa
cynepCTAPuk <CTAPuk@gmail.com>
2019-01-27 18:01:27 +0300

    FilledListMaker

--- javaBruceEckel/generics/FilledListMaker.java
+++ javaBruceEckel/generics/FilledListMaker.java
@@ -1,4 +1,17 @@
 package generics;
-
-public class FilledListMaker {
-}
+//: generics/FilledListMaker.java
+import java.util.*;
+public class FilledListMaker<T> {
+    List<T> create(T t, int n) {
+        List<T> result = new ArrayList<>();
+        for(int i = 0; i < n; i++) result.add(t);
+        return result;
+    }
+    public static void main(String[] args) {
+        FilledListMaker<String> stringMaker = new FilledListMaker<>();
+        List<String> list = stringMaker.create("Hello", 4);
+        System.out.println(list);
+    }
+} /* Output:
+[Hello, Hello, Hello, Hello]
+*///:~
