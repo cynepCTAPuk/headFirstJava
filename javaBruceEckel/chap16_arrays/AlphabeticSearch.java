0c814677051df384a88acde19642c9ab1f999037
cynepCTAPuk <CTAPuk@gmail.com>
2019-01-28 20:22:31 +0300

    AlphabeticSearch

--- javaBruceEckel/chap16_arrays/AlphabeticSearch.java
+++ javaBruceEckel/chap16_arrays/AlphabeticSearch.java
@@ -1,11 +1,12 @@
-package chap16_arrays;
 //: arrays/AlphabeticSearch.java
 // Searching with a Comparator.
+package chap16_arrays;
 import java.util.*;
 import util.*;
 public class AlphabeticSearch {
     public static void main(String[] args) {
         String[] sa = Generated.array(new String[28], new RandomGenerator.String(5));
+        System.out.println(Arrays.toString(sa));
         Arrays.sort(sa, String.CASE_INSENSITIVE_ORDER);
         System.out.println(Arrays.toString(sa));
         int index = Arrays.binarySearch(sa, sa[10], String.CASE_INSENSITIVE_ORDER);
