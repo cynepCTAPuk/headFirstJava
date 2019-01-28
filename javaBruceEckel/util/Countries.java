a2b4612edea7f6cb26a2680366dcd9639742c236
cynepCTAPuk <CTAPuk@gmail.com>
2019-01-28 21:58:59 +0300

    Countries

--- javaBruceEckel/util/Countries.java
+++ javaBruceEckel/util/Countries.java
@@ -82,117 +82,156 @@ public class Countries {
             {"THE MALDIVES","Male"},
             {"MONGOLIA","Ulan Bator"},
             {"MYANMAR (BURMA)","Rangoon"},
-            {"NEPAL","Katmandu"}, {"NORTH KOREA","P’yongyang"},
-            {"OMAN","Muscat"}, {"PAKISTAN","Islamabad"},
-            {"PHILIPPINES","Manila"}, {"QATAR","Doha"},
-            {"SAUDI ARABIA","Riyadh"}, {"SINGAPORE","Singapore"},
-            {"SOUTH KOREA","Seoul"}, {"SRI LANKA","Colombo"},
+            {"NEPAL","Katmandu"},
+            {"NORTH KOREA","P’yongyang"},
+            {"OMAN","Muscat"},
+            {"PAKISTAN","Islamabad"},
+            {"PHILIPPINES","Manila"},
+            {"QATAR","Doha"},
+            {"SAUDI ARABIA","Riyadh"},
+            {"SINGAPORE","Singapore"},
+            {"SOUTH KOREA","Seoul"},
+            {"SRI LANKA","Colombo"},
             {"SYRIA","Damascus"},
             {"TAIWAN (REPUBLIC OF CHINA)","Taipei"},
-            {"THAILAND","Bangkok"}, {"TURKEY","Ankara"},
+            {"THAILAND","Bangkok"},
+            {"TURKEY","Ankara"},
             {"UNITED ARAB EMIRATES","Abu Dhabi"},
-            {"VIETNAM","Hanoi"}, {"YEMEN","Sana’a"},
+            {"VIETNAM","Hanoi"},
+            {"YEMEN","Sana’a"},
 // Australia and Oceania
-            {"AUSTRALIA","Canberra"}, {"FIJI","Suva"},
+            {"AUSTRALIA","Canberra"},
+            {"FIJI","Suva"},
             {"KIRIBATI","Bairiki"},
             {"MARSHALL ISLANDS","Dalap-Uliga-Darrit"},
-            {"MICRONESIA","Palikir"}, {"NAURU","Yaren"},
-            {"NEW ZEALAND","Wellington"}, {"PALAU","Koror"},
+            {"MICRONESIA","Palikir"},
+            {"NAURU","Yaren"},
+            {"NEW ZEALAND","Wellington"},
+            {"PALAU","Koror"},
             {"PAPUA NEW GUINEA","Port Moresby"},
-            {"SOLOMON ISLANDS","Honaira"}, {"TONGA","Nuku’alofa"},
-            {"TUVALU","Fongafale"}, {"VANUATU","< Port-Vila"},
+            {"SOLOMON ISLANDS","Honaira"},
+            {"TONGA","Nuku’alofa"},
+            {"TUVALU","Fongafale"},
+            {"VANUATU","< Port-Vila"},
             {"WESTERN SAMOA","Apia"},
 // Eastern Europe and former USSR
-            {"ARMENIA","Yerevan"}, {"AZERBAIJAN","Baku"},
+            {"ARMENIA","Yerevan"},
+            {"AZERBAIJAN","Baku"},
             {"BELARUS (BYELORUSSIA)","Minsk"},
-            {"BULGARIA","Sofia"}, {"GEORGIA","Tbilisi"},
-            {"KAZAKSTAN","Almaty"}, {"KYRGYZSTAN","Alma-Ata"},
-            {"MOLDOVA","Chisinau"}, {"RUSSIA","Moscow"},
-            {"TAJIKISTAN","Dushanbe"}, {"TURKMENISTAN","Ashkabad"},
-            {"UKRAINE","Kyiv"}, {"UZBEKISTAN","Tashkent"},
+            {"BULGARIA","Sofia"},
+            {"GEORGIA","Tbilisi"},
+            {"KAZAKSTAN","Almaty"},
+            {"KYRGYZSTAN","Alma-Ata"},
+            {"MOLDOVA","Chisinau"},
+            {"RUSSIA","Moscow"},
+            {"TAJIKISTAN","Dushanbe"},
+            {"TURKMENISTAN","Ashkabad"},
+            {"UKRAINE","Kyiv"},
+            {"UZBEKISTAN","Tashkent"},
 // Europe
-            {"ALBANIA","Tirana"}, {"ANDORRA","Andorra la Vella"},
-            {"AUSTRIA","Vienna"}, {"BELGIUM","Brussels"},
-            {"BOSNIA","-"}, {"HERZEGOVINA","Sarajevo"},
-            {"CROATIA","Zagreb"}, {"CZECH REPUBLIC","Prague"},
-            {"DENMARK","Copenhagen"}, {"ESTONIA","Tallinn"},
-            {"FINLAND","Helsinki"}, {"FRANCE","Paris"},
-            {"GERMANY","Berlin"}, {"GREECE","Athens"},
-            {"HUNGARY","Budapest"}, {"ICELAND","Reykjavik"},
-            {"IRELAND","Dublin"}, {"ITALY","Rome"},
-            {"LATVIA","Riga"}, {"LIECHTENSTEIN","Vaduz"},
-            {"LITHUANIA","Vilnius"}, {"LUXEMBOURG","Luxembourg"},
-            {"MACEDONIA","Skopje"}, {"MALTA","Valletta"},
-            {"MONACO","Monaco"}, {"MONTENEGRO","Podgorica"},
-            {"THE NETHERLANDS","Amsterdam"}, {"NORWAY","Oslo"},
-            {"POLAND","Warsaw"}, {"PORTUGAL","Lisbon"},
-            {"ROMANIA","Bucharest"}, {"SAN MARINO","San Marino"},
-            {"SERBIA","Belgrade"}, {"SLOVAKIA","Bratislava"},
-            {"SLOVENIA","Ljuijana"}, {"SPAIN","Madrid"},
-            {"SWEDEN","Stockholm"}, {"SWITZERLAND","Berne"},
-            {"UNITED KINGDOM","London"}, {"VATICAN CITY","---"},
+            {"ALBANIA","Tirana"},
+            {"ANDORRA","Andorra la Vella"},
+            {"AUSTRIA","Vienna"},
+            {"BELGIUM","Brussels"},
+            {"BOSNIA","-"},
+            {"HERZEGOVINA","Sarajevo"},
+            {"CROATIA","Zagreb"},
+            {"CZECH REPUBLIC","Prague"},
+            {"DENMARK","Copenhagen"},
+            {"ESTONIA","Tallinn"},
+            {"FINLAND","Helsinki"},
+            {"FRANCE","Paris"},
+            {"GERMANY","Berlin"},
+            {"GREECE","Athens"},
+            {"HUNGARY","Budapest"},
+            {"ICELAND","Reykjavik"},
+            {"IRELAND","Dublin"},
+            {"ITALY","Rome"},
+            {"LATVIA","Riga"},
+            {"LIECHTENSTEIN","Vaduz"},
+            {"LITHUANIA","Vilnius"},
+            {"LUXEMBOURG","Luxembourg"},
+            {"MACEDONIA","Skopje"},
+            {"MALTA","Valletta"},
+            {"MONACO","Monaco"},
+            {"MONTENEGRO","Podgorica"},
+            {"THE NETHERLANDS","Amsterdam"},
+            {"NORWAY","Oslo"},
+            {"POLAND","Warsaw"},
+            {"PORTUGAL","Lisbon"},
+            {"ROMANIA","Bucharest"},
+            {"SAN MARINO","San Marino"},
+            {"SERBIA","Belgrade"},
+            {"SLOVAKIA","Bratislava"},
+            {"SLOVENIA","Ljuijana"},
+            {"SPAIN","Madrid"},
+            {"SWEDEN","Stockholm"},
+            {"SWITZERLAND","Berne"},
+            {"UNITED KINGDOM","London"},
+            {"VATICAN CITY","---"},
 // North and Central America
             {"ANTIGUA AND BARBUDA","Saint John’s"},
             {"BAHAMAS","Nassau"},
-            {"BARBADOS","Bridgetown"}, {"BELIZE","Belmopan"},
-            {"CANADA","Ottawa"}, {"COSTA RICA","San Jose"},
-            {"CUBA","Havana"}, {"DOMINICA","Roseau"},
+            {"BARBADOS","Bridgetown"},
+            {"BELIZE","Belmopan"},
+            {"CANADA","Ottawa"},
+            {"COSTA RICA","San Jose"},
+            {"CUBA","Havana"},
+            {"DOMINICA","Roseau"},
             {"DOMINICAN REPUBLIC","Santo Domingo"},
             {"EL SALVADOR","San Salvador"},
             {"GRENADA","Saint George’s"},
             {"GUATEMALA","Guatemala City"},
             {"HAITI","Port-au-Prince"},
-            {"HONDURAS","Tegucigalpa"}, {"JAMAICA","Kingston"},
-            {"MEXICO","Mexico City"}, {"NICARAGUA","Managua"},
-            {"PANAMA","Panama City"}, {"ST. KITTS","-"},
-            {"NEVIS","Basseterre"}, {"ST. LUCIA","Castries"},
+            {"HONDURAS","Tegucigalpa"},
+            {"JAMAICA","Kingston"},
+            {"MEXICO","Mexico City"},
+            {"NICARAGUA","Managua"},
+            {"PANAMA","Panama City"},
+            {"ST. KITTS","-"},
+            {"NEVIS","Basseterre"},
+            {"ST. LUCIA","Castries"},
             {"ST. VINCENT AND THE GRENADINES","Kingstown"},
             {"UNITED STATES OF AMERICA","Washington, D.C."},
 // South America
             {"ARGENTINA","Buenos Aires"},
             {"BOLIVIA","Sucre (legal)/La Paz(administrative)"},
-            {"BRAZIL","Brasilia"}, {"CHILE","Santiago"},
-            {"COLOMBIA","Bogota"}, {"ECUADOR","Quito"},
-            {"GUYANA","Georgetown"}, {"PARAGUAY","Asuncion"},
-            {"PERU","Lima"}, {"SURINAME","Paramaribo"},
+            {"BRAZIL","Brasilia"},
+            {"CHILE","Santiago"},
+            {"COLOMBIA","Bogota"},
+            {"ECUADOR","Quito"},
+            {"GUYANA","Georgetown"},
+            {"PARAGUAY","Asuncion"},
+            {"PERU","Lima"},
+            {"SURINAME","Paramaribo"},
             {"TRINIDAD AND TOBAGO","Port of Spain"},
-            {"URUGUAY","Montevideo"}, {"VENEZUELA","Caracas"},
+            {"URUGUAY","Montevideo"},
+            {"VENEZUELA","Caracas"},
     };
     // Use AbstractMap by implementing entrySet()
-    private static class FlyweightMap
-            extends AbstractMap<String,String> {
-        private static class Entry
-                implements Map.Entry<String,String> {
+    private static class FlyweightMap extends AbstractMap<String,String> {
+        private static class Entry implements Map.Entry<String,String> {
             int index;
             Entry(int index) { this.index = index; }
-            public boolean equals(Object o) {
-                return DATA[index][0].equals(o);
-            }
+            public boolean equals(Object o) { return DATA[index][0].equals(o); }
             public String getKey() { return DATA[index][0]; }
             public String getValue() { return DATA[index][1]; }
             public String setValue(String value) {
                 throw new UnsupportedOperationException();
             }
-            public int hashCode() {
-                return DATA[index][0].hashCode();
-            }
+            public int hashCode() { return DATA[index][0].hashCode(); }
         }
         // Use AbstractSet by implementing size() & iterator()
-        static class EntrySet
-                extends AbstractSet<Map.Entry<String,String>> {
+        static class EntrySet extends AbstractSet<Map.Entry<String,String>> {
             private int size;
             EntrySet(int size) {
-                if(size < 0)
-                    this.size = 0;
-// Can’t be any bigger than the array:
-                else if(size > DATA.length)
-                    this.size = DATA.length;
-                else
-                    this.size = size;
+                if(size < 0) this.size = 0;
+                // Can’t be any bigger than the array:
+                else if(size > DATA.length) this.size = DATA.length;
+                else this.size = size;
             }
             public int size() { return size; }
-            private class Iter
-                    implements Iterator<Map.Entry<String,String>> {
+            private class Iter implements Iterator<Map.Entry<String,String>> {
                 // Only one Entry object per Iterator:
                 private Entry entry = new Entry(-1);
                 public boolean hasNext() {
@@ -206,13 +245,11 @@ public class Countries {
                     throw new UnsupportedOperationException();
                 }
             }
-            public
-            Iterator<Map.Entry<String,String>> iterator() {
+            public Iterator<Map.Entry<String,String>> iterator() {
                 return new Iter();
             }
         }
-        private static Set<Map.Entry<String,String>> entries =
-                new EntrySet(DATA.length);
+        private static Set<Map.Entry<String,String>> entries = new EntrySet(DATA.length);
         public Set<Map.Entry<String,String>> entrySet() {
             return entries;
         }
@@ -232,13 +269,12 @@ public class Countries {
     public static Map<String,String> capitals(int size) {
         return select(size); // A partial map
     }
-    static List<String> names =
-            new ArrayList<String>(map.keySet());
+    static List<String> names = new ArrayList<>(map.keySet());
     // All the names:
     public static List<String> names() { return names; }
     // A partial list:
     public static List<String> names(int size) {
-        return new ArrayList<String>(select(size).keySet());
+        return new ArrayList<>(select(size).keySet());
     }
     public static void main(String[] args) {
         print(capitals(10));
