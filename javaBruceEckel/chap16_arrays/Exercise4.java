dd71ef5e4e0ea1da9aae600d575f672347f960ad
cynepCTAPuk <CTAPuk@gmail.com>
2019-01-28 16:19:32 +0300

    Exercise 4

--- javaBruceEckel/chap16_arrays/Exercise4.java
+++ javaBruceEckel/chap16_arrays/Exercise4.java
@@ -6,6 +6,12 @@ public class Exercise4 {
     static double[][][] threeDimArray(int size1, int size2, int size3, double start, double end) {
         Random random = new Random();
         double[][][] result = new double[size1][size2][size3];
+
+        if (start > end) {
+            System.out.println("May be you need to change limits for array");
+            return result;
+        }
+
         for (int i = 0; i < size1; i++) {
             for (int j = 0; j < size2; j++) {
                 for (int k = 0; k <size3 ; k++) {
