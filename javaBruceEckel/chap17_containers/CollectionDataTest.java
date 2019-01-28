8dd581963f673dcb55a4f4bafa58704ac6850966
cynepCTAPuk <CTAPuk@gmail.com>
2019-01-28 21:16:20 +0300

    CollectionDataTest

--- javaBruceEckel/chap17_containers/CollectionDataTest.java
+++ javaBruceEckel/chap17_containers/CollectionDataTest.java
@@ -11,8 +11,8 @@ class Government implements Generator<String> {
 }
 public class CollectionDataTest {
     public static void main(String[] args) {
-        Set<String> set = new LinkedHashSet<String>(
-                new CollectionData<String>(new Government(), 15));
+        Set<String> set = new LinkedHashSet<>(
+                new CollectionData<>(new Government(), 15));
         // Using the convenience method:
         set.addAll(CollectionData.list(new Government(), 15));
         System.out.println(set);
