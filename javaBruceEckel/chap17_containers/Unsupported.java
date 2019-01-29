27f2ba5da793cbb17d8aac2519a5643a9c98f0f2
cynepCTAPuk <CTAPuk@gmail.com>
2019-01-29 21:44:19 +0300

    Unsupported

--- javaBruceEckel/chap17_containers/Unsupported.java
+++ javaBruceEckel/chap17_containers/Unsupported.java
@@ -6,12 +6,12 @@ public class Unsupported {
     static void test(String msg, List<String> list) {
         System.out.println("\n--- " + msg + " ---");
         Collection<String> c1 = list;
-        System.out.println("c1 " + c1);
+        System.out.println("c1 = list " + c1);
         Collection<String> subList = list.subList(1,8);
-        System.out.println("sublist " + subList);
+        System.out.println("sublist 1-8 " + subList);
         // Copy of the sublist:
         Collection<String> c2 = new ArrayList<>(subList);
-        System.out.println("c2 " + c2);
+        System.out.println("c2 = sublist " + c2);
         try { c1.retainAll(c2); } catch(Exception e) {
             System.out.println("retainAll(): " + e); }
         try { c1.removeAll(c2); } catch(Exception e) {
@@ -26,7 +26,7 @@ public class Unsupported {
             System.out.println("remove(): " + e); }
         // The List.set() method modifies the value but
         // doesn’t change the size of the data structure:
-        try { list.set(0, "X");
+        try { list.set(0, "X"); System.out.println("list " + list);
         } catch(Exception e) { System.out.println("List.set(): " + e); }
     }
     public static void main(String[] args) {
