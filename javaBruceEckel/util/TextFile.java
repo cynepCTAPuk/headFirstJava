3604636b8d6ae1a210cb46d5eb1b378a7df139ba
cynepCTAPuk <CTAPuk@gmail.com>
2019-01-29 18:53:51 +0300

    TextFile

--- javaBruceEckel/util/TextFile.java
+++ javaBruceEckel/util/TextFile.java
@@ -11,20 +11,18 @@ public class TextFile extends ArrayList<String> {
     public static String read(String fileName) {
         StringBuilder sb = new StringBuilder();
         try {
-            BufferedReader in = new BufferedReader(new FileReader(
-                    new File(fileName).getAbsoluteFile()));
+            BufferedReader in =
+                    new BufferedReader(
+                            new FileReader(
+                                    new File(fileName).getAbsoluteFile()));
             try {
                 String s;
                 while ((s = in.readLine()) != null) {
                     sb.append(s);
                     sb.append("\n");
                 }
-            } finally {
-                in.close();
-            }
-        } catch (IOException e) {
-            throw new RuntimeException(e);
-        }
+            } finally { in.close(); }
+        } catch (IOException e) { throw new RuntimeException(e); }
         return sb.toString();
     }
 
