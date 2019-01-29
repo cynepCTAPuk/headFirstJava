373ff9cd52effc78ed7cbf95deea78fe39a378fc
cynepCTAPuk <CTAPuk@gmail.com>
2019-01-29 21:38:29 +0300

    Unsupported

--- javaBruceEckel/chap17_containers/Unsupported.java
+++ javaBruceEckel/chap17_containers/Unsupported.java
@@ -4,40 +4,33 @@ package chap17_containers;
 import java.util.*;
 public class Unsupported {
     static void test(String msg, List<String> list) {
-        System.out.println("--- " + msg + " ---");
-        Collection<String> c = list;
+        System.out.println("\n--- " + msg + " ---");
+        Collection<String> c1 = list;
+        System.out.println("c1 " + c1);
         Collection<String> subList = list.subList(1,8);
-// Copy of the sublist:
+        System.out.println("sublist " + subList);
+        // Copy of the sublist:
         Collection<String> c2 = new ArrayList<>(subList);
-        try { c.retainAll(c2); } catch(Exception e) {
-            System.out.println("retainAll(): " + e);
-        }
-        try { c.removeAll(c2); } catch(Exception e) {
-            System.out.println("removeAll(): " + e);
-        }
-        try { c.clear(); } catch(Exception e) {
-            System.out.println("clear(): " + e);
-        }
-        try { c.add("X"); } catch(Exception e) {
-            System.out.println("add(): " + e);
-        }
-        try { c.addAll(c2); } catch(Exception e) {
-            System.out.println("addAll(): " + e);
-        }
-        try { c.remove("C"); } catch(Exception e) {
-            System.out.println("remove(): " + e);
-        }
-// The List.set() method modifies the value but
-// doesn’t change the size of the data structure:
-        try {
-            list.set(0, "X");
-        } catch(Exception e) {
-            System.out.println("List.set(): " + e);
-        }
+        System.out.println("c2 " + c2);
+        try { c1.retainAll(c2); } catch(Exception e) {
+            System.out.println("retainAll(): " + e); }
+        try { c1.removeAll(c2); } catch(Exception e) {
+            System.out.println("removeAll(): " + e); }
+        try { c1.clear(); } catch(Exception e) {
+            System.out.println("clear(): " + e); }
+        try { c1.add("X"); } catch(Exception e) {
+            System.out.println("add(): " + e); }
+        try { c1.addAll(c2); } catch(Exception e) {
+            System.out.println("addAll(): " + e); }
+        try { c1.remove("C"); } catch(Exception e) {
+            System.out.println("remove(): " + e); }
+        // The List.set() method modifies the value but
+        // doesn’t change the size of the data structure:
+        try { list.set(0, "X");
+        } catch(Exception e) { System.out.println("List.set(): " + e); }
     }
     public static void main(String[] args) {
-        List<String> list =
-                Arrays.asList("A B C D E F G H I J K L".split(" "));
+        List<String> list = Arrays.asList("A B C D E F G H I J K L".split(" "));
         test("Modifiable Copy", new ArrayList<String>(list));
         test("Arrays.asList()", list);
         test("unmodifiableList()",
