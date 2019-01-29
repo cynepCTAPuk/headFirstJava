f5e5368eb4464d30b1e5fe009839dbc43055431d
cynepCTAPuk <CTAPuk@gmail.com>
2019-01-29 18:18:27 +0300

    Exercise02

--- javaBruceEckel/chap17_containers/Exercise02.java
+++ javaBruceEckel/chap17_containers/Exercise02.java
@@ -6,12 +6,11 @@ import static util.Countries.*;
 
 public class Exercise02 {
     public static void main(String[] args) {
-        Map<String, String> hashMap = new HashMap<>(capitals(8));
-        System.out.println("hashMap " + hashMap);
-
-        List<String> linkedList = new LinkedList<>(names(7));
-        System.out.println("linkedList " +linkedList);
-        Collections.shuffle(linkedList);
-        System.out.println("linkedList " +linkedList);
+        Map<String, String> treeMap = new TreeMap<>(capitals());
+        System.out.println("treeMap " + treeMap);
+        System.out.println(treeMap.size());
+        Set<String> treeSet = new TreeSet<>(names());
+        System.out.println("treeSet " +treeSet);
+        System.out.println(treeSet.size());
     }
 }
