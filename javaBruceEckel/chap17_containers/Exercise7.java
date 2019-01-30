5aefb84837e4ceac170339c15d2d2551384ad352
cynepCTAPuk <CTAPuk@gmail.com>
2019-01-30 18:01:59 +0300

    Exercise 7

--- javaBruceEckel/chap17_containers/Exercise7.java
+++ javaBruceEckel/chap17_containers/Exercise7.java
@@ -1,26 +1,26 @@
 package chap17_containers;
-
 import util.*;
-
 import java.util.*;
-
 public class Exercise7 {
     public static void main(String[] args) {
-        List<String> arrayList = new ArrayList<>(Countries.names(5));
+        ArrayList<String> arrayList = new ArrayList<>(Countries.names(5));
         Iterator<String> ita = arrayList.iterator();
         while (ita.hasNext()) System.out.print(ita.next() + " ");
         System.out.println();
 
-        List<String> linkedList = new LinkedList<>(Countries.names(5));
+        List<String> linkedList = new LinkedList<>(Arrays.asList("12345".split("")));
         Iterator<String> itl = linkedList.iterator();
         while (itl.hasNext()) System.out.print(itl.next() + " ");
         System.out.println();
 
-        ListIterator<String> lit = arrayList.listIterator();
-        while (lit.hasNext()) {
-            lit.next();
-            lit.add(((LinkedList<String>) linkedList).pollFirst());
+        ListIterator<String> lit1 = arrayList.listIterator();
+        while (lit1.hasNext()) {
+            lit1.next();
+            lit1.add(((LinkedList<String>) linkedList).pollFirst());
         }
         System.out.println(arrayList);
+        arrayList.removeAll(linkedList);
+        System.out.println(arrayList);
+
     }
 }
