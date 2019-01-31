3f9fb18a9787992f238f1519379edd8491914c4c
cynepCTAPuk <CTAPuk@gmail.com>
2019-01-31 18:20:03 +0300

    Groundhog

--- javaBruceEckel/chap17_containers/Groundhog.java
+++ javaBruceEckel/chap17_containers/Groundhog.java
@@ -1,4 +1,9 @@
+//: containers/Groundhog.java
+// Looks plausible, but doesn’t work as a HashMap key.
 package chap17_containers;
-
 public class Groundhog {
-}
+    protected int number;
+    public Groundhog(int n) { number = n; }
+    public String toString() { return "Groundhog #" + number;
+    }
+} ///:~
