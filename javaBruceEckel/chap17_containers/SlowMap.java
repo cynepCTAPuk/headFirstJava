c08b460d36f20ef93fdf2df1d12ea773687b28a8
cynepCTAPuk <CTAPuk@gmail.com>
2019-01-31 19:36:53 +0300

    SlowMap

--- javaBruceEckel/chap17_containers/SlowMap.java
+++ javaBruceEckel/chap17_containers/SlowMap.java
@@ -3,30 +3,34 @@
 package chap17_containers;
 import util.*;
 import java.util.*;
-public class SlowMap<K,V> extends AbstractMap<K,V> {
+public class SlowMap<K, V> extends AbstractMap<K, V> {
     private List<K> keys = new ArrayList<>();
     private List<V> values = new ArrayList<>();
+
     public V put(K key, V value) {
         V oldValue = get(key); // The old value or null
-        if(!keys.contains(key)) {
+        if (!keys.contains(key)) {
             keys.add(key);
             values.add(value);
         } else values.set(keys.indexOf(key), value);
         return oldValue;
     }
+
     public V get(Object key) { // key is type Object, not K
-        if(!keys.contains(key)) return null;
+        if (!keys.contains(key)) return null;
         return values.get(keys.indexOf(key));
     }
-    public Set<Map.Entry<K,V>> entrySet() {
-        Set<Map.Entry<K,V>> set= new HashSet<>();
+
+    public Set<Map.Entry<K, V>> entrySet() {
+        Set<Map.Entry<K, V>> set = new HashSet<>();
         Iterator<K> ki = keys.iterator();
         Iterator<V> vi = values.iterator();
-        while(ki.hasNext()) set.add(new MapEntry<K,V>(ki.next(), vi.next()));
+        while (ki.hasNext()) set.add(new MapEntry<K, V>(ki.next(), vi.next()));
         return set;
     }
+
     public static void main(String[] args) {
-        SlowMap<String,String> m= new SlowMap<>();
+        SlowMap<String, String> m = new SlowMap<>();
         m.putAll(Countries.capitals(15));
         System.out.println(m);
         System.out.println(m.get("CONGO"));
