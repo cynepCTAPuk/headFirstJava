7fe06748ab478cd8c6a6dd18efa8a762ddfc039a
cynepCTAPuk <CTAPuk@gmail.com>
2019-01-31 18:21:36 +0300

    Prediction

--- javaBruceEckel/chap17_containers/Prediction.java
+++ javaBruceEckel/chap17_containers/Prediction.java
@@ -1,4 +1,11 @@
+// Predicting the weather with groundhogs.
 package chap17_containers;
-
+import java.util.*;
 public class Prediction {
-}
+    private static Random rand = new Random(47);
+    private boolean shadow = rand.nextDouble() > 0.5;
+    public String toString() {
+        if(shadow) return "Six more weeks of Winter!";
+        else return "Early Spring!";
+    }
+} ///:~
