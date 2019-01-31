b8d8718d6eb4bd017fdbdac60f39a87c476f1fe8
cynepCTAPuk <CTAPuk@gmail.com>
2019-01-31 20:51:03 +0300

    MapEntry

--- javaBruceEckel/chap17_containers/MapEntry.java
+++ javaBruceEckel/chap17_containers/MapEntry.java
@@ -19,8 +19,7 @@ public class MapEntry<K,V> implements Map.Entry<K,V> {
     public boolean equals(Object o) {
         if(!(o instanceof MapEntry)) return false;
         MapEntry me = (MapEntry)o;
-        return (key == null ?
-                        me.getKey() == null :
+        return (key == null ? me.getKey() == null :
                         key.equals(me.getKey())) && (value == null ?
                         me.getValue()== null : value.equals(me.getValue()));
     }
