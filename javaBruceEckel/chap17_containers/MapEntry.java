5866baf028c7d5efbeecf34a8cb70d7e3a702923
cynepCTAPuk <CTAPuk@gmail.com>
2019-01-31 21:02:12 +0300

    MapEntry

--- javaBruceEckel/chap17_containers/MapEntry.java
+++ javaBruceEckel/chap17_containers/MapEntry.java
@@ -5,14 +5,10 @@ import java.util.*;
 public class MapEntry<K,V> implements Map.Entry<K,V> {
     private K key;
     private V value;
-    public MapEntry(K key, V value) {
-        this.key = key;
-        this.value = value;
-    }
+    public MapEntry(K key, V value) { this.key = key; this.value = value;}
     public K getKey() { return key;}
     public V getValue() { return value;}
-    public V setValue(V v) { V result = value; value = v; return result;
-    }
+    public V setValue(V v) { V result = value; value = v; return result;}
     public int hashCode() {
         return (key==null ? 0 : key.hashCode()) ^ (value==null ? 0 : value.hashCode());
     }
