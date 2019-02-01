0443c1a7c5b0f732770fe7d122b4bf894c496789
cynepCTAPuk <CTAPuk@gmail.com>
2019-02-01 18:46:23 +0300

    StringHashCode

--- javaBruceEckel/chap17_containers/StringHashCode.java
+++ javaBruceEckel/chap17_containers/StringHashCode.java
@@ -1,4 +1,16 @@
 //: containers/StringHashCode.java
+/*
+Field type                  Calculation
+boolean                     c = ( f ? 0 : 1)
+byte, char, short, or int   c = (int)f
+long                        c = (int)(f ^ (f>>>32))
+float                       c = Float.floatToIntBits(f);
+double      long l = Double.doubleToLongBits(f); c = (int)(1 ^ (l>>>32))
+Object, where equals( ) calls equals( ) for this field  c = f.hashCode( )
+Array                       Apply above rules to each element
+result = (int) 17
+result = 37 * result + c
+ */
 package chap17_containers;
 public class StringHashCode {
     public static void main(String[] args) {
