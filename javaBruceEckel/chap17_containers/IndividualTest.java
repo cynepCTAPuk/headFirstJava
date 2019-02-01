1920cb6d3fcf3a71c0deb9e327c25d6d433c6ad8
cynepCTAPuk <CTAPuk@gmail.com>
2019-02-01 19:21:04 +0300

    IndividualTest

--- javaBruceEckel/chap17_containers/IndividualTest.java
+++ javaBruceEckel/chap17_containers/IndividualTest.java
@@ -1,1 +1,15 @@
+//: containers/IndividualTest.java
 package chap17_containers;
+import chap11_holding.MapOfList;
+import typeinfo.pets.*;
+import java.util.*;
+public class IndividualTest {
+    public static void main(String[] args) {
+        Set<Individual> pets = new TreeSet<>();
+        for(List<? extends Pet> lp : MapOfList.petPeople.values())
+            for(Pet p : lp) pets.add(p);
+        System.out.println(pets);
+    }
+} /* Output:
+[Cat Elsie May, Cat Pinkola, Cat Shackleton, Cat Stanford aka Stinky el Negro, Cymric Molly, Dog Margrett, Mutt Spot, Pug Louie aka Louis Snorkelstein Dupree, Rat Fizzy, Rat Freckly, Rat Fuzzy]
+*///:~
