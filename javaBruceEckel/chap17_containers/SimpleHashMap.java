eb3f959e32aedf7e7e407e47019aa64cb7a1ab69
cynepCTAPuk <CTAPuk@gmail.com>
2019-02-01 13:33:29 +0300

    SimpleHashMap

--- javaBruceEckel/chap17_containers/SimpleHashMap.java
+++ javaBruceEckel/chap17_containers/SimpleHashMap.java
@@ -13,10 +13,9 @@ public class SimpleHashMap<K,V> extends AbstractMap<K,V> {
     public V put(K key, V value) {
         V oldValue = null;
         int index = Math.abs(key.hashCode()) % SIZE;
-        if(buckets[index] == null)
-            buckets[index] = new LinkedList<MapEntry<K,V>>();
+        if(buckets[index] == null) buckets[index] = new LinkedList<MapEntry<K,V>>();
         LinkedList<MapEntry<K,V>> bucket = buckets[index];
-        MapEntry<K,V> pair = new MapEntry<K,V>(key, value);
+        MapEntry<K,V> pair = new MapEntry<>(key, value);
         boolean found = false;
         ListIterator<MapEntry<K,V>> it = bucket.listIterator();
         while(it.hasNext()) {
@@ -35,22 +34,19 @@ public class SimpleHashMap<K,V> extends AbstractMap<K,V> {
         int index = Math.abs(key.hashCode()) % SIZE;
         if(buckets[index] == null) return null;
         for(MapEntry<K,V> iPair : buckets[index])
-            if(iPair.getKey().equals(key))
-                return iPair.getValue();
+            if(iPair.getKey().equals(key)) return iPair.getValue();
         return null;
     }
     public Set<Map.Entry<K,V>> entrySet() {
-        Set<Map.Entry<K,V>> set= new HashSet<Map.Entry<K,V>>();
+        Set<Map.Entry<K,V>> set= new HashSet<>();
         for(LinkedList<MapEntry<K,V>> bucket : buckets) {
             if(bucket == null) continue;
-            for(MapEntry<K,V> mpair : bucket)
-                set.add(mpair);
+            for(MapEntry<K,V> mpair : bucket) set.add(mpair);
         }
         return set;
     }
     public static void main(String[] args) {
-        SimpleHashMap<String,String> m =
-                new SimpleHashMap<String,String>();
+        SimpleHashMap<String,String> m = new SimpleHashMap<>();
         m.putAll(Countries.capitals(25));
         System.out.println(m);
         System.out.println(m.get("ERITREA"));
