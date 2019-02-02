ac2063d146f9b8b47f0ec99c09d16e06067dadb2
cynepCTAPuk <CTAPuk@gmail.com>
2019-02-02 20:32:50 +0300

    Swap2

--- test/logicalOps/Swap2.java
+++ test/logicalOps/Swap2.java
@@ -2,9 +2,9 @@ package logicalOps;
 
 public class Swap2 {
     public static void main(String[] args) {
-        int x = 5, y = 7;
-        int t = y;
-        y = x;
-        x = t;
+        int a = 5, b = 7;
+        int c = b;
+        b = a;
+        a = c;
     }
 }
