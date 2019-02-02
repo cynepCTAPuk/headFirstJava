99e2b2674f2e25cc89449ea991252972b8669e8c
cynepCTAPuk <CTAPuk@gmail.com>
2019-02-02 20:32:19 +0300

    Swap1

--- test/logicalOps/Swap1.java
+++ test/logicalOps/Swap1.java
@@ -2,14 +2,9 @@ package logicalOps;
 
 public class Swap1 {
     public static void main(String[] args) {
-        int x = 5, y = 7;
-        System.out.println(Integer.toBinaryString(x) + " " + x);
-        System.out.println(Integer.toBinaryString(y) + " " + y);
-        x = x ^ y;
-        System.out.println(Integer.toBinaryString(x) + " " + x);
-        y = x ^ y;
-        System.out.println(Integer.toBinaryString(y) + " " + y);
-        x = x ^ y;
-        System.out.println(Integer.toBinaryString(x) + " " + x);
+        int a = 5, b = 7;
+        a = a ^ b;
+        b = a ^ b;
+        a = a ^ b;
     }
 }
