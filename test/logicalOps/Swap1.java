af1ba5f24e8ae954eab59f3fea4c44d24d72a1be
cynepCTAPuk <CTAPuk@gmail.com>
2019-02-02 20:39:46 +0300

    Swap1

--- test/logicalOps/Swap1.java
+++ test/logicalOps/Swap1.java
@@ -8,3 +8,34 @@ public class Swap1 {
         a = a ^ b;
     }
 }
+/*
+$ javap -c Swap1.class
+Compiled from "Swap1.java"
+public class Swap1 {
+  public Swap1();
+    Code:
+       0: aload_0
+       1: invokespecial #1                  // Method java/lang/Object."<init>":()V
+       4: return
+
+  public static void main(java.lang.String[]);
+    Code:
+       0: iconst_5
+       1: istore_1
+       2: bipush        7
+       4: istore_2
+       5: iload_1
+       6: iload_2
+       7: ixor
+       8: istore_1
+       9: iload_1
+      10: iload_2
+      11: ixor
+      12: istore_2
+      13: iload_1
+      14: iload_2
+      15: ixor
+      16: istore_1
+      17: return
+}
+ */
