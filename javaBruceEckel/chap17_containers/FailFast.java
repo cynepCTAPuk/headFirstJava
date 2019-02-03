a672da942f11d0c850faec1fe6a3220a451b617e
cynepCTAPuk <CTAPuk@gmail.com>
2019-02-03 16:04:43 +0300

    FailFast

--- javaBruceEckel/chap17_containers/FailFast.java
+++ javaBruceEckel/chap17_containers/FailFast.java
@@ -1,4 +1,18 @@
+//: containers/FailFast.java
+// Demonstrates the "fail-fast" behavior.
 package chap17_containers;
-
+import java.util.*;
 public class FailFast {
-}
+    public static void main(String[] args) {
+        Collection<String> c = new ArrayList<>();
+        Iterator<String> it = c.iterator();
+        c.add("An object");
+        try {
+            String s = it.next();
+        } catch(ConcurrentModificationException e) {
+            System.out.println(e);
+        }
+    }
+} /* Output:
+java.util.ConcurrentModificationException
+*///:~
