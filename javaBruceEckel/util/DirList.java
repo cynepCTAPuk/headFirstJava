fa39b37d4c82e705f57533dc2ca8ec96e0aa780a
cynepCTAPuk <CTAPuk@gmail.com>
2019-02-03 23:06:15 +0300

    DirList

--- javaBruceEckel/util/DirList.java
+++ javaBruceEckel/util/DirList.java
@@ -2,37 +2,34 @@
 // Display a directory listing using regular expressions.
 // {Args: "D.*\.java"}
 package util;
-import java.util.regex.*;
 import java.io.*;
 import java.util.*;
+import java.util.regex.*;
+
 public class DirList {
     public static void main(String[] args) {
         String dir = ".";
         File path = new File(dir);
-//        String[] list = path.list();
         String[] list;
-        if(args.length == 0) {
+        if (args.length == 0) {
             list = path.list();
         } else {
             list = path.list(new DirFilter(args[0]));
         }
         Arrays.sort(list, String.CASE_INSENSITIVE_ORDER);
-//        System.out.println(Arrays.toString(list));
 //        for(String dirItem : list) System.out.println(dirItem);
-        for (int i = 0; i < list.length ; i++) {
-            if(i%3 == 1 ) System.out.println();
-            System.out.printf("%-24s ",list[i]);
+//        System.out.println(Arrays.toString(list));
+        for (int i = 0; i < list.length; i++) {
+            System.out.printf("%-24s ", list[i]);
+            if (i % 3 == 2) System.out.println();
         }
     }
 }
+
 class DirFilter implements FilenameFilter {
     private Pattern pattern;
-    public DirFilter(String regex) {
-        pattern = Pattern.compile(regex);
-    }
-    public boolean accept(File dir, String name) {
-        return pattern.matcher(name).matches();
-    }
+    public DirFilter(String regex) { pattern = Pattern.compile(regex); }
+    public boolean accept(File dir, String name) { return pattern.matcher(name).matches(); }
 } /* Output:
 DirectoryDemo.java
 DirList.java
