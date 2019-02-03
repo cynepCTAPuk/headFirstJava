8fdccfa9736a96fe4e09675c142075b8eb0bd8cd
cynepCTAPuk <CTAPuk@gmail.com>
2019-02-03 22:50:00 +0300

    DirList

--- javaBruceEckel/util/DirList.java
+++ javaBruceEckel/util/DirList.java
@@ -1,4 +1,36 @@
+//: io/DirList.java
+// Display a directory listing using regular expressions.
+// {Args: "D.*\.java"}
 package util;
-
+import java.util.regex.*;
+import java.io.*;
+import java.util.*;
 public class DirList {
+    public static void main(String[] args) {
+        String dir = ".";
+        File path = new File(dir);
+//        String[] list = path.list();
+        String[] list;
+        if(args.length == 0) {
+            list = path.list();
+        } else {
+            list = path.list(new DirFilter(args[0]));
+        }
+        Arrays.sort(list, String.CASE_INSENSITIVE_ORDER);
+        for(String dirItem : list) System.out.println(dirItem);
+    }
 }
+class DirFilter implements FilenameFilter {
+    private Pattern pattern;
+    public DirFilter(String regex) {
+        pattern = Pattern.compile(regex);
+    }
+    public boolean accept(File dir, String name) {
+        return pattern.matcher(name).matches();
+    }
+} /* Output:
+DirectoryDemo.java
+DirList.java
+DirList2.java
+DirList3.java
+*///:~
