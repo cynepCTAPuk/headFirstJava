1bf73029d84e4ac28c6739c8eca5ced3bbe56882
cynepCTAPuk <CTAPuk@gmail.com>
2019-02-04 14:37:41 +0300

    DirList3

--- javaBruceEckel/chap18_io/DirList3.java
+++ javaBruceEckel/chap18_io/DirList3.java
@@ -11,11 +11,11 @@ public class DirList3 {
         String[] list;
         if (args.length == 0) list = path.list();
         else list = path.list(new FilenameFilter() {
-                private Pattern pattern = Pattern.compile(args[0]);
-                public boolean accept(File dir, String name) {
-                    return pattern.matcher(name).matches();
-                }
-            });
+            private Pattern pattern = Pattern.compile(args[0]);
+            public boolean accept(File dir, String name) {
+                return pattern.matcher(name).matches();
+            }
+        });
         Arrays.sort(list, String.CASE_INSENSITIVE_ORDER);
         for (String dirItem : list) System.out.println(dirItem);
     }
