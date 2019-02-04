29e51e2231e30b90b47bdcf924abf3db9e250406
cynepCTAPuk <CTAPuk@gmail.com>
2019-02-04 17:54:13 +0300

    Exercise 2

--- BruceEckel/chap18_io/Exercise2.java
+++ BruceEckel/chap18_io/Exercise2.java
@@ -1,32 +1,34 @@
 package chap18_io;
 
 import java.io.*;
+import java.util.*;
 
 public class Exercise2 {
-    static String[] printDirs(String dir) {
+    static File[] printDirs(String dir) {
         File path = new File(dir);
-        String[] list = path.list();
+        File[] list = path.listFiles();
+        Arrays.sort(list);
+
         for (int i = 0; i < list.length; i++) {
-            File file = new File(list[i]);
-            if (file.isDirectory()) {
-                System.out.printf("[%-22s] ", list[i]);
+            if (list[i].isDirectory()) {
+                System.out.printf("[%-26s] ", list[i].getName());
                 if (i % 3 == 2) System.out.println();
             }
         }
         System.out.println();
-        printFiles(dir);
+//        printFiles(dir);
         return list;
     }
 
     static void printFiles(String dir) {
         File path = new File(dir);
-        String[] list = path.list();
+        File[] list = path.listFiles();
+        Arrays.sort(list);
         for (int i = 0; i < list.length; i++) {
-            File file = new File(list[i]);
-            if (file.isFile()) {
-                System.out.printf("%-20s ", file.getName());
-                System.out.printf("%,6d \n", file.length());
-                System.out.printf("%s \n", file.getAbsolutePath());
+            if (list[i].isFile()) {
+                System.out.printf("%-20s ", list[i].getName());
+                System.out.printf("%,6d \n", list[i].length());
+                System.out.printf("%s \n", list[i].getAbsolutePath());
             }
         }
         System.out.println();
--- BruceEckel/chap18_io/Exercise2a.java
+++ BruceEckel/chap18_io/Exercise2a.java
@@ -3,37 +3,17 @@ package chap18_io;
 import java.io.*;
 
 public class Exercise2a {
-    static String[] printDirs(String dir) {
-        File path = new File(dir);
-        String[] list = path.list();
-        for (int i = 0; i < list.length; i++) {
-            File file = new File(list[i]);
-            if (file.isDirectory()) {
-                System.out.printf("[%-22s] ", list[i]);
-                if (i % 3 == 2) System.out.println();
-            }
-        }
-        System.out.println();
-        printFiles(dir);
-        return list;
-    }
-
-    static void printFiles(String dir) {
-        File path = new File(dir);
-        String[] list = path.list();
-        for (int i = 0; i < list.length; i++) {
-            File file = new File(list[i]);
-            if (file.isFile()) {
-                System.out.printf("%-20s ", file.getName());
-                System.out.printf("%,6d \n", file.length());
-                System.out.printf("%s \n", file.getAbsolutePath());
-            }
-        }
-        System.out.println();
-    }
-
     public static void main(String[] args) {
-        String dir = "C://";
-        printDirs(dir);
+        File dir = new File("C://V//");
+
+        if (dir.isDirectory()) {
+            for (File item : dir.listFiles()) {
+                if (item.isDirectory()) {
+                    System.out.println(item.getName() + "  \t folder");
+                } else {
+                    System.out.println(item.getName() + "\t file");
+                }
+            }
+        }
     }
 }
