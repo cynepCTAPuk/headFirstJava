70d7f39c184a85e838b9ef68dc37eb28dd419321
cynepCTAPuk <CTAPuk@gmail.com>
2019-02-04 15:53:51 +0300

    Exercise 2

--- BruceEckel/chap18_io/Exercise2.java
+++ BruceEckel/chap18_io/Exercise2.java
@@ -1,21 +1,29 @@
 package chap18_io;
 import java.io.*;
+import java.util.*;
 public class Exercise2 {
     public static void main(String[] args) {
         String dir = ".";
         File path = new File(dir);
         String[] list = path.list();
+        Arrays.sort(list, String.CASE_INSENSITIVE_ORDER);
 
-        for (int i = 0; i < list.length ; i++) {
-            if(new File(list[i]).isDirectory()) {
-                System.out.printf("[%-24s] ", list[i]);
+        for (int i = 0; i < list.length ; i++) { // Directory
+            File file = new File(list[i]);
+            if(file.isDirectory()) {
+                System.out.printf("[%-22s] ", list[i]);
                 if (i % 3 == 2) System.out.println();
             }
         }
-        for (int i = 0; i < list.length ; i++) {
-            if(new File(list[i]).isFile()) {
-                System.out.printf("%-22s ", list[i]);
+        System.out.println();
+
+        for (int i = 0; i < list.length ; i++) { // File
+            File file = new File(list[i]);
+            if(file.isFile()) {
+                System.out.printf("%-20s ", file.getName());
+                System.out.printf("%,6d \n", file.length());
             }
+
         }
     }
 }
