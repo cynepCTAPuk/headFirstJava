698ec78486510f1d7a1594d0eae1cd1c8a308999
cynepCTAPuk <CTAPuk@gmail.com>
2019-02-04 17:54:53 +0300

    Exercise 2a

--- BruceEckel/chap18_io/Exercise2a.java
+++ BruceEckel/chap18_io/Exercise2a.java
@@ -5,14 +5,10 @@ import java.io.*;
 public class Exercise2a {
     public static void main(String[] args) {
         File dir = new File("C://V//");
-
         if (dir.isDirectory()) {
             for (File item : dir.listFiles()) {
-                if (item.isDirectory()) {
-                    System.out.println(item.getName() + "  \t folder");
-                } else {
-                    System.out.println(item.getName() + "\t file");
-                }
+                if (item.isDirectory()) { System.out.println(item.getName() + "  \t folder");
+                } else { System.out.println(item.getName() + "\t file"); }
             }
         }
     }
