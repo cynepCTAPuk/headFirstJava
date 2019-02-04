b52804b4b7e3a2a7755df87d799160c2c8f53f46
cynepCTAPuk <CTAPuk@gmail.com>
2019-02-04 17:39:22 +0300

    Exercise 2a

---
+++ BruceEckel/chap18_io/Exercise2a.java
@@ -0,0 +1,39 @@
+package chap18_io;
+
+import java.io.*;
+
+public class Exercise2a {
+    static String[] printDirs(String dir) {
+        File path = new File(dir);
+        String[] list = path.list();
+        for (int i = 0; i < list.length; i++) {
+            File file = new File(list[i]);
+            if (file.isDirectory()) {
+                System.out.printf("[%-22s] ", list[i]);
+                if (i % 3 == 2) System.out.println();
+            }
+        }
+        System.out.println();
+        printFiles(dir);
+        return list;
+    }
+
+    static void printFiles(String dir) {
+        File path = new File(dir);
+        String[] list = path.list();
+        for (int i = 0; i < list.length; i++) {
+            File file = new File(list[i]);
+            if (file.isFile()) {
+                System.out.printf("%-20s ", file.getName());
+                System.out.printf("%,6d \n", file.length());
+                System.out.printf("%s \n", file.getAbsolutePath());
+            }
+        }
+        System.out.println();
+    }
+
+    public static void main(String[] args) {
+        String dir = "C://";
+        printDirs(dir);
+    }
+}
