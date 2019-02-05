1006e95ccd78fb9156871b992bdba0981a7e0629
cynepCTAPuk <CTAPuk@gmail.com>
2019-02-05 18:07:28 +0300

    BufferedInputFile

--- BruceEckel/chap18_io/BufferedInputFile.java
+++ BruceEckel/chap18_io/BufferedInputFile.java
@@ -1,4 +1,18 @@
+//: io/BufferedInputFile.java
 package chap18_io;
-
+import java.io.*;
 public class BufferedInputFile {
-}
+    // Throw exceptions to console:
+    public static String read(String filename) throws IOException {
+        // Reading input by lines:
+        BufferedReader in = new BufferedReader( new FileReader(filename));
+        String s;
+        StringBuilder sb = new StringBuilder();
+        while((s = in.readLine())!= null) sb.append(s + "\n");
+        in.close();
+        return sb.toString();
+    }
+    public static void main(String[] args)
+            throws IOException { System.out.print(read("chap18_io/BufferedInputFile.java"));
+    }
+} /* (Execute to see output) *///:~
