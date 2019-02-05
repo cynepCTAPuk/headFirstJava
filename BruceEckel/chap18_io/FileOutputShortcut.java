d4e24a631c28af2c8e6093cab240681a7afec464
cynepCTAPuk <CTAPuk@gmail.com>
2019-02-05 21:40:38 +0300

    FileOutputShortcut

--- BruceEckel/chap18_io/FileOutputShortcut.java
+++ BruceEckel/chap18_io/FileOutputShortcut.java
@@ -1,4 +1,21 @@
+//: io/FileOutputShortcut.java
 package chap18_io;
 
+import java.io.*;
+
 public class FileOutputShortcut {
-}
+    static String newFile = "c:/000/FileOutputShortcut.out";
+
+    public static void main(String[] args) throws IOException {
+        String readFile = "chap18_io/FileOutputShortcut.java";
+        BufferedReader in = new BufferedReader(
+                new StringReader( BufferedInputFile.read(readFile)));
+        PrintWriter out = new PrintWriter(newFile); // Here’s the shortcut:
+        int lineCount = 1;
+        String s;
+        while ((s = in.readLine()) != null) out.println(lineCount++ + ": " + s);
+        out.close();
+        // Show the stored newFile:
+        System.out.println(BufferedInputFile.read(newFile));
+    }
+} /* (Execute to see output) *///:~
