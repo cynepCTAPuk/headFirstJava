6b371d9134c31c553feea70650dbc6781dbbafed
cynepCTAPuk <CTAPuk@gmail.com>
2019-02-05 21:10:38 +0300

    MemoryInput

--- BruceEckel/chap18_io/MemoryInput.java
+++ BruceEckel/chap18_io/MemoryInput.java
@@ -1,4 +1,14 @@
+//: io/MemoryInput.java
 package chap18_io;
 
+import java.io.*;
+
 public class MemoryInput {
-}
+    public static void main(String[] args) throws IOException {
+        StringReader in = new StringReader(
+                BufferedInputFile.read(
+                        "chap18_io/MemoryInput.java"));
+        int c;
+        while ((c = in.read()) != -1) System.out.print((char) c);
+    }
+} /* (Execute to see output) *///:~
