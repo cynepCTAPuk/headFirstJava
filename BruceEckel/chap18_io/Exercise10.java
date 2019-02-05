ebe90b1cd86160ec2bce684a47861fa902aae348
cynepCTAPuk <CTAPuk@gmail.com>
2019-02-05 21:03:50 +0300

    Exercise 10

--- BruceEckel/chap18_io/Exercise10.java
+++ BruceEckel/chap18_io/Exercise10.java
@@ -1,29 +1,37 @@
-//: chap18_io/Exercise7.java
+//: chap18_io/Exercise10.java
 package chap18_io;
-
 import java.io.*;
 import java.util.*;
-import java.util.regex.*;
 
 public class Exercise10 {
-    // Throw exceptions to console:
     public static String read(String filename) throws IOException {
-        // Reading input by lines:
         BufferedReader in = new BufferedReader(new FileReader(filename));
         String s;
         StringBuilder sb = new StringBuilder();
-        while ((s = in.readLine()) != null) sb.append(s + "\n");
+        while ((s = in.readLine()) != null) sb.append(s).append("\n");
         in.close();
         return sb.toString();
     }
 
     public static void main(String[] args) throws IOException {
-        String file = read("chap18_io/Exercise7.java");
-        String find = "public";
+        String [] words;
+        if (args.length > 1) {
+            words = new String[args.length];
+            for (int i = 0; i < args.length; i++) words[i] = args[i];
+        } else words = new String[]{"chap18_io/Exercise10.java", "public", "while"};
+
+        String file = read(words[0]);
         LinkedList list = new LinkedList(Arrays.asList(file.split("\\n")));
-        while(!list.isEmpty()) {
+
+        while (!list.isEmpty()) {
             String s = String.valueOf(list.poll());
-            if(s.contains(find)) System.out.println(s);
+            for (int i = 1; i < words.length; i++) {
+                String word = words[i];
+                if (s.contains(word)) {
+                    System.out.println(s);
+                    break;
+                }
+            }
         }
     }
 } /* (Execute to see output) *///:~
