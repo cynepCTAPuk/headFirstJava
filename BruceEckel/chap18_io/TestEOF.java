a3769f87e8ad7a2d41e036195534e323001b6d7d
cynepCTAPuk <CTAPuk@gmail.com>
2019-02-06 14:31:50 +0300

    TestEOF

--- BruceEckel/chap18_io/TestEOF.java
+++ BruceEckel/chap18_io/TestEOF.java
@@ -1,14 +1,12 @@
 //: io/TestEOF.java
 // Testing for end of newFile while reading a byte at a time.
 package chap18_io;
-
 import java.io.*;
-
 public class TestEOF {
     public static void main(String[] args) throws IOException {
+        String file = "chap18_io/TestEOF.java";
         DataInputStream in = new DataInputStream(
-                new BufferedInputStream(
-                        new FileInputStream("chap18_io/TestEOF.java")));
+                new BufferedInputStream( new FileInputStream(file)));
         while (in.available() != 0) System.out.print((char) in.readByte());
     }
 } /* (Execute to see output) *///:~
