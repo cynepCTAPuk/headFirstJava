0644eaef06caf9ac14427519388ef10eda2f78ea
cynepCTAPuk <CTAPuk@gmail.com>
2019-02-06 14:24:55 +0300

    Exercise 12

---
+++ BruceEckel/chap18_io/Exercise12.java
@@ -0,0 +1,32 @@
+//: chap18_io/Exercise12.java
+package chap18_io;
+import java.io.*;
+import java.util.*;
+public class Exercise12 {
+    public static String read(String filename) throws IOException {
+        BufferedReader in = new BufferedReader(new FileReader(filename));
+        String s;
+        StringBuilder sb = new StringBuilder();
+        while ((s = in.readLine()) != null) sb.append(s + "\n");
+        in.close();
+        return sb.toString();
+    }
+
+    public static String write(String filename, String text) throws IOException {
+        BufferedReader in = new BufferedReader(new FileReader(filename));
+        String s;
+        StringBuilder sb = new StringBuilder();
+        while ((s = in.readLine()) != null) sb.append(s + "\n");
+        in.close();
+        return sb.toString();
+    }
+
+    public static void main(String[] args) throws IOException {
+        String file;
+        if (args.length > 0) file = read(args[0]);
+        else file = read("chap18_io/Exercise12.java");
+
+        LinkedList list = new LinkedList(Arrays.asList(file.split("\\n")));
+        while (!list.isEmpty()) System.out.println(list.poll());
+    }
+} /* (Execute to see output) *///:~
