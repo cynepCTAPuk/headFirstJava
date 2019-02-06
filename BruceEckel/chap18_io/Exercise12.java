bd35e9de78f778576c1081ebbf4aa5ed105f3f31
cynepCTAPuk <CTAPuk@gmail.com>
2019-02-06 16:04:17 +0300

    Exercise 12

--- BruceEckel/chap18_io/Exercise12.java
+++ BruceEckel/chap18_io/Exercise12.java
@@ -1,6 +1,5 @@
 //: chap18_io/Exercise12.java
 package chap18_io;
-
 import java.io.*;
 import java.util.*;
 
@@ -14,24 +13,24 @@ public class Exercise12 {
         return sb.toString();
     }
 
-    public static String write(String filename, String text) throws IOException {
-        BufferedReader in = new BufferedReader(new FileReader(filename));
-        String s;
-        StringBuilder sb = new StringBuilder();
-        while ((s = in.readLine()) != null) sb.append(s + "\n");
-        in.close();
-        return sb.toString();
-    }
-
     public static void main(String[] args) throws IOException {
         String file;
-        if (args.length > 0) file = read(args[0]);
-        else file = read("chap18_io/Exercise12.java");
-
-        LinkedList list = new LinkedList(Arrays.asList(file.split("\\n")));
-        int i = 0;
-        while (!list.isEmpty()) {
-            System.out.printf("%-2d %s\n", ++i, list.poll());
+        PrintWriter out;
+        if (args.length == 2) {
+            file = read(args[0]);
+            out = new PrintWriter(args[1]); // Here’s the shortcut:
+        } else {
+            file = read("chap18_io/Exercise12.java");
+            out = new PrintWriter("c://000/Exercise12.out"); // Here’s the shortcut:
         }
+
+        List list = Arrays.asList(file.split("\n"));
+        int i = 0;
+        for (Object o : list) {
+            ++i;
+            out.print(i + " " + o + "\n");
+            System.out.print(i + " " + o + "\n");
+        }
+        out.close();
     }
 } /* (Execute to see output) *///:~
