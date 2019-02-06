31e8fc096b0b9fecc1baff68ee99a40a31acdd08
cynepCTAPuk <CTAPuk@gmail.com>
2019-02-06 18:41:16 +0300

    Exercise 19

---
+++ BruceEckel/chap18_io/Exercise19.java
@@ -0,0 +1,21 @@
+package chap18_io;
+
+import util.*;
+
+import java.io.*;
+import java.util.*;
+
+public class Exercise19 {
+    public static void main(String[] args) throws IOException {
+        String file = "chap18_io/Exercise19.java";
+        byte[] bytes = BinaryFile.read(file);
+
+        Map<Byte, Integer> treeMap = new TreeMap<>();
+        for (Byte b : bytes) {
+//            if(b == '\n') continue;
+            if (treeMap.containsKey(b)) treeMap.put(b, treeMap.get(b) + 1);
+            else treeMap.put(b, 1);
+        }
+        System.out.println(treeMap);
+    }
+}
