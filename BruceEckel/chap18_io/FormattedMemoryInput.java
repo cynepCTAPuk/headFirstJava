c93293b71bf59fb262e9545fa18085b8be2af4d8
cynepCTAPuk <CTAPuk@gmail.com>
2019-02-06 14:29:30 +0300

    FormattedMemoryInput

--- BruceEckel/chap18_io/FormattedMemoryInput.java
+++ BruceEckel/chap18_io/FormattedMemoryInput.java
@@ -5,11 +5,11 @@ import java.io.*;
 
 public class FormattedMemoryInput {
     public static void main(String[] args) throws IOException {
+        String file = "chap18_io/FormattedMemoryInput.java";
         try {
             DataInputStream in = new DataInputStream(
                     new ByteArrayInputStream(
-                            BufferedInputFile.read(
-                                    "chap18_io/FormattedMemoryInput.java").getBytes()));
+                            BufferedInputFile.read( file).getBytes()));
             while (true) System.out.print((char) in.readByte());
         } catch (EOFException e) {
             System.err.println("End of stream");
