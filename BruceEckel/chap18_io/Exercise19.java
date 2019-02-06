db53b56cdb450967a0aa534099d3ae698f76aaac
cynepCTAPuk <CTAPuk@gmail.com>
2019-02-06 19:03:17 +0300

    Exercise 19

--- BruceEckel/chap18_io/Exercise19.java
+++ BruceEckel/chap18_io/Exercise19.java
@@ -8,7 +8,7 @@ public class Exercise19 {
         String file = "chap18_io/Exercise19.java";
         byte[] bytes = BinaryFile.read(file);
 
-        TreeMap<Byte, Integer> treeMap = new TreeMap<>();
+        Map<Byte, Integer> treeMap = new TreeMap<>();
         for (Byte b : bytes) {
             char c = Character.highSurrogate(b);
             if (treeMap.containsKey(b)) treeMap.put(b, treeMap.get(b) + 1);
