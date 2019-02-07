49e016e50df1bcc19b36ffc50c7f36bad1c9f2c9
cynepCTAPuk <CTAPuk@gmail.com>
2019-02-07 18:11:16 +0300

    Redirecting

--- BruceEckel/chap18_io/Redirecting.java
+++ BruceEckel/chap18_io/Redirecting.java
@@ -1,22 +1,25 @@
 //: io/Redirecting.java
 // Demonstrates standard I/O redirection.
 package chap18_io;
+
 import java.io.*;
+
 public class Redirecting {
     public static void main(String[] args) throws IOException {
+        String inFile = "chap18_io/Redirecting.java";
+        String outFile = "c:/000/test.out";
+
         PrintStream console = System.out;
-        BufferedInputStream in = new BufferedInputStream(
-                new FileInputStream("chap18_io/Redirecting.java"));
-        PrintStream out = new PrintStream(
-                new BufferedOutputStream(
-                        new FileOutputStream("c:/000/test.out")));
+        BufferedInputStream in = new BufferedInputStream(new FileInputStream(inFile));
+        PrintStream out = new PrintStream(new BufferedOutputStream(new FileOutputStream(outFile)));
         System.setIn(in);
         System.setOut(out);
         System.setErr(out);
-        BufferedReader br = new BufferedReader( new InputStreamReader(System.in));
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         String s;
-        while((s = br.readLine()) != null)
+        while ((s = br.readLine()) != null) {
             System.out.println(s);
+        }
         out.close(); // Remember this!
         System.setOut(console);
     }
