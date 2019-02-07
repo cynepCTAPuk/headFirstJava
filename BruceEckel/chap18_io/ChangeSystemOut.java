79e32432be6205ca70c339f18f046f4d66a9d16f
cynepCTAPuk <CTAPuk@gmail.com>
2019-02-07 17:41:40 +0300

    ChangeSystemOut

--- BruceEckel/chap18_io/ChangeSystemOut.java
+++ BruceEckel/chap18_io/ChangeSystemOut.java
@@ -1,4 +1,12 @@
+//: io/ChangeSystemOut.java
+// Turn System.out into a PrintWriter.
 package chap18_io;
-
+import java.io.*;
 public class ChangeSystemOut {
-}
+    public static void main(String[] args) {
+        PrintWriter out = new PrintWriter(System.out, true);
+        out.println("Hello, world");
+    }
+} /* Output:
+Hello, world
+*///:~
