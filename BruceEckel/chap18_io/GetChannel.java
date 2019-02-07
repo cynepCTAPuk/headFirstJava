7421205ab5c388125c7f2090278abc13da2343ed
cynepCTAPuk <CTAPuk@gmail.com>
2019-02-07 20:01:37 +0300

    GetChannel

--- BruceEckel/chap18_io/GetChannel.java
+++ BruceEckel/chap18_io/GetChannel.java
@@ -1,7 +1,6 @@
 //: io/GetChannel.java
 // Getting channels from streams
 package chap18_io;
-
 import java.io.*;
 import java.nio.*;
 import java.nio.channels.*;
@@ -10,17 +9,20 @@ public class GetChannel {
     private static final int BSIZE = 1024;
 
     public static void main(String[] args) throws Exception {
+        String file = "c:/000/data.txt";
+
         // Write a file:
-        FileChannel fc = new FileOutputStream("data.txt").getChannel();
+        FileChannel fc = new FileOutputStream(file).getChannel();
         fc.write(ByteBuffer.wrap("Some text ".getBytes()));
         fc.close();
         // Add to the end of the file:
-        fc = new RandomAccessFile("data.txt", "rw").getChannel();
+        fc = new RandomAccessFile(file, "rw").getChannel();
         fc.position(fc.size()); // Move to the end
         fc.write(ByteBuffer.wrap("Some more".getBytes()));
         fc.close();
+
         // Read the file:
-        fc = new FileInputStream("data.txt").getChannel();
+        fc = new FileInputStream(file).getChannel();
         ByteBuffer buff = ByteBuffer.allocate(BSIZE);
         fc.read(buff);
         buff.flip();
