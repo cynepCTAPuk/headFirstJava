19a62d212efe6ab10a56fe71b6104c4528676cc7
cynepCTAPuk <CTAPuk@gmail.com>
2019-02-07 15:14:14 +0300

    Exercise 21

--- BruceEckel/chap18_io/Exercise21.java
+++ BruceEckel/chap18_io/Exercise21.java
@@ -3,13 +3,20 @@
 // {RunByHand}
 package chap18_io;
 import java.io.*;
-
 public class Exercise21 {
     public static void main(String[] args) throws IOException {
-        BufferedReader stdin = new BufferedReader( new InputStreamReader(System.in));
         String s;
-        while((s = stdin.readLine()) != null && s.length()!= 0)
-            System.out.println(s.toUpperCase());
-        // An empty line or Ctrl-Z terminates the program
+        String file = "chap18_io/Exercise21.java";
+        if (args.length == 1) {
+            BufferedReader stdin = new BufferedReader(new FileReader(args[0]));
+            while ((s = stdin.readLine()) != null) {
+                System.out.println(s.toUpperCase());
+            }
+        } else {
+            BufferedReader stdin = new BufferedReader(new InputStreamReader(System.in));
+            // An empty line or Ctrl-Z terminates the program
+            while ((s = stdin.readLine()) != null && s.length() != 0)
+                System.out.println(s.toUpperCase());
+        }
     }
 } ///:~
