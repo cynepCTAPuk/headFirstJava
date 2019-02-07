f1158cdcb1262b780db16226499611871d8809cb
cynepCTAPuk <CTAPuk@gmail.com>
2019-02-07 19:14:31 +0300

    OSExecute

--- BruceEckel/util/OSExecute.java
+++ BruceEckel/util/OSExecute.java
@@ -2,7 +2,9 @@
 // Run an operating system command
 // and send the output to the console.
 package util;
+
 import java.io.*;
+import java.util.*;
 
 public class OSExecute {
     public static void command(String command) {
@@ -30,5 +32,36 @@ public class OSExecute {
         }
         if (err)
             throw new OSExecuteException("Errors executing " + command);
+    }
+
+    public static List command(String command, int i) {
+        boolean err = false;
+        List listRes = new ArrayList();
+        List<String> listErr = new ArrayList();
+        try {
+            Process process = new ProcessBuilder(command.split(" ")).start();
+            BufferedReader results = new BufferedReader(new InputStreamReader(process.getInputStream()));
+            String s;
+            while ((s = results.readLine()) != null) {
+                listRes.add(s);
+            }
+            BufferedReader errors = new BufferedReader(new InputStreamReader(process.getErrorStream()));
+            // Report errors and return nonzero value to calling process if there are problems:
+            while ((s = errors.readLine()) != null) {
+                System.err.println(s);
+                err = true;
+            }
+        } catch (Exception e) {
+            // Compensate for Windows 2000, which throws an exception for the default command line:
+            if (!command.startsWith("CMD /C")) {
+                command("CMD /C " + command);
+            } else {
+                throw new RuntimeException(e);
+            }
+        }
+        if (err) {
+            throw new OSExecuteException("Errors executing " + command);
+        }
+        return listRes;
     }
 } ///:~
