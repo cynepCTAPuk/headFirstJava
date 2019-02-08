75eda0680dabf28074c584a37c9a9b3de98f9de6
cynepCTAPuk <CTAPuk@gmail.com>
2019-02-08 19:47:43 +0300

    LargeMappedFiles

--- BruceEckel/chap18_io/LargeMappedFiles.java
+++ BruceEckel/chap18_io/LargeMappedFiles.java
@@ -7,15 +7,14 @@ import java.nio.channels.*;
 import java.io.*;
 import static util.Print.*;
 public class LargeMappedFiles {
-    static int length = 0x8FFFFFF; // 128 MB
+    static int length = 0x7FFFFFF; // 128 MB
     public static void main(String[] args) throws Exception {
         MappedByteBuffer out =
-                new RandomAccessFile("c:/000/test.dat", "rw").getChannel()
+                new RandomAccessFile("c:/000/test.dat", "rw")
+                        .getChannel()
                         .map(FileChannel.MapMode.READ_WRITE, 0, length);
-        for(int i = 0; i < length; i++)
-            out.put((byte)'x');
+        for(int i = 0; i < length; i++) out.put((byte)'x');
         print("Finished writing");
-        for(int i = length/2; i < length/2 + 6; i++)
-            printnb((char)out.get(i));
+        for(int i = length/2; i < length/2 + 6; i++) printnb((char)out.get(i));
     }
 } ///:~
