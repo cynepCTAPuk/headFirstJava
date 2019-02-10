ec2856c7c943684783aaf5b798708217050a0f0e
cynepCTAPuk <CTAPuk@gmail.com>
2019-02-10 20:02:30 +0300

    FreezeAlien

--- BruceEckel/chap18_io/FreezeAlien.java
+++ BruceEckel/chap18_io/FreezeAlien.java
@@ -1,4 +1,11 @@
+//: io/FreezeAlien.java
+// Create a serialized output file.
 package chap18_io;
-
+import java.io.*;
 public class FreezeAlien {
-}
+    public static void main(String[] args) throws Exception {
+        ObjectOutput out = new ObjectOutputStream( new FileOutputStream("c/000/X.file"));
+        Alien quellek = new Alien();
+        out.writeObject(quellek);
+    }
+} ///:~
