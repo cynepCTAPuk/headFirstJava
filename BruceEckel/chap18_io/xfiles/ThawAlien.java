7d882c34e774c16918bd0f793ecaa76c2ab0ff14
cynepCTAPuk <CTAPuk@gmail.com>
2019-02-10 20:10:33 +0300

    ThawAlien

--- BruceEckel/chap18_io/xfiles/ThawAlien.java
+++ BruceEckel/chap18_io/xfiles/ThawAlien.java
@@ -7,7 +7,7 @@ import java.io.*;
 public class ThawAlien {
     public static void main(String[] args) throws Exception {
         ObjectInputStream in = new ObjectInputStream(
-                new FileInputStream(new File("..", "X.file")));
+                new FileInputStream(new File("c:/000/X.file")));
         Object mystery = in.readObject();
         System.out.println(mystery.getClass());
     }
