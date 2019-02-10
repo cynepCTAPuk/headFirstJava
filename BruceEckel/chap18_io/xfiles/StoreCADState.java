b9abeaaa5a9ba675ca36c2142c3dde0ba3a28997
cynepCTAPuk <CTAPuk@gmail.com>
2019-02-10 20:58:00 +0300

    StoreCADState

--- BruceEckel/chap18_io/xfiles/StoreCADState.java
+++ BruceEckel/chap18_io/xfiles/StoreCADState.java
@@ -34,9 +34,7 @@ abstract class Shape implements Serializable {
 }
 class Circle extends Shape {
     private static int color = RED;
-    public Circle(int xVal, int yVal, int dim) {
-        super(xVal, yVal, dim);
-    }
+    public Circle(int xVal, int yVal, int dim) { super(xVal, yVal, dim); }
     public void setColor(int newColor) { color = newColor; }
     public int getColor() { return color; }
 }
@@ -51,32 +49,30 @@ class Square extends Shape {
 }
 class Line extends Shape {
     private static int color = RED;
-    public static void
-    serializeStaticState(ObjectOutputStream os) throws IOException { os.writeInt(color); }
-    public static void
-    deserializeStaticState(ObjectInputStream os) throws IOException { color = os.readInt(); }
-    public Line(int xVal, int yVal, int dim) {
-        super(xVal, yVal, dim);
+    public static void serializeStaticState(ObjectOutputStream os) throws IOException {
+        os.writeInt(color);
     }
+    public static void deserializeStaticState(ObjectInputStream os) throws IOException {
+        color = os.readInt();
+    }
+    public Line(int xVal, int yVal, int dim) { super(xVal, yVal, dim); }
     public void setColor(int newColor) { color = newColor; }
     public int getColor() { return color; }
 }
 public class StoreCADState {
     public static void main(String[] args) throws Exception {
-        List<Class<? extends Shape>> shapeTypes = new ArrayList<Class<? extends Shape>>();
+        List<Class<? extends Shape>> shapeTypes = new ArrayList<>();
         // Add references to the class objects:
         shapeTypes.add(Circle.class);
         shapeTypes.add(Square.class);
         shapeTypes.add(Line.class);
         List<Shape> shapes = new ArrayList<>();
         // Make some shapes:
-        for(int i = 0; i < 10; i++)
-            shapes.add(Shape.randomFactory());
+        for(int i = 0; i < 10; i++) shapes.add(Shape.randomFactory());
         // Set all the static colors to GREEN:
-        for(int i = 0; i < 10; i++)
-            ((Shape)shapes.get(i)).setColor(Shape.GREEN);
+        for(int i = 0; i < 10; i++) shapes.get(i).setColor(Shape.GREEN);
         // Save the state vector:
-        ObjectOutputStream out = new ObjectOutputStream(new FileOutputStream("CADState.out"));
+        ObjectOutputStream out = new ObjectOutputStream(new FileOutputStream("c:/000/CADState.out"));
         out.writeObject(shapeTypes);
         Line.serializeStaticState(out);
         out.writeObject(shapes);
