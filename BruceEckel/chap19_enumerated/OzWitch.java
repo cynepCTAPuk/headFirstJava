1382fb23861026da25e1781b0a97e832c7cd9f26
cynepCTAPuk <CTAPuk@gmail.com>
2019-02-11 18:05:56 +0300

    OzWitch

--- BruceEckel/chap19_enumerated/OzWitch.java
+++ BruceEckel/chap19_enumerated/OzWitch.java
@@ -1,4 +1,31 @@
+//: enumerated/OzWitch.java
+// The witches in the land of Oz.
 package chap19_enumerated;
+import java.util.*;
 
-public class OzWitch {
-}
+import static util.Print.*;
+
+public enum OzWitch {
+    // Instances must be defined first, before methods:
+    WEST("Miss Gulch, aka the Wicked Witch of the West"),
+    NORTH("Glinda, the Good Witch of the North"),
+    EAST("Wicked Witch of the East, wearer of the Ruby " +
+            "Slippers, crushed by Dorothy’s house"),
+    SOUTH("Good by inference, but missing");
+    private String description;
+
+    // Constructor must be package or private access:
+    private OzWitch(String description) { this.description = description; }
+
+    public String getDescription() { return description; }
+
+    public static void main(String[] args) {
+        for (OzWitch witch : OzWitch.values()) print(witch + ":\t" + witch.getDescription());
+        System.out.println(Arrays.toString(OzWitch.values()));
+    }
+} /* Output:
+WEST: Miss Gulch, aka the Wicked Witch of the West
+NORTH: Glinda, the Good Witch of the North
+EAST: Wicked Witch of the East, wearer of the Ruby Slippers, crushed by Dorothy’s house
+SOUTH: Good by inference, but missing
+*///:~
