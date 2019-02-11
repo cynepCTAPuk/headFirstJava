672f25dd34eedda6c41de23fe5a94a863595d5e1
cynepCTAPuk <CTAPuk@gmail.com>
2019-02-11 17:56:19 +0300

    Burrito

--- BruceEckel/chap19_enumerated/Burrito.java
+++ BruceEckel/chap19_enumerated/Burrito.java
@@ -1,4 +1,17 @@
+//: enumerated/Burrito.java
 package chap19_enumerated;
-
+import static chap19_enumerated.Spiciness.*;
 public class Burrito {
-}
+    Spiciness degree;
+    public Burrito(Spiciness degree) { this.degree = degree;}
+    public String toString() { return "Burrito is "+ degree;}
+    public static void main(String[] args) {
+        System.out.println(new Burrito(NOT));
+        System.out.println(new Burrito(MEDIUM));
+        System.out.println(new Burrito(HOT));
+    }
+} /* Output:
+Burrito is NOT
+Burrito is MEDIUM
+Burrito is HOT
+*///:~
