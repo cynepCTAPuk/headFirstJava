6d328814cb85a7830ea1bd3fd346a1b0477ecae0
cynepCTAPuk <CTAPuk@gmail.com>
2019-02-11 18:11:35 +0300

    SpaceShip

--- BruceEckel/chap19_enumerated/SpaceShip.java
+++ BruceEckel/chap19_enumerated/SpaceShip.java
@@ -1,4 +1,24 @@
+//: enumerated/SpaceShip.java
 package chap19_enumerated;
 
-public class SpaceShip {
-}
+import java.util.*;
+
+public enum SpaceShip {
+    SCOUT, CARGO, TRANSPORT, CRUISER, BATTLESHIP, MOTHERSHIP;
+    public String toString() {
+        String id = name();
+        String lower = id.substring(1).toLowerCase();
+        return id.charAt(0) + lower;
+    }
+    public static void main(String[] args) {
+        for(SpaceShip s : values()) { System.out.println(s); }
+        System.out.println(Arrays.toString(values()));
+    }
+} /* Output:
+Scout
+Cargo
+Transport
+Cruiser
+Battleship
+Mothership
+*///:~
