ec3dfe52849f122bb450b25707b9703c1afdb855
cynepCTAPuk <CTAPuk@gmail.com>
2019-02-11 18:21:39 +0300

    TrafficLight

--- BruceEckel/chap19_enumerated/TrafficLight.java
+++ BruceEckel/chap19_enumerated/TrafficLight.java
@@ -1,4 +1,38 @@
+//: enumerated/TrafficLight.java
+// Enums in switch statements.
 package chap19_enumerated;
+import static util.Print.*;
 
+// Define an enum type:
+enum Signal { GREEN, YELLOW, RED, }
 public class TrafficLight {
-}
+    Signal color = Signal.RED;
+    public void change() {
+        switch(color) {
+            // Note that you don’t have to say Signal.RED in the case statement:
+            case RED: color = Signal.GREEN;
+                break;
+            case GREEN: color = Signal.YELLOW;
+                break;
+            case YELLOW: color = Signal.RED;
+                break;
+        }
+    }
+    public String toString() { return "The traffic light is " + color; }
+    public static void main(String[] args) throws InterruptedException {
+        TrafficLight t = new TrafficLight();
+        for(int i = 0; i < 7; i++) {
+            print(t);
+            Thread.sleep(1_000);
+            t.change();
+        }
+    }
+} /* Output:
+The traffic light is RED
+The traffic light is GREEN
+The traffic light is YELLOW
+The traffic light is RED
+The traffic light is GREEN
+The traffic light is YELLOW
+The traffic light is RED
+*///:~
