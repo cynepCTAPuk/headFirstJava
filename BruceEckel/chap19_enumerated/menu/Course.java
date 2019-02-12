7f4c431303ea40966913e08d2e046993d79932f4
cynepCTAPuk <CTAPuk@gmail.com>
2019-02-12 15:24:41 +0300

    Course

--- BruceEckel/chap19_enumerated/menu/Course.java
+++ BruceEckel/chap19_enumerated/menu/Course.java
@@ -1,4 +1,20 @@
+//: enumerated/menu/Course.java
 package chap19_enumerated.menu;
 
-public class Course {
-}
+import util.*;
+
+public enum Course {
+    APPETIZER(Food.Appetizer.class),
+    MAINCOURSE(Food.MainCourse.class),
+    DESSERT(Food.Dessert.class),
+    COFFEE(Food.Coffee.class);
+    private Food[] values;
+
+    private Course(Class<? extends Food> kind) {
+        values = kind.getEnumConstants();
+    }
+
+    public Food randomSelection() {
+        return Enums.random(values);
+    }
+} ///:~
