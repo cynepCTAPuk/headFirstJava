4a6f15639e0be46a5842ff67d2813872d451561a
cynepCTAPuk <CTAPuk@gmail.com>
2019-02-12 14:56:52 +0300

    NonEnum

--- BruceEckel/chap19_enumerated/NonEnum.java
+++ BruceEckel/chap19_enumerated/NonEnum.java
@@ -1,4 +1,16 @@
+//: enumerated/NonEnum.java
 package chap19_enumerated;
-
 public class NonEnum {
-}
+    public static void main(String[] args) {
+        Class<Integer> intClass = Integer.class;
+        try {
+            for(Object en : intClass.getEnumConstants())
+                System.out.println(en);
+        } catch(Exception e) {
+            e.printStackTrace();
+//            System.out.println(e);
+        }
+    }
+} /* Output:
+java.lang.NullPointerException
+*///:~
