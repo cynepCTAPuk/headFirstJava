1c2b782d349570ea5ea350cf16f8244d690529b2
cynepCTAPuk <CTAPuk@gmail.com>
2019-02-12 15:36:21 +0300

    AlarmPoints

---
+++ BruceEckel/chap19_enumerated/AlarmPoints.java
@@ -0,0 +1,5 @@
+//: enumerated/AlarmPoints.java
+package chap19_enumerated;
+public enum AlarmPoints {
+    STAIR1, STAIR2, LOBBY, OFFICE1, OFFICE2, OFFICE3, OFFICE4, BATHROOM, UTILITY, KITCHEN
+} ///:~
