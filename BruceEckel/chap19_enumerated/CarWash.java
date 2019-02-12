f388cc7b4b1403435576ee2506cbe4cde783862e
cynepCTAPuk <CTAPuk@gmail.com>
2019-02-12 16:22:04 +0300

    CarWash

--- BruceEckel/chap19_enumerated/CarWash.java
+++ BruceEckel/chap19_enumerated/CarWash.java
@@ -4,13 +4,20 @@ import java.util.*;
 import static util.Print.*;
 public class CarWash {
     public enum Cycle {
-        UNDERBODY { void action() { print("Spraying the underbody"); }},
-        WHEELWASH { void action() { print("Washing the wheels"); }},
-        PREWASH {void action() { print("Loosening the dirt"); }},
-        BASIC { void action() { print("The basic wash"); }},
-        HOTWAX { void action() { print("Applying hot wax"); }},
-        RINSE { void action() { print("Rinsing"); }},
-        BLOWDRY {void action() { print("Blowing dry"); }};
+        UNDERBODY {
+            void action() { print("Spraying the underbody"); }},
+        WHEELWASH {
+            void action() { print("Washing the wheels"); }},
+        PREWASH {
+            void action() { print("Loosening the dirt"); }},
+        BASIC {
+            void action() { print("The basic wash"); }},
+        HOTWAX {
+            void action() { print("Applying hot wax"); }},
+        RINSE {
+            void action() { print("Rinsing"); }},
+        BLOWDRY {
+            void action() { print("Blowing dry"); }};
         abstract void action();
     }
     EnumSet<Cycle> cycles = EnumSet.of(Cycle.BASIC, Cycle.RINSE);
@@ -20,7 +27,7 @@ public class CarWash {
             c.action();
     }
     public String toString() { return cycles.toString(); }
-    
+
     public static void main(String[] args) {
         CarWash wash = new CarWash();
         print(wash);
