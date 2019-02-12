43f7ab6d2593f240680ab855c0b31dd80659c802
cynepCTAPuk <CTAPuk@gmail.com>
2019-02-12 15:21:29 +0300

    TypeOfFood

--- BruceEckel/chap19_enumerated/menu/TypeOfFood.java
+++ BruceEckel/chap19_enumerated/menu/TypeOfFood.java
@@ -1,4 +1,13 @@
+//: enumerated/menu/TypeOfFood.java
 package chap19_enumerated.menu;
 
+import static chap19_enumerated.menu.Food.*;
+
 public class TypeOfFood {
-}
+    public static void main(String[] args) {
+        Food food = Appetizer.SALAD;
+        food = MainCourse.LASAGNE;
+        food = Dessert.GELATO;
+        food = Coffee.CAPPUCCINO;
+    }
+} ///:~
