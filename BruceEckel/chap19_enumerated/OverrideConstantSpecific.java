5595cd7610fbba0b1ca5c5d2afba9ebfda7e0efc
cynepCTAPuk <CTAPuk@gmail.com>
2019-02-12 16:25:31 +0300

    OverrideConstantSpecific

--- BruceEckel/chap19_enumerated/OverrideConstantSpecific.java
+++ BruceEckel/chap19_enumerated/OverrideConstantSpecific.java
@@ -4,7 +4,7 @@ import static util.Print.*;
 public enum OverrideConstantSpecific {
     NUT, BOLT, WASHER { void f() { print("Overridden method"); }};
     void f() { print("default behavior"); }
-    
+
     public static void main(String[] args) {
         for(OverrideConstantSpecific ocs : values()) {
             printnb(ocs + ": ");
