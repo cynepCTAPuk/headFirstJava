35ff954188acdc1115220e00ebda336f84c106ac
cynepCTAPuk <CTAPuk@gmail.com>
2019-02-12 16:08:08 +0300

    ConstantSpecificMethod

--- BruceEckel/chap19_enumerated/ConstantSpecificMethod.java
+++ BruceEckel/chap19_enumerated/ConstantSpecificMethod.java
@@ -4,21 +4,9 @@ import java.util.*;
 import java.text.*;
 
 public enum ConstantSpecificMethod {
-    DATE_TIME {
-        String getInfo() {
-            return DateFormat.getDateInstance().format(new Date());
-        }
-    },
-    CLASSPATH {
-        String getInfo() {
-            return System.getenv("PATH");
-        }
-    },
-    VERSION {
-        String getInfo() {
-            return System.getProperty("java.version");
-        }
-    };
+    DATE_TIME { String getInfo() { return DateFormat.getDateInstance().format(new Date());}},
+    CLASSPATH { String getInfo() { return System.getenv("PATH");}},
+    VERSION { String getInfo() { return System.getProperty("java.version");}};
     abstract String getInfo();
 
     public static void main(String[] args) {
