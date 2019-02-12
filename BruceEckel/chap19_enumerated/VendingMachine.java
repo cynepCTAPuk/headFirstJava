48c2b44780baf92f25d757331fbd82c73d73db73
cynepCTAPuk <CTAPuk@gmail.com>
2019-02-12 18:08:11 +0300

    VendingMachine

--- BruceEckel/chap19_enumerated/VendingMachine.java
+++ BruceEckel/chap19_enumerated/VendingMachine.java
@@ -1,36 +1,36 @@
 //: enumerated/VendingMachine.java
 // {Args: VendingMachineInput.txt}
 package chap19_enumerated;
-import java.util.*;
 import util.*;
+import java.util.*;
 import static chap19_enumerated.Input.*;
 import static util.Print.*;
+
 enum Category {
     MONEY(NICKEL, DIME, QUARTER, DOLLAR),
     ITEM_SELECTION(TOOTHPASTE, CHIPS, SODA, SOAP),
     QUIT_TRANSACTION(ABORT_TRANSACTION),
     SHUT_DOWN(STOP);
     private Input[] values;
-    Category(Input... types) { values = types; }
-    private static EnumMap<Input,Category> categories = new EnumMap<>(Input.class);
+    Category(Input... types) { values = types;}
+    private static EnumMap<Input, Category> categories = new EnumMap<>(Input.class);
     static {
-        for(Category c : Category.class.getEnumConstants())
-            for(Input type : c.values)
+        for (Category c : Category.class.getEnumConstants())
+            for (Input type : c.values)
                 categories.put(type, c);
     }
-    public static Category categorize(Input input) {
-        return categories.get(input);
-    }
+    public static Category categorize(Input input) { return categories.get(input);}
 }
+
 public class VendingMachine {
     private static State state = State.RESTING;
     private static int amount = 0;
     private static Input selection = null;
-    enum StateDuration { TRANSIENT } // Tagging enum
+    enum StateDuration {TRANSIENT} // Tagging enum
     enum State {
         RESTING {
             void next(Input input) {
-                switch(Category.categorize(input)) {
+                switch (Category.categorize(input)) {
                     case MONEY:
                         amount += input.amount();
                         state = ADDING_MONEY;
@@ -43,13 +43,13 @@ public class VendingMachine {
         },
         ADDING_MONEY {
             void next(Input input) {
-                switch(Category.categorize(input)) {
+                switch (Category.categorize(input)) {
                     case MONEY:
                         amount += input.amount();
                         break;
                     case ITEM_SELECTION:
                         selection = input;
-                        if(amount < selection.amount())
+                        if (amount < selection.amount())
                             print("Insufficient money for " + selection);
                         else state = DISPENSING;
                         break;
@@ -71,46 +71,48 @@ public class VendingMachine {
         },
         GIVING_CHANGE(StateDuration.TRANSIENT) {
             void next() {
-                if(amount > 0) {
+                if (amount > 0) {
                     print("Your change: " + amount);
                     amount = 0;
                 }
                 state = RESTING;
             }
         },
-        TERMINAL { void output() { print("Halted"); } };
+        TERMINAL { void output() { print("Halted");}};
         private boolean isTransient = false;
-        State() {}
-        State(StateDuration trans) { isTransient = true; }
+        State() { }
+        State(StateDuration trans) { isTransient = true;}
         void next(Input input) {
-            throw new RuntimeException("Only call " +
-                    "next(Input input) for non-transient states");
+            throw new RuntimeException("Only call next(Input input) for non-transient states");
         }
         void next() {
-            throw new RuntimeException("Only call next() for " +
-                    "StateDuration.TRANSIENT states");
+            throw new RuntimeException("Only call next() for StateDuration.TRANSIENT states");
         }
-        void output() { print(amount); }
+        void output() { print(amount);}
     }
+
     static void run(Generator<Input> gen) {
-        while(state != State.TERMINAL) {
+        while (state != State.TERMINAL) {
             state.next(gen.next());
-            while(state.isTransient)
+            while (state.isTransient)
                 state.next();
             state.output();
         }
     }
+
     public static void main(String[] args) {
         Generator<Input> gen = new RandomInputGenerator();
-        if(args.length == 1)
+        if (args.length == 1)
             gen = new FileInputGenerator(args[0]);
         run(gen);
     }
 }
+
 // For a basic sanity check:
 class RandomInputGenerator implements Generator<Input> {
-    public Input next() { return Input.randomSelection(); }
+    public Input next() { return Input.randomSelection();}
 }
+
 // Create Inputs from a file of ‘;’-separated strings:
 class FileInputGenerator implements Generator<Input> {
     private Iterator<String> input;
@@ -118,7 +120,7 @@ class FileInputGenerator implements Generator<Input> {
         input = new TextFile(fileName, ";").iterator();
     }
     public Input next() {
-        if(!input.hasNext())
+        if (!input.hasNext())
             return null;
         return Enum.valueOf(Input.class, input.next().trim());
     }
