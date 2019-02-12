138750841fb458d049501f813608426150070935
cynepCTAPuk <CTAPuk@gmail.com>
2019-02-12 18:15:04 +0300

    Input

--- BruceEckel/chap19_enumerated/Input.java
+++ BruceEckel/chap19_enumerated/Input.java
@@ -4,11 +4,9 @@ import java.util.*;
 public enum Input {
     NICKEL(5), DIME(10), QUARTER(25), DOLLAR(100),
     TOOTHPASTE(200), CHIPS(75), SODA(100), SOAP(50),
-    ABORT_TRANSACTION {
-        public int amount() { // Disallow
+    ABORT_TRANSACTION { public int amount() { // Disallow
             throw new RuntimeException("ABORT.amount()");
-        }
-    },
+        }},
     STOP { // This must be the last instance.
         public int amount() { // Disallow
             throw new RuntimeException("SHUT_DOWN.amount()");
