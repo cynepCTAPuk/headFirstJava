621f3d1902f3e0f68d49e607ff58bcb66cb5d950
cynepCTAPuk <CTAPuk@gmail.com>
2019-02-13 18:50:12 +0300

    MainThread

--- BruceEckel/concurrency/MainThread.java
+++ BruceEckel/concurrency/MainThread.java
@@ -4,6 +4,9 @@ public class MainThread {
     public static void main(String[] args) {
         LiftOff launch = new LiftOff();
         launch.run();
+        System.out.println();
+        LiftOff launch1 = new LiftOff(5);
+        launch1.run();
     }
 } /* Output:
 #0(9), #0(8), #0(7), #0(6), #0(5), #0(4), #0(3), #0(2), #0(1), #0(Liftoff!),
