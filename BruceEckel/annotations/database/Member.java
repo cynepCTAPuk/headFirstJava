53e2de9ebf1cc7a4209354fa7d4229c369a14e94
cynepCTAPuk <CTAPuk@gmail.com>
2019-02-13 16:39:17 +0300

    Uniqueness

--- BruceEckel/annotations/database/Member.java
+++ BruceEckel/annotations/database/Member.java
@@ -5,8 +5,7 @@ public class Member {
     @SQLString(30) String firstName;
     @SQLString(50) String lastName;
     @SQLInteger Integer age;
-    @SQLString(value = 30,
-            constraints = @Constraints(primaryKey = true))
+    @SQLString(value = 30, constraints = @Constraints(primaryKey = true))
     String handle;
     static int memberCount;
     public String getHandle() { return handle; }
