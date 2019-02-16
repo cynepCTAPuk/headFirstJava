76bcc4a12f0b63f5debb59c2952b38f442f47d4c
cynepCTAPuk <CTAPuk@gmail.com>
2019-02-16 17:14:19 +0300

    AttemptLocking

--- BruceEckel/concurrency/AttemptLocking.java
+++ BruceEckel/concurrency/AttemptLocking.java
@@ -7,27 +7,15 @@ public class AttemptLocking {
     private ReentrantLock lock = new ReentrantLock();
     public void untimed() {
         boolean captured = lock.tryLock();
-        try {
-            System.out.println("tryLock(): " + captured);
-        } finally {
-            if(captured)
-                lock.unlock();
-        }
+        try { System.out.println("tryLock(): " + captured);
+        } finally { if(captured) lock.unlock();}
     }
     public void timed() {
         boolean captured = false;
-        try {
-            captured = lock.tryLock(2, TimeUnit.SECONDS);
-        } catch(InterruptedException e) {
-            throw new RuntimeException(e);
-        }
-        try {
-            System.out.println("tryLock(2, TimeUnit.SECONDS): " +
-                    captured);
-        } finally {
-            if(captured)
-                lock.unlock();
-        }
+        try {captured = lock.tryLock(2, TimeUnit.SECONDS);
+        } catch(InterruptedException e) {throw new RuntimeException(e);}
+        try { System.out.println("tryLock(2, TimeUnit.SECONDS): " + captured);
+        } finally { if(captured) lock.unlock();}
     }
     public static void main(String[] args) {
         final AttemptLocking al = new AttemptLocking();
