9ed5bc4a416bcb80002e3405d392dddb7f776a3e
cynepCTAPuk <CTAPuk@gmail.com>
2019-02-17 17:25:06 +0300

    ThreadLocalVariableHolder

--- BruceEckel/concurrency/ThreadLocalVariableHolder.java
+++ BruceEckel/concurrency/ThreadLocalVariableHolder.java
@@ -13,22 +13,16 @@ class Accessor implements Runnable {
             Thread.yield();
         }
     }
-    public String toString() {
-        return "#" + id + ": " + ThreadLocalVariableHolder.get();
-    }
+    public String toString() { return "#" + id + ": " + ThreadLocalVariableHolder.get();}
 }
 public class ThreadLocalVariableHolder {
-    private static ThreadLocal<Integer> value =
-            new ThreadLocal<Integer>() {
-                private Random rand = new Random(47);
-                protected synchronized Integer initialValue() {
-                    return rand.nextInt(10000);
-                }
-            };
-    public static void increment() {
-        value.set(value.get() + 1);
-    }
-    public static int get() { return value.get(); }
+    private static ThreadLocal<Integer> value = new ThreadLocal<>() {
+        private Random rand = new Random(47);
+        protected synchronized Integer initialValue() { return rand.nextInt(10_000);}
+    };
+    public static void increment() { value.set(value.get() + 1);}
+    public static int get() { return value.get();}
+
     public static void main(String[] args) throws Exception {
         ExecutorService exec = Executors.newCachedThreadPool();
         for(int i = 0; i < 5; i++)
