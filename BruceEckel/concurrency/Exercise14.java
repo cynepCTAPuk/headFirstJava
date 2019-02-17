0479e76367c37cb164b768972a23fd05a85c334a
cynepCTAPuk <CTAPuk@gmail.com>
2019-02-17 16:42:55 +0300

    Exercise 14

---
+++ BruceEckel/concurrency/Exercise14.java
@@ -0,0 +1,16 @@
+package concurrency;
+
+import java.util.*;
+
+public class Exercise14 implements Runnable{
+    int i = 0;
+    public int getI() { return i;}
+    void add() { i++;}
+    public void run() { add();}
+
+    public static void main(String[] args) {
+        Exercise14 ex = new Exercise14();
+        Timer timer = new Timer();
+    }
+
+}
