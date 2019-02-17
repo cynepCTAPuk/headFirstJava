4202278246cdb0d6c133e943343bcc7460c03897
cynepCTAPuk <CTAPuk@gmail.com>
2019-02-17 17:43:44 +0300

    CHLoop1

--- test/misc/CHLoop1.java
+++ test/misc/CHLoop1.java
@@ -1,5 +1,13 @@
 package misc;
 
 public class CHLoop1 {
-    psvm
+    public static void main(String[] args) {
+        int i = 1500;
+        while (1500 <= i && i <= 2700) {
+            if (i % 7 == 0 && i % 5 == 0) {
+                System.out.println(i);
+            }
+            i++;
+        }
+    }
 }
