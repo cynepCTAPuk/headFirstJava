dad8e99941e5bdd8a4e5a238cacd8025d194b485
cynepCTAPuk <CTAPuk@gmail.com>
2019-02-19 20:37:31 +0300

    SubmitSwingProgram

---
+++ BruceEckel/gui/SubmitSwingProgram.java
@@ -0,0 +1,29 @@
+//: gui/SubmitSwingProgram.java
+package gui;
+import javax.swing.*;
+import java.util.concurrent.*;
+public class SubmitSwingProgram extends JFrame {
+    JLabel label;
+    public SubmitSwingProgram() {
+        super("Hello Swing");
+        label = new JLabel("A Label");
+        add(label);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setSize(300, 100);
+        setLocation(500,400);
+        setVisible(true);
+    }
+    static SubmitSwingProgram ssp;
+
+    public static void main(String[] args) throws Exception {
+        SwingUtilities.invokeLater(new Runnable() {
+            public void run() { ssp = new SubmitSwingProgram(); }
+        });
+        TimeUnit.SECONDS.sleep(3);
+        SwingUtilities.invokeLater(new Runnable() {
+            public void run() {
+                ssp.label.setText("Hey! This is Different!");
+            }
+        });
+    }
+} ///:~
