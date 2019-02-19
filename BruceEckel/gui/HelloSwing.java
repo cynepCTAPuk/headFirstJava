c700ebb761f25fb8bf2625ad76557b9384cc6624
cynepCTAPuk <CTAPuk@gmail.com>
2019-02-19 20:22:27 +0300

    HelloSwing

--- BruceEckel/gui/HelloSwing.java
+++ BruceEckel/gui/HelloSwing.java
@@ -6,6 +6,7 @@ public class HelloSwing {
         JFrame frame = new JFrame("Hello Swing");
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setSize(300, 100);
+        frame.setLocation(500,400);
         frame.setVisible(true);
     }
 } ///:~
