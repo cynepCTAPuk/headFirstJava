cd9c73a27540458afff845ab2908d03824582a30
cynepCTAPuk <CTAPuk@gmail.com>
2019-02-19 21:20:57 +0300

    Exercise 2

--- BruceEckel/util/Tuple.java
+++ BruceEckel/util/Tuple.java
@@ -1,6 +1,6 @@
 //: net/mindview/util/Tuple.java
 // Tuple library using type argument inference.
-package net.mindview.util;
+package util;
 
 public class Tuple {
   public static <A,B> TwoTuple<A,B> tuple(A a, B b) {
