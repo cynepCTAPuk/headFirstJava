df8a22d8dc4ab3958b30db82b5501bc4995d8a2d
cynepCTAPuk <CTAPuk@gmail.com>
2019-02-19 21:05:31 +0300

    Exercise3

---
+++ BruceEckel/gui/Exercise3.java
@@ -0,0 +1,10 @@
+package gui;
+
+import util.*;
+import javax.swing.*;
+public class Exercise3 {
+    public static void main(String[] args) {
+        JFrame frame = new JFrame("Hello Swing");
+        SwingConsole console;
+    }
+}
