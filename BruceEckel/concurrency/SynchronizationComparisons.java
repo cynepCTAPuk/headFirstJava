664c33202c73890f0e5e378a8b924f165974ddd6
cynepCTAPuk <CTAPuk@gmail.com>
2019-02-19 17:49:10 +0300

    SynchronizationComparisons

--- BruceEckel/concurrency/SynchronizationComparisons.java
+++ BruceEckel/concurrency/SynchronizationComparisons.java
@@ -7,6 +7,7 @@ import java.util.concurrent.atomic.*;
 import java.util.concurrent.locks.*;
 import java.util.*;
 import static util.Print.*;
+
 abstract class Accumulator {
     public static long cycles = 5_000L;
     // Number of Modifiers and Readers during each test:
@@ -22,32 +23,23 @@ abstract class Accumulator {
     static {
         // Load the array of random numbers:
         Random rand = new Random(47);
-        for(int i = 0; i < SIZE; i++)
-            preLoaded[i] = rand.nextInt();
+        for(int i = 0; i < SIZE; i++) preLoaded[i] = rand.nextInt();
     }
     public abstract void accumulate();
     public abstract long read();
     private class Modifier implements Runnable {
         public void run() {
-            for(long i = 0; i < cycles; i++)
-                accumulate();
-            try {
-                barrier.await();
-            } catch(Exception e) {
-                throw new RuntimeException(e);
-            }
+            for(long i = 0; i < cycles; i++) accumulate();
+            try { barrier.await();
+            } catch(Exception e) { throw new RuntimeException(e);}
         }
     }
     private class Reader implements Runnable {
         private volatile long value;
         public void run() {
-            for(long i = 0; i < cycles; i++)
-                value = read();
-            try {
-                barrier.await();
-            } catch(Exception e) {
-                throw new RuntimeException(e);
-            }
+            for(long i = 0; i < cycles; i++) value = read();
+            try { barrier.await();
+            } catch(Exception e) { throw new RuntimeException(e);}
         }
     }
     public void timedTest() {
@@ -56,19 +48,18 @@ abstract class Accumulator {
             exec.execute(new Modifier());
             exec.execute(new Reader());
         }
-        try {
-            barrier.await();
-        } catch(Exception e) {
-            throw new RuntimeException(e);
-        }
+        try { barrier.await();
+        } catch(Exception e) { throw new RuntimeException(e);}
         duration = System.nanoTime() - start;
         printf("%-13s: %,15d\n", id, duration);
     }
     public static void
     report(Accumulator acc1, Accumulator acc2) {
-        printf("%-23s: %5.2f\n", acc1.id + "/" + acc2.id, (double)acc1.duration/(double)acc2.duration);
+        printf("%-23s: %5.2f\n", acc1.id + "/" +
+                acc2.id, (double)acc1.duration/(double)acc2.duration);
     }
 }
+
 class BaseLine extends Accumulator {
     { id = "BaseLine"; }
     public void accumulate() {
@@ -77,16 +68,16 @@ class BaseLine extends Accumulator {
     }
     public long read() { return value; }
 }
+
 class SynchronizedTest extends Accumulator {
     { id = "synchronized"; }
     public synchronized void accumulate() {
         value += preLoaded[index++];
         if(index >= SIZE) index = 0;
     }
-    public synchronized long read() {
-        return value;
-    }
+    public synchronized long read() { return value;}
 }
+
 class LockTest extends Accumulator {
     { id = "Lock"; }
     private Lock lock = new ReentrantLock();
@@ -101,11 +92,8 @@ class LockTest extends Accumulator {
     }
     public long read() {
         lock.lock();
-        try {
-            return value;
-        } finally {
-            lock.unlock();
-        }
+        try { return value;
+        } finally { lock.unlock();}
     }
 }
 class AtomicTest extends Accumulator {
@@ -117,34 +105,32 @@ class AtomicTest extends Accumulator {
         // But it still gives us a performance indicator:
         int i = index.getAndIncrement();
         value.getAndAdd(preLoaded[i]);
-        if(++i >= SIZE)
-            index.set(0);
+        if(++i >= SIZE) index.set(0);
     }
     public long read() { return value.get(); }
 }
 public class SynchronizationComparisons {
     static BaseLine baseLine = new BaseLine();
-    static SynchronizedTest synch = new SynchronizedTest();
+    static SynchronizedTest synchro = new SynchronizedTest();
     static LockTest lock = new LockTest();
     static AtomicTest atomic = new AtomicTest();
     static void test() {
         print("==============================");
         printf("%-12s : %,15d\n", "Cycles", Accumulator.cycles);
         baseLine.timedTest();
-        synch.timedTest();
+        synchro.timedTest();
         lock.timedTest();
         atomic.timedTest();
-        Accumulator.report(synch, baseLine);
+        Accumulator.report(synchro, baseLine);
         Accumulator.report(lock, baseLine);
         Accumulator.report(atomic, baseLine);
-        Accumulator.report(synch, lock);
-        Accumulator.report(synch, atomic);
+        Accumulator.report(synchro, lock);
+        Accumulator.report(synchro, atomic);
         Accumulator.report(lock, atomic);
     }
     public static void main(String[] args) {
         int iterations = 5; // Default
-        if(args.length > 0) // Optionally change iterations
-            iterations = Integer.valueOf(args[0]);
+        if(args.length > 0) iterations = Integer.valueOf(args[0]); // Optionally change iterations
         // The first time fills the thread pool:
         print("Warmup");
         baseLine.timedTest();
