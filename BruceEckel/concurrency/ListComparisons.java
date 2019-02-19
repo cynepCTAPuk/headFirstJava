f0f256b8e2e3e73bbe3de459097240409f34f2cb
cynepCTAPuk <CTAPuk@gmail.com>
2019-02-19 18:45:26 +0300

    ListComparisons

--- BruceEckel/concurrency/ListComparisons.java
+++ BruceEckel/concurrency/ListComparisons.java
@@ -57,7 +57,7 @@ public class ListComparisons {
         new SynchronizedArrayListTest(10, 0);
         new SynchronizedArrayListTest(9, 1);
         new SynchronizedArrayListTest(5, 5);
-
+        System.out.println();
         new CopyOnWriteArrayListTest(10, 0);
         new CopyOnWriteArrayListTest(9, 1);
         new CopyOnWriteArrayListTest(5, 5);
