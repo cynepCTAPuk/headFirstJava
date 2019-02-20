b4dd360d45adaf0a62be4a1316b917cd7cca94bc
cynepCTAPuk <CTAPuk@gmail.com>
2019-02-20 14:20:07 +0300

    SwingConsole

--- BruceEckel/util/SwingConsole.java
+++ BruceEckel/util/SwingConsole.java
@@ -4,15 +4,18 @@
 package util;
 
 import javax.swing.*;
+import java.awt.*;
+
 public class SwingConsole {
     public static void run(final JFrame f, final int width, final int height) {
         SwingUtilities.invokeLater(new Runnable() {
             public void run() {
+                Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
                 f.setTitle(f.getClass().getSimpleName());
                 f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
                 f.setSize(width, height);
                 f.setVisible(true);
-                f.setLocation(500,400);
+                f.setLocation((screenSize.width - width)/3,(screenSize.height - height)/3);
             }
         });
     }
