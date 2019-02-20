01630cc732c226a0d1da07612660a357f04b87a2
cynepCTAPuk <CTAPuk@gmail.com>
2019-02-20 14:38:29 +0300

    SwingConsole

--- BruceEckel/util/SwingConsole.java
+++ BruceEckel/util/SwingConsole.java
@@ -10,12 +10,16 @@ public class SwingConsole {
     public static void run(final JFrame f, final int width, final int height) {
         SwingUtilities.invokeLater(new Runnable() {
             public void run() {
-                Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
+                GraphicsDevice gd = GraphicsEnvironment.
+                        getLocalGraphicsEnvironment().getDefaultScreenDevice();
+                int screenWidth = gd.getDisplayMode().getWidth();
+                int screenHeight = gd.getDisplayMode().getHeight();
+
                 f.setTitle(f.getClass().getSimpleName());
                 f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
                 f.setSize(width, height);
+                f.setLocation((screenWidth - width)/3,(screenHeight - height)/3);
                 f.setVisible(true);
-                f.setLocation((screenSize.width - width)/3,(screenSize.height - height)/3);
             }
         });
     }
