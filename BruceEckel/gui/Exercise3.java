80cdac362009015419df1c636320206a59917c12
cynepCTAPuk <CTAPuk@gmail.com>
2019-02-20 13:45:05 +0300

    Exercise 3

--- BruceEckel/gui/Exercise3.java
+++ BruceEckel/gui/Exercise3.java
@@ -1,13 +1,10 @@
 package gui;
 
 import util.*;
+import static util.SwingConsole.*;
 import javax.swing.*;
-public class Exercise3 {
+public class Exercise3 extends JFrame{
     public static void main(String[] args) {
-        JFrame frame = new JFrame("Hello Swing");
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.setSize(300, 100);
-        frame.setLocation(500,400);
-        frame.setVisible(true);
+        run(new JFrame(), 300, 100);
     }
 }
