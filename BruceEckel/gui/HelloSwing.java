e78cef4ce4793bd730300dac7b71fd954727cf3d
cynepCTAPuk <CTAPuk@gmail.com>
2019-02-20 14:36:31 +0300

    HelloSwing

--- BruceEckel/gui/HelloSwing.java
+++ BruceEckel/gui/HelloSwing.java
@@ -7,10 +7,17 @@ public class HelloSwing {
     public static void main(String[] args) {
         JFrame frame = new JFrame("Hello Swing");
         Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
+
+        GraphicsDevice gd = GraphicsEnvironment.getLocalGraphicsEnvironment().getDefaultScreenDevice();
+        int screenWidth = gd.getDisplayMode().getWidth();
+        int screenHeight = gd.getDisplayMode().getHeight();
+
         int frameSizeWidth = 300;
         int frameSizeHeight = 100;
-        int frameLocateX = (screenSize.width - frameSizeWidth)/2;
-        int frameLocateY = (screenSize.height - frameSizeHeight)/2;
+//        int frameLocateX = (screenSize.width - frameSizeWidth)/2;
+//        int frameLocateY = (screenSize.height - frameSizeHeight)/2;
+        int frameLocateX = (screenWidth - frameSizeWidth)/2;
+        int frameLocateY = (screenHeight - frameSizeHeight)/2;
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setSize(frameSizeWidth, frameSizeHeight);
         frame.setLocation(frameLocateX,frameLocateY);
