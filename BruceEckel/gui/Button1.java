3eb42cb757f9dc515058282250a54e1414eeed85
cynepCTAPuk <CTAPuk@gmail.com>
2019-02-20 14:48:50 +0300

    Button1

--- BruceEckel/gui/Button1.java
+++ BruceEckel/gui/Button1.java
@@ -14,6 +14,7 @@ public class Button1 extends JFrame {
         setLayout(new FlowLayout());
         add(b1);
         add(b2);
+        add(new JButton("Button 3"));
     }
     public static void main(String[] args) {
         SwingConsole.run(new Button1(), 200, 100);
