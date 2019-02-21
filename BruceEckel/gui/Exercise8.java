647bc5cb171a763b27fce056e02ab17778717585
cynepCTAPuk <CTAPuk@gmail.com>
2019-02-21 17:52:23 +0300

    Exercise8

--- BruceEckel/gui/Exercise8.java
+++ BruceEckel/gui/Exercise8.java
@@ -1,38 +1,41 @@
-//: strings/TestRegularExpression.java
-// Allows you to easily try out regular expressions.
-// {Args: abcabcabcdefabc "abc+" "(abc)+" "(abc){2,}" }
 package gui;
+
 import util.*;
+
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.*;
 
 public class Exercise8 extends JFrame {
-    private JTextArea jTextArea = new JTextArea(10, 55);
-    private JTextField jTextField = new JTextField(10);
-    private JButton jButton = new JButton("Search");
-    private Component component = new Component() {
-        public void setCursor(Cursor cursor) {
-            super.setCursor(cursor);
-        }
-    };
+    private JTextArea jTextArea = new JTextArea("jTextArea", 10, 55);
+    private JTextField jTextField = new JTextField("jTextField", 10);
+    private JButton jButton = new JButton("jButton");
+    private Cursor cursor = new Cursor(0);
 
     Exercise8() {
         jButton.addActionListener(new ActionListener() {
             public void actionPerformed(ActionEvent e) {
-                String exp = jTextArea.getText();
-                String reg = jTextField.getText();
+                jTextArea.setText(cursor.toString());
+                jTextField.setText(cursor.getName());
             }
+        });
+        jTextArea.addMouseMotionListener(new MouseMotionListener() {
+            public void mouseMoved(MouseEvent e) {
+                final int x = e.getX();
+                final int y = e.getY();
+                // only display a hand if the cursor is over the items
+                jTextArea.setCursor(new Cursor(12));
+            }
+
+            public void mouseDragged(MouseEvent e) {}
         });
 
         jTextArea.setFont(new Font("monospaced", Font.PLAIN, 12));
-
         setLayout(new FlowLayout());
         add(new JScrollPane(jTextArea));
         add(jTextField);
         add(jButton);
     }
-
 
     public static void main(String[] args) {
         SwingConsole.run(new Exercise8(), 410, 500);
