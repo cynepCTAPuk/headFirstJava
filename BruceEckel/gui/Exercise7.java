86b09279700577f9459cf92c603fc708d9c7dc0b
cynepCTAPuk <CTAPuk@gmail.com>
2019-02-21 17:11:26 +0300

    Exercise7

--- BruceEckel/gui/Exercise7.java
+++ BruceEckel/gui/Exercise7.java
@@ -9,46 +9,103 @@ import java.awt.*;
 import java.awt.event.*;
 
 public class Exercise7 extends JFrame {
-    private JButton jButton = new JButton("Clear Input");
-    private JCheckBox jCheckBox = new JCheckBox();
-    private JCheckBoxMenuItem jCheckBoxMenuItem = new JCheckBoxMenuItem();
-    private JMenu jMenu = new JMenu();
-    private JMenuItem jMenuItem = new JMenuItem();
-    private JPopupMenu jPopupMenu = new JPopupMenu();
-    private JRadioButton jRadioButton = new JRadioButton();
-    private JRadioButtonMenuItem jRadioButtonMenuItem = new JRadioButtonMenuItem();
-    private JToggleButton jToggleButton = new JToggleButton();
-    private JToolBar jToolBar = new JToolBar();
+    String[] strings = "ABCDEF".split("");
+    private JButton jButton = new JButton("jButton");
+    private JCheckBox jCheckBox = new JCheckBox("jCheckBox");
+    private JCheckBoxMenuItem jCheckBoxMenuItem = new JCheckBoxMenuItem("jCheckBoxMenuItem");
+    private JComboBox<String> jComboBox = new JComboBox<>(strings);
+    private JFileChooser jFileChooser = new JFileChooser();
+    private JMenu jMenu = new JMenu("jMenu");
+    private JMenuItem jMenuItem = new JMenuItem("jMenuItem");
+    private JRadioButton jRadioButton = new JRadioButton("jRadioButton");
+    private JRadioButtonMenuItem jRadioButtonMenuItem =
+            new JRadioButtonMenuItem("jRadioButtonMenuItem");
+    private JTextField jTextField = new JTextField("jTextField", 55);
+    private JToggleButton jToggleButton = new JToggleButton("jToggleButton");
 
-    private JTextArea jTextArea = new JTextArea(10, 55);
-    private JTextField jTextField = new JTextField(10);
-
+    private JTextArea jTextArea = new JTextArea("jTextArea",20,15);
 
     Exercise7() {
         jButton.addActionListener(new ActionListener() {
             public void actionPerformed(ActionEvent e) {
                 String s = jButton.getClass().getSimpleName();
-                jTextArea.setText(s);
+                jTextField.setText(s);
             }
         });
         jCheckBox.addActionListener(new ActionListener() {
             public void actionPerformed(ActionEvent e) {
                 String s = jCheckBox.getClass().getSimpleName();
-                jTextArea.setText(s);
+                jTextField.setText(s);
+            }
+        });
+        jCheckBoxMenuItem.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                String s = jCheckBoxMenuItem.getClass().getSimpleName();
+                jTextField.setText(s);
+            }
+        });
+        jComboBox.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                String s = jComboBox.getClass().getSimpleName();
+                int i = jComboBox.getSelectedIndex();
+                jTextField.setText(s + " " + i);
+            }
+        });
+        jFileChooser.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                String s = jFileChooser.getClass().getSimpleName();
+                jTextField.setText(s);
+            }
+        });
+        jMenu.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                String s = jMenu.getClass().getSimpleName();
+                jTextField.setText(s);
+            }
+        });
+        jMenuItem.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                String s = jMenuItem.getClass().getSimpleName();
+                jTextField.setText(s);
+            }
+        });
+        jRadioButton.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                String s = jRadioButton.getClass().getSimpleName();
+                jTextField.setText(s);
+            }
+        });
+        jRadioButtonMenuItem.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                String s = jRadioButtonMenuItem.getClass().getSimpleName();
+                jTextField.setText(s);
+            }
+        });
+        jToggleButton.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                String s = jToggleButton.getClass().getSimpleName();
+                jTextField.setText(s);
             }
         });
 
-        jTextArea.setFont(new Font("monospaced", Font.PLAIN, 12));
+        jTextField.setFont(new Font("monospaced", Font.PLAIN, 12));
 
         setLayout(new FlowLayout());
-        add(new JScrollPane(jTextArea));
+
         add(jTextField);
         add(jButton);
         add(jCheckBox);
-
+        add(jCheckBoxMenuItem);
+        add(jComboBox);
+        add(jFileChooser);
+        add(jMenu);
+        add(jMenuItem);
+        add(jRadioButton);
+        add(jRadioButtonMenuItem);
+        add(jToggleButton);
     }
 
     public static void main(String[] args) {
-        SwingConsole.run(new Exercise7(), 410, 500);
+        SwingConsole.run(new Exercise7(), 500, 500);
     }
 }
