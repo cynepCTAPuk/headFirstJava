0040cdc5dee4b3d85e163d2a87bc039031bd339b
cynepCTAPuk <CTAPuk@gmail.com>
2019-02-22 16:38:43 +0300

    Exercise14

---
+++ BruceEckel/gui/Exercise14.java
@@ -0,0 +1,29 @@
+//: gui/TextPane.java
+// The JTextPane control is a little editor.
+package gui;
+import util.*;
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.*;
+import static util.SwingConsole.*;
+
+public class Exercise14 extends JFrame {
+    private JButton b = new JButton("Add Text");
+    private JTextArea ta = new JTextArea();
+    private static Generator sg = new RandomGenerator.String(7);
+    public Exercise14() {
+        b.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                for(int i = 0; i < 3; i++) {
+//                    ta.setText(ta.getText() + sg.next() + "\n");
+                    ta.append(sg.next() + "\n");
+                }
+            }
+        });
+        add(new JScrollPane(ta));
+        add(BorderLayout.SOUTH, b);
+    }
+    public static void main(String[] args) {
+        run(new Exercise14(), 475, 425);
+    }
+} ///:~
