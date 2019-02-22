80258e6a8265ca7120bb16860b45ec96faeaa973
cynepCTAPuk <CTAPuk@gmail.com>
2019-02-22 16:27:47 +0300

    Borders

--- BruceEckel/gui/Borders.java
+++ BruceEckel/gui/Borders.java
@@ -5,7 +5,9 @@ import javax.swing.*;
 import javax.swing.border.*;
 import java.awt.*;
 import static util.SwingConsole.*;
+
 public class Borders extends JFrame {
+
     static JPanel showBorder(Border b) {
         JPanel jp = new JPanel();
         jp.setLayout(new BorderLayout());
@@ -15,16 +17,18 @@ public class Borders extends JFrame {
         jp.setBorder(b);
         return jp;
     }
+
     public Borders() {
-        setLayout(new GridLayout(2,4));
+        setLayout(new GridLayout(2, 4));
         add(showBorder(new TitledBorder("Title")));
         add(showBorder(new EtchedBorder()));
         add(showBorder(new LineBorder(Color.BLUE)));
-        add(showBorder( new MatteBorder(5,5,30,30,Color.GREEN)));
-        add(showBorder( new BevelBorder(BevelBorder.RAISED)));
-        add(showBorder( new SoftBevelBorder(BevelBorder.LOWERED)));
-        add(showBorder(new CompoundBorder( new EtchedBorder(), new LineBorder(Color.RED))));
+        add(showBorder(new MatteBorder(5, 10, 15, 20, Color.GREEN)));
+        add(showBorder(new BevelBorder(BevelBorder.RAISED)));
+        add(showBorder(new SoftBevelBorder(BevelBorder.LOWERED)));
+        add(showBorder(new CompoundBorder(new EtchedBorder(), new LineBorder(Color.RED))));
     }
+
     public static void main(String[] args) {
         run(new Borders(), 500, 300);
     }
