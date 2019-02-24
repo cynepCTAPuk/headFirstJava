b586956e8b010b1ea3b8d7fb30bb3931d73cdf03
cynepCTAPuk <CTAPuk@gmail.com>
2019-02-24 16:21:37 +0300

    SineDraw

--- BruceEckel/gui/SineWave.java
+++ BruceEckel/gui/SineWave.java
@@ -4,27 +4,30 @@ package gui;
 import javax.swing.*;
 import javax.swing.event.*;
 import java.awt.*;
+import java.util.*;
+
 import static util.SwingConsole.*;
 class SineDraw extends JPanel {
-    private static final int SCALEFACTOR = 200;
+    private static final int SCALEFACTOR = 1_000;
     private int cycles;
     private int points;
     private double[] sines;
     private int[] pts;
-    public SineDraw() { setCycles(5); }
+    public SineDraw() { setCycles(1);}
     public void paintComponent(Graphics g) {
         super.paintComponent(g);
         int maxWidth = getWidth();
-        double hstep = (double)maxWidth / (double)points;
+        double hstep = (double) maxWidth / (double) points;
         int maxHeight = getHeight();
         pts = new int[points];
-        for(int i = 0; i < points; i++)
-            pts[i] = (int)(sines[i] * maxHeight/2 * .95 + maxHeight/2);
+        for (int i = 0; i < points; i++)
+            pts[i] = (int) (maxHeight / 2 - sines[i] * maxHeight / 2 * .95);
         g.setColor(Color.RED);
-        for(int i = 1; i < points; i++) {
-            int x1 = (int)((i - 1) * hstep);
-            int x2 = (int)(i * hstep);
-            int y1 = pts[i-1];
+
+        for (int i = 1; i < points; i++) {
+            int x1 = (int) ((i - 1) * hstep);
+            int x2 = (int) (i * hstep);
+            int y1 = pts[i - 1];
             int y2 = pts[i];
             g.drawLine(x1, y1, x2, y2);
         }
@@ -33,26 +36,29 @@ class SineDraw extends JPanel {
         cycles = newCycles;
         points = SCALEFACTOR * cycles * 2;
         sines = new double[points];
-        for(int i = 0; i < points; i++) {
+        for (int i = 0; i < points; i++) {
             double radians = (Math.PI / SCALEFACTOR) * i;
             sines[i] = Math.sin(radians);
         }
         repaint();
     }
 }
+
 public class SineWave extends JFrame {
     private SineDraw sines = new SineDraw();
-    private JSlider adjustCycles = new JSlider(1, 30, 5);
+    private JSlider adjustCycles = new JSlider(1, 30, 1);
+
     public SineWave() {
         add(sines);
         adjustCycles.addChangeListener(new ChangeListener() {
             public void stateChanged(ChangeEvent e) {
                 sines.setCycles(
-                        ((JSlider)e.getSource()).getValue());
+                        ((JSlider) e.getSource()).getValue());
             }
         });
         add(BorderLayout.SOUTH, adjustCycles);
     }
+
     public static void main(String[] args) {
         run(new SineWave(), 700, 400);
     }
