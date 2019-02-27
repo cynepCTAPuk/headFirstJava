03d65ed984e55a32ba2db62591c3a9a591f8ccfb
cynepCTAPuk <CTAPuk@gmail.com>
2019-02-27 22:57:20 +0300

    Change

--- java_puzzlers/02_expressive_puzzlers/puzzle_1/Oddity.java
+++ java_puzzlers/02_expressive_puzzlers/puzzle_1/Oddity.java
@@ -2,11 +2,13 @@ package puzzle_1;
 
 public class Oddity {
     public static boolean isOdd(int i) {
-        return i % 2 == 1;
+//        return (i < 0 ? -i : i) % 2 == 1;
+//        return i % 2 != 0;
+        return (i & 1) != 0;
     }
 
     public static void main(String[] args) {
-        int number = 5;
-        System.out.println("number " + number + " is odd?\n" +  isOdd(5));
+        int number = -5;
+        System.out.println("number " + number + " is odd?\n" + isOdd(number));
     }
 }
--- java_puzzlers/02_expressive_puzzlers/puzzle_2/Change.java
+++ java_puzzlers/02_expressive_puzzlers/puzzle_2/Change.java
@@ -1,12 +1,8 @@
+package puzzle_2;
+
 public class Change {
     public static void main(String args[]) {
         System.out.println(2.00 - 1.10);
-
-        double a = 2.00;
-        double b = 1.10;
-        System.out.println( (a - b));
-
         System.out.println((float)(2.00 - 1.10));
-        System.out.println((float)(a - b));
     }
 }
