e21867c3aa7af0a805ffcd662df6db5186ad35dc
cynepCTAPuk <CTAPuk@gmail.com>
2019-02-27 23:08:56 +0300

    Change

--- java_puzzlers/02_expressive_puzzlers/puzzle_2/Change.java
+++ java_puzzlers/02_expressive_puzzlers/puzzle_2/Change.java
@@ -1,8 +1,15 @@
 package puzzle_2;
 
+import java.math.*;
+
 public class Change {
     public static void main(String args[]) {
         System.out.println(2.00 - 1.10);
-        System.out.println((float)(2.00 - 1.10));
+        // Poor solution - still uses binary floating-point!
+        System.out.printf("%.2f\n", 2.00 - 1.10);
+
+        System.out.println((200 -110) + " cents");
+
+        System.out.println(new BigDecimal("2.00").subtract(new BigDecimal("1.10")));
     }
 }
