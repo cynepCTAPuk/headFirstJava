f9a4f4873613aee88406bde2f6572fdc7e4003cd
cynepCTAPuk <CTAPuk@gmail.com>
2019-02-28 19:20:00 +0300

    CleverSwap

--- java_puzzlers/02_expressive_puzzlers/puzzle_7/CleverSwap1.java
+++ java_puzzlers/02_expressive_puzzlers/puzzle_7/CleverSwap1.java
@@ -1,6 +1,6 @@
 package puzzle_7;
 
-public class CleverSwap {
+public class CleverSwap1 {
     public static void main(String[] args) {
         int x = 1984;
         System.out.println("x = " + Integer.toHexString(x));
@@ -9,7 +9,7 @@ public class CleverSwap {
         System.out.println("y = " + Integer.toHexString(y));
         System.out.println("y = " + Integer.toBinaryString(y));
         System.out.println();
-
+        
         x ^= y ^= x ^ y;
         System.out.println("x = " + x + "; y = " + y);
 
