ad4024d7c066b05527ed163ed099a388944afc53
cynepCTAPuk <CTAPuk@gmail.com>
2019-02-28 20:28:09 +0300

    Tweedledum

--- java_puzzlers/02_expressive_puzzlers/puzzle_9/Tweedledum.java
+++ java_puzzlers/02_expressive_puzzlers/puzzle_9/Tweedledum.java
@@ -1,5 +1,9 @@
 package puzzle_9;
 
+/*
+ To avoid unpleasant surprises, do not use compound assignment operators
+ on variables of type byte, short, or char.
+ */
 public class Tweedledum {
     public static void main(String[] args) {
         // Put your declarations for x and i here
