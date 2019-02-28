a23d7239293dc4a79622b12e562d8107c8e4ee12
cynepCTAPuk <CTAPuk@gmail.com>
2019-02-28 21:41:32 +0300

    AnimalFarm

--- java_puzzlers/03_puzzlers_with_character/puzzle_13/AnimalFarm.java
+++ java_puzzlers/03_puzzlers_with_character/puzzle_13/AnimalFarm.java
@@ -5,7 +5,8 @@ public class AnimalFarm {
         final String pig = "length: 10";
         final String dog = "length: " + pig.length();
         System.out.println("Animals are equal: " + pig == dog);
-        
+
+        System.out.println("Animals are equal: " + (pig == dog));
         System.out.println("Animals are equal: " + pig.equals(dog));
     }
 }
