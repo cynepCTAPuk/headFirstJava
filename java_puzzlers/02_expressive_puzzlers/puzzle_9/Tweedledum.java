2309f81076b691677be7a9a9a8e0f7ff4d9ab6e4
cynepCTAPuk <CTAPuk@gmail.com>
2019-02-28 20:21:37 +0300

    Tweedledum

--- java_puzzlers/02_expressive_puzzlers/puzzle_9/Tweedledum.java
+++ java_puzzlers/02_expressive_puzzlers/puzzle_9/Tweedledum.java
@@ -3,11 +3,9 @@ package puzzle_9;
 public class Tweedledum {
     public static void main(String[] args) {
         // Put your declarations for x and i here
-        byte x = 7; //  vk
-        byte i = x; //  vk
-
+        short x = 7;
+        byte i = 7;
         x += i;     // Must be LEGAL
 //        x = x + i;  // Must be ILLEGAL
-        System.out.println(x + " "+i);  //  vk
     }
 }
