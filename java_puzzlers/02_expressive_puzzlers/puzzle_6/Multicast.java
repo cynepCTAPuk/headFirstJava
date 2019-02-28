2c8483c0876450e58075364cd402390430a9322c
cynepCTAPuk <CTAPuk@gmail.com>
2019-02-28 16:29:14 +0300

    Multicast

--- java_puzzlers/02_expressive_puzzlers/puzzle_6/Multicast.java
+++ java_puzzlers/02_expressive_puzzlers/puzzle_6/Multicast.java
@@ -1,11 +1,15 @@
+package puzzle_6;
+
 public class Multicast {
     public static void main(String[] args) {
         System.out.println((int) (char) (byte) -1);
+        System.out.println((byte) -1);
+        System.out.println((char) (byte) -1);
 
+        System.out.println();
+        System.out.println(Integer.toBinaryString(-1));
         System.out.println(Integer.toBinaryString((byte) -1));
         System.out.println(Integer.toBinaryString((char) (byte) -1));
         System.out.println(Integer.toBinaryString((int) (char) (byte) -1));
-        System.out.println(Integer.toBinaryString(65535));
-        System.out.println((1 << 16) - 1);
     }
 }
