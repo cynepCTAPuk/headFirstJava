2dbe178c8653f666dd118bea901c165e613583bf
cynepCTAPuk <CTAPuk@gmail.com>
2019-02-28 20:38:36 +0300

    Tweedledee

--- java_puzzlers/02_expressive_puzzlers/puzzle_10/Tweedledee.java
+++ java_puzzlers/02_expressive_puzzlers/puzzle_10/Tweedledee.java
@@ -1,15 +1,11 @@
+package puzzle_10;
+
 public class Tweedledee {
     public static void main(String[] args) {
         // Put your declarations for x and i here
-        int x = 1; // vk
-        char i = 'a'; // vk
-
-        System.out.format("char i = 'a';\t-> %3d%n", (int)i);
-
+        Object x = "Buy ";
+        String i = "Effective Java!";
         x = x + i;  // Must be LEGAL
-        System.out.format("x = x + i;\t\t-> %3d%n", x);
-
         x += i;     // Must be ILLEGAL
-        System.out.format("x += i;\t\t\t-> %3d%n", x);
     }
 }
