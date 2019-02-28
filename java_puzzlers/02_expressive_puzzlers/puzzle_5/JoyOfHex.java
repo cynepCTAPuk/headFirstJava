d34b5501455e6837a79a4e7beedb5730094cc380
cynepCTAPuk <CTAPuk@gmail.com>
2019-02-28 16:22:18 +0300

    JoyOfHex

--- java_puzzlers/02_expressive_puzzlers/puzzle_5/JoyOfHex.java
+++ java_puzzlers/02_expressive_puzzlers/puzzle_5/JoyOfHex.java
@@ -2,7 +2,8 @@ package puzzle_5;
 
 public class JoyOfHex {
     public static void main(String[] args) {
-        System.out.println(
-            Long.toHexString(0x100000000L + 0xcafebabe));
+        System.out.println( Long.toHexString(0x100000000L + 0xcafebabe));
+        
+        System.out.println( Long.toHexString(0x100000000L + 0xcafebabeL));
     }
 }
