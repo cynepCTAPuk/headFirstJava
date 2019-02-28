ff4fda0e297b198e6b4bb8c73906cf3eed79bba6
cynepCTAPuk <CTAPuk@gmail.com>
2019-02-28 20:50:41 +0300

    LastLaugh

--- java_puzzlers/03_puzzlers_with_character/puzzle_11/LastLaugh.java
+++ java_puzzlers/03_puzzlers_with_character/puzzle_11/LastLaugh.java
@@ -3,10 +3,9 @@ package puzzle_11;
 public class LastLaugh {
     public static void main(String args[]) {
         System.out.print("H" + "a");
-        System.out.print('H' + 'a');
-        System.out.println();
-        System.out.println(Integer.toBinaryString('H') + " " + (int) 'H');
-        System.out.println(Integer.toBinaryString('a') + " " + (int) 'a');
-        System.out.println(72 + 97);
+        System.out.println('H' + 'a');
+        System.out.println("" + 'H' + 'a');
+        System.out.println("2 + 2 = " + 2 + 2);
+        System.out.printf("%c%c",'H','a');
     }
 }
