23fbc4bfcc7aa0f40568fc92db00c5464c5e3e38
cynepCTAPuk <CTAPuk@gmail.com>
2019-02-28 22:08:30 +0300

    LinePrinter

--- java_puzzlers/03_puzzlers_with_character/puzzle_16/LinePrinter.java
+++ java_puzzlers/03_puzzlers_with_character/puzzle_16/LinePrinter.java
@@ -1,11 +1,10 @@
+package puzzle_16;
+
 public class LinePrinter {
   public static void main(String[] args) {
-//     Note: \\u000A is Unicode;
-// representation of linefeed (LF)
-    char c = 0x000A;
-    System.out.print(1);
-    System.out.print(c);
-    System.out.print(2);
-    System.out.print(c);
+    // Note: \\u000A is Unicode representation of linefeed (LF)
+//    char c = 0x000A;
+    char c = '\n';
+    System.out.println(c);
   }
 }
