5cec60761bf17469c049fade7bf7e1572c676e63
cynepCTAPuk <CTAPuk@gmail.com>
2019-02-28 21:03:00 +0300

    Abc

--- java_puzzlers/03_puzzlers_with_character/puzzle_11/LastLaugh.java
+++ java_puzzlers/03_puzzlers_with_character/puzzle_11/LastLaugh.java
@@ -4,6 +4,7 @@ public class LastLaugh {
     public static void main(String args[]) {
         System.out.print("H" + "a");
         System.out.println('H' + 'a');
+
         System.out.println("" + 'H' + 'a');
         System.out.println("2 + 2 = " + 2 + 2);
         System.out.printf("%c%c",'H','a');
--- java_puzzlers/03_puzzlers_with_character/puzzle_12/Abc.java
+++ java_puzzlers/03_puzzlers_with_character/puzzle_12/Abc.java
@@ -1,7 +1,14 @@
+package puzzle_12;
+
 public class Abc {
     public static void main(String[] args) {
         String letters = "ABC";
         char[] numbers = { '1', '2', '3' };
         System.out.println(letters + " easy as " + numbers);
+
+        System.out.println(letters + " easy as " + String.valueOf(numbers));
+
+        System.out.print(letters + " easy as ");
+        System.out.println(numbers);
     }
 }
