8526d5a9b253189f0af5baa1a7a5f4b8a71671ca
cynepCTAPuk <CTAPuk@gmail.com>
2019-03-01 12:12:04 +0300

    StringCheese

--- java_puzzlers/03_puzzlers_with_character/puzzle_18/StringCheese.java
+++ java_puzzlers/03_puzzlers_with_character/puzzle_18/StringCheese.java
@@ -1,19 +1,12 @@
+package puzzle_18;
+
 public class StringCheese {
-    public static void main(String args[]) {
-        byte bytes[] = new byte[256];
-
-        for(int i = 0; i < 256; i++) {
-            bytes[i] = (byte)i;
-
-            System.out.print((char)i);
-            if(i%40==0) System.out.println();
-        }
-
-        System.out.println();
-        System.out.println();
+    public static void main(String[] args) {
+        byte[] bytes = new byte[256];
+        for (int i = 0; i < 256; i++)
+            bytes[i] = (byte) i;
         String str = new String(bytes);
-
-        for(int i = 0, n = str.length(); i < n; i++)
-            System.out.print((int)str.charAt(i) + " ");
+        for (int i = 0, n = str.length(); i < n; i++)
+            System.out.print((int) str.charAt(i) + " ");
     }
 }
