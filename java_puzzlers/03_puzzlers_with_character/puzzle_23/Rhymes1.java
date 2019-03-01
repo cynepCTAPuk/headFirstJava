ca33f02202d6295840b4bc8cf37cddbf6a52e2d0
cynepCTAPuk <CTAPuk@gmail.com>
2019-03-01 13:55:22 +0300

    Rhymes1

---
+++ java_puzzlers/03_puzzlers_with_character/puzzle_23/Rhymes1.java
@@ -0,0 +1,9 @@
+package puzzle_23;
+import java.util.*;
+
+public class Rhymes1 {
+    private static Random rnd = new Random();
+    public static void main(String args[]) {
+        System.out.println("PGM".charAt(rnd.nextInt(3)) + "ain");
+    }
+}
