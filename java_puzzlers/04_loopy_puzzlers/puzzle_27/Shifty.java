5b114b244733591673fc32973e0972938ba53bfa
cynepCTAPuk <CTAPuk@gmail.com>
2019-03-01 20:46:55 +0300

    Looper

--- java_puzzlers/04_loopy_puzzlers/puzzle_27/Shifty.java
+++ java_puzzlers/04_loopy_puzzlers/puzzle_27/Shifty.java
@@ -1,18 +1,17 @@
+package puzzle_27;
+
 public class Shifty {
     public static void main(String[] args) {
         int i = 0;
-        int count = 0;
-        System.out.format("%12s   %5s %2s %1s%n","number","true","count","i");
-        while ((-1 << i) != 0) {
-            System.out.format("%15d", -1 << i);
-            System.out.print(" ");
-            System.out.format("%5s", i != 0);
-            System.out.print(" ");
-            i++;
-            count++;
-            System.out.format("(%2d -%3d)%n", count, i);
-            if (count > 33) break;
-//            if (count % 10 == 0) System.out.println();
-        }
+//        while (-1 << i != 0) i++;
+//        System.out.println(i);
+
+        int intDistance = 0;
+        for (int val = -1; val != 0; val <<= 1) intDistance++;
+        System.out.println(intDistance);
+
+        int longDistance = 0;
+        for (long val = -1; val != 0; val <<= 1) longDistance++;
+        System.out.println(longDistance);
     }
 }
--- java_puzzlers/04_loopy_puzzlers/puzzle_28/Looper.java
+++ java_puzzlers/04_loopy_puzzlers/puzzle_28/Looper.java
@@ -1,10 +1,14 @@
+package puzzle_28;
+
 public class Looper {
     public static void main(String[] args) {
         // Place your declaration for i here
-        double i = 0;
+//        double i = 1.0 / 0.0;
+//        double i = Double.POSITIVE_INFINITY;
+        double i = 1.0e40;
+        System.out.println(i);
         while (i == i + 1) {
-            System.out.println(i == i + 1);
-            break;
         }
+//        In summary, it is possible to represent infinity as a double or a float.
     }
 }
