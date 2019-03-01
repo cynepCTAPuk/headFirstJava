be98ff63127f38ba9029da953ce29baf5227a8bc
cynepCTAPuk <CTAPuk@gmail.com>
2019-03-01 13:19:50 +0300

    MeToo

--- java_puzzlers/03_puzzlers_with_character/puzzle_21/com/javapuzzlers/MeToo.java
+++ java_puzzlers/03_puzzlers_with_character/puzzle_21/com/javapuzzlers/MeToo.java
@@ -1,12 +1,16 @@
-package com.javapuzzlers;
+package puzzle_21.com.javapuzzlers;
 
 import java.io.*;
+import java.util.regex.*;
 
 public class MeToo {
     public static void main(String[] args) {
-//    System.out.println(MeToo.class.getName().replaceAll("\\.", File.separator) + ".class");
-        System.out.println(MeToo.class.getName()
-                .replaceAll("\\.", File.separator + "\\") + ".class");
-        System.out.println(File.separator);
+//        System.out.println(MeToo.class.getName(). replaceAll("\\.", File.separator) + ".class");
+
+        System.out.println(MeToo.class.getName().replaceAll(
+                "\\.", Matcher.quoteReplacement(File.separator)) + ".class");
+
+        System.out.println(MeToo.class.getName().replaceAll(".", File.separator) + ".class");
+        System.out.println(MeToo.class.getName().replace('.', File.separatorChar) + ".class");
     }
 }
