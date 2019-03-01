48384b2cd7ae4a535d7f673e78717cb9f156536c
cynepCTAPuk <CTAPuk@gmail.com>
2019-03-01 16:55:27 +0300

    BigDelight

--- java_puzzlers/04_loopy_puzzlers/puzzle_24/BigDelight.java
+++ java_puzzlers/04_loopy_puzzlers/puzzle_24/BigDelight.java
@@ -1,8 +1,20 @@
+package puzzle_24;
 class BigDelight {
     public static void main(String[] args) {
         for (byte b = Byte.MIN_VALUE; b < Byte.MAX_VALUE; b++) {
-            if (b == 0x90)
-                System.out.print("Joy!");
+            if (b == 0x90) System.out.print("Joy!");
         }
+
+        for (byte b = Byte.MIN_VALUE; b < Byte.MAX_VALUE; b++) {
+            if (b == (byte) 0x90) System.out.print("Joy!");
+        }
+        System.out.println();
+        for (byte b = Byte.MIN_VALUE; b < Byte.MAX_VALUE; b++) {
+            if ((b & 0xff) == 0x90) System.out.print("Joy!");
+        }
+        System.out.println();
+        for (byte b = Byte.MIN_VALUE; b < Byte.MAX_VALUE; b++)
+            if (b == TARGET) System.out.print("Joy!");
     }
+    private static final byte TARGET = (byte) 0x90; // Broken!
 }
