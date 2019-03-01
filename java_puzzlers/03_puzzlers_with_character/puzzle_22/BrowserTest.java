e49873e29a816cb9c2b5ae32714cc1d307985191
cynepCTAPuk <CTAPuk@gmail.com>
2019-03-01 13:29:56 +0300

    BrowserTest

--- java_puzzlers/03_puzzlers_with_character/puzzle_22/BrowserTest.java
+++ java_puzzlers/03_puzzlers_with_character/puzzle_22/BrowserTest.java
@@ -1,42 +1,9 @@
-import java.awt.*;
-import java.io.*;
-import java.net.*;
+package puzzle_22;
 
 public class BrowserTest {
-    public static void main(String[] args) throws URISyntaxException, IOException {
+    public static void main(String[] args) {
         System.out.print("iexplore:");
-        String url_1 = "http://www.example.com";
-        String url_2 = "http://www.google.com";
+        http://www.google.com;
         System.out.println(":maximize");
-
-/*
-        if (Desktop.isDesktopSupported() && Desktop.getDesktop()
-                .isSupported(Desktop.Action.BROWSE)) {
-            Desktop.getDesktop().browse(new URI(url_1));
-        }
-*/
-
-/*
-        if (Desktop.isDesktopSupported()) {
-            Desktop desktop = Desktop.getDesktop();
-            try {
-                desktop.browse(new URI(url_2));
-            } catch (IOException | URISyntaxException e) {
-                e.printStackTrace();
-            }
-        } else {
-            Runtime runtime = Runtime.getRuntime();
-            try {
-                runtime.exec("xdg-open " + url_2);
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
-        }
-*/
-
-/*
-        Desktop desktop = Desktop.getDesktop();
-        desktop.browse(URI.create("chrome"));
-*/
     }
 }
