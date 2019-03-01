0685de0a19a56452b0f91731def92a47e59bd6bc
cynepCTAPuk <CTAPuk@gmail.com>
2019-03-01 13:07:55 +0300

    Me

--- java_puzzlers/03_puzzlers_with_character/puzzle_20/com/javapuzzlers/Me.java
+++ java_puzzlers/03_puzzlers_with_character/puzzle_20/com/javapuzzlers/Me.java
@@ -1,11 +1,15 @@
-package com.javapuzzlers;
+package puzzle_20.com.javapuzzlers;
+
+import java.util.regex.*;
 
 public class Me {
-   public static void main(String[] args) {
-      System.out.println(
-              Me.class.getName().replaceAll(".", "/") + ".class");
-      System.out.println(Me.class.getName());
+    public static void main(String[] args) {
+        System.out.println(Me.class.getName().replaceAll(
+                ".", "/") + ".class");
 
-      System.out.println("Never Say Never Again".replaceAll("e","^"));
-   }
+        System.out.println(Me.class.getName().replaceAll(
+                "\\.", "/") + ".class");
+        System.out.println(Me.class.getName().replaceAll(
+                Pattern.quote("."), "/") + ".class");
+    }
 }
