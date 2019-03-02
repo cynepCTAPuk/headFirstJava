d8fb03997f17bf900a941bfffa1d396e30164a2f
cynepCTAPuk <CTAPuk@gmail.com>
2019-03-02 23:53:45 +0300

    GhostOfLooper

--- java_puzzlers/04_loopy_puzzlers/puzzle_31/GhostOfLooper.java
+++ java_puzzlers/04_loopy_puzzlers/puzzle_31/GhostOfLooper.java
@@ -4,11 +4,11 @@ public class GhostOfLooper {
     public static void main(String[] args) {
         // Place your declaration for i here
 //        byte i = -1;
-        short i = -1;
+        short i = -2;
+        
         while (i != 0) {
             i >>>= 1;
-            System.out.printf("(%,d) ", i);
-//            break;
+            System.out.printf("(%d) ", i);
         }
     }
 }
