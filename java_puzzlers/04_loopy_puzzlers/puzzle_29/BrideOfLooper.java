b164d6cfc7ec7ef98ca75a87ea9ed6f8e97f80fc
cynepCTAPuk <CTAPuk@gmail.com>
2019-03-02 21:46:44 +0300

    BrideOfLooper

--- java_puzzlers/04_loopy_puzzlers/puzzle_29/BrideOfLooper.java
+++ java_puzzlers/04_loopy_puzzlers/puzzle_29/BrideOfLooper.java
@@ -3,7 +3,9 @@ package puzzle_29;
 public class BrideOfLooper {
     public static void main(String[] args) {
         // Place your declaration for i here
-        double i = 0.00/0.00;
+        // Any floating-point operation evaluates to NaN if one or more of its operands are NaN.
+//        double i = 0.00/0.00;
+        double i = Double.NaN;
         while (i != i) {
             System.out.println("Got it!");
             break;
