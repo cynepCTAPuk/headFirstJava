1ee32ad0fe9c487d767ea2a397874cb61dab906a
cynepCTAPuk <CTAPuk@gmail.com>
2019-03-02 21:50:27 +0300

    SonOfLooper

--- java_puzzlers/04_loopy_puzzlers/puzzle_30/SonOfLooper.java
+++ java_puzzlers/04_loopy_puzzlers/puzzle_30/SonOfLooper.java
@@ -1,8 +1,11 @@
+package puzzle_30;
+
 public class SonOfLooper {
     public static void main(String[] args) {
         // Place your declaration for i here
-        int i = 2;
+        String i = "Buy seventeen copies of Effective Java!";
         while (i != i + 0) {
+            System.out.println("Got it!");
             break;
         }
     }
