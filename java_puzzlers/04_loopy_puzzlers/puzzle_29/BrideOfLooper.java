9f25d26823f4bb068052088084b39892809bdc43
cynepCTAPuk <CTAPuk@gmail.com>
2019-03-02 21:38:27 +0300

    BrideOfLooper

--- java_puzzlers/04_loopy_puzzlers/puzzle_29/BrideOfLooper.java
+++ java_puzzlers/04_loopy_puzzlers/puzzle_29/BrideOfLooper.java
@@ -1,8 +1,11 @@
+package puzzle_29;
+
 public class BrideOfLooper {
     public static void main(String[] args) {
         // Place your declaration for i here
-        int i = 2;
+        double i = 0.00/0.00;
         while (i != i) {
+            System.out.println("Got it!");
             break;
         }
     }
