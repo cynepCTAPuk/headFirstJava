d4e44cd14b562648a3ddaf66c1625f1aaf7d6b66
cynepCTAPuk <CTAPuk@gmail.com>
2019-03-03 20:40:12 +0300

    Thing

--- java_puzzlers/06_classy_puzzlers/puzzle_53/MyThing.java
+++ java_puzzlers/06_classy_puzzlers/puzzle_53/MyThing.java
@@ -1,13 +1,14 @@
 package puzzle_53;
 
 public class MyThing extends Thing {
-    private final int arg;
+//    private final int arg;
 
     /*
      * This constructor is illegal. Rewrite it so that it has the same
      * effect but is legal.
      */
     public MyThing() {
-        super(arg = (int) System.currentTimeMillis());
+        super();
+//        super(arg = (int) System.currentTimeMillis());
     }
 }
