2aab0bde55a8fcf9543b39728f6eb3bc58d0f947
cynepCTAPuk <CTAPuk@gmail.com>
2019-03-03 17:55:12 +0300

    HelloGoodbye1

---
+++ java_puzzlers/05_exceptional_puzzlers/puzzle_39/HelloGoodbye1.java
@@ -0,0 +1,14 @@
+package puzzle_39;
+
+public class HelloGoodbye1 {
+    public static void main(String[] args) {
+        System.out.println("Hello world");
+        Runtime.getRuntime().addShutdownHook(
+                new Thread() {
+                    public void run() {
+                        System.out.println("Goodbye world");
+                    }
+                });
+        System.exit(0);
+    }
+}
