e9dee3bbfb3a2fa9e70f4624352dc6dc0107267c
cynepCTAPuk <CTAPuk@gmail.com>
2019-03-03 18:54:14 +0300

    Loop2

---
+++ java_puzzlers/05_exceptional_puzzlers/puzzle_42/Loop2.java
@@ -0,0 +1,16 @@
+package puzzle_42;
+
+public class Loop2 {
+    public static void main(String[] args) {
+        int[][] tests = {{6, 5, 4, 3, 2, 1}, {1, 2}, {1, 2, 3}, {1, 2, 3, 4}, {1}};
+        int successCount = 0;
+        for (int[] test : tests)
+            if (thirdElementIsThree(test))
+                successCount++;
+        System.out.println(successCount);
+    }
+
+    private static boolean thirdElementIsThree(int[] a) {
+        return a.length >= 3 && a[2] == 3;
+    }
+}
