71c9b6a93f8986c46c6be8220ad44219c8c1eaae
cynepCTAPuk <CTAPuk@gmail.com>
2019-03-03 18:24:58 +0300

    NewCopy

---
+++ java_puzzlers/05_exceptional_puzzlers/puzzle_41/NewCopy.java
@@ -0,0 +1,22 @@
+package puzzle_41;
+
+import java.io.*;
+
+public class NewCopy {
+    static void copy(String src, String dest) throws IOException {
+        try (FileInputStream in = new FileInputStream(src)) {
+            try (FileOutputStream out = new FileOutputStream(dest)) {
+                byte[] buf = new byte[1024];
+                int n;
+                while ((n = in.read(buf)) > 0) out.write(buf, 0, n);
+//                out.write(new byte[]{Byte.valueOf(" "), 66}, 0, 2);
+            }
+        }
+    }
+
+    public static void main(String[] args) throws IOException {
+//        if (args.length != 2) System.out.println("Usage: java Copy <source> <dest>");
+//        else copy(args[0], args[1]);
+        copy("c:/000/content.txt", "c:/000/content2.txt");
+    }
+}
