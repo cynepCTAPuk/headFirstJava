22ddd4305fa012ecf69094b714cf906138c54887
cynepCTAPuk <CTAPuk@gmail.com>
2019-03-03 12:14:11 +0300

    Count

--- java_puzzlers/04_loopy_puzzlers/puzzle_34/Count.java
+++ java_puzzlers/04_loopy_puzzlers/puzzle_34/Count.java
@@ -1,9 +1,18 @@
+package puzzle_34;
+
 public class Count {
     public static void main(String[] args) {
         final int START = 2_000_000_000;
         int count = 0;
-        for (float f = START; f < START + 50; f++)
+        for (float f = START; f < START + 50; f++) {
             count++;
+            System.out.print(count + " ");
+        }
+        System.out.printf("%,d\n", START);
+        System.out.printf("%,d\n", Integer.MAX_VALUE);
         System.out.println(count);
+
+        float e = START;
+        System.out.println(e == e + 50);
     }
 }
