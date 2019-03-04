8343ccf066954d25984ce6bc3f61e7fae27a6736
cynepCTAPuk <CTAPuk@gmail.com>
2019-03-04 15:14:38 +0300

    DecimalFormatDemo

--- test/DecimalFormatDemo.java
+++ test/DecimalFormatDemo.java
@@ -5,17 +5,17 @@ public class DecimalFormatDemo {
     static public void customFormat(String pattern, double value) {
         DecimalFormat myFormatter = new DecimalFormat(pattern);
         String output = myFormatter.format(value);
-        System.out.println(value + "\t\t" + pattern + "  " + output);
+        System.out.printf("%,12.4f %12s %12s\n", value, pattern, output);
     }
 
     static public void main(String[] args) {
-        customFormat("000000.000", 12345.67);
-        customFormat("###.##", 123456.789);
-        customFormat("###,###.###", 123456.789);
-        customFormat("$###,###.###", 12345.67);
+        customFormat(" 000,000.000", 123456.789);
+        customFormat(" ###,###.###", 123456.789);
+        customFormat("$###,###.###", 123456.789);
+        System.out.println();
 
-        System.out.println(new DecimalFormat("###,###.##")
-                .format(1234567.890)+" \tDecimalFormat");
-        System.out.format("123456.789", "###,###.##");
+        double f = 123456.789;
+        String ff = new DecimalFormat("###,###.###").format(f) + "\tDecimalFormat";
+        System.out.println(ff);
     }
 }
