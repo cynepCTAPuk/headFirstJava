6b8fef2e6440226e2a9e22461371297716275590
cynepCTAPuk <CTAPuk@gmail.com>
2019-03-05 17:09:09 +0300

    ReadFile

---
+++ MikeMcGrath/ReadFile.java
@@ -0,0 +1,14 @@
+import java.io.*;
+public class ReadFile {
+    public static void main(String[] args) {
+        try {
+            FileReader file = new FileReader("ReadFile.java");
+            BufferedReader buffer = new BufferedReader(file);
+            String line;
+            while ((line = buffer.readLine()) != null) System.out.println(line);
+            buffer.close() ;
+        } catch (IOException e) {
+            System.out.println("Произошла ошибка чтения");
+        }
+    }
+}
