d084f20358e417fbb960f847a876d09089dd1dfa
cynepCTAPuk <CTAPuk@gmail.com>
2019-03-05 16:51:14 +0300

    ListFiles

--- MikeMcGrath/ListFiles.java
+++ MikeMcGrath/ListFiles.java
@@ -2,13 +2,11 @@ import java.io.*;
 
 public class ListFiles {
     public static void main(String[] args) {
-        File dir = new File("c:/000");
+        File dir = new File("c:/000/111");
         if (dir.exists()) {
             String[] files = dir.list();
             System.out.println(files.length + " файлов найдено...");
-            for (int i = 0; i < files.length; i++) {
-                System.out.println(files[i]);
-            }
+            for (String file : files) System.out.println(file);
         } else System.out.println("Каталог не найден.");
     }
 }
