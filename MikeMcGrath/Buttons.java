f05292ceb7827bd33f1de99d358a6f30499dd276
cynepCTAPuk <CTAPuk@gmail.com>
2019-03-05 19:42:51 +0300

    Buttonz

---
+++ MikeMcGrath/Buttons.java
@@ -0,0 +1,26 @@
+import javax.swing.*;
+
+public class Buttons extends JFrame {
+    JPanel jPanel = new JPanel();
+    ImageIcon tick = new ImageIcon( "c:/000/529.gif" ) ;
+    ImageIcon cross = new ImageIcon( "c:/000/539.gif" ) ;
+    JButton btn = new JButton( "Нажми меня" ) ;
+    JButton tickBtn = new JButton( tick ) ;
+    JButton crossBtn = new JButton( "СТОП" , cross ) ;
+
+    public Buttons() {
+        super("Окно Swing");
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
+        setSize(200, 400);
+        setLocationRelativeTo(null);
+        add(jPanel);
+        jPanel.add(btn);
+        jPanel.add(tickBtn);
+        jPanel.add(crossBtn);
+        setVisible(true);
+    }
+
+    public static void main(String[] args) {
+        Buttons gui = new Buttons();
+    }
+}
---
+++ MikeMcGrath/Buttonz.java
@@ -0,0 +1,31 @@
+import javax.swing.*;
+
+public class Buttonz extends JFrame {
+    JPanel jPanel = new JPanel();
+    ClassLoader ldr = this.getClass().getClassLoader() ;
+    java.net.URL tickURL = ldr.getResource( "c:/000/529.gif" ) ;
+    java.net.URL crossURL = ldr.getResource( "c:/000/539.gif" ) ;
+
+    ImageIcon tick = new ImageIcon( tickURL ) ;
+    ImageIcon cross = new ImageIcon( crossURL ) ;
+
+    JButton btn = new JButton( "Нажми меня" ) ;
+    JButton tickBtn = new JButton( tick ) ;
+    JButton crossBtn = new JButton( "СТОП" , cross ) ;
+
+    public Buttonz() {
+        super("Окно Swing");
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
+        setSize(200, 400);
+        setLocationRelativeTo(null);
+        add(jPanel);
+        jPanel.add(btn);
+        jPanel.add(tickBtn);
+        jPanel.add(crossBtn);
+        setVisible(true);
+    }
+
+    public static void main(String[] args) {
+        Buttonz gui = new Buttonz();
+    }
+}
