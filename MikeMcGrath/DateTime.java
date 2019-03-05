13224ccadc7151807c3268f851d608f435d7bc9b
cynepCTAPuk <CTAPuk@gmail.com>
2019-03-05 19:08:04 +0300

    DateTime

--- MikeMcGrath/DateTime.java
+++ MikeMcGrath/DateTime.java
@@ -1,4 +1,6 @@
-import java.time.LocalDateTime;
+import java.time.*;
+
+import static java.lang.String.*;
 
 public class DateTime {
     public static void main(String[] args) {
@@ -8,15 +10,15 @@ public class DateTime {
         System.out.println("Теперь текущее время " + date);
 
         String fields = "";
-        fields += "\nГод:\t\t\t" + date.getYear();
-        fields += "\nМесяц:\t\t\t" + date.getMonth();
-        fields += "\nНомер месяца:\t\t" + date.getMonthValue();
-        fields += "\nДень недели:\t\t" + date.getDayOfWeek();
-        fields += "\nДень месяца:\t\t" + date.getDayOfMonth();
-        fields += "\nДень в году:\t\t" + date.getDayOfYear();
-        fields += "\nЧас (0-23):\t\t" + date.getHour();
-        fields += "\nМинута:\t\t\t" + date.getMinute();
-        fields += "\nСекунда:\t\t" + date.getSecond();
+        fields += format("\n%-15s%10s", "Год:", date.getYear());
+        fields += format("\n%-15s%10s", "Месяц:", date.getMonth());
+        fields += format("\n%-15s%10s", "Номер месяца:", date.getMonthValue());
+        fields += format("\n%-15s%10s", "День недели:", date.getDayOfWeek());
+        fields += format("\n%-15s%10s", "День месяца:", date.getDayOfMonth());
+        fields += format("\n%-15s%10s", "День в году:", date.getDayOfYear());
+        fields += format("\n%-15s%10s", "Час (0-23):", date.getHour());
+        fields += format("\n%-15s%10s", "Минута:", date.getMinute());
+        fields += format("\n%-15s%10s", "Секунда:", date.getSecond());
         System.out.println(fields);
     }
 }
