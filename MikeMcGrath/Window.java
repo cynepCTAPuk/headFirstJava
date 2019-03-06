e2194f4a26c0536460117228649677646af9d03d
cynepCTAPuk <CTAPuk@gmail.com>
2019-03-06 16:50:36 +0300

    Window

--- MikeMcGrath/Window.java
+++ MikeMcGrath/Window.java
@@ -1,14 +1,14 @@
 import javax.swing.*;
 
 public class Window extends JFrame {
-    JPanel pn1 = new JPanel();
+    JPanel pnl = new JPanel();
 
     public Window() {
         super("Окно Swing");
         setDefaultCloseOperation(EXIT_ON_CLOSE);
         setSize(500, 200);
         setLocationRelativeTo(null);
-        add(pn1);
+        add(pnl);
         setVisible(true);
     }
 
