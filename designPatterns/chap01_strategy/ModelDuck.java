f93087da93cda5e22c77cda77676357125733a3e
cynepCTAPuk <CTAPuk@gmail.com>
2019-03-11 20:12:03 +0300

    Pattern Strategy

--- designPatterns/chap01_strategy/ModelDuck.java
+++ designPatterns/chap01_strategy/ModelDuck.java
@@ -3,7 +3,7 @@ package chap01_strategy;
 public class ModelDuck extends Duck {
     public ModelDuck() {
         flyBehavior = new FlyNoWay();
-        quackBehavior = new Squeak();
+        quackBehavior = new Quack();
     }
     public void display() {
         System.out.println("-I'm a model duck-");
