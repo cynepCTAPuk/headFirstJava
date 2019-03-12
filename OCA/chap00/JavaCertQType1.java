19a798de178a10a34ab3ea1966be2d24adf4cea4
cynepCTAPuk <CTAPuk@gmail.com>
2019-03-12 20:05:00 +0300

    JavaCertQType1

---
+++ OCA/chap00/JavaCertQType1.java
@@ -0,0 +1,10 @@
+package chap00;
+
+class JavaCertQType1 {
+    public static void main(String... cmd) {
+        main("private", cmd);
+    }
+    private static void main(String type, String[] args) {
+        System.out.println(args[0] + args[1]);
+    }
+}
