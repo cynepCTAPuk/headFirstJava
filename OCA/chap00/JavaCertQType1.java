de70b064e2d8e2db9ded5f6a6ecdaac6f31dd2d5
cynepCTAPuk <CTAPuk@gmail.com>
2019-03-12 20:13:10 +0300

    JavaCertQuesType3

--- OCA/chap00/JavaCertQType1.java
+++ OCA/chap00/JavaCertQType1.java
@@ -1,5 +1,16 @@
 package chap00;
-
+/*
+What is the output when class JavaCertQType1 is executed using
+the following command (choose 1 option):
+java JavaCertQType1 1 11 EJava Guru
+1
+1 11
+111
+12
+1 11 EJava Guru
+Compilation error
+Runtime exception
+ */
 class JavaCertQType1 {
     public static void main(String... cmd) {
         main("private", cmd);
---
+++ OCA/chap00/JavaCertQuesType3.java
@@ -0,0 +1,28 @@
+package chap00;
+/*
+What is the output (choose 1 option)?
+Code outputs Spring
+Code outputs Summer
+Code outputs Autumn
+Code outputs Winter
+Compilation error
+Runtime exception
+ */
+class JavaCertQuesType3 {
+/*
+    public static void main(String args[]) {
+        System.out.println("Spring");
+    }
+*/
+    public static void main(String... args) {
+        System.out.println("Summer");
+    }
+/*
+    public static void main(String[] cmd) {
+        System.out.println("Autumn");
+    }
+*/
+    public static void main() {
+        System.out.println("Winter");
+    }
+}
