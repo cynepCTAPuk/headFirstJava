aa9454a179a96c4efb20a35cd1119a2dfc17219d
cynepCTAPuk <CTAPuk@gmail.com>
2019-03-13 00:18:36 +0300

    Ввводные классы

---
+++ OCA/chap01/Comments.java
@@ -0,0 +1,10 @@
+package chap01;
+
+public class Comments {
+    public static void main(String[] args) {
+        String name = /* Harry */ "Paul";
+        System.out.println(name);
+        name = "/* Harry */ Paul";
+        System.out.println(name);
+    }
+}
