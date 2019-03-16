d0c3c5aacb19862e4774092fb2ef3b51454257a3
cynepCTAPuk <CTAPuk@gmail.com>
2019-03-16 17:27:43 +0300

    Capitalization throw while loop

---
+++ test/geekBrains/tasks/chap4_03.java
@@ -0,0 +1,18 @@
+package tasks;
+
+public class chap4_03 {
+    public static void main(String[] args) {
+        String a = "в леСу Родилась еЛОчка";
+        System.out.println(a);
+        StringBuilder a1 = new StringBuilder();
+        int i = 0;
+        while (i < a.length()) {
+            String c = a.substring(i, i + 1);
+            if (i == 0 || (a.charAt(i - 1) == ' ')) c = c.toUpperCase();
+            else c = c.toLowerCase();
+            a1.append(c);
+            i++;
+        }
+        System.out.println(a1);
+    }
+}
