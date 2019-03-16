9cd7537d7f5b6016730d5f86d292f390977e05d0
cynepCTAPuk <CTAPuk@gmail.com>
2019-03-16 17:28:18 +0300

    Capitalization throw fori loop

--- test/geekBrains/tasks/chap4_02.java
+++ test/geekBrains/tasks/chap4_02.java
@@ -7,9 +7,8 @@ public class chap4_02 {
         StringBuilder a1 = new StringBuilder();
         for (int i = 0; i < a.length(); i++) {
             String c = a.substring(i, i + 1);
-            if (i == 0 || (a.charAt(i - 1) == ' ')) {
-                c = c.toUpperCase();
-            } else c = c.toLowerCase();
+            if (i == 0 || (a.charAt(i - 1) == ' ')) c = c.toUpperCase();
+            else c = c.toLowerCase();
             a1.append(c);
         }
         System.out.println(a1);
