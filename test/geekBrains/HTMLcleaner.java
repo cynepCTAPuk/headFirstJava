7f747cc12e8dd58704b3fe063f7663bfeec7f379
cynepCTAPuk <CTAPuk@gmail.com>
2019-03-16 20:41:42 +0300

    HTMLcleaner https://habr.com/ru/top/ parsing //a[@class=post__title_link]

---
+++ test/geekBrains/HTMLcleaner.java
@@ -0,0 +1,19 @@
+import org.htmlcleaner.*;
+
+import java.io.*;
+import java.net.*;
+
+public class HTMLcleaner {
+
+    public static void main(String[] args) throws IOException, XPatherException {
+        HtmlCleaner cleaner = new HtmlCleaner();
+        final String siteUrl = "https://habr.com/ru/top/";
+        TagNode node = cleaner.clean(new URL(siteUrl));
+
+        Object[] objects = node.evaluateXPath("//a[@class='post__title_link']");
+        for (Object o : objects) {
+            System.out.println(((TagNode) o).getText());
+        }
+
+    }
+}
