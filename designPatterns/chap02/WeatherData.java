2418673495c18a59461a900980284ef87fa75a71
cynepCTAPuk <CTAPuk@gmail.com>
2019-03-24 19:46:36 +0300

    implementation interface WeatherData

--- designPatterns/chap02/WeatherData.java
+++ designPatterns/chap02/WeatherData.java
@@ -1,26 +1,36 @@
 package chap02;
+import java.util.ArrayList;
 
-public class WeatherData {
-    // Объявления переменных экземпляров
+public class WeatherData implements Subject {
+    private ArrayList<Observer> observers;
+    private float temperature;
+    private float humidity;
+    private float pressure;
+
+    public WeatherData() {
+        observers = new ArrayList();
+    }
+    public void registerObserver(Observer o) {
+        observers.add(o);
+    }
+    public void removeObserver(Observer o) {
+        int i = observers.indexOf(o);
+        if (i >= 0) observers.remove(i);
+    }
+    public void notifyObservers() {
+        for (int i = 0; i < observers.size(); i++) {
+            Observer observer = observers.get(i);
+            observer.update(temperature, humidity, pressure);
+        }
+    }
     public void measurementsChanged() {
-        float temp = getTemperature();
-        float humidity = getHumidity();
-        float pressure = getPressure();
-//        currentConditionsDisplay.update(temp, humidity, pressure);
-//        statisticsDisplay.update(temp, humidity, pressure);
-//        forecastDisplay.update(temp, humidity, pressure);
+        notifyObservers();
     }
-
-    private float getPressure() {
-        return 0;
+    public void setMeasurements(float temperature, float humidity, float pressure) {
+        this.temperature = temperature;
+        this.humidity = humidity;
+        this.pressure = pressure;
+        measurementsChanged();
     }
-
-    private float getHumidity() {
-        return 0;
-    }
-
-    private float getTemperature() {
-        return 0;
-    }
-// Другие методы WeatherData
+    // Другие методы WeatherData
 }
