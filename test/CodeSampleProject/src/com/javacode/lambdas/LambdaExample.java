395458ed6d1482ca87f29f7a79f4bdfd3537619a
cynepCTAPuk <CTAPuk@gmail.com>
2019-03-27 21:11:29 +0300

    Lambda

---
+++ test/CodeSampleProject/src/com/javacode/lambdas/LambdaExample.java
@@ -0,0 +1,28 @@
+package com.javacode.lambdas;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@FunctionalInterface
+interface ElementProcessor {
+    double process(int element);
+}
+
+public class LambdaExample {
+    public static void main(String[] args) {
+        List<Integer> intList = new ArrayList<>();
+        intList.add(1);
+        intList.add(2);
+        intList.add(3);
+        intList.add(4);
+        intList.add(5);
+
+        List<Double> doubleList = new ArrayList<>();
+        doubleList.add(6.4);
+        doubleList.add(8.6);
+        doubleList.add(1.23);
+        doubleList.add(4.13);
+        doubleList.add(12.2);
+
+    }
+}
