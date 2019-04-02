78269d65082d0717860da791143dad9ae53d6105
cynepCTAPuk <CTAPuk@gmail.com>
2019-04-02 23:06:00 +0300

    Streams overview

--- CodeSampleProject/src/com/javacode/streams/StreamsOverviewMain.java
+++ CodeSampleProject/src/com/javacode/streams/StreamsOverviewMain.java
@@ -4,11 +4,13 @@ import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.*;
+import java.util.function.Consumer;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 public class StreamsOverviewMain {
     private static List<Employee> employeeList = new ArrayList<>();
+    private static List<Employee> secondList = new ArrayList<>();
     private static Map<Integer, Employee> employeeMap = null;
 
     public static void main(String[] args) throws IOException {
@@ -24,26 +26,93 @@ public class StreamsOverviewMain {
 //        employeeList.add(new Employee(10, "Mike", "Yellow", 60_000));
 //        employeeList.add(new Employee(11, "Victoria", "Pink", 75_000));
 
-//        testStreamFormList();
-        testStreamFromFile();
+        secondList.add(new Employee(1, "Alex", "Black", 50_000));
+        secondList.add(new Employee(2, "John", "Green", 75_000));
+        secondList.add(new Employee(6, "Sam", "Brown", 80_000));
+        secondList.add(new Employee(9, "Tony", "Grey", 90_000));
+        secondList.add(new Employee(10, "Mike", "Yellow", 60_000));
+        secondList.add(new Employee(11, "Victoria", "Pink", 75_000));
+        secondList.add(new Employee(16, "Sean", "Magenta", 80_000));
+        secondList.add(new Employee(19, "Kate", "Black", 88_000));
+//        secondList.add(new Employee(9, "Tony", "Grey", 90_000));
+//        secondList.add(new Employee(10, "Mike", "Yellow", 60_000));
+//        secondList.add(new Employee(11, "Victoria", "Pink", 75_000));
+
+        testStreamFormList();
+//        testStreamFromFile();
     }
 
     private static void testStreamFormList() {
+/*
         employeeList.stream()
                 .filter(e -> e.getSalary() > 60_000)
                 .filter(e -> e.getId() < 10)
                 .collect(Collectors.toList())
                 .forEach(System.out::println);
+*/
+
         Integer[] ids = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20};
-        Stream.of(ids).map(StreamsOverviewMain::findById)
+
+/*
+        Stream.of(ids)
+                .map(StreamsOverviewMain::findById)
                 .filter(Objects::nonNull)
                 .collect(Collectors.toList())
                 .forEach(System.out::println);
+*/
+/*
+        Random r = new Random();
+        Integer integer = Stream.of(ids)
+                .filter(i -> i % 2 == 0)
+                .filter(i -> i % 3 == 0)
+                .filter(i -> i % 5 == 0)
+                .findFirst()
+//                .orElse(0);
+                .orElseGet(()->r.nextInt());
+        System.out.println(integer);
+*/
+        Stream.of(ids)
+                .filter(i -> i % 2 == 0)
+                .filter(i -> i % 3 == 0)
+                .limit(1)
+                .forEach(System.out::println);
+
+/*
         Optional<Employee> first = Stream.of(ids)
                 .map(StreamsOverviewMain::findById)
                 .filter(Objects::nonNull)
                 .findFirst();
         System.out.println(first);
+*/
+/*
+        OptionalDouble average = Stream.of(ids)
+                .map(StreamsOverviewMain::findById)
+                .filter(Objects::nonNull)
+                .mapToInt(Employee::getSalary)
+                .average();
+        System.out.println(average);
+*/
+
+        List<List<Employee>> departments = new ArrayList<>();
+        departments.add(employeeList);
+        departments.add(secondList);
+
+//        departments.stream().flatMap(l -> l.stream().map(e -> e.getFirstName())).forEach(System.out::println);
+
+/*
+        Stream.of(ids).map(e -> String.format("%,3d", e)).forEach(System.out::print);
+        System.out.println();
+        Stream.of(ids)
+//                .peek(e -> e = e * 2)
+                .map(e -> String.format("%,3d", e * 2))
+                .forEach(System.out::print);
+        System.out.println();
+*/
+/*
+        Consumer<Integer> c = e -> e = e * 2;
+        Stream.of(ids).forEach(c);
+        System.out.println(c);
+*/
     }
 
     private static Employee findById(int id) {
@@ -63,5 +132,4 @@ public class StreamsOverviewMain {
                 .sorted()
                 .forEach(System.out::println);
     }
-
 }
