f7b000e200c0369692546bcd4bd491f13262f21a
cynepCTAPuk <CTAPuk@gmail.com>
2019-04-02 20:12:34 +0300

    Streams overview

---
+++ CodeSampleProject/src/com/javacode/streams/Employee.java
@@ -0,0 +1,45 @@
+package com.javacode.streams;
+
+public class Employee {
+    private int id;
+    String firstName;
+    String lastName;
+    Integer salary;
+
+    public Employee(int id, String firstName, String lastName, Integer salary) {
+        this.id = id;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.salary = salary;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public Integer getSalary() {
+        return salary;
+    }
+
+    public void setSalary(Integer salary) {
+        this.salary = salary;
+    }
+
+    @Override
+    public String toString() {
+        return "Employee{" +
+                "id=" + id +
+                ", firstName='" + firstName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                String.format(", salary=%,d", salary) +
+                '}';
+    }
+}
