597485e70bce0618a7da12af0f0d0018529ca753
cynepCTAPuk <CTAPuk@gmail.com>
2019-04-03 14:37:09 +0300

    Streams overview Sort and Reduce

--- CodeSampleProject/src/com/javacode/streams/Employee.java
+++ CodeSampleProject/src/com/javacode/streams/Employee.java
@@ -2,11 +2,11 @@ package com.javacode.streams;
 
 public class Employee {
     private int id;
-    String firstName;
-    String lastName;
-    Integer salary;
+    private String firstName;
+    private String lastName;
+    private int salary;
 
-    public Employee(int id, String firstName, String lastName, Integer salary) {
+    public Employee(int id, String firstName, String lastName, int salary) {
         this.id = id;
         this.firstName = firstName;
         this.lastName = lastName;
@@ -17,19 +17,31 @@ public class Employee {
         return id;
     }
 
+    public void setId(int id) {
+        this.id = id;
+    }
+
     public String getFirstName() {
         return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
     }
 
     public String getLastName() {
         return lastName;
     }
 
-    public Integer getSalary() {
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public int getSalary() {
         return salary;
     }
 
-    public void setSalary(Integer salary) {
+    public void setSalary(int salary) {
         this.salary = salary;
     }
 
@@ -41,5 +53,28 @@ public class Employee {
                 ", lastName='" + lastName + '\'' +
                 String.format(", salary=%,d", salary) +
                 '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Employee employee = (Employee) o;
+
+        if (id != employee.id) return false;
+        if (salary != employee.salary) return false;
+        if (firstName != null ? !firstName.equals(employee.firstName) : employee.firstName != null) return false;
+        return lastName != null ? lastName.equals(employee.lastName) : employee.lastName == null;
+
+    }
+
+    @Override
+    public int hashCode() {
+        int result = id;
+        result = 31 * result + (firstName != null ? firstName.hashCode() : 0);
+        result = 31 * result + (lastName != null ? lastName.hashCode() : 0);
+        result = 31 * result + salary;
+        return result;
     }
 }
---
+++ CodeSampleProject/src/com/javacode/streams/StreamsOverviewMain1.java
@@ -0,0 +1,159 @@
+package com.javacode.streams;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+public class StreamsOverviewMain1 {
+    private static List<Employee> employeeList = new ArrayList<>();
+    private static List<Employee> secondList = new ArrayList<>();
+    private static Map<Integer, Employee> employeeMap = null;
+
+    public static void main(String[] args) throws IOException {
+        employeeList.add(new Employee(1, "Alex", "Black", 50_000));
+        employeeList.add(new Employee(2, "John", "Green", 75_000));
+        employeeList.add(new Employee(6, "Sam", "Brown", 80_000));
+        employeeList.add(new Employee(9, "Tony", "Grey", 90_000));
+        employeeList.add(new Employee(10, "Mike", "Yellow", 60_000));
+        employeeList.add(new Employee(11, "Victoria", "Pink", 75_000));
+        employeeList.add(new Employee(16, "Sean", "Magenta", 80_000));
+        employeeList.add(new Employee(19, "Kate", "Black", 88_000));
+        employeeList.add(new Employee(9, "Tony", "Grey", 90_000));
+        employeeList.add(new Employee(10, "Mike", "Yellow", 60_000));
+        employeeList.add(new Employee(11, "Victoria", "Pink", 75_000));
+
+//        testStreamFormList();
+//        testStreamFromFile();
+        testSortAndReduce();
+    }
+
+    private static void testSortAndReduce() {
+/*
+        Employee employee = employeeList.stream()
+                .max((e1, e2) -> e1.getId() - e2.getId()).get();
+        System.out.println(employee);
+*/
+/*
+        Employee employee1 = employeeList.stream()
+                .min(Comparator.comparingInt(Employee::getSalary)).get();
+        System.out.println(employee1);
+*/
+        employeeList.stream()
+                .sorted((s1, s2) -> s1.getFirstName().compareTo(s2.getFirstName()))
+                .distinct()
+//                .sorted(Comparator.comparing(Employee::getFirstName))
+                .collect(Collectors.toList())
+                .forEach(System.out::println);
+/*
+        Employee identity = new Employee(0, "", "", 0);
+        Employee reduceEmployee = employeeList.stream()
+                .reduce(identity, (e1, e2) -> {
+                    e1.setId(e1.getId() + e2.getId());
+                    e1.setSalary(e1.getSalary() + e2.getSalary());
+                    return e1;
+                });
+        System.out.println(reduceEmployee);
+*/
+    }
+
+    private static void testStreamFormList() {
+/*
+        employeeList.stream()
+                .filter(e -> e.getSalary() > 60_000)
+                .filter(e -> e.getId() < 10)
+                .collect(Collectors.toList())
+                .forEach(System.out::println);
+*/
+
+        Integer[] ids = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20};
+
+/*
+        Stream.of(ids)
+                .map(StreamsOverviewMain::findById)
+                .filter(Objects::nonNull)
+                .collect(Collectors.toList())
+                .forEach(System.out::println);
+*/
+/*
+        Random r = new Random();
+        Integer integer = Stream.of(ids)
+                .filter(i -> i % 2 == 0)
+                .filter(i -> i % 3 == 0)
+                .filter(i -> i % 5 == 0)
+                .findFirst()
+//                .orElse(0);
+                .orElseGet(()->r.nextInt());
+        System.out.println(integer);
+*/
+        Stream.of(ids)
+                .filter(i -> i % 2 == 0)
+                .filter(i -> i % 3 == 0)
+                .skip(2)
+                .limit(1)
+                .forEach(System.out::println);
+
+/*
+        Optional<Employee> first = Stream.of(ids)
+                .map(StreamsOverviewMain::findById)
+                .filter(Objects::nonNull)
+                .findFirst();
+        System.out.println(first);
+*/
+/*
+        OptionalDouble average = Stream.of(ids)
+                .map(StreamsOverviewMain::findById)
+                .filter(Objects::nonNull)
+                .mapToInt(Employee::getSalary)
+                .average();
+        System.out.println(average);
+*/
+
+        List<List<Employee>> departments = new ArrayList<>();
+        departments.add(employeeList);
+        departments.add(secondList);
+
+/*
+        departments.stream().flatMap(l -> l.stream()
+                .map(e -> e.getFirstName())).forEach(System.out::println);
+*/
+
+/*
+        Stream.of(ids).map(e -> String.format("%,3d", e)).forEach(System.out::print);
+        System.out.println();
+        Stream.of(ids)
+//                .peek(e -> e = e * 2)
+                .map(e -> String.format("%,3d", e * 2))
+                .forEach(System.out::print);
+        System.out.println();
+*/
+/*
+        Consumer<Integer> c = e -> e = e * 2;
+        Stream.of(ids).forEach(c);
+        System.out.println(c);
+*/
+    }
+
+    private static void testStreamFromFile() throws IOException {
+        Files.lines((Paths.get("c:/000/words.txt")))
+                .filter(e -> e.length() > 4)
+                .map(String::toUpperCase)
+                .distinct()
+                .sorted()
+                .forEach(System.out::println);
+    }
+
+    private static Employee findById(int id) {
+        if (employeeMap == null) {
+            employeeMap = employeeList.stream()
+                    .distinct()
+                    .collect(Collectors.toMap(Employee::getId, e -> e));
+        }
+        return employeeMap.get(id);
+    }
+}
