6100d79166d47e7893e882712e1f5d5e6c21dec6
cynepCTAPuk <CTAPuk@gmail.com>
2019-04-06 22:47:12 +0300

    ---

--- test/eclipse/ValueTypes/src/com/example/Main.java
+++ test/eclipse/ValueTypes/src/com/example/Main.java
@@ -5,7 +5,12 @@ public class Main
 
 	public static void main(String[] args)
 	{
-		byte bt = 127; // 0x1F
+		byte bt = 127; // 0x7F;
+		for (int i = 33; i < 128; i++)
+		{
+			System.out.print((char) i + " ");
+		}
+		System.out.println((char) (bt - 2));
 		short sh = 20;
 		int in = 1_000;
 		long lg = 12_345_678_901L;
@@ -13,12 +18,36 @@ public class Main
 		in = (int) lg;
 
 		System.out.printf("%,d -> %,d\n", lg, sh);
-		System.out.printf("%,d -> %,d", lg, in);
-		
+		System.out.printf("%,d -> %,d\n", lg, in);
+
 		float fl = 1.0F;
-		double dbl = 1.0;
-		
+		double db = 1.0; // 1D
 
+		int x = 5;
+		double y = x / 2;
+
+		System.out.println(y);
+
+		System.out.printf("%,d\n", Byte.MAX_VALUE);
+		System.out.printf("%,d\n", Short.MAX_VALUE);
+		System.out.printf("%,d\n", Integer.MAX_VALUE);
+		System.out.printf("%,d\n", Integer.MAX_VALUE + 1);
+		System.out.printf("%,d\n", Integer.MIN_VALUE);
+		System.out.printf("%,d\n", Long.MAX_VALUE);
+
+		boolean a = true;
+		boolean b = false;
+		boolean c;
+		c = a && b; // and
+		System.out.println(c);
+
+		c = a & b; // and
+		System.out.println(c);
+
+		c = a || b; // OR
+		System.out.println(c);
+
+		c = !a; // NOT
+		System.out.println(c);
 	}
-
 }
