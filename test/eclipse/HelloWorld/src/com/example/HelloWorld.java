c99460c463c40a414efb1798e642d591ca22a681
cynepCTAPuk <CTAPuk@gmail.com>
2019-04-06 20:21:55 +0300

    ---

--- test/eclipse/HelloWorld/src/com/example/HelloWorld.java
+++ test/eclipse/HelloWorld/src/com/example/HelloWorld.java
@@ -5,17 +5,6 @@ import java.util.Arrays;
 import java.util.List;
 
 public class HelloWorld {
-	String name;
-	
-
-	public HelloWorld(String name) {
-		this.name = name;
-	}
-
-	@Override
-	public String toString() {
-		return "HelloWorld [string=" + name + "]";
-	}
 
 	public static void main(String[] args) {
 
@@ -34,9 +23,7 @@ public class HelloWorld {
 			System.out.printf("%s ", s);
 		}
 		System.out.println();
-		
-		System.out.println(new HelloWorld(" "));
-		
+
 		System.out.println("Hello World!");
 	}
 }
