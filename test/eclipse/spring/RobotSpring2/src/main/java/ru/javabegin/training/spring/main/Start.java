6182bafe525d346ea1724890992176e587c09a6e
cynepCTAPuk <CTAPuk@gmail.com>
2019-04-11 18:47:36 +0300

    first spring test project

--- test/eclipse/spring/RobotSpring2/src/main/java/ru/javabegin/training/spring/main/Start.java
+++ test/eclipse/spring/RobotSpring2/src/main/java/ru/javabegin/training/spring/main/Start.java
@@ -8,10 +8,10 @@ import ru.javabegin.training.spring.impls.robot.ModelT1000;
 
 public class Start {
 	public static void main(String[] args) {
-		ApplicationContext context = new ClassPathXmlApplicationContext("context.xml");
+		ApplicationContext context = new ClassPathXmlApplicationContext("Beans/context.xml");
 		ModelT1000 t1000 = (ModelT1000) context.getBean("t1000");
 		t1000.dance();
-//		t1000.fire();
+		t1000.action();
 		((ConfigurableApplicationContext) context).close();
 	}
 }
