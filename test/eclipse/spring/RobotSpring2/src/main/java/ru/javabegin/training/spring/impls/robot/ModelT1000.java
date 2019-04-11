e6c5656ff859fca68f32b8b132b731f148814aeb
cynepCTAPuk <CTAPuk@gmail.com>
2019-04-11 20:38:25 +0300

    first spring test project

--- test/eclipse/spring/RobotSpring2/src/main/java/ru/javabegin/training/spring/impls/robot/ModelT1000.java
+++ test/eclipse/spring/RobotSpring2/src/main/java/ru/javabegin/training/spring/impls/robot/ModelT1000.java
@@ -13,11 +13,29 @@ public class ModelT1000 implements Robot {
 
 	public ModelT1000() {
 	}
-
 	public ModelT1000(Hand hand, Leg leg, Head head) {
 		super();
 		this.hand = hand;
 		this.leg = leg;
+		this.head = head;
+	}
+
+	public Hand getHand() {
+		return hand;
+	}
+	public void setHand(Hand hand) {
+		this.hand = hand;
+	}
+	public Leg getLeg() {
+		return leg;
+	}
+	public void setLeg(Leg leg) {
+		this.leg = leg;
+	}
+	public Head getHead() {
+		return head;
+	}
+	public void setHead(Head head) {
 		this.head = head;
 	}
 
