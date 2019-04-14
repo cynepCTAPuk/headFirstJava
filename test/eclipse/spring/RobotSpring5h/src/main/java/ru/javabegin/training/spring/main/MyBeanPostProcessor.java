5ed3e000b8560cf55bd54acd9c199b2e6c655c23
cynepCTAPuk <CTAPuk@gmail.com>
2019-04-14 19:37:24 +0300

    first spring test project v5h

---
+++ test/eclipse/spring/RobotSpring5h/src/main/java/ru/javabegin/training/spring/main/MyBeanPostProcessor.java
@@ -0,0 +1,18 @@
+package ru.javabegin.training.spring.main;
+
+import org.springframework.beans.BeansException;
+import org.springframework.beans.factory.config.BeanPostProcessor;
+
+public class MyBeanPostProcessor implements BeanPostProcessor {
+
+    @Override
+    public Object postProcessBeforeInitialization(Object object, String name) throws BeansException {
+	System.out.println(object + " - postProcessBeforeInitialization()");
+	return object;
+    }
+
+    @Override
+    public Object postProcessAfterInitialization(Object object, String name) throws BeansException {
+	return object;
+    }
+}
