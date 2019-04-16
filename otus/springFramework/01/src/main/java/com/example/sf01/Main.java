0bb6cc61b95b78171f7976340e07dc2809ccc05f
cynepCTAPuk <CTAPuk@gmail.com>
2019-04-16 22:43:17 +0300

    spring-framework-01

--- otus/springFramework/01/src/main/java/com/example/sf01/Main.java
+++ otus/springFramework/01/src/main/java/com/example/sf01/Main.java
@@ -1,11 +1,18 @@
 package com.example.sf01;
 
-import com.example.sf01.domain.Person;
+import com.example.sf01.service.PersonService;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class Main {
 
     public static void main(String[] args) {
-        Person ivan = null;
-        System.out.println("name: " + ivan.getName() + " age: " + ivan.getAge());
+
+        ApplicationContext context = new ClassPathXmlApplicationContext(
+                "/spring-context.xml");
+        PersonService s;
+        s = context.getBean(PersonService.class);
+        System.out.println(s);
+        System.out.println(s.getByName("Ivan"));
     }
 }
--- otus/springFramework/01/src/main/java/com/example/sf01/dao/PersonDaoSimple.java
+++ otus/springFramework/01/src/main/java/com/example/sf01/dao/PersonDaoSimple.java
@@ -3,8 +3,10 @@ package com.example.sf01.dao;
 import com.example.sf01.domain.Person;
 
 public class PersonDaoSimple implements PersonDao {
+    int age = 18;
 
-    public Person findByName(String name) {
-        return new Person(name, 18);
-    }
+    public int getAge() {return age;}
+    public void setAge(int age) {this.age = age;}
+
+    public Person findByName(String name) {return new Person(name, age);}
 }
