04b368e59ecb88151da42c1b3865537f5ea82e47
cynepCTAPuk <CTAPuk@gmail.com>
2019-04-17 19:24:54 +0300

    spring-framework-01

--- otus/springFramework/01/src/main/java/com/example/sf01/dao/PersonDao.java
+++
@@ -1,8 +0,0 @@
-package com.example.sf01.dao;
-
-import com.example.sf01.domain.Person;
-
-public interface PersonDao {
-
-    Person findByName(String name);
-}
--- otus/springFramework/01/src/main/java/com/example/sf01/service/PersonService.java
+++
@@ -1,8 +0,0 @@
-package com.example.sf01.service;
-
-import com.example.sf01.domain.Person;
-
-public interface PersonService {
-
-    Person getByName(String name);
-}
