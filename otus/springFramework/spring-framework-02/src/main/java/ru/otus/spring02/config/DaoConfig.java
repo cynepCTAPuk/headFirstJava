5f18d1053bc08d40b94a54588a93051d8c06c7b5
cynepCTAPuk <CTAPuk@gmail.com>
2019-04-18 15:21:15 +0300

    spring-framework-02

--- otus/springFramework/spring-framework-02/src/main/java/ru/otus/spring02/config/DaoConfig.java
+++ otus/springFramework/spring-framework-02/src/main/java/ru/otus/spring02/config/DaoConfig.java
@@ -2,12 +2,14 @@ package ru.otus.spring02.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.stereotype.Service;
 import ru.otus.spring02.dao.PersonDao;
 import ru.otus.spring02.dao.PersonDaoSimple;
 
-@Configuration
+//@Configuration
+@Service
 public class DaoConfig {
-    @Bean
+//    @Bean
     public PersonDao personDao() {
         return new PersonDaoSimple();
     }
