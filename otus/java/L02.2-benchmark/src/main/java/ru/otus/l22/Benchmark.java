b2b747378ec74b625e702194e8821f6ed8d3b3b7
cynepCTAPuk <CTAPuk@gmail.com>
2019-04-18 20:12:24 +0300

    L02.2-benchmark

--- otus/java/L02.2-benchmark/src/main/java/ru/otus/l22/Benchmark.java
+++ otus/java/L02.2-benchmark/src/main/java/ru/otus/l22/Benchmark.java
@@ -4,48 +4,48 @@ import java.util.function.Supplier;
 
 @SuppressWarnings("MismatchedReadAndWriteOfArray")
 class Benchmark {
-  private Object[] array;
-  private final int size;
+    private Object[] array;
+    private final int size;
 
-  Benchmark() {
-    size = 10 * 1000 * 1000;
-  }
+    Benchmark() {
+        size = 10 * 1000 * 1000;
+    }
 
-  Benchmark(int size) {
-    this.size = size;
-    array = new Object[size];
-  }
+    Benchmark(int size) {
+        this.size = size;
+        array = new Object[size];
+    }
 
-  void prepare() {
-    long memPerArray = getMemChanges(() -> {
-      array = new Object[size];
-    });
-    System.out.println("Reference size: " + memPerArray / size);
-  }
+    void prepare() {
+        long memPerArray = getMemChanges(() -> {
+            array = new Object[size];
+        });
+        System.out.println("Reference size: " + memPerArray / size);
+    }
 
-  void measure(Supplier<Object> supplier, String name) {
-    long memChanges = getMemChanges(() -> {
-      int i = 0;
-      while (i < size) {
-        array[i] = supplier.get();
-        i++;
-      }
-    });
-    System.out.println(name + " size: " + Math.round((double) memChanges / size));
-  }
+    void measure(Supplier<Object> supplier, String name) {
+        long memChanges = getMemChanges(() -> {
+            int i = 0;
+            while (i < size) {
+                array[i] = supplier.get();
+                i++;
+            }
+        });
+        System.out.println(name + " size: " + Math.round((double) memChanges / size));
+    }
 
-  void clean() {
-    array = new Object[size];
-    BlockingGC.collect();
-  }
+    void clean() {
+        array = new Object[size];
+        BlockingGC.collect();
+    }
 
-  private static long getMemChanges(Runnable create) {
-    Runtime runtime = Runtime.getRuntime();
-    BlockingGC.collect();
-    long memBefore = runtime.totalMemory() - runtime.freeMemory();
-    create.run();
-    BlockingGC.collect();
-    long memAfter = runtime.totalMemory() - runtime.freeMemory();
-    return memAfter - memBefore;
-  }
+    private static long getMemChanges(Runnable create) {
+        Runtime runtime = Runtime.getRuntime();
+        BlockingGC.collect();
+        long memBefore = runtime.totalMemory() - runtime.freeMemory();
+        create.run();
+        BlockingGC.collect();
+        long memAfter = runtime.totalMemory() - runtime.freeMemory();
+        return memAfter - memBefore;
+    }
 }
