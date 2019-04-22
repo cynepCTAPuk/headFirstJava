1e603014650fe0310121b0663c9c1896c98778f2
cynepCTAPuk <CTAPuk@gmail.com>
2019-04-22 12:27:56 +0300

    chap02_observer

--- designPatterns/chap02_observer/swing/SwingObserverExample.java
+++ designPatterns/chap02_observer/swing/SwingObserverExample.java
@@ -4,34 +4,34 @@ import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import javax.swing.*;
-	
+
 public class SwingObserverExample {
-	JFrame frame;
-	
-	public static void main(String[] args) {
-		SwingObserverExample example = new SwingObserverExample();
-		example.go();
-	}
-	
-	public void go() {
-		frame = new JFrame();
-		JButton button = new JButton("Should I do it?");
+    JFrame frame;
 
-		// Without lambdas
-		//button.addActionListener(new AngelListener());
-		//button.addActionListener(new DevilListener());
+    public static void main(String[] args) {
+        SwingObserverExample example = new SwingObserverExample();
+        example.go();
+    }
 
-		// With lambdas
-		button.addActionListener(event -> System.out.println("Don't do it, you might regret it!"));
-		button.addActionListener(event -> System.out.println("Come on, do it!"));
-		frame.getContentPane().add(BorderLayout.CENTER, button);
+    public void go() {
+        frame = new JFrame();
+        JButton button = new JButton("Should I do it?");
 
-		// Set frame properties 
-		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		frame.getContentPane().add(BorderLayout.CENTER, button);
-		frame.setSize(300,300);
-		frame.setVisible(true);
-	}
+        // Without lambdas
+        //button.addActionListener(new AngelListener());
+        //button.addActionListener(new DevilListener());
+
+        // With lambdas
+        button.addActionListener(event -> System.out.println("Don't do it, you might regret it!"));
+        button.addActionListener(event -> System.out.println("Come on, do it!"));
+        frame.getContentPane().add(BorderLayout.CENTER, button);
+
+        // Set frame properties
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.getContentPane().add(BorderLayout.CENTER, button);
+        frame.setSize(300, 300);
+        frame.setVisible(true);
+    }
 	
 	/*
 	 * Remove these two inner classes to use lambda expressions instead.
