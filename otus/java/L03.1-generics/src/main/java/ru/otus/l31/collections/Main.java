477b9f51b08baf44f2757e7855d56bbf59f2521d
cynepCTAPuk <CTAPuk@gmail.com>
2019-04-22 15:57:55 +0300

    L03.1-generics

--- otus/java/L03.1-generics/src/main/java/ru/otus/l31/collections/Main.java
+++ otus/java/L03.1-generics/src/main/java/ru/otus/l31/collections/Main.java
@@ -1,24 +1,56 @@
 package ru.otus.l31.collections;
 
 import java.util.*;
+import java.util.stream.Collectors;
 
 /**
  * Created by tully.
  */
 @SuppressWarnings("ResultOfMethodCallIgnored")
 public class Main {
-    private static final int MEASURE_COUNT = 1_000;
+    private static final int MEASURE_COUNT = 1;
+    private static final int min = 0;
+    private static final int max = 9_999_999;
 
     public static void main(String... args) {
-        Collection<Integer> example = new LinkedList<>();
-        int min = 0;
-        int max = 9_999_999;
-        for (int i = min; i < max + 1; i++) {
-            example.add(i);
-        }
+        Collection<Integer> arrayList = new ArrayList<>();
+        fillCollection(arrayList);
+//        Collections.shuffle((List<Integer>) arrayList);
+//        calcTime(() -> Collections.sort((List<Integer>) arrayList);
+        calcTime(() -> arrayList.contains(max));
+        System.out.println(" - ArraySet");
 
-        //Collections.shuffle((List<Integer>) example);
-        calcTime(() -> example.contains(min));
+        Collection<Integer> hashSet = new HashSet<>();
+        fillCollection(hashSet);
+//        Collections.shuffle((List<Integer>) hashSet);
+        calcTime(() -> hashSet.contains(max));
+        System.out.println(" - HashSet");
+
+        Collection<Integer> linkedList = new LinkedList<>();
+        fillCollection(linkedList);
+//        Collections.shuffle((List<Integer>) linkedList);
+        calcTime(() -> linkedList.contains(max));
+        System.out.println(" - LinkedList");
+
+/*
+        Collection<Integer> linkedHashSet = new LinkedHashSet<>();
+        fillCollection(linkedHashSet);
+//        Collections.shuffle((List<Integer>) linkedHashSet);
+        calcTime(() -> linkedHashSet.contains(max));
+        System.out.println(" - LinkedHashSet");
+*/
+
+/*
+        Collection<Integer> treeSet = new TreeSet<>();
+        fillCollection(treeSet);
+//        Collections.shuffle((List<Integer>) treeSet);
+        calcTime(() -> treeSet.contains(max));
+        System.out.println(" - TreeSet");
+*/
+    }
+
+    private static void fillCollection(Collection<Integer> example) {
+        for (int i = min; i < max + 1; i++) example.add(i);
     }
 
     private static void calcTime(Runnable runnable) {
@@ -26,7 +58,7 @@ public class Main {
         for (int i = 0; i < MEASURE_COUNT; i++) runnable.run();
         long finishTime = System.nanoTime();
         long timeNs = (finishTime - startTime) / MEASURE_COUNT;
-        System.out.println("Time spent: "
-                + timeNs + "ns (" + timeNs / 1_000_000 + "ms)");
+        System.out.printf("Time spent: %,14d ns ( %,5d ms)",
+                timeNs, timeNs / 1_000_000);
     }
 }
