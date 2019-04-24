9e5238e17b4e2391bada6bc9dc16db350f26480d
cynepCTAPuk <CTAPuk@gmail.com>
2019-04-24 17:54:42 +0300

    decorator

--- designPatterns/chap03_decorator/own/InputTest.java
+++ designPatterns/chap03_decorator/own/InputTest.java
@@ -5,14 +5,20 @@ import java.io.*;
 public class InputTest {
     public static void main(String[] args) {
         int c;
-        try {
-            InputStream in = new LowerCaseInputStream(new BufferedInputStream(
-                    new FileInputStream("c:/000/test.txt")));
+        try (InputStream in = new BufferedInputStream(
+                new FileInputStream("c:/000/test.txt"))) {
             while ((c = in.read()) >= 0) {
                 System.out.print((char) c);
             }
-        } catch (FileNotFoundException e) {
+        } catch (IOException e) {
             e.printStackTrace();
+        }
+        System.out.println();
+        try (InputStream in = new LowerCaseInputStream(new BufferedInputStream(
+                new FileInputStream("c:/000/test.txt")))) {
+            while ((c = in.read()) >= 0) {
+                System.out.print((char) c);
+            }
         } catch (IOException e) {
             e.printStackTrace();
         }
--- designPatterns/chap03_decorator/own/LowerCaseInputStream.java
+++ designPatterns/chap03_decorator/own/LowerCaseInputStream.java
@@ -21,7 +21,7 @@ public class LowerCaseInputStream extends FilterInputStream {
     @Override
     public int read() throws IOException {
         int c = in.read();
-        return (c==-1 ? c:Character.toLowerCase((char)c));
+        return (c == -1 ? c : Character.toLowerCase((char) c));
     }
 
     @Override
