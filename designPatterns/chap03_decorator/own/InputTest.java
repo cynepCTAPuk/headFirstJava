6c8bbb42cc4e9eb33192a2989d4cdfe0661f1a7e
cynepCTAPuk <CTAPuk@gmail.com>
2019-04-24 16:31:14 +0300

    decorator

---
+++ designPatterns/chap03_decorator/own/InputTest.java
@@ -0,0 +1,20 @@
+package chap03_decorator.own;
+
+import java.io.*;
+
+public class InputTest {
+    public static void main(String[] args) {
+        int c;
+        try {
+            InputStream in = new LowerCaseInputStream(new BufferedInputStream(
+                    new FileInputStream("c:/000/test.txt")));
+            while ((c = in.read()) >= 0) {
+                System.out.print((char) c);
+            }
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
