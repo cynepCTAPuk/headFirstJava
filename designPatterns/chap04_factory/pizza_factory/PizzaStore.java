cf5ea270aadab48f7a52ec0225c74549610ea73b
cynepCTAPuk <CTAPuk@gmail.com>
2019-04-24 19:56:44 +0300

    factory

--- designPatterns/chap04_factory/pizza_factory/PizzaStore.java
+++ designPatterns/chap04_factory/pizza_factory/PizzaStore.java
@@ -8,7 +8,7 @@ abstract class PizzaStore {
 
     Pizza orderPizza(String type) {
         Pizza pizza;
-        pizza = factory.createPizza(type);
+        pizza = createPizza(type);
         pizza.prepare();
         pizza.bake();
         pizza.cut();
