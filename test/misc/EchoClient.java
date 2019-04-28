34ff590592214f71b018f75ab039a0ac268b8fa7
cynepCTAPuk <CTAPuk@gmail.com>
2019-04-28 20:42:13 +0300

    test misc

--- test/misc/EchoClient.java
+++ test/misc/EchoClient.java
@@ -9,27 +9,30 @@ public class EchoClient {
         }
         PrintWriter out = null;
         BufferedReader networkIn = null;
-        try {
-            Socket theSocket = new Socket(hostname, 7);
-            networkIn = new BufferedReader(new InputStreamReader(theSocket.getInputStream()));
-            BufferedReader userIn = new BufferedReader(new InputStreamReader(System.in));
-            out = new PrintWriter(theSocket.getOutputStream());
-            System.out.println("Connected to echo server");
-            while (true) {
-                String theLine = userIn.readLine();
-                if (theLine.equals(".")) break;
-                out.println(theLine);
-                out.flush();
-                System.out.println(networkIn.readLine());
-            }
-        } // end try
-        catch (IOException e) {
-            System.err.println(e);
-        } finally {
+        for (int i = 1; i < 100; i++) {
+            System.out.printf("\nport N: %3d ", i);
             try {
-                if (networkIn != null) networkIn.close();
-                if (out != null) out.close();
-            } catch (IOException e) {
+                Socket theSocket = new Socket(hostname, i);
+                networkIn = new BufferedReader(new InputStreamReader(theSocket.getInputStream()));
+                BufferedReader userIn = new BufferedReader(new InputStreamReader(System.in));
+                out = new PrintWriter(theSocket.getOutputStream());
+                System.out.println("Connected to echo server");
+                while (true) {
+                    String theLine = userIn.readLine();
+                    if (theLine.equals(".")) break;
+                    out.println(theLine);
+                    out.flush();
+                    System.out.println(networkIn.readLine());
+                }
+            } // end try
+            catch (IOException e) {
+                System.err.println(e);
+            } finally {
+                try {
+                    if (networkIn != null) networkIn.close();
+                    if (out != null) out.close();
+                } catch (IOException e) {
+                }
             }
         }
     } // end main
--- test/misc/MyBirthDay.java
+++ test/misc/MyBirthDay.java
@@ -8,15 +8,12 @@ public class MyBirthDay {
         LocalDate bd = LocalDate.of(1962, Month.SEPTEMBER, 10);
         LocalDate today = LocalDate.now();
         LocalTime time = LocalTime.now();
-        System.out.println("Today is " + today + " Time is " + time);
         System.out.printf("Today is %s Time is %s\n", today, time);
 
         Period p1 = Period.between(bd, today);
         long p2 = ChronoUnit.DAYS.between(bd, today);
-        System.out.println("You are " +
-                p1.getYears() + " years, " +
-                p1.getMonths() + " months, " +
-                p1.getDays() + " days old. (" + p2 + " days total)");
+        System.out.printf("You are %d years, %d moths %d days old (%,d days)\n",
+                p1.getYears(), p1.getMonths(), p1.getDays(), p2);
 
         LocalDate nextBDay = bd.withYear(today.getYear());
 
@@ -26,8 +23,7 @@ public class MyBirthDay {
 
         p1 = Period.between(today, nextBDay);
         p2 = ChronoUnit.DAYS.between(today, nextBDay);
-        System.out.println("There are " +
-                p1.getMonths() + " months and " +
-                p1.getDays() + " days until your next birthday. (" + p2 + " - total days)");
+        System.out.printf("There are %d months and %d days until your next birthday (%d days)\n",
+                p1.getMonths(), p1.getDays(),p2);
     }
 }
