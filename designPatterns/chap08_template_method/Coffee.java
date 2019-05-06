5f7f9c662e67fb9fad4053e06d9217ac466a83ce
cynepCTAPuk <CTAPuk@gmail.com>
2019-05-06 14:37:55 +0300

    pattern Template method

---
+++ designPatterns/chap08_template_method/Coffee.java
@@ -0,0 +1,12 @@
+package chap08_template_method;
+
+public class Coffee extends CaffeineBeverage {
+    @Override
+    void brew() {
+        System.out.println("Dripping Coffee through filter");
+    }
+    @Override
+    void addCondements() {
+        System.out.println("Adding Sugar and Milk");
+    }
+}
---
+++ designPatterns/chap08_template_method/Tea.java
@@ -0,0 +1,12 @@
+package chap08_template_method;
+
+public class Tea extends CaffeineBeverage {
+    @Override
+    public void brew() {
+        System.out.println("Steeping the tea");
+    }
+    @Override
+    void addCondements() {
+        System.out.println("Adding Lemon");
+    }
+}
