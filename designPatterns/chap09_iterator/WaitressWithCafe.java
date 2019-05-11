e9d59616c7bbdbf44b877229de112efd291d856c
cynepCTAPuk <CTAPuk@gmail.com>
2019-05-11 19:49:22 +0300

    pattern Iterator

---
+++ designPatterns/chap09_iterator/WaitressWithCafe.java
@@ -0,0 +1,62 @@
+package chap09_iterator;
+
+import java.util.Iterator;
+
+public class WaitressWithCafe {
+    private Menu pancakeHouseMenu;
+    private Menu dinerMenu;
+    private Menu cafeMenu;
+
+    public WaitressWithCafe(Menu pancakeHouseMenu, Menu dinerMenu, Menu cafeMenu) {
+        this.pancakeHouseMenu = pancakeHouseMenu;
+        this.dinerMenu = dinerMenu;
+        this.cafeMenu = cafeMenu;
+    }
+
+    private void printMenu(Iterator iterator) {
+        while (iterator.hasNext()) {
+            MenuItem menuItem = (MenuItem) iterator.next();
+            System.out.println(menuItem.getName() + ", " +
+                    menuItem.getPrice() + " -- " + menuItem.getDescription());
+        }
+    }
+
+    public void printMenu() {
+        Iterator<MenuItem> pancakeIterator = pancakeHouseMenu.createIterator();
+        Iterator<MenuItem> dinerIterator = dinerMenu.createIterator();
+        Iterator<MenuItem> cafeIterator = cafeMenu.createIterator();
+        System.out.println("MENU\n---\nBREAKFAST");
+        printMenu(pancakeIterator);
+        System.out.println("\nLUNCH");
+        printMenu(dinerIterator);
+        System.out.println("\nDINNER");
+        printMenu(cafeIterator);
+    }
+
+    public void printBreakfastMenu() {
+        Iterator pancakeIterator = pancakeHouseMenu.createIterator();
+        System.out.println("MENU\n---\nBREAKFAST");
+        printMenu(pancakeIterator);
+    }
+
+    public void printLunchMenu() {
+        Iterator dinerIterator = dinerMenu.createIterator();
+        System.out.println("MENU\n---\nLUNCH");
+        printMenu(dinerIterator);
+    }
+
+    public void printVegetarianMenu() {
+    }
+
+    public boolean isItemVegetarian(MenuItem menuItem) {
+        return menuItem.isVegetarian();
+    }
+
+    public static void main(String[] args) {
+        Menu pancakeHouseMenu = new PancakeHouseMenu();
+        Menu dinerMenu = new DinerMenu();
+        Menu cafeMenu = new CafeMenu();
+        WaitressWithCafe waiter = new WaitressWithCafe(pancakeHouseMenu, dinerMenu, cafeMenu);
+        waiter.printMenu();
+    }
+}
