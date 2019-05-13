4867653800060acf6bee66fd4c1563d15ada1c9b
cynepCTAPuk <CTAPuk@gmail.com>
2019-05-13 13:52:24 +0300

    pattern Strategy

--- designPatterns/chap01_strategy/Duck.java
+++ designPatterns/chap01_strategy/Duck.java
@@ -19,9 +19,9 @@ public abstract class Duck {
     public void swim() { System.out.println("All ducks float, even decoys!");}
 
     public void performFly() { flyBehavior.fly();}
-    public void setFlyBehavior(FlyBehavior fb) { flyBehavior = fb;}
-
     public void performQuack() { quackBehavior.quack();}
+
+    public void setFlyBehavior(FlyBehavior fb) { flyBehavior = fb;}
     public void setQuackBehavior(QuackBehavior qb) { quackBehavior = qb;}
 }
 
