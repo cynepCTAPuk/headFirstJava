e2c531fb0fb8fa12b3d9b4a0f72a41018697deee
cynepCTAPuk <CTAPuk@gmail.com>
2019-05-13 11:49:33 +0300

    pattern Composite

---
+++ designPatterns/chap09_composite/menu/Waitress.java
@@ -0,0 +1,13 @@
+package chap09_composite.menu;
+
+public class Waitress {
+    private MenuComponent allMenus;
+
+    public Waitress(MenuComponent allMenus) {
+        this.allMenus = allMenus;
+    }
+
+    public void printMenu() {
+        allMenus.print();
+    }
+}
