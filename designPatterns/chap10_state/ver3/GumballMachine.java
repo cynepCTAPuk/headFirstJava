958d48ce3c3e51c4d0d5c049b9eab87e47bd0cfb
cynepCTAPuk <CTAPuk@gmail.com>
2019-05-14 12:30:56 +0300

    pattern State

--- designPatterns/chap10_state/ver3/GumballMachine.java
+++ designPatterns/chap10_state/ver3/GumballMachine.java
@@ -35,6 +35,7 @@ public class GumballMachine {
     public void refill(int count) {
         this.count += count;
         System.out.println("The gumball machine was just refilled; it's new count is: " + this.count);
+        state.refill();
     }
 
     public void setState(State state) {
