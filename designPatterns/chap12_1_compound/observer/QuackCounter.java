c432cc5f53c73241af070fe6ed4032cf6182e76c
cynepCTAPuk <CTAPuk@gmail.com>
2019-05-27 21:26:22 +0300

    Compound pattern

--- designPatterns/chap12_1_compound/observer/QuackCounter.java
+++ designPatterns/chap12_1_compound/observer/QuackCounter.java
@@ -7,27 +7,22 @@ public class QuackCounter implements Quackable {
     public QuackCounter(Quackable duck) {
         this.duck = duck;
     }
-
     @Override
     public void quack() {
         duck.quack();
         numberOfQuacks++;
     }
-
     public static int getQuacks() {
         return numberOfQuacks;
     }
-
     @Override
     public void registerObserver(Observer observer) {
         duck.registerObserver(observer);
     }
-
     @Override
     public void notifyObservers() {
         duck.notifyObservers();
     }
-
     @Override
     public String toString() {
         return duck.toString();
