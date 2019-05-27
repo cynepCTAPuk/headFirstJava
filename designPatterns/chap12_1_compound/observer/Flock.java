b8a2edd05292bf7c9599aa0ef4bb947090182f76
cynepCTAPuk <CTAPuk@gmail.com>
2019-05-27 21:27:32 +0300

    Compound pattern

--- designPatterns/chap12_1_compound/observer/Flock.java
+++ designPatterns/chap12_1_compound/observer/Flock.java
@@ -9,7 +9,6 @@ public class Flock implements Quackable {
     public void add(Quackable duck) {
         ducks.add(duck);
     }
-
     public void quack() {
         Iterator<Quackable> iterator = ducks.iterator();
         while (iterator.hasNext()) {
@@ -17,7 +16,6 @@ public class Flock implements Quackable {
             duck.quack();
         }
     }
-
     @Override
     public void registerObserver(Observer observer) {
         Iterator<Quackable> iterator = ducks.iterator();
@@ -26,10 +24,8 @@ public class Flock implements Quackable {
             duck.registerObserver(observer);
         }
     }
-
     @Override
     public void notifyObservers() {}
-
     @Override
     public String toString() {
         return "Flock of Ducks";
