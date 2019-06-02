98c08077b0da3e1bf5a11f26f03d54e6425d63bc
cynepCTAPuk <CTAPuk@gmail.com>
2019-06-02 20:58:14 +0300

    Pazzle 11

--- java_puzzlers/03_puzzlers_with_character/puzzle_11/LastLaugh.java
+++ java_puzzlers/03_puzzlers_with_character/puzzle_11/LastLaugh.java
@@ -2,8 +2,8 @@ package puzzle_11;
 
 public class LastLaugh {
     public static void main(String args[]) {
-        System.out.print("H" + "a");
-        System.out.println('H' + 'a');
+        System.out.println("H" + "a"); // Ha
+        System.out.println('H' + 'a'); // (int)'H' + (int)'a'
 
         System.out.println("" + 'H' + 'a');
         System.out.println("2 + 2 = " + 2 + 2);
