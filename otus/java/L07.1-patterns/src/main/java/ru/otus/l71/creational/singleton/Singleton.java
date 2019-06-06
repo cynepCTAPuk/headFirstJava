c0062b56c5cc1f96d05db8ef0f0401a5548118df
cynepCTAPuk <CTAPuk@gmail.com>
2019-06-06 15:41:28 +0300

    pattern Object Singleton

--- otus/java/L07.1-patterns/src/main/java/ru/otus/l71/creational/singleton/Singleton.java
+++ otus/java/L07.1-patterns/src/main/java/ru/otus/l71/creational/singleton/Singleton.java
@@ -9,21 +9,13 @@ public class Singleton {
     private static Singleton singleton;
     private String message;
 
-    private Singleton() {
-    }
+    private Singleton() {}
 
     public static Singleton instance() {
-        if (singleton == null) {
-            singleton = new Singleton();
-        }
+        if (singleton == null) singleton = new Singleton();
         return singleton;
     }
 
-    public String getMessage() {
-        return message;
-    }
-
-    public void setMessage(String message) {
-        this.message = message;
-    }
+    public String getMessage() {return message;}
+    public void setMessage(String message) {this.message = message;}
 }
