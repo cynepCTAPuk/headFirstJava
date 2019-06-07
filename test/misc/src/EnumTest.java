65d972b8df168c5ae3ace744981042f23dcfdce7
cynepCTAPuk <CTAPuk@gmail.com>
2019-06-07 17:22:31 +0300

    Enum Test

--- test/misc/src/EnumTest.java
+++ test/misc/src/EnumTest.java
@@ -4,56 +4,50 @@ public class EnumTest {
             @Override
             String sayHello() {
                 return "Hello";
-            }
-        },
+            }},
         POLAND("PLN") {
             @Override
             String sayHello() {
                 return "Dzien dobry";
-            }
-        },
+            }},
         GERMANY("EUR") {
             @Override
             String sayHello() {
                 return "Hallo";
-            }
-        },
+            }},
         LAOPAPAS {
             @Override
             String sayHello() {
                 return "Lapapioooo";
-            }
-        },
+            }},
         ZIMKABU() {
             @Override
             String sayHello() {
                 return "Shakalaka";
-            }
-        };
+            }};
 
         String currency;
-
+        Country() {
+        }
         Country(String currency) {
             this.currency = currency;
         }
-
-        Country() {
-        }
-
         boolean hasCurrency() {
             return currency != null;
         }
-
         abstract String sayHello();
     }
 
     public static void main(String[] args) {
-        System.out.printf("%-10s|%8s|%8s\n", "Country", "Currency", "Greetings");
+        System.out.printf("|   %-10s|%8s|  %-12s\n", "Country", "Currency", "Greetings");
+        System.out.printf("|---%-10s|%8s|%14s\n", "-".repeat(10), "-".repeat(8), "-".repeat(14));
         for (Country country : Country.values()) {
             if (country.hasCurrency()) {
-                System.out.printf("%-10s:  %-6s:  %-8s\n", country, country.currency, country.sayHello());
+                System.out.printf("|%2d %-10s:  %-6s:  %-12s\n",
+                        country.ordinal() + 1, country, country.currency, country.sayHello());
             } else {
-                System.out.printf("%-10s:  %-6s:  %-8s\n", country, "-", country.sayHello());
+                System.out.printf("|%2d %-10s:  %-6s:  %-12s\n",
+                        country.ordinal() + 1, country, "N/D", country.sayHello());
             }
         }
     }
