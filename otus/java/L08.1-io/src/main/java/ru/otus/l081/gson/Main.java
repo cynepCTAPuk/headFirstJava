96379f6c143a24fb75301b30f25c12ba8145ce7c
cynepCTAPuk <CTAPuk@gmail.com>
2019-06-07 20:48:40 +0300

    gson

--- otus/java/L08.1-io/src/main/java/ru/otus/l081/gson/Main.java
+++ otus/java/L08.1-io/src/main/java/ru/otus/l081/gson/Main.java
@@ -8,13 +8,13 @@ import com.google.gson.Gson;
 public class Main {
     public static void main(String[] args) {
         BagOfPrimitives obj = new BagOfPrimitives(22, "test", 10);
-        System.out.println(obj);
+        System.out.println("1 " + obj);                 // 1
         Gson gson = new Gson();
         String json = gson.toJson(obj);
-        System.out.println(json);
+        System.out.println("2 " + json);                // 2
 
         BagOfPrimitives obj2 = gson.fromJson(json, BagOfPrimitives.class);
-        System.out.println(obj.equals(obj2));
-        System.out.println(obj2);
+        System.out.println("3 " + obj.equals(obj2));    // 3
+        System.out.println("4 " + obj2);                // 4
     }
 }
