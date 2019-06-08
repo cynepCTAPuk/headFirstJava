2e5f40f9df153648be287cc2cc1b04621d3b9e9e
cynepCTAPuk <CTAPuk@gmail.com>
2019-06-08 19:51:52 +0300

    serialization

--- otus/java/L08.1-io/src/main/java/ru/otus/l081/io/Main.java
+++ otus/java/L08.1-io/src/main/java/ru/otus/l081/io/Main.java
@@ -13,19 +13,19 @@ public class Main {
 
     private void writeObject() throws IOException, ClassNotFoundException {
         Student student = new Student(42, "tully", "java");
-        System.out.println("0 " + student);
+        System.out.println("0 " + student);                 // 0
         String file = "C:/000/L08.1-io/student.out";
         JavaIOHelper.writeObject(file, student);
 
         Person readStudent1 = JavaIOHelper.readObject(file, Person.class);
-        System.out.println("1 " + readStudent1);
+        System.out.println("1 " + readStudent1);            // 1
 
         byte[] bytes = JavaIOHelper.serialize(student);
         JavaIOHelper.writeToFile(bytes, file);
 
         bytes = JavaIOHelper.readFromFile(file);
         Person readStudent2 = (Person) JavaIOHelper.deserialize(bytes);
-        System.out.println("2 " + readStudent2);
+        System.out.println("2 " + readStudent2);            // 2
     }
 
     private void writeTextFile() throws IOException {
@@ -36,7 +36,7 @@ public class Main {
 
         textBytes = JavaIOHelper.readFromFile(textFile);
         String readString = new String(textBytes);
-        System.out.println("3 " + readString);
+        System.out.println("3 " + readString);              // 3
     }
 
 }
