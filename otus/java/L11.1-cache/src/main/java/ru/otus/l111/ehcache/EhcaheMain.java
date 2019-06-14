36f36e9803d27b52e339a0d151ab321ac70bb8c5
cynepCTAPuk <CTAPuk@gmail.com>
2019-06-14 23:32:47 +0300

    ehcache

--- otus/java/L11.1-cache/src/main/java/ru/otus/l111/ehcache/EhcaheMain.java
+++ otus/java/L11.1-cache/src/main/java/ru/otus/l111/ehcache/EhcaheMain.java
@@ -29,7 +29,7 @@ public class EhcaheMain {
                 false, true);
 //        cacheNamesExample();
 //        evictedExample();
-//        lifeExample();
+        lifeExample();
 //        idleExample();
     }
 
