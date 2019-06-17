b2ab7e7110042465651d09c5b323535c7ad06d6a
cynepCTAPuk <CTAPuk@gmail.com>
2019-06-17 13:43:37 +0300

    MongoDB

--- otus/java/L11.2.2-mongo/src/main/java/ru/otus/mongo/MongoMain.java
+++ otus/java/L11.2.2-mongo/src/main/java/ru/otus/mongo/MongoMain.java
@@ -50,26 +50,20 @@ public class MongoMain {
     }
 
     private void example() {
-        String collectionName = "user23";
+        String collectionName = "user25";
 
         insert(collectionName, new Document("name", "Tully").append("company", "Umbrella INC"));
         listDatabaseNames();
 
         FindIterable<Document> iterable = findByName(collectionName, "Tully");
-
         System.out.println("\nFind by name result: ");
-        for (Document document : iterable) {
-            System.out.println(document);
-        }
+        for (Document document : iterable) System.out.println(document);
 
         ObjectId id = insert(collectionName, new Document("name", "Sully").append("age", 42));
 
         FindIterable<Document> iterableID = findById(collectionName, id);
-
         System.out.println("\nFind by id result: ");
-        for (Document document : iterableID) {
-            System.out.println(document);
-        }
+        for (Document document : iterableID) System.out.println(document);
 
         mongoClient.close();
     }
@@ -91,13 +85,12 @@ public class MongoMain {
     private void listDatabaseNames() {
         MongoIterable<String> dbs = mongoClient.listDatabaseNames();
         System.out.println("\nDatabase names:");
-        for (String db : dbs) {
-            System.out.println(db);
-        }
+        for (String db : dbs) System.out.println(db);
     }
 
     private ObjectId insert(String collectionName, Document doc) {
-        MongoCollection<Document> collection = mongoDatabase.getCollection(collectionName); //creates if not exist
+        MongoCollection<Document> collection = mongoDatabase.getCollection(collectionName);
+        //creates if not exist
         collection.insertOne(doc);
         return (ObjectId) doc.get("_id");
     }
