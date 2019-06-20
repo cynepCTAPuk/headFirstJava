54aa2e300220224f64030ea6b594486ffb2c5485
cynepCTAPuk <CTAPuk@gmail.com>
2019-06-20 13:45:59 +0300

    log4j test

--- test/log4j/src/main/java/com/example/log4j_test/OrderLogicOld.java
+++ test/log4j/src/main/java/com/example/log4j_test/OrderLogicOld.java
@@ -1,27 +1,18 @@
 package com.example.log4j_test;
-
-import org.apache.log4j.Logger;
-
 /*
 https://devcolibri.com/учимся-ввести-логирования-с-помощью-log4j/
  */
-public class OrderLogic {
-    // Инициализация логера
-    private static final Logger log = Logger.getLogger(OrderLogic.class);
+public class OrderLogicOld {
 
     public void doOrder(){
         // какае-то логика
         System.out.println("Заказ оформлен!");
-        // логируем инфо
-        log.info("Это информационное сообщение!");
         addToCart();
     }
 
     private void addToCart() {
         // добавление товара в корзину
         System.out.println("Товар добавлен в корзину");
-        // логируем ошибку
-        log.error("Это сообщение ошибки");
     }
 
 }
