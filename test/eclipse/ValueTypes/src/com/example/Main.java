9ae6bf6d7544d4d8eb0ff38a673f63572f4e1cc8
cynepCTAPuk <CTAPuk@gmail.com>
2019-06-21 16:44:43 +0300

    -

--- test/eclipse/ValueTypes/src/com/example/Main.java
+++ test/eclipse/ValueTypes/src/com/example/Main.java
@@ -1,16 +1,17 @@
 package com.example;
 
-public class Main
-{
+public class Main {
 
-	public static void main(String[] args)
-	{
-		byte bt = 127; // 0x7F;
-		for (int i = 33; i < 128; i++)
-		{
-			System.out.print((char) i + " ");
+	public static void main(String[] args) {
+		int n = 0;
+		for (int i = 33; i < 128; i++) {
+			System.out.printf("%3d:%2c ", i, (char) i);
+			if (++n % 10 == 0)
+				System.out.println();
 		}
-		System.out.println((char) (bt - 2));
+		System.out.println();
+		System.out.println();
+
 		short sh = 20;
 		int in = 1_000;
 		long lg = 12_345_678_901L;
@@ -20,34 +21,26 @@ public class Main
 		System.out.printf("%,d -> %,d\n", lg, sh);
 		System.out.printf("%,d -> %,d\n", lg, in);
 
-		float fl = 1.0F;
-		double db = 1.0; // 1D
-
 		int x = 5;
 		double y = x / 2;
-
 		System.out.println(y);
+		System.out.println();
 
-		System.out.printf("%,d\n", Byte.MAX_VALUE);
-		System.out.printf("%,d\n", Short.MAX_VALUE);
-		System.out.printf("%,d\n", Integer.MAX_VALUE);
-		System.out.printf("%,d\n", Integer.MAX_VALUE + 1);
-		System.out.printf("%,d\n", Integer.MIN_VALUE);
-		System.out.printf("%,d\n", Long.MAX_VALUE);
+		System.out.printf("%-22s%,26d\n", "Byte.MAX_VALUE", Byte.MAX_VALUE);
+		System.out.printf("%-22s%,26d\n", "Short.MAX_VALUE", Short.MAX_VALUE);
+		System.out.printf("%-22s%,26d\n", "int.max 1<<31 - 1", (1 << 31)-1);
+		System.out.printf("%-22s%,26d\n", "Integer.MAX_VALUE", Integer.MAX_VALUE);
+		System.out.printf("%-22s%,26d\n", "Integer.MAX_VALUE + 1", Integer.MAX_VALUE + 1);
+		System.out.printf("%-22s%,26d\n", "Integer.MIN_VALUE", Integer.MIN_VALUE);
+		System.out.printf("%-22s%,26d\n", "long.max 1<<63 -1", (1L << 63)-1);
+		System.out.printf("%-22s%,26d\n", "Long.MAX_VALUE", Long.MAX_VALUE);
+		System.out.println();
 
 		boolean a = true;
 		boolean b = false;
-		boolean c;
-		c = a && b; // and
-		System.out.println(c);
-
-		c = a & b; // and
-		System.out.println(c);
-
-		c = a || b; // OR
-		System.out.println(c);
-
-		c = !a; // NOT
-		System.out.println(c);
+		System.out.println(a && b);
+		System.out.println(a & b);
+		System.out.println(a || b);
+		System.out.println(!a);
 	}
 }
