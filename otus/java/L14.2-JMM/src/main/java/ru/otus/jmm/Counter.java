6b703447251462376221370435658e0ce4ec4117
cynepCTAPuk <CTAPuk@gmail.com>
2019-07-05 17:27:46 +0300

    JMM

--- otus/java/L14.2-JMM/src/main/java/ru/otus/jmm/Counter.java
+++ otus/java/L14.2-JMM/src/main/java/ru/otus/jmm/Counter.java
@@ -1,25 +1,32 @@
 package ru.otus.jmm;
 
+import java.util.concurrent.atomic.AtomicLong;
+
 public class Counter {
-    static final int N = 100_000;
-    static final int J = 10;
-    long val = 0;
+    static final int N = 2;
+    static final int J = 1_000_000_000;
+    static AtomicLong val = new AtomicLong(0);
 
     void inc() {
-        val++;
+        val.incrementAndGet();
     }
 
     public static void main(String[] args) throws InterruptedException {
+        long t1 = System.currentTimeMillis();
         Counter c = new Counter();
-        Runnable r = () -> {
+        Runnable runnable = () -> {
             for (int i = 0; i < J; i++) c.inc();
         };
         Thread[] ts = new Thread[N];
         for (int i = 0; i < N; i++) {
-            ts[i] = new Thread(r);
+            ts[i] = new Thread(runnable);
             ts[i].start();
         }
         for (int i = 0; i < N; i++) ts[i].join();
-        System.out.printf("Count: %,d\n", c.val);
+        long t2 = System.currentTimeMillis();
+        double d = ((N * J - c.val.longValue()) * 100.00) / (N * J);
+        System.out.printf("Time: %,d\n", t2-t1);
+        System.out.printf("Count: %,d\n", c.val.longValue());
+        System.out.printf("Процент потери данных: %.2f%s\n", d, "%");
     }
 }
