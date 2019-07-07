544d08a69819c7ab010454ff17f105324f201320
cynepCTAPuk <CTAPuk@gmail.com>
2019-07-07 19:29:31 +0300

    Thread

--- otus/java/L14.1-thread/src/main/java/ru/otus/l141a/RandomRun.java
+++ otus/java/L14.1-thread/src/main/java/ru/otus/l141a/RandomRun.java
@@ -7,8 +7,9 @@ class RandomRun {
     void start() {
         long startTimeNS = System.nanoTime();
         for (int i = 0; i < MAX_THREADS_COUNT; ++i) {
-            new Thread(() -> System.out.println(Thread.currentThread().getName() + "\t: "
-                    + (System.nanoTime() - startTimeNS) + "ns")).start();
+//            new Thread(() -> System.out.println(Thread.currentThread().getName() + "\t: " + (System.nanoTime() - startTimeNS) + "ns")).start();
+            new Thread(() -> System.out.printf("%-9s : %,11d\n",
+                    Thread.currentThread().getName(),(System.nanoTime() - startTimeNS))).start();
         }
     }
 }
