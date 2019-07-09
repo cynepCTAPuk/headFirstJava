551c5216470f61c5c095bba7e17d8f7b3b487d14
cynepCTAPuk <CTAPuk@gmail.com>
2019-07-09 23:34:40 +0300

    SkillBox webinar 190709

--- skillbox/webinar190709/src/main/java/Main.java
+++ skillbox/webinar190709/src/main/java/Main.java
@@ -1,3 +1,14 @@
+import com.dropbox.core.DbxException;
+import com.dropbox.core.DbxRequestConfig;
+import com.dropbox.core.v2.DbxClientV2;
+
 public class Main {
-    
+    private static final String ACCESS_TOKEN =
+        "_uAHwPs06v8AAAAAAABJrLKBpFQwNJgNbYQVdcHDDHKfMgH5KD7o4rnHRx_f_4IE";
+
+    public static void main(String args[]) throws DbxException {
+        DbxRequestConfig config = DbxRequestConfig.newBuilder(
+                "dropbox/java-tutorial").build();
+        DbxClientV2 client = new DbxClientV2(config, ACCESS_TOKEN);
+    }
 }
