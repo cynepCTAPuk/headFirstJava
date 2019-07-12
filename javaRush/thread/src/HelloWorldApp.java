24d79f5028c837530479b3f013f1fce80ebd8b45
cynepCTAPuk <CTAPuk@gmail.com>
2019-07-12 16:16:49 +0300

    ---

--- javaRush/thread/src/HelloWorldApp.java
+++
@@ -1,7 +0,0 @@
-class HelloWorldApp {
-    public static void main(String[] args) {
-        while (true) {
-//            do something, для демонстрации потоков и visualvm (см. в v/java)
-        }
-    }
-}
--- javaRush/thread/src/Thread30.java
+++ javaRush/thread/src/Thread30.java
@@ -5,14 +5,23 @@ public class Thread30 {
     static class Friend {
         private final String name;
 
-        public Friend(String name) {this.name = name;}
-        public String getName() {return this.name;}
+        public Friend(String name) {
+            this.name = name;
+        }
+
+        public String getName() {
+            return this.name;
+        }
 
         public synchronized void bow(Friend bower) {
             System.out.format("%s: %s has bowed to me!%n", this.name, bower.getName());
             bower.bowBack(this);
         }
 
+        // с большой вероятностью проявится deadlock
+        // jps - покажет id запущенной JVM
+        // jstack id - покажет проблему в запущенной JVM
+        // в данном случае достаточно убрать synchronized из метода bowBack
         public synchronized void bowBack(Friend bower) {
             System.out.format("%s: %s has bowed back to me!%n", this.name, bower.getName());
         }
@@ -22,6 +31,6 @@ public class Thread30 {
         final Friend alphonse = new Friend("Alphonse");
         final Friend gaston = new Friend("Gaston");
         new Thread(() -> alphonse.bow(gaston)).start();
-//        new Thread(() -> gaston.bow(alphonse)).start();
+        new Thread(() -> gaston.bow(alphonse)).start();
     }
 }
