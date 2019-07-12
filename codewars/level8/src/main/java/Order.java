a7b18492d2c2da5a5f5f51243aa01783b66d85da
cynepCTAPuk <CTAPuk@gmail.com>
2019-07-12 16:33:26 +0300

    Order strings by number inside word

---
+++ codewars/level8/src/main/java/Order.java
@@ -0,0 +1,10 @@
+public class Order {
+    public static String order(String words) {
+        String result = null;
+        String[] strings = words.split(" ");
+
+
+
+        return result;
+    }
+}
