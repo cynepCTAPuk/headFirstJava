2eba1aa47c62a957ba8520a415242f604c203996
cynepCTAPuk <CTAPuk@gmail.com>
2019-07-13 20:55:59 +0300

    Equal Sides Of An Array - Kata

--- codewars/level7/src/main/java/Kata.java
+++ codewars/level7/src/main/java/Kata.java
@@ -1,20 +1,22 @@
 public class Kata {
     public static int findEvenIndex(int[] arr) {
-        int sumLeft = 0;
-        int sumRite = 0;
         int l = 0;
-        int r = arr.length;
-        while (l != (r - 1)) {
-            if (sumLeft <= sumRite) sumLeft += arr[l++];
+        int r = arr.length - 1;
+        int sumLeft = arr[l];
+        int sumRite = arr[r];
+        while (l != (r - 2)) {
+            if (sumLeft <= sumRite) sumLeft += arr[++l];
             else sumRite += arr[--r];
             System.out.println(l + ":" + r + " " + sumLeft + " - " + sumRite);
         }
-        if (sumLeft == sumRite) return l;
+        if (sumLeft == sumRite) return r;
         else return -1;
     }
 
     public static void main(String[] args) {
-//        System.out.println(findEvenIndex(new int[]{1, 2, 3, 4, 3, 2, 1}));
-        System.out.println(findEvenIndex(new int[]{1, 100, 50, -51, 1, 1}));
+        System.out.println("*".repeat(10) + " " +
+                findEvenIndex(new int[]{1, 2, 3, 4, 3, 2, 1}));
+        System.out.println("*".repeat(10) + " " +
+                findEvenIndex(new int[]{1, 100, 50, -51, 1, 1}));
     }
 }
