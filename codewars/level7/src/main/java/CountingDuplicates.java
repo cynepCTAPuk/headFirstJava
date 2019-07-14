20806118fa82ab5947ae0f2c7b8d24ae922955fa
cynepCTAPuk <CTAPuk@gmail.com>
2019-07-14 03:11:11 +0300

    Counting Duplicates

--- codewars/level7/src/main/java/CountingDuplicates.java
+++ codewars/level7/src/main/java/CountingDuplicates.java
@@ -1,10 +1,17 @@
 public class CountingDuplicates {
     public static int duplicateCount(String text) {
-
-        return 0;
-    }
-
-    public static void main(String[] args) {
-        System.out.println(duplicateCount("abcda"));
+        text = text.toLowerCase();
+        int counter = 0;
+        for (int i = 0; i < text.length(); i++) {
+            String str1 = text.substring(i, i + 1);
+            for (int j = i + 1; j < text.length(); j++) {
+                String str2 = text.substring(j, j + 1);
+                if (str2.equals(str1) && !str2.equals("*")) {
+                    text = text.replaceAll(str2, "*");
+                    counter++;
+                }
+            }
+        }
+        return counter;
     }
 }
