8908baf3b7cc537dc476800579a05edbd52995c6
cynepCTAPuk <CTAPuk@gmail.com>
2019-07-17 00:37:01 +0300

    Primes in numbers

---
+++ codewars/level6/src/main/java/PrimesInNumbers.java
@@ -0,0 +1,16 @@
+public class PrimesInNumbers {
+    public static String factors(int n) {
+        String result = "";
+        for (int fac = 2; fac <= n; ++fac) {
+            int count;
+            for (count = 0; n % fac == 0; ++count) n /= fac;
+            if (count > 0) result += "(" + fac + (count > 1 ? "**" + count : "") + ")";
+        }
+        return result;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(factors(86240));
+        System.out.println(factors(7775460));
+    }
+}
--- codewars/level6/src/main/java/PrimesInNumbers1.java
+++ codewars/level6/src/main/java/PrimesInNumbers1.java
@@ -1,24 +1,24 @@
-public class PrimeDecomp {
+public class PrimesInNumbers1 {
     public static String factors(int n) {
-        int m = 2;
+        int factor = 2;
         StringBuffer result = new StringBuffer();
         while (n != 1) {
-            if (qty(n, m) != 0) {
-                result.append("(").append(m);
-                if (qty(n, m) != 1) result.append("**").append(qty(n, m));
+            if (count(n, factor) != 0) {
+                result.append("(").append(factor);
+                if (count(n, factor) != 1) result.append("**").append(count(n, factor));
                 result.append(")");
             }
-            n = (int) (n / Math.pow(m, qty(n, m)));
-            m++;
+            n = (int) (n / Math.pow(factor, count(n, factor)));
+            factor++;
         }
         return result.toString();
     }
 
-    static int qty(int n, int m) {
+    static int count(int n, int factor) {
         int k = 0;
-        while (n % m == 0) {
+        while (n % factor == 0) {
             k++;
-            n /= m;
+            n /= factor;
         }
         return k;
     }
