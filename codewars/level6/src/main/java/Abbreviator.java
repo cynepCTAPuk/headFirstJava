b0edc69d064eed3a116f7640f995461746483d58
cynepCTAPuk <CTAPuk@gmail.com>
2019-07-17 17:56:30 +0300

    Word a10n (abbreviation)

---
+++ codewars/level6/src/main/java/Abbreviator.java
@@ -0,0 +1,32 @@
+public class Abbreviator {
+    public static String abbreviate(String string) {
+        String result = "";
+        int i = 0;
+        while (i < string.length()) {
+            int n = 0;
+            if (Character.isLetter(string.charAt(i))) {
+                while (((i + n) < string.length()) && (Character.isLetter(string.charAt(i + n)))) {
+                    n++;
+                }
+                if (n < 4) {
+                    result += string.substring(i, i + n);
+                } else {
+                    result += string.substring(i, i + 1) + (n - 2) + string.substring(i + n - 1, i + n);
+                }
+            } else {
+                n++;
+                result += string.substring(i, i  + 1);
+            }
+            i = i + n;
+        }
+        return result;
+    }
+
+
+    public static void main(String[] args) {
+        System.out.println(abbreviate("internationalization"));
+        System.out.println(abbreviate("*= internationalization"));
+        System.out.println(abbreviate("abc  internationalization"));
+        System.out.println(abbreviate("abc"));
+    }
+}
