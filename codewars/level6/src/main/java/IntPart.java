100b83965380c7dc8a8a39b8b7640fcac7a84a5f
cynepCTAPuk <CTAPuk@gmail.com>
2019-07-17 20:46:48 +0300

    Getting along with Integer Partitions

---
+++ codewars/level6/src/main/java/IntPart.java
@@ -0,0 +1,16 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class IntPart {
+    public static String part(long n) {
+
+
+        return null;
+    }
+
+    public static List eNum(long n){
+        List<Integer> list = new ArrayList<>();
+        
+        return null;
+    }
+}
