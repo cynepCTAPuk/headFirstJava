77cf2b24b80e11e46bda404d502d7f223af8e0b8
cynepCTAPuk <CTAPuk@gmail.com>
2019-07-20 20:23:04 +0300

    Persistent Bugger.

---
+++ codewars/level6/src/main/java/Persist.java
@@ -0,0 +1,30 @@
+class Persist {
+    public static int persistence(long n) {
+        long m = n;
+        int steps = 0;
+        while (String.valueOf(m).length() > 1) {
+            steps++;
+            long[] ints = digits(m);
+            m = 1;
+            for (long e : ints) m *= e;
+        }
+        return steps;
+    }
+
+    static long[] digits(long n) {
+        int length = String.valueOf(n).length();
+        long[] ints = new long[length];
+        for (int i = 0; i < length; i++) {
+            ints[i] = n % 10;
+            n /= 10;
+        }
+        return ints;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(persistence(39));
+        System.out.println(persistence(4));
+        System.out.println(persistence(25));
+        System.out.println(persistence(999));
+    }
+}
