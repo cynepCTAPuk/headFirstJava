87be7ceab8f2d3efa664a3ddb04ebaef2b4f4297
cynepCTAPuk <CTAPuk@gmail.com>
2019-07-23 12:47:40 +0300

    Sort - one, three, two

--- codewars/level5/src/main/java/Dinglemouse.java
+++ codewars/level5/src/main/java/Dinglemouse.java
@@ -1,19 +1,16 @@
 import java.util.Arrays;
 
 public class Dinglemouse {
-    private static final String[] units = {"", "one", "two", "three", "four", "five",
-            "six", "seven", "eight", "nine", "ten", "eleven", "twelve", "thirteen", "fourteen",
-            "fifteen", "sixteen", "seventeen", "eighteen", "nineteen"};
-    private static final String[] tens = {"", "", "twenty", "thirty", "forty", "fifty",
-            "sixty", "seventy", "eighty", "ninety"};
+    private static final String[] UNITS = {"", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine", "ten", "eleven", "twelve", "thirteen", "fourteen", "fifteen", "sixteen", "seventeen", "eighteen", "nineteen"};
+    private static final String[] TENS = {"", "", "twenty", "thirty", "forty", "fifty", "sixty", "seventy", "eighty", "ninety"};
 
     private static class Pair implements Comparable {
-        String string;
         Integer integer;
+        String string;
 
-        public Pair(String string, Integer integer) {
-            this.string = string;
+        public Pair(Integer integer, String string) {
             this.integer = integer;
+            this.string = string;
         }
 
         public Integer getInteger() {
@@ -32,20 +29,19 @@ public class Dinglemouse {
     }
 
     private static String convertNumberToWord(final int n) {
-        if (n < 20) return units[n];
-        if (n < 100) return tens[n / 10] + ((n % 10 != 0) ? " " : "") + units[n % 10];
-        return units[n / 100]
-                + " hundred" + ((n % 100 != 0) ? " " : "") + convertNumberToWord(n % 100);
+        if (n < 20) return UNITS[n];
+        if (n < 100) return TENS[n / 10] + ((n % 10 != 0) ? " " : "") + UNITS[n % 10];
+        return UNITS[n / 100] + " hundred" + ((n % 100 != 0) ? " " : "") + convertNumberToWord(n % 100);
     }
 
     public static int[] sort(final int[] array) {
         Pair[] pairs = new Pair[array.length];
         for (int i = 0; i < array.length; i++) {
-            if (array[i] == 0) {
-                pairs[i] = new Pair("zero", array[i]);
-            } else pairs[i] = new Pair(convertNumberToWord(array[i]), array[i]);
+            if (array[i] == 0) pairs[i] = new Pair(array[i], "zero");
+            else pairs[i] = new Pair(array[i], convertNumberToWord(array[i]));
         }
         Arrays.sort(pairs);
+
         int[] ints = new int[array.length];
         for (int i = 0; i < array.length; i++)
             ints[i] = pairs[i].getInteger();
---
+++ codewars/level5/src/main/java/Dinglemouse2.java
@@ -0,0 +1,37 @@
+import java.util.Arrays;
+import java.util.Comparator;
+
+public class Dinglemouse2 {
+    private static String[] UNITS = {"", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine", "ten", "eleven", "twelve", "thirteen", "fourteen", "fifteen", "sixteen", "seventeen", "eighteen", "nineteen"};
+    private static String[] TENS = {"", "", "twenty", "thirty", "forty", "fifty", "sixty", "seventy", "eighty", "ninety"};
+
+    public static int[] sort(final int array[]) {
+        return Arrays
+                .stream(array)
+                .boxed()
+                .sorted(Comparator.comparing(Dinglemouse2::toName))
+                .map(String::valueOf)
+                .mapToInt(Integer::valueOf)
+                .toArray();
+    }
+
+    public static String toName(int i) {
+        return i == 0 ? "zero" : toWords((i / 100) % 10, " hundred") + ((i / 100) % 10 != 0 ? " " : "") + toWords(i % 100);
+    }
+
+    private static String toWords(int i, String toAppend) {
+        return toWords(i) + (i > 0 ? toAppend : "");
+    }
+
+    private static String toWords(int i) {
+        return i > 19 ? TENS[i / 10] + (i / 10 != 0 ? " " : "") + UNITS[i % 10] : UNITS[i];
+    }
+
+    public static void main(String[] args) {
+        System.out.println(Arrays.toString(sort(new int[]{0, 1, 2, 3, 4, 5, 6, 7, 8, 9})));
+
+        System.out.println(Arrays.toString(sort(new int[]{8, 8, 9, 9, 10, 10})));
+        System.out.println(Arrays.toString(sort(new int[]{1, 2, 3, 4})));
+        System.out.println(Arrays.toString(sort(new int[]{9, 99, 999})));
+    }
+}
