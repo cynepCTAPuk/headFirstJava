c73ecacb94afb3f5c43b95cec06a8678a8534f42
cynepCTAPuk <CTAPuk@gmail.com>
2019-07-24 19:54:13 +0300

    Bird Mountain

--- codewars/level5/src/main/java/BirdMountain.java
+++ codewars/level5/src/main/java/BirdMountain.java
@@ -1,19 +1,35 @@
-import java.util.Arrays;
-
 public class BirdMountain {
+    private static char[][] chars;
 
     public static int peakHeight(char[][] mountain) {
-        int n = 1;
-        for (int i = n; i < mountain.length; i++) {
-            for (int j = n; j < mountain[0].length; j++) {
-                if (i == n & mountain[i][j] == '^') mountain[i][j] = 'n';
-                if (i == mountain.length - 1 & mountain[i][j] == '^') mountain[i][j] = '1';
-                
-            }
+        chars = mountain;
+        int count = 0;
+        int prev = 0;
+        int n = 0;
+        char d = '1';
+        while (true) {
+            for (int i = 0; i < chars.length; i++)
+                for (int j = 0; j < chars[0].length; j++)
+                    if (isTouch(i, j, d)) {
+                        count++;
+                        chars[i][j] = d;
+                    }
+            if (count == prev) break;
+            else prev = count;
+            n++;
+            d++;
         }
-        n++;
-
         return n;
+    }
+
+    public static boolean isTouch(int i, int j, char d) {
+        char curr = chars[i][j];
+        if (i == 0 | j == 0 | i == chars.length - 1 | j == chars[0].length - 1) return curr == '^';
+        char left = chars[i][j - 1];
+        char right = chars[i][j + 1];
+        char top = chars[i - 1][j];
+        char bottom = chars[i + 1][j];
+        return (left < d | top < d | bottom < d | right < d) && curr == '^';
     }
 
     public static String printChars(char[][] chars) {
@@ -37,7 +53,8 @@ public class BirdMountain {
                 "  ^^^^^^      ".toCharArray(),
                 "  ^^^^        ".toCharArray()
         };
-        peakHeight(mountain);
+        System.out.println(peakHeight(mountain));
         System.out.println(printChars(mountain));
+//        System.out.println((int) ' ' + " " + (int) '^' + " " + (int) '1' + " " + ('1' + 1));
     }
 }
