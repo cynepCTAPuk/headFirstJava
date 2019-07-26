d537cc8825863fca832de62f4e027134e7286368
cynepCTAPuk <CTAPuk@gmail.com>
2019-07-26 20:38:36 +0300

    http://www.examclouds.com/ tests

---
+++ test/misc/src/examcloud/com/LocalDateTest.java
@@ -0,0 +1,10 @@
+package examcloud.com;
+
+import java.time.LocalDate;
+import java.util.Date;
+
+public class LocalDateTest {
+    public static void main(String[] args) {
+        LocalDate localDate;
+    }
+}
