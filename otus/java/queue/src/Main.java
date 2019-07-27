bb39c9656b3789354d038ba1b92a8bcb87e284da
cynepCTAPuk <CTAPuk@gmail.com>
2019-07-27 21:30:18 +0300

    otus java 30-Executors

--- otus/java/queue/src/Main.java
+++ otus/java/queue/src/Main.java
@@ -1,6 +1,7 @@
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Random;
+import java.util.concurrent.ExecutionException;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.Future;
@@ -10,10 +11,10 @@ public class Main {
     final static int SIZE = 1_000;
 
     public static void main(String[] args) {
-        MyQueue<Integer> queue = new MyQueue<>();
+        MyQueue<Integer> queue = new MyQueue<>(0);
         Random random = new Random(SEED);
-        int sum1 = 0, sum2 = 0;
 
+        int sum1 = 0;
         for (int i = 0; i < SIZE; i++) {
             int element = random.nextInt();
             queue.add(element);
@@ -21,7 +22,8 @@ public class Main {
         }
         System.out.printf("sum1 = %,d%n", sum1);
 
-        while (true) {
+        int sum2 = 0;
+        for (int i = 0; i < SIZE; i++) {
             Integer element = queue.poll();
             if (element == null) break;
             sum2 += element;
@@ -29,10 +31,8 @@ public class Main {
         System.out.printf("sum2 = %,d%n", sum2);
         System.out.flush();
 
-/*
         List<Future<Integer>> results = new ArrayList<>();
         ExecutorService executorService = Executors.newFixedThreadPool(2);
-
         Random random2 = new Random(SEED);
         for (int i = 0; i < SIZE; i++) {
             executorService.submit(() -> {
@@ -42,6 +42,16 @@ public class Main {
             Future<Integer> future = executorService.submit(() -> queue.poll());
             results.add(future);
         }
-*/
+        int sum3 = 0;
+        for (Future<Integer> result : results) {
+            try {
+                sum3 += result.get();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            } catch (ExecutionException e) {
+                e.printStackTrace();
+            }
+        }
+        System.out.printf("sum3 = %,d%n", sum3);
     }
 }
--- otus/java/queue/src/MyQueue.java
+++ otus/java/queue/src/MyQueue.java
@@ -3,12 +3,18 @@ import java.util.Queue;
 
 public class MyQueue<T> {
     private Queue<T> queue = new LinkedList<>();
+    T def;
+
+    public MyQueue(T def) {
+        this.def = def;
+    }
 
     public void add(T e) {
         queue.add(e);
     }
 
     public T poll() {
-        return queue.poll();
+        T value = queue.poll();
+        return value == null ? def : value;
     }
 }
