ba4623e5c1dd03410b8f61df3aa11d62d8e5cf6a
cynepCTAPuk <CTAPuk@gmail.com>
2019-07-28 19:48:56 +0300

    chapter 15 Lambda Expressions

--- HerbertSchildt/src/chap15_Lambda/MethodRefDemo2.java
+++ HerbertSchildt/src/chap15_Lambda/MethodRefDemo2.java
@@ -1,14 +1,14 @@
 package chap15_Lambda;
 
-class MyStringOps {
-    static String strReverse(String str) {
+class MyStringOps1 {
+    String strReverse(String str) {
         String result = "";
         for (int i = str.length() - 1; i >= 0; i--) result += str.charAt(i);
         return result;
     }
 }
 
-public class MethodRefDemo {
+public class MethodRefDemo2 {
     static String stringOp(StringFunc sf, String s) {
         return sf.func(s);
     }
@@ -17,7 +17,9 @@ public class MethodRefDemo {
         String inStr = "Lambdas add power to Java";
         String outStr;
 
-        outStr = stringOp(MyStringOps::strReverse, inStr);
+        MyStringOps1 strOps = new MyStringOps1();
+
+        outStr = stringOp(strOps::strReverse, inStr);
         System.out.println("Original string: " + inStr);
         System.out.println("String reversed: " + outStr);
     }
