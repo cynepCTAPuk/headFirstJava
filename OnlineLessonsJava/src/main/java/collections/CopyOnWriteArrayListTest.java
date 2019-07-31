494cbbb6dcc99107dd5487cfdff14a9196cde8cd
cynepCTAPuk <CTAPuk@gmail.com>
2019-07-31 23:01:40 +0300

    Advanced Java - Collections

--- OnlineLessonsJava/src/main/java/collections/CopyOnWriteArrayListTest.java
+++ OnlineLessonsJava/src/main/java/collections/CopyOnWriteArrayListTest.java
@@ -6,35 +6,34 @@ import java.util.List;
 import java.util.concurrent.*;
 
 public class CopyOnWriteArrayListTest {
-    static List<Integer> copyOnWriteArrayList = new CopyOnWriteArrayList<>();
-    static List<Integer> synchronizedList = Collections.synchronizedList(new ArrayList<>());
+    private static List<Integer> copyOnWriteArrayList = new CopyOnWriteArrayList<>();
+    private static List<Integer> synchronizedList = Collections.synchronizedList(new ArrayList<>());
+    private static final int SIZE = 100_000;
 
     public static void main(String[] args) throws ExecutionException, InterruptedException {
-        int size = 100_000;
-        fillList(synchronizedList, size);
-        fillList(copyOnWriteArrayList, size);
+        fillList(synchronizedList);
+        fillList(copyOnWriteArrayList);
 
         System.out.println("List synchronized: ");
-        checkListByGet(synchronizedList, size);
+        checkList(synchronizedList);
 
         System.out.println("CopyOnWriteArrayList: ");
-        checkListByGet(copyOnWriteArrayList, size);
+        checkList(copyOnWriteArrayList);
     }
 
-    private static void checkListByGet(List<Integer> list, int size)
-            throws ExecutionException, InterruptedException {
+    private static void checkList(List<Integer> list) throws ExecutionException, InterruptedException {
         CountDownLatch latch = new CountDownLatch(1);
         ExecutorService ex = Executors.newFixedThreadPool(2);
-        Future<Long> f1 = ex.submit(new ListRunner(0, size / 2, list, latch));
-        Future<Long> f2 = ex.submit(new ListRunner(size / 2, size, list, latch));
+        Future<Long> f1 = ex.submit(new ListRunner(0, SIZE / 2, list, latch));
+        Future<Long> f2 = ex.submit(new ListRunner(SIZE / 2, SIZE, list, latch));
         latch.countDown();
         System.out.printf("Thread 1: %,8d\n", f1.get() / 1_000);
-        System.out.printf("Thread 1: %,8d\n", f2.get() / 1_000);
+        System.out.printf("Thread 2: %,8d\n", f2.get() / 1_000);
         ex.shutdown();
     }
 
-    private static void fillList(List<Integer> list, int size) {
-        for (int i = 0; i < size; i++) list.add(i);
+    private static void fillList(List<Integer> list) {
+        for (int i = 0; i < SIZE; i++) list.add(i);
     }
 
     static class ListRunner implements Callable<Long> {
@@ -54,7 +53,10 @@ public class CopyOnWriteArrayListTest {
         public Long call() throws InterruptedException {
             latch.await();
             long startTime = System.nanoTime();
-            for (int i = start; i < end; i++) list.get(i);
+            for (int i = start; i < end; i++) {
+                list.get(i);
+//                if (i % 100_000 == 0) list.add(i);
+            }
             return System.nanoTime() - startTime;
         }
     }
