8e2b4948b621a4ace65aa192ab7a1a6cb0a9246d
cynepCTAPuk <CTAPuk@gmail.com>
2019-08-03 20:19:21 +0300

    Herbert Schildt chap09 interface - private method

--- HerbertSchildt/src/chap09_Package_Interface/IntStack.java
+++ HerbertSchildt/src/chap09_Package_Interface/IntStack.java
@@ -1,14 +1,34 @@
 package chap09_Package_Interface;
 
-// Define an integer stack interface
+// 1) Define an integer stack interface
+// 2) Another version of IntStack that has a private interface method that is used by two default methods
+
 interface IntStack {
     void push(int item); // store an item
 
     int pop(); // retrieve an item
+
     // Because clear() has a default, it need not be
     // implemented by a preexisting class that uses IntStack.
     default void clear() {
         System.out.println("clear() not implemented");
         throw new UnsupportedOperationException();
+    }
+
+    // A default method that returns an array that contains the top n elements on the stack
+    default int[] popNElements(int n) {
+        // Return the requested elements
+        return getElements(n);
+    }
+
+    default int[] skipAndPopNElements(int skip, int n) {
+        getElements(skip);
+        return getElements(n);
+    }
+
+    private int[] getElements(int n) {
+        int[] elements = new int[n];
+        for (int i = 0; i < n; i++) elements[i] = pop();
+        return elements;
     }
 }
