ad32f014ffe324de2b2775db015b2ef5e392553a
cynepCTAPuk <CTAPuk@gmail.com>
2019-08-06 15:20:07 +0300

    Herbert Schildt chap11 Multithreading Programming - Creating Thread

---
+++ HerbertSchildt/src/chap11_Multithreading/ExtendThreadDemo.java
@@ -0,0 +1,40 @@
+package chap11_Multithreading;
+
+// Create a second thread by extending Thread
+class NewThread1 extends Thread {
+    public NewThread1() {
+        // Create a new, second thread
+        super("Demo Thread");
+        System.out.println("Child thread: " + this);
+    }
+
+    // This is the entry point for the second thread
+    @Override
+    public void run() {
+        try {
+            for (int i = 5; i > 0; i--) {
+                System.out.println("Child Thread: " + i);
+                Thread.sleep(500);
+            }
+        } catch (InterruptedException e) {
+            System.out.println("Child interrupted");
+        }
+        System.out.println("Exiting child thread");
+    }
+}
+
+public class ExtendThreadDemo {
+    public static void main(String[] args) {
+        NewThread1 nt = new NewThread1(); // create a new thread
+        nt.start(); // Start the thread
+        try {
+            for (int i = 5; i > 0; i--) {
+                System.out.println("Main Thread: " + i);
+                Thread.sleep(1_000);
+            }
+        } catch (InterruptedException e) {
+            System.out.println("Main thread interrupted");
+        }
+        System.out.println("Main thread exiting");
+    }
+}
