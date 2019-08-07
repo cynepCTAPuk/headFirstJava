ae6620899707bdb45b3df36509d76bfa44fc8107
cynepCTAPuk <CTAPuk@gmail.com>
2019-08-07 20:25:40 +0300

    Herbert Schildt chap28 The Concurrency Utilities - CyclicBarrier

---
+++ HerbertSchildt/src/chap28_Concurrency/CyclicBarrierDemo.java
@@ -0,0 +1,46 @@
+package chap28_Concurrency;
+
+import java.util.concurrent.BrokenBarrierException;
+import java.util.concurrent.CyclicBarrier;
+
+// An example of CyclicBarrierDemo
+public class CyclicBarrierDemo {
+    public static void main(String[] args) {
+        CyclicBarrier cyclicBarrier = new CyclicBarrier(3, new BarrierAction());
+        System.out.println("Starting");
+        new Thread(new MyThread1(cyclicBarrier,"A")).start();
+        new Thread(new MyThread1(cyclicBarrier,"B")).start();
+        new Thread(new MyThread1(cyclicBarrier,"C")).start();
+    }
+}
+
+// A thread of execution that uses a CyclicBarrier
+class MyThread1 implements Runnable {
+    CyclicBarrier cyclicBarrier;
+    String name;
+
+    public MyThread1(CyclicBarrier cyclicBarrier, String name) {
+        this.cyclicBarrier = cyclicBarrier;
+        this.name = name;
+    }
+
+    @Override
+    public void run() {
+        System.out.println(name);
+        try {
+            cyclicBarrier.await();
+        } catch (BrokenBarrierException e) {
+            System.out.println(e);
+        } catch (InterruptedException e) {
+            System.out.println(e);
+        }
+    }
+}
+
+class BarrierAction implements Runnable {
+
+    @Override
+    public void run() {
+        System.out.println("Barrier Reached!");
+    }
+}
