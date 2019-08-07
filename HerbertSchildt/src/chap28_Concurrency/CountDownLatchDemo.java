9d79fef19cff3e16ff02baa3e42e1fff25219536
cynepCTAPuk <CTAPuk@gmail.com>
2019-08-07 20:12:51 +0300

    Herbert Schildt chap28 The Concurrency Utilities - CountDownLatch

--- HerbertSchildt/src/chap28_Concurrency/CountDownLatchDemo.java
+++ HerbertSchildt/src/chap28_Concurrency/CountDownLatchDemo.java
@@ -8,6 +8,7 @@ public class CountDownLatchDemo {
         CountDownLatch countDownLatch = new CountDownLatch(5);
         System.out.println("Starting");
         new Thread(new MyThread(countDownLatch)).start();
+        System.out.println("countDownLatch.getCount(): " + countDownLatch.getCount());
         try {
             countDownLatch.await();
         } catch (InterruptedException e) {
@@ -29,6 +30,7 @@ class MyThread implements Runnable {
         for (int i = 0; i < 5; i++) {
             System.out.println(i);
             countDownLatch.countDown(); // decrement count
+            System.out.println("countDownLatch.getCount(): " + countDownLatch.getCount());
         }
     }
 }
