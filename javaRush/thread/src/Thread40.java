3e843175a872a817ba4bfa01116f269cafd1b343
cynepCTAPuk <CTAPuk@gmail.com>
2019-08-08 20:55:26 +0300

    Threadом Java не испортишь

--- javaRush/thread/src/Thread40.java
+++ javaRush/thread/src/Thread40.java
@@ -1,5 +1,9 @@
-public class Thread40 {
+/*
+https://javarush.ru/groups/posts/2065-threadom-java-ne-isportishjh--chastjh-iv---callable-future-i-druzjhja
+Thread'ом Java не испортишь: Часть IV — Callable, Future и друзья
+ */
 
+public class Thread40 {
     public static void main(String[] args) throws InterruptedException {
         Runnable task = () -> System.out.println("Hello World");
         new Thread(task).start();
--- javaRush/thread/src/Thread50.java
+++ javaRush/thread/src/Thread50.java
@@ -1,6 +1,10 @@
+/*
+ https://javarush.ru/groups/posts/2078-threadom-java-ne-isportishjh--chastjh-v---executor-threadpool-fork-join-pool
+ Thread'ом Java не испортишь: Часть V — Executor, ThreadPool, Fork Join
+*/
 public class Thread50 {
 
-    public static void main(String[] args) throws InterruptedException {
+    public static void main(String[] args) {
         Runnable task = () -> System.out.println("Task executed");
         Thread thread = new Thread(task);
         thread.start();
