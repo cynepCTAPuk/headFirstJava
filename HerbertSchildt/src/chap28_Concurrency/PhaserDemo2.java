bbec9442abc87e52ea50deb78ac29c08a1e5406d
cynepCTAPuk <CTAPuk@gmail.com>
2019-08-08 13:50:42 +0300

    Herbert Schildt chap28 The Concurrency Utilities - Phaser

---
+++ HerbertSchildt/src/chap28_Concurrency/PhaserDemo2.java
@@ -0,0 +1,65 @@
+package chap28_Concurrency;
+// Extend Phaser and override onAdvance() so that only a specific number of phases are executed
+
+import java.util.concurrent.Phaser;
+
+// Extend MyPhaser to allow only a specific number of phases to be executed
+class MyPhaser extends Phaser {
+    int numPhases;
+
+    MyPhaser(int parties, int phaseCount) {
+        super(parties);
+        numPhases = phaseCount - 1;
+    }
+
+    // Override onAdvance() to execute the specified number of phases
+    @Override
+    protected boolean onAdvance(int phase, int registeredParties) {
+        // This println() statement is for illustration only.
+        // Normally, onAdvance() will not display output.
+        System.out.println("Phase " + phase + " completed\n");
+        // if all phases have completed, return true
+        if (phase == numPhases || registeredParties == 0) return true;
+        return false;
+    }
+}
+
+class MyThread3 implements Runnable {
+    Phaser phaser;
+    String name;
+
+    public MyThread3(Phaser phaser, String name) {
+        this.phaser = phaser;
+        this.name = name;
+        phaser.register();
+    }
+
+    @Override
+    public void run() {
+        while (!phaser.isTerminated()) {
+            System.out.println("Thread " + name + " Beginning Phase " + phaser.getPhase());
+            phaser.arriveAndAwaitAdvance();
+            // Pause a bit to prevent umbled output.This is for illustration only.
+            // It is not required for the proper operation of the phase
+            try {
+                Thread.sleep(100);
+            } catch (InterruptedException e) {
+                System.out.println(e);
+            }
+        }
+    }
+}
+
+public class PhaserDemo2 {
+    public static void main(String[] args) {
+        MyPhaser myPhaser = new MyPhaser(1, 4);
+        System.out.println("Starting\n");
+        new Thread(new MyThread3(myPhaser, "A")).start();
+        new Thread(new MyThread3(myPhaser, "B")).start();
+        new Thread(new MyThread3(myPhaser, "C")).start();
+        // Wait for the specified number of phases to complete
+        while (!myPhaser.isTerminated()) myPhaser.arriveAndAwaitAdvance();
+        System.out.println("The Phase is terminated");
+    }
+
+}
