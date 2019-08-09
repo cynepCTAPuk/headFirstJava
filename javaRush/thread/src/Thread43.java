afb0c8fd622b597f5b96fd1055825b1bd9e7bbf3
cynepCTAPuk <CTAPuk@gmail.com>
2019-08-09 15:21:54 +0300

    Threadом Java не испортишь: Часть IV — Callable, Future и друзья

--- javaRush/thread/src/Thread43.java
+++ javaRush/thread/src/Thread43.java
@@ -1,21 +1,26 @@
 import java.util.Arrays;
 import java.util.List;
 import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.ExecutionException;
 import java.util.stream.Stream;
 
 public class Thread43 {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException, ExecutionException {
         CompletableFuture<String> completed = CompletableFuture.completedFuture("Просто значение");
-
         CompletableFuture<Void> voidCompletableFuture = CompletableFuture.runAsync(() ->
-                System.out.println("run " + Thread.currentThread().getName())
-        );
-
+                System.out.println("run " + Thread.currentThread().getName()));
         CompletableFuture<String> supplier = CompletableFuture.supplyAsync(() -> {
             System.out.println("supply " + Thread.currentThread().getName());
             return "Значение";
         });
+
+        Thread.sleep(1_000);
+        System.out.println(completed);
+        System.out.println(completed.get());
+        System.out.println(voidCompletableFuture);
+        System.out.println(supplier);
+        System.out.println(supplier.get());
 
         List<String> array = Arrays.asList("one", "two");
         Stream<String> stringStream = array.stream().map(value -> {
