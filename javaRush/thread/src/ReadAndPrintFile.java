b8465ba7afc49ec3500ab7aca9ef59cfc6eddfd1
cynepCTAPuk <CTAPuk@gmail.com>
2019-08-09 17:24:23 +0300

    Threadом Java не испортишь: Часть IV — Callable, Future и друзья

---
+++ javaRush/thread/src/ReadAndPrintFile.java
@@ -0,0 +1,21 @@
+import java.io.BufferedReader;
+import java.io.FileReader;
+
+public class ReadAndPrintFile {
+    public static void main(String[] args) {
+        read("src/ReadAndPrintFile.java");
+    }
+
+    private static void read(String file) {
+        StringBuilder stringBuilder = new StringBuilder();
+        try (BufferedReader reader = new BufferedReader(new FileReader(file))) {
+            String string;
+            while ((string = reader.readLine()) != null) {
+                stringBuilder.append(string).append("\n");
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        System.out.println(stringBuilder);
+    }
+}
--- javaRush/thread/src/Thread45.java
+++ javaRush/thread/src/Thread45.java
@@ -6,7 +6,8 @@ public class Thread45 {
     public static class NewsService {
         static String getMessage() {
             try {
-                Thread.sleep(3_000);
+                System.out.println("Ждём 2 секунды");
+                Thread.sleep(2_000);
             } catch (InterruptedException e) {
                 e.printStackTrace();
             }
@@ -16,24 +17,28 @@ public class Thread45 {
 
     public static void main(String[] args) throws ExecutionException, InterruptedException {
         Supplier newsSupplier = NewsService::getMessage;
+        System.out.println(newsSupplier.get());
 
         CompletableFuture<String> reader = CompletableFuture.supplyAsync(newsSupplier);
-        CompletableFuture.completedFuture("!!")
-                .thenCombine(reader, (a, b) -> b + a)
+
+        CompletableFuture
+                .completedFuture("!!")
+                .thenCombine(reader, (a, b) -> b + a + b)
                 .thenAccept(System.out::println)
                 .get();
 
-        CompletableFuture.completedFuture(2L)
+        CompletableFuture
+                .completedFuture(2L)
                 .thenCompose(val -> CompletableFuture.completedFuture(val + 2))
                 .thenAccept(System.out::println);
 
-        CompletableFuture.completedFuture(2L)
+        CompletableFuture
+                .completedFuture(2L)
                 .thenApply(a -> {
                     throw new IllegalStateException("error");
                 })
                 .thenApply(a -> 3L)
                 .exceptionally(ex -> 0L)
                 .thenAccept(System.out::println);
-
     }
 }
