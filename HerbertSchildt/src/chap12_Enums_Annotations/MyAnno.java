f22dc58dc31e9753b12dcfe9d7da7ca6376dabbc
cynepCTAPuk <CTAPuk@gmail.com>
2019-08-12 23:21:59 +0300

    Herbert Schildt chap12 Enums_Annotations Annotations

--- HerbertSchildt/src/chap12_Enums_Annotations/MyAnno.java
+++ HerbertSchildt/src/chap12_Enums_Annotations/MyAnno.java
@@ -1,6 +1,10 @@
 package chap12_Enums_Annotations;
 
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+
 // A simple annotation type
+@Retention(RetentionPolicy.RUNTIME)
 @interface MyAnno {
     String str();
     int val();
