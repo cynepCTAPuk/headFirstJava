9fe1e940873b2b4be9f90e42dfed9e163074aea1
cynepCTAPuk <CTAPuk@gmail.com>
2019-08-12 16:40:48 +0300

    Herbert Schildt chap12 Enums_Annotations Autoboxind and unboxing

---
+++ HerbertSchildt/src/chap12_Enums_Annotations/AutoBox.java
@@ -0,0 +1,11 @@
+package chap12_Enums_Annotations;
+
+// Demonstrate autoboxing/unboxing
+public class AutoBox {
+    public static void main(String[] args) {
+        Integer iOb = 100; // autobox an int
+        int i = iOb;
+        System.out.println(i + " " + iOb);
+
+    }
+}
---
+++ HerbertSchildt/src/chap12_Enums_Annotations/AutoBox2.java
@@ -0,0 +1,17 @@
+package chap12_Enums_Annotations;
+
+// Autoboxing/unboxing takes place with method parameters and return values
+public class AutoBox2 {
+    // Take an Integer parameter and return an int value
+    static int m(Integer v) {
+        return v; // auto-unbox to int
+    }
+
+    public static void main(String[] args) {
+        // Pass an int to m() and assign the return value to an Integer. Here, the argument 100
+        // into an Integer. The return value is also autoboxed into an Integer
+        Integer iOb = m(100);
+        System.out.println(iOb);
+
+    }
+}
