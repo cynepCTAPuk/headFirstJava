821d2869d68ab85ec60d593ede9bfa329936fc2d
cynepCTAPuk <CTAPuk@gmail.com>
2019-08-14 15:23:17 +0300

    Horstmann - Core Java - chap03

--- Horstmann/vol1/src/chap03_Fundamental/Constants2.java
+++ Horstmann/vol1/src/chap03_Fundamental/Constants2.java
@@ -7,6 +7,9 @@ public class Constants2 {
         double paperWidth = 8.5;
         double paperHeight = 11;
         System.out.println("Paper size in centimeters: "
-                + paperWidth * CM_PER_INCH + " by " + paperHeight * CM_PER_INCH);
+                + paperWidth * CM_PER_INCH + " by "
+                + paperHeight * CM_PER_INCH);
+
+        System.out.println("The square root of \u03c0 is " + Math.sqrt(Math.PI));
     }
 }
