a8a223f562e0826a3c10fe3d5a0515e4903aec56
cynepCTAPuk <CTAPuk@gmail.com>
2019-08-15 17:46:33 +0300

    Horstmann - Core Java - chap03 - Unicode

--- Horstmann/vol1/src/chap03_Fundamental/UnicodeTest.java
+++ Horstmann/vol1/src/chap03_Fundamental/UnicodeTest.java
@@ -14,16 +14,16 @@ public class UnicodeTest {
 
 // this character:  http://www.isthisthingon.org/unicode/index.php?page=1F&subpage=4&glyph=1F495
 // using code points here, not U+n notation for equivalence with U+n, below would be 0xnnnn
-        int k = 0;
-        int codePoint = 127_000 + k;
+        int codePoint = 127_744 - 1;
         System.out.printf("%,d\n", codePoint);
-        for (int i = codePoint; i < codePoint + 1000; i++) {
+        for (int i = codePoint; i < 128_704 + 2; i++) {
             char[] charPair = Character.toChars(i); // converting to char[] pair
 //            System.out.print(charPair.length);
-            System.out.print(charPair);
-            System.out.print(" ");
+//            System.out.print(charPair);
             // and to String, containing the character we want
             String symbol = new String(charPair);
+            System.out.print(symbol);
+            System.out.print(" ");
         }
 // we now have str with the desired character as the first item confirm
 // that we indeed have character with code point 128149
