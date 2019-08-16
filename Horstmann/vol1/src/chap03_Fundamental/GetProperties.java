8dcb48e9c5bc667055bd4dd9c5f33b87346e2305
cynepCTAPuk <CTAPuk@gmail.com>
2019-08-16 14:22:09 +0300

    Horstmann, CoreJava, volume1 - chap03 - get System Properties

--- Horstmann/vol1/src/chap03_Fundamental/GetProperties.java
+++ Horstmann/vol1/src/chap03_Fundamental/GetProperties.java
@@ -2,10 +2,11 @@ package chap03_Fundamental;
 
 public class GetProperties {
     public static void main(String[] args) {
-        String str = System.getProperties().toString();
-//        System.out.println(str);
+        String systemProperties = System.getProperties().toString();
+//        System.out.println(systemProperties);
+//        String[] strings = systemProperties.split(", ");
+//        for(String string: strings) System.out.println(string);
 
-        String[] strings = str.split(", ");
-        for(String string: strings) System.out.println(string);
+        System.out.println(systemProperties.replace(", ", "\n"));
     }
 }
