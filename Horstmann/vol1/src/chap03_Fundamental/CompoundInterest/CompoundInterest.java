c92c4382a1198f9d97f0814ae082cdffb6c1b152
cynepCTAPuk <CTAPuk@gmail.com>
2019-08-17 00:02:37 +0300

    Horstmann, CoreJava, volume1 - chap03 - Compound Interest

--- Horstmann/vol1/src/chap03_Fundamental/CompoundInterest/CompoundInterest.java
+++ Horstmann/vol1/src/chap03_Fundamental/CompoundInterest/CompoundInterest.java
@@ -1,57 +1,52 @@
+package chap03_Fundamental.CompoundInterest;
+
 /**
  * This program shows how to store tabular data in a 2D array.
- * @version 1.40 2004-02-10
+ *
  * @author Cay Horstmann
+ * @version 1.40 2004-02-10
  */
-public class CompoundInterest
-{
-   public static void main(String[] args)
-   {
-      final double STARTRATE = 10;
-      final int NRATES = 6;
-      final int NYEARS = 10;
+public class CompoundInterest {
+    public static void main(String[] args) {
+        final double STARTRATE = 1;
+        final int NRATES = 9;
+        final int NYEARS = 9;
+        final int INITIAL_SUM = 30_000;
 
-      // set interest rates to 10 . . . 15%
-      double[] interestRate = new double[NRATES];
-      for (int j = 0; j < interestRate.length; j++)
-         interestRate[j] = (STARTRATE + j) / 100.0;
+        // set interest rates to 10 . . . 15%
+        double[] interestRate = new double[NRATES];
+        for (int j = 0; j < interestRate.length; j++) interestRate[j] = (STARTRATE + j) / 100.0;
 
-      double[][] balances = new double[NYEARS][NRATES];
+        double[][] balances = new double[NYEARS][NRATES];
+        // set initial balances to 10000
+        for (int j = 0; j < balances[0].length; j++) balances[0][j] = INITIAL_SUM;
 
-      // set initial balances to 10000
-      for (int j = 0; j < balances[0].length; j++)
-         balances[0][j] = 10000;
+        // compute interest for future years
+        for (int i = 1; i < balances.length; i++) {
+            for (int j = 0; j < balances[i].length; j++) {
+                // get last year's balances from previous row
+                double oldBalance = balances[i - 1][j];
+                // compute interest
+                double interest = oldBalance * interestRate[j];
+                // compute this year's balances
+                balances[i][j] = oldBalance + interest;
+            }
+        }
 
-      // compute interest for future years
-      for (int i = 1; i < balances.length; i++)
-      {
-         for (int j = 0; j < balances[i].length; j++)
-         {
-            // get last year's balances from previous row
-            double oldBalance = balances[i - 1][j];
+        // print one row of interest rates
+        for (int j = 0; j < interestRate.length; j++)
+            System.out.printf("%10.0f%%", 100 * interestRate[j]);
 
-            // compute interest
-            double interest = oldBalance * interestRate[j];
+        System.out.println();
 
-            // compute this year's balances
-            balances[i][j] = oldBalance + interest;
-         }
-      }
+        // print balance table
+        int i = 0;
+        for (double[] row : balances) {
+            System.out.printf("%2d", i++);
+            // print table row
+            for (double b : row) System.out.printf("%,11.2f", b);
 
-      // print one row of interest rates
-      for (int j = 0; j < interestRate.length; j++)
-         System.out.printf("%9.0f%%", 100 * interestRate[j]);
-
-      System.out.println();
-
-      // print balance table
-      for (double[] row : balances)
-      {
-         // print table row
-         for (double b : row)
-            System.out.printf("%10.2f", b);
-
-         System.out.println();
-      }
-   }
+            System.out.println();
+        }
+    }
 }
