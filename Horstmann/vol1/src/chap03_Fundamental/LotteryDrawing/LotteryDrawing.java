7e03af027b0715cbb7eb55577861c6b27a7b8848
cynepCTAPuk <CTAPuk@gmail.com>
2019-08-16 23:47:45 +0300

    Horstmann, CoreJava, volume1 - chap03 - Array

--- Horstmann/vol1/src/chap03_Fundamental/LotteryDrawing/LotteryDrawing.java
+++ Horstmann/vol1/src/chap03_Fundamental/LotteryDrawing/LotteryDrawing.java
@@ -17,12 +17,13 @@ public class LotteryDrawing {
         System.out.print("What is the highest number you can draw? ");
         int n = in.nextInt();
         // fill an array with numbers 1 2 3 . . . n
+
         int[] numbers = new int[n];
-        for (int i = 0; i < numbers.length; i++)
-            numbers[i] = i + 1;
+        for (int i = 0; i < numbers.length; i++) numbers[i] = i + 1;
 
         // draw k numbers and put them into a second array
         int[] result = new int[k];
+
         for (int i = 0; i < result.length; i++) {
             // make a random index between 0 and n - 1
             int r = (int) (Math.random() * n);
@@ -36,7 +37,6 @@ public class LotteryDrawing {
         // print the sorted array
         Arrays.sort(result);
         System.out.println("Bet the following combination. It'll make you rich!");
-        for (int r : result)
-            System.out.println(r);
+        for (int r : result) System.out.println(r);
     }
 }
