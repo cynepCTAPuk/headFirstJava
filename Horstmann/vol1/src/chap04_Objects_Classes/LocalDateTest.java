616acfd8d10dfa77c85274793600712852caac0e
cynepCTAPuk <CTAPuk@gmail.com>
2019-08-18 21:24:30 +0300

    Horstmann, CoreJava, volume1 - chap04 - LocalDate test

--- Horstmann/vol1/src/chap04_Objects_Classes/LocalDateTest.java
+++ Horstmann/vol1/src/chap04_Objects_Classes/LocalDateTest.java
@@ -1,16 +1,18 @@
 package chap04_Objects_Classes;
 
 import java.time.LocalDate;
+import java.util.Calendar;
+import java.util.GregorianCalendar;
 
 public class LocalDateTest {
     public static void main(String[] args) {
         LocalDate now = LocalDate.now();
-        System.out.println(now);
+        System.out.println("Today is: " + now);
 
-        LocalDate date = LocalDate.of(1986, 5, 20);
-        System.out.println(date);
+        LocalDate date = LocalDate.of(2018, 6, 1);
+        System.out.println("Java begin: " + date);
 
-        System.out.println("Now until date: " + now.until(date));
+        System.out.println("Now until date: " + now.until(date) + "\n");
 
         int year = date.getYear();
         int month = date.getMonthValue();
@@ -25,5 +27,11 @@ public class LocalDateTest {
         System.out.println(year + "/" + (month < 10 ? "0" : "") + month
                 + "/" + (day < 10 ? "0" : "") + day);
 
+        GregorianCalendar someDay = new GregorianCalendar(2018, 6, 1);
+        System.out.println(someDay.get(Calendar.YEAR) + "-"
+                + someDay.get(Calendar.MONTH) + "-" + someDay.get(Calendar.DAY_OF_MONTH));
+        someDay.add(Calendar.DAY_OF_YEAR, 1_000);
+        System.out.println(someDay.get(Calendar.YEAR) + "-"
+                + someDay.get(Calendar.MONTH) + "-" + someDay.get(Calendar.DAY_OF_MONTH));
     }
 }
