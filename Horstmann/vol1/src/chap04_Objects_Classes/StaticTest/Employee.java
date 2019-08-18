40b1e92e57556bd9b4e3b6d2e5bc42a8e987b720
cynepCTAPuk <CTAPuk@gmail.com>
2019-08-18 23:42:07 +0300

    Horstmann, CoreJava, volume1 - chap04 -

---
+++ Horstmann/vol1/src/chap04_Objects_Classes/StaticTest/Employee.java
@@ -0,0 +1,42 @@
+package chap04_Objects_Classes.StaticTest;
+
+class Employee {
+    private static int nextId = 1;
+
+    private String name;
+    private double salary;
+    private int id;
+
+    public Employee(String n, double s) {
+        name = n;
+        salary = s;
+        id = 0;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public double getSalary() {
+        return salary;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId() {
+        id = nextId; // set id to next available id
+        nextId++;
+    }
+
+    public static int getNextId() {
+        return nextId; // returns static field
+    }
+
+    // unit test
+    public static void main(String[] args) {
+        var e = new Employee("Harry", 50_000);
+        System.out.println(e.getName() + " " + e.getSalary());
+    }
+}
