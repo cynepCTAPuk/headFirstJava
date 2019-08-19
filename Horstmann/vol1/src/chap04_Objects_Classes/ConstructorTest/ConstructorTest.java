a67c9bcb1bdcf86108443cced513e8be958f8222
cynepCTAPuk <CTAPuk@gmail.com>
2019-08-19 14:31:38 +0300

    Horstmann, CoreJava, volume1 - chap04 -

--- Horstmann/vol1/src/chap04_Objects_Classes/ConstructorTest/ConstructorTest.java
+++ Horstmann/vol1/src/chap04_Objects_Classes/ConstructorTest/ConstructorTest.java
@@ -19,58 +19,8 @@ public class ConstructorTest {
 
         // print out information about all Employee objects
         for (Employee e : staff)
-            System.out.println("name=" + e.getName() + ",id=" + e.getId()
-                    + ",salary=" + e.getSalary());
+            System.out.println("name=" + e.getName() + "," +
+                    "id=" + e.getId() + ",salary=" + e.getSalary());
     }
 }
 
-class Employee {
-    private static int nextId;
-    private int id;
-    private String name = ""; // instance field initialization
-    private double salary;
-
-    // static initialization block
-    static {
-        var generator = new Random();
-        // set nextId to a random number between 0 and 9999
-        nextId = generator.nextInt(10_000);
-        System.out.println(nextId);
-    }
-
-    // object initialization block
-    {
-        id = nextId;
-        nextId++;
-    }
-
-    // three overloaded constructors
-    public Employee(String n, double s) {
-        name = n;
-        salary = s;
-    }
-
-    public Employee(double s) {
-        // calls the Employee(String, double) constructor
-        this("Employee #" + nextId, s);
-    }
-
-    // the default constructor
-    public Employee() {
-        // name initialized to ""--see above
-        // salary not explicitly set--initialized to 0
-        // id initialized in initialization block
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public double getSalary() {
-        return salary;
-    }
-
-    public int getId() {
-        return id;
-    }
-}
