7d320c4d686038e5074156170a4be96f3de73220
cynepCTAPuk <CTAPuk@gmail.com>
2019-08-20 18:27:33 +0300

    Horstmann, CoreJava, volume1 - chap04 - Documentation Comments

--- Horstmann/vol1/src/chap04_Objects_Classes/Card.java
+++ Horstmann/vol1/src/chap04_Objects_Classes/Card.java
@@ -1,20 +1,33 @@
 package chap04_Objects_Classes;
 
 /**
- * A {@code Card} object represents a playing card, such as "Queen of Hearts". 
+ * A @code Card object represents a playing card, such as "Queen of Hearts". 
  * A card has a suit (Diamond, Heart, Spade or Club) and 
  * a value (1 = Ace, 2 . . . 10, 11 = Jack, 12 = Queen, 13 = King)
- */
-
-/**
+ *
  * Объект класса <code>Card</code> имитирует игральную карту,
  * например, даму червей. Карта имеет масть и ранг
  * ( 1=туз, 2...10, 11=валет, 12=дама, 13=король).
+ *
+ *  * @author Cay Horstmann
+ *  * @version 1.13 2018-04-10
  */
 public class Card {
+    /**
+     * The "Diamond" card suit
+     */
+    public static final int DIAMOND = 0;
     /**
      * The "Hearts" card suit
      */
     public static final int HEARTS = 1;
+    /**
+     * The "Spade" card suit
+     */
+    public static final int SPADE = 2;
+    /**
+     * The "Club" card suit
+     */
+    public static final int CLUB = 3;
 
 }
