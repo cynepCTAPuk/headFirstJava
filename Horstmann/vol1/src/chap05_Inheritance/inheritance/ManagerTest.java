48c20f4f715169c942d1038bb5dae1d25445a2f4
cynepCTAPuk <CTAPuk@gmail.com>
2019-08-21 14:04:20 +0300

    Horstmann, CoreJava, volume1 - chap05 -

--- Horstmann/vol1/src/chap05_Inheritance/inheritance/ManagerTest.java
+++ Horstmann/vol1/src/chap05_Inheritance/inheritance/ManagerTest.java
@@ -9,16 +9,15 @@ package chap05_Inheritance.inheritance;
 public class ManagerTest {
     public static void main(String[] args) {
         // construct a Manager object
-        var boss = new Manager("Carl Cracker", 80000, 1987, 12, 15);
-        boss.setBonus(5000);
+        var boss = new Manager("Carl Cracker", 80_000, 1987, 12, 15);
+        boss.setBonus(5_000);
 
         var staff = new Employee[3];
 
         // fill the staff array with Manager and Employee objects
-
         staff[0] = boss;
-        staff[1] = new Employee("Harry Hacker", 50000, 1989, 10, 1);
-        staff[2] = new Employee("Tommy Tester", 40000, 1990, 3, 15);
+        staff[1] = new Employee("Harry Hacker", 50_000, 1989, 10, 1);
+        staff[2] = new Employee("Tommy Tester", 40_000, 1990, 3, 15);
 
         // print out information about all Employee objects
         for (Employee e : staff)
