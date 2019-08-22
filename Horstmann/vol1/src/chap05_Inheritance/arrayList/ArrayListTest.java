4164cd9f0b4443a2ae8bc7bb428e115bd5de0c09
cynepCTAPuk <CTAPuk@gmail.com>
2019-08-22 19:37:56 +0300

    Horstmann, CoreJava, volume1 - chap05 - ArrayList

--- Horstmann/vol1/src/chap05_Inheritance/arrayList/ArrayListTest.java
+++ Horstmann/vol1/src/chap05_Inheritance/arrayList/ArrayListTest.java
@@ -15,22 +15,28 @@ public class ArrayListTest {
         staff.add(new Employee("Harry Hacker", 50000, 1989, 10, 1));
         staff.add(new Employee("Tony Tester", 40000, 1990, 3, 15));
         staff.add(new Employee("Carl Cracker", 75000, 1987, 12, 15));
-//        staff.sort(Comparator.comparing(Employee::getName));
-//        staff.sort((o1, o2) -> (int) (o2.getSalary() - o1.getSalary()));
-//        staff.sort((o1, o2) -> o2.getHireDay().compareTo(o1.getHireDay()));
         {
             var backup = new Employee[staff.size()];
             staff.toArray(backup);
             for (Employee e : backup)
                 System.out.printf("%-13s - %,9.1f\n", e.getName(), e.getSalary());
         }
+        System.out.println("Sort");
+//        staff.sort(Comparator.comparing(Employee::getName));
+//        staff.sort((o1, o2) -> (int) (o2.getSalary() - o1.getSalary()));
+//        staff.sort((o1, o2) -> o2.getHireDay().compareTo(o1.getHireDay()));
         staff.sort(ArrayListTest::compare);
         // raise everyone's salary by 5%
         for (Employee e : staff) e.raiseSalary(5);
         // print out information about all Employee objects
         print(staff);
-        staff.remove(0);
+
+        System.out.println("Remove: " + staff.remove(0).getName());
         print(staff);
+
+        var list = new ArrayList<Integer>();
+        list.add(30);
+        System.out.println(list.get(0));
     }
 
     private static void print(ArrayList<Employee> staff) {
