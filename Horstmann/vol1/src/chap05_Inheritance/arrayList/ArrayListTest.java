5fd13b6d5d11a3f57b4ebe7a4b8bb91681349a68
cynepCTAPuk <CTAPuk@gmail.com>
2019-08-22 16:00:26 +0300

    Horstmann, CoreJava, volume1 - chap05 - ArrayList

--- Horstmann/vol1/src/chap05_Inheritance/arrayList/ArrayListTest.java
+++ Horstmann/vol1/src/chap05_Inheritance/arrayList/ArrayListTest.java
@@ -11,20 +11,24 @@ import java.util.ArrayList;
 public class ArrayListTest {
     public static void main(String[] args) {
         // fill the staff array list with three Employee objects
-        var staff = new ArrayList<Employee>();
+        var staff = new ArrayList<Employee>(5);
 
-        staff.add(new Employee("Carl Cracker", 75000, 1987, 12, 15));
         staff.add(new Employee("Harry Hacker", 50000, 1989, 10, 1));
         staff.add(new Employee("Tony Tester", 40000, 1990, 3, 15));
+        staff.add(new Employee("Carl Cracker", 75000, 1987, 12, 15));
+
+//        staff.sort(Comparator.comparing(Employee::getName));
+        staff.sort((o1, o2) -> (int) (o2.getSalary() - o1.getSalary()));
+//        staff.sort((o1, o2) -> o1.getHireDay().compareTo(o2.getHireDay()));
+//        staff.sort((o1, o2) -> o2.getHireDay().compareTo(o1.getHireDay()));
 
         // raise everyone's salary by 5%
-        for (Employee e : staff)
-            e.raiseSalary(5);
+        for (Employee e : staff) e.raiseSalary(5);
 
         // print out information about all Employee objects
         for (Employee e : staff)
             System.out.println(
                     "name=" + e.getName() + ",salary=" + e.getSalary() + ",hireDay=" + e.getHireDay());
-        
+
     }
 }
