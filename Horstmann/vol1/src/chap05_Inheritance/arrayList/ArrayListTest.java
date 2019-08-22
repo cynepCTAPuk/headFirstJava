9f2e6f4563c2c7a8c548c8a6298d04c7aae91d01
cynepCTAPuk <CTAPuk@gmail.com>
2019-08-22 18:52:49 +0300

    Horstmann, CoreJava, volume1 - chap05 - ArrayList

--- Horstmann/vol1/src/chap05_Inheritance/arrayList/ArrayListTest.java
+++ Horstmann/vol1/src/chap05_Inheritance/arrayList/ArrayListTest.java
@@ -11,7 +11,7 @@ import java.util.ArrayList;
 public class ArrayListTest {
     public static void main(String[] args) {
         // fill the staff array list with three Employee objects
-        var staff = new ArrayList<Employee>(5);
+        var staff = new ArrayList<Employee>();
         staff.add(new Employee("Harry Hacker", 50000, 1989, 10, 1));
         staff.add(new Employee("Tony Tester", 40000, 1990, 3, 15));
         staff.add(new Employee("Carl Cracker", 75000, 1987, 12, 15));
