9cbdaf12b14395b3e19c165644d3ea7216891de0
cynepCTAPuk <CTAPuk@gmail.com>
2019-08-22 14:58:23 +0300

    Horstmann, CoreJava, volume1 - chap05 - hash code

--- Horstmann/vol1/src/chap05_Inheritance/equals/HashCodeTest.java
+++ Horstmann/vol1/src/chap05_Inheritance/equals/HashCodeTest.java
@@ -1,4 +1,13 @@
 package chap05_Inheritance.equals;
 
 public class HashCodeTest {
+    public static void main(String[] args) {
+        var s = "Ok";
+        var sb = new StringBuilder(s);
+        System.out.println(s.hashCode() + " " + sb.hashCode());
+        
+        var t = new String("Ok");
+        var tb = new StringBuilder(t);
+        System.out.println(t.hashCode() + " " + tb.hashCode());
+    }
 }
