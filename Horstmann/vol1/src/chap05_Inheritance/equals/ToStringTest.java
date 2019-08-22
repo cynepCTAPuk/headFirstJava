cbb9606cddd2547e7f0df0ed161a29373fb6e384
cynepCTAPuk <CTAPuk@gmail.com>
2019-08-22 15:23:47 +0300

    Horstmann, CoreJava, volume1 - chap05 - toString

--- Horstmann/vol1/src/chap05_Inheritance/equals/ToStringTest.java
+++ Horstmann/vol1/src/chap05_Inheritance/equals/ToStringTest.java
@@ -1,7 +1,13 @@
 package chap05_Inheritance.equals;
 
+import java.util.Arrays;
+
 public class ToStringTest {
     public static void main(String[] args) {
         System.out.println(System.out);
+
+        int[] luckyNumbers = {2, 3, 5, 7, 11, 13};
+        System.out.println(luckyNumbers);
+        System.out.println(Arrays.toString(luckyNumbers));
     }
 }
