a444a334dc2d83693fb7db23b8e792015a494f4e
cynepCTAPuk <CTAPuk@gmail.com>
2019-08-23 20:49:07 +0300

    Horstmann, CoreJava, volume1 - chap05 - Reflection

--- Horstmann/vol1/src/chap05_Inheritance/resources/ResourceTest.java
+++ Horstmann/vol1/src/chap05_Inheritance/resources/ResourceTest.java
@@ -1,9 +1,11 @@
-package resources;
+package chap05_Inheritance.resources;
 
-import java.io.*;
-import java.net.*;
-import java.nio.charset.*;
-import javax.swing.*;
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.URL;
+import java.nio.charset.StandardCharsets;
+import javax.swing.ImageIcon;
+import javax.swing.JOptionPane;
 
 /**
  * @version 1.5 2018-03-15
@@ -14,15 +16,16 @@ public class ResourceTest
    public static void main(String[] args) throws IOException
    {
       Class cl = ResourceTest.class;
-      URL aboutURL = cl.getResource("about.gif");
+      URL aboutURL = cl.getResource("/chap05_Inheritance/resources/about.gif");
       var icon = new ImageIcon(aboutURL);
 
-      InputStream stream = cl.getResourceAsStream("data/about.txt");
+      InputStream stream = cl.getResourceAsStream("/chap05_Inheritance/resources/data/about.txt");
       var about = new String(stream.readAllBytes(), "UTF-8");
 
-      InputStream stream2 = cl.getResourceAsStream("/corejava/title.txt");      
+      InputStream stream2 = cl.getResourceAsStream("/chap05_Inheritance/corejava/title.txt");
       var title = new String(stream2.readAllBytes(), StandardCharsets.UTF_8).trim();
 
-      JOptionPane.showMessageDialog(null, about, title, JOptionPane.INFORMATION_MESSAGE, icon);
+      JOptionPane.showMessageDialog(null, about, title,
+              JOptionPane.INFORMATION_MESSAGE, icon);
    }
 }
