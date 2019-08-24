de343aa4ac73269f88ffd87e9f6bc58ef01f6103
cynepCTAPuk <CTAPuk@gmail.com>
2019-08-24 21:38:25 +0300

    Horstmann, CoreJava, volume1 - chap05 - Reflection

--- Horstmann/vol1/src/chap05_Inheritance/arrays/CopyOfTest.java
+++ Horstmann/vol1/src/chap05_Inheritance/arrays/CopyOfTest.java
@@ -12,10 +12,12 @@ import java.util.Arrays;
 public class CopyOfTest {
     public static void main(String[] args) {
         int[] a = {1, 2, 3};
+        System.out.println(Arrays.toString(a));
         a = (int[]) goodCopyOf(a, 10);
         System.out.println(Arrays.toString(a));
 
         String[] b = {"Tom", "Dick", "Harry"};
+        System.out.println(Arrays.toString(b));
         b = (String[]) goodCopyOf(b, 10);
         System.out.println(Arrays.toString(b));
 
@@ -41,15 +43,15 @@ public class CopyOfTest {
      * This method grows an array by allocating a new array of the same type and
      * copying all elements.
      *
-     * @param a the array to grow. This can be an object array or a primitive
-     *          type array
+     * @param a the array to grow. This can be an object array or a primitive type array
      * @return a larger array that contains all elements of a.
      */
     public static Object goodCopyOf(Object a, int newLength) {
         Class cl = a.getClass();
-        if (!cl.isArray())
-            return null;
+        if (!cl.isArray()) return null;
+
         Class componentType = cl.getComponentType();
+        System.out.println("componentType: " + componentType);
         int length = Array.getLength(a);
         Object newArray = Array.newInstance(componentType, newLength);
         System.arraycopy(a, 0, newArray, 0, Math.min(length, newLength));
