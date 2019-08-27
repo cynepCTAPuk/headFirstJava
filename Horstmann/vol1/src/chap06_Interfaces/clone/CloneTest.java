f92e776c817b230a1427ad62263fa2ad0c0e3e8a
cynepCTAPuk <CTAPuk@gmail.com>
2019-08-27 15:14:39 +0300

    Horstmann, CoreJava, volume1 - chap06 - example interfaces - clone

--- Horstmann/vol1/src/chap06_Interfaces/clone/CloneTest.java
+++ Horstmann/vol1/src/chap06_Interfaces/clone/CloneTest.java
@@ -1,5 +1,8 @@
 package chap06_Interfaces.clone;
 
+import java.lang.reflect.Field;
+import java.lang.reflect.Modifier;
+
 /**
  * This program demonstrates cloning.
  *
@@ -9,14 +12,27 @@ package chap06_Interfaces.clone;
 public class CloneTest {
     public static void main(String[] args) throws CloneNotSupportedException {
         var original = new Employee("John Q. Public", 50000);
+        var clone = original.clone();
         original.setHireDay(2000, 1, 1);
 
         Employee copy = original.clone();
-
         copy.raiseSalary(10);
         copy.setHireDay(2002, 12, 31);
 
         System.out.println("original=" + original);
         System.out.println("copy    =" + copy);
+
+        Class cl = clone.getClass();
+        System.out.println("clone   =" + clone);
+
+        Field[] fields = cl.getDeclaredFields();
+        for (Field f : fields) {
+            Class type = f.getType();
+            String name = f.getName();
+            System.out.print("   ");
+            String modifiers = Modifier.toString(f.getModifiers());
+            if (modifiers.length() > 0) System.out.print(modifiers + " ");
+            System.out.println(type.getSimpleName() + " " + name + ";");
+        }
     }
 }
