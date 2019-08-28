41b9f0d7970c0af878ce3f7aa307db4f49047981
cynepCTAPuk <CTAPuk@gmail.com>
2019-08-28 12:02:59 +0300

    Horstmann, CoreJava, volume1 - chap06 - Inner class

--- Horstmann/vol1/src/chap06_Interfaces/localInnerClass/LocalInnerClassTest.java
+++ Horstmann/vol1/src/chap06_Interfaces/localInnerClass/LocalInnerClassTest.java
@@ -1,8 +1,9 @@
 package chap06_Interfaces.localInnerClass;
 
-import java.awt.*;
-import java.awt.event.*;
-import java.time.*;
+import java.awt.Toolkit;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.time.Instant;
 
 import javax.swing.*;
 
@@ -15,7 +16,7 @@ import javax.swing.*;
 public class LocalInnerClassTest {
     public static void main(String[] args) {
         var clock = new TalkingClock();
-        clock.start(1000, true);
+        clock.start(1_000, true);
 
         // keep program running until the user selects "Ok"
         JOptionPane.showMessageDialog(null, "Quit program?");
@@ -36,8 +37,8 @@ class TalkingClock {
     public void start(int interval, boolean beep) {
         class TimePrinter implements ActionListener {
             public void actionPerformed(ActionEvent event) {
-                System.out.println("At the tone, the time is "
-                        + Instant.ofEpochMilli(event.getWhen()));
+                System.out.println(
+                        "At the tone, the time is " + Instant.ofEpochMilli(event.getWhen()));
                 if (beep) Toolkit.getDefaultToolkit().beep();
             }
         }
