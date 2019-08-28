20b39f2d94dabc7550ad12b490e306bb5ac8014d
cynepCTAPuk <CTAPuk@gmail.com>
2019-08-28 11:44:21 +0300

    Horstmann, CoreJava, volume1 - chap06 - Comparator.comparing

--- Horstmann/vol1/src/chap06_Interfaces/comparator/ComparatorTest.java
+++ Horstmann/vol1/src/chap06_Interfaces/comparator/ComparatorTest.java
@@ -1,23 +1,27 @@
 package chap06_Interfaces.comparator;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Collections;
-import java.util.Comparator;
+import java.util.*;
 
 public class ComparatorTest {
     public static void main(String[] args) {
         var people = new ArrayList<Person>();
-        people.add(new Person("John", "Forest", 42));
-        people.add(new Person("Bill", "White", 21));
+        people.add(new Person("Johnathan", "Forest", 37));
+        people.add(new Person("Billy", "White", 21));
         people.add(new Person("Mary", "Song", 21));
-        people.add(new Person("Anna", "Carpenter", 33));
-        people.add(new Person("Tom", "Bear", 19));
-
-        System.out.println(people);
+        people.add(new Person("Anna", "Carpenter", 43));
+        people.add(new Person("Tom", "Bear", 28));
 
         Collections.sort(people, Comparator.comparing(Person::getAge)
                 .thenComparing(Person::getLastName));
+        System.out.println(people);
+
+//        Collections.sort(people, Comparator.comparing(Person::getFirstName,
+//                (a, b) -> Integer.compare(a.length(), b.length())));
+        Collections.sort(people, Comparator.comparingInt(p -> p.getFirstName().length()));
+        System.out.println(people);
+
+        Collections.sort(people,Comparator.comparing(Person::getLastName,
+                Comparator.nullsFirst(Comparator.naturalOrder())));
         System.out.println(people);
     }
 }
