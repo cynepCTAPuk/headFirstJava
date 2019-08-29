64f1e317181b3dad81bcdba8f21cedd0be2917b6
cynepCTAPuk <CTAPuk@gmail.com>
2019-08-29 14:02:09 +0300

    Horstmann, CoreJava, volume1 - chap07 - Exceptions - StackTrace

--- Horstmann/vol1/src/chap07_Exceptions/stackTrace/StackTraceTest.java
+++ Horstmann/vol1/src/chap07_Exceptions/stackTrace/StackTraceTest.java
@@ -1,5 +1,7 @@
 package chap07_Exceptions.stackTrace;
 
+import java.io.PrintWriter;
+import java.io.StringWriter;
 import java.util.Scanner;
 
 /**
@@ -17,12 +19,20 @@ public class StackTraceTest {
      */
     public static int factorial(int n) {
         System.out.println("factorial(" + n + "):");
+/*
+        var t = new Throwable();
+        var out = new StringWriter();
+        t.printStackTrace(new PrintWriter(out));
+        String description = out.toString();
+        System.out.println(description);
+*/
         var walker = StackWalker.getInstance();
         walker.forEach(System.out::println);
+
         int r;
         if (n <= 1) r = 1;
         else r = n * factorial(n - 1);
-        System.out.println("return " + r);
+        System.out.printf("return %,d\n", r);
         return r;
     }
 
@@ -32,5 +42,11 @@ public class StackTraceTest {
             int n = in.nextInt();
             factorial(n);
         }
+/*
+        var in = new Scanner(System.in);
+        System.out.print("Enter n: ");
+        int n = in.nextInt();
+        factorial(n);
+*/
     }
 }
