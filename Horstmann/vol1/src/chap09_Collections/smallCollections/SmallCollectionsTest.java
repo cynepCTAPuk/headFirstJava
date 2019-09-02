525bfec775cf12176281acfb3d9ed9690feb03d9
cynepCTAPuk <CTAPuk@gmail.com>
2019-09-02 18:55:14 +0300

    Horstmann, CoreJava, volume1 - chap09 - Collections - Small Collections

--- Horstmann/vol1/src/chap09_Collections/smallCollections/SmallCollectionsTest.java
+++ Horstmann/vol1/src/chap09_Collections/smallCollections/SmallCollectionsTest.java
@@ -1,32 +1,29 @@
 package chap09_Collections.smallCollections;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Map;
-import java.util.Set;
+import java.util.*;
 
 public class SmallCollectionsTest {
     public static void main(String[] args) {
         List<String> names1 = List.of("Peter", "Paul", "Mary");
-        System.out.println(names1);
+        System.out.println("list: " + names1);
 //        names1.add("CTAPuk"); // java.lang.UnsupportedOperationException
-        Set<Integer> numbers = Set.of(1, 2, 3);
-        System.out.println(numbers);
+
+        Set<Integer> numbers1 = Set.of(1, 2, 3);
+        System.out.println("set:  " + numbers1);
+
         Map<String, Integer> scores1 = Map.of("Peter", 1, "Paul", 2, "Mary", 3);
-        System.out.print("1: ");
-        System.out.print(scores1.keySet());
-        System.out.println(scores1.values());
-
+        System.out.println("1: " + scores1.keySet() + ":" + scores1.values());
         Map<Integer, String> scores2 = Map.of(1, "Peter", 2, "Paul", 3, "Mary");
-        System.out.print("2: ");
-        System.out.print(scores2.keySet());
-        System.out.println(scores2.values());
+        System.out.println("2: " + scores2.keySet() + ":" + scores2.values());
 
-        var names2 = new ArrayList<>(List.of("Peter", "Paul", "Mary"));
+        var names2 = new TreeSet<>(names1);
         System.out.println(names2);
         names2.add("CTAPuk");
         System.out.println(names2);
-        var names3 = new ArrayList<>(numbers);
-        System.out.println(names3);
+
+        var numbers2 = new ArrayList<>(numbers1);
+        System.out.println(numbers2);
+        numbers2.add(4);
+        System.out.println(numbers2);
     }
 }
