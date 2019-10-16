094c4f799d8a1f5fd824e6be9969f6b367c87cbd
cynepCTAPuk <CTAPuk@gmail.com>
2019-10-16 14:33:36 +0300

    Task 01 & 02

---
+++ openedu.ru/CodeTheory/src/t01.java
@@ -0,0 +1,11 @@
+public class t01 {
+    public static void main(String[] args) {
+        String кульбит = "1101101001100110010";
+        String битовый = "011001011110101000101010";
+        String бит = "0110010";
+        String куль = "110110100110";
+
+        String answer = куль + битовый;
+        System.out.println(answer);
+    }
+}
---
+++ openedu.ru/CodeTheory/src/t02.java
@@ -0,0 +1,8 @@
+public class t02 {
+    public static void main(String[] args) {
+        String Д = "010";
+        String Ы = "100";
+        String К = "10";
+        
+    }
+}
