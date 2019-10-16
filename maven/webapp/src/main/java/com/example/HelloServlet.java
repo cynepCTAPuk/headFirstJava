33a8363ddae8a2fe1ce3dc5441ca1270cb7fb4c9
cynepCTAPuk <CTAPuk@gmail.com>
2019-10-16 20:16:57 +0300

    WebApp

--- maven/webapp/src/main/java/com/example/HelloServlet.java
+++ maven/webapp/src/main/java/com/example/HelloServlet.java
@@ -1,8 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package com.example;
 
 import java.io.IOException;
@@ -13,15 +8,15 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 /**
- *
  * @author CTAPuk
  */
 @WebServlet(urlPatterns = "/hello")
 public class HelloServlet extends HttpServlet {
 
     @Override
-    protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        resp.getWriter().append("Hello World! " + req.getMethod());
+    protected void service(HttpServletRequest req, HttpServletResponse response)
+            throws ServletException, IOException {
+        response.getWriter().append("Hello World! " + req.getMethod());
     }
-    
+
 }
