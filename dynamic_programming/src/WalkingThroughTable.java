82a9440a617fca5d2eaab96b8709335d70f30810
cynepCTAPuk <CTAPuk@gmail.com>
2019-10-19 22:42:54 +0300

    Dynamic Programming

--- dynamic_programming/src/WalkingThroughTable.java
+++ dynamic_programming/src/WalkingThroughTable.java
@@ -4,20 +4,35 @@
  * (влево и вверх перемещаться запрещено).
  * Посчитайте, сколько есть способов у игрока попасть в правую нижнюю клетку.
  */
-public class PedestrianWalk {
+public class WalkingThroughTable {
     public static void main(String[] args) {
-        int i = 20;
-        int j = 20;
+        int i = 36;
+        int j = 36;
         long t0;
 
         t0 = System.nanoTime();
         System.out.println(f(i, j));
         System.out.printf("%,15d\n", System.nanoTime() - t0);
 
+        t0 = System.nanoTime();
+        System.out.println(f1(i, j));
+        System.out.printf("%,15d\n", System.nanoTime() - t0);
     }
 
     private static int f(int i, int j) {
         if (i == 1 || j == 1) return 1;
         return f(i - 1, j) + f(i, j - 1);
+    }
+
+    private static int f1(int n, int m) {
+        int[][] array = new int[n][m];
+        for (int i = 0; i < n; i++) array[i][0] = 1;
+        for (int j = 1; j < m; j++) array[0][j] = 1;
+
+        for (int i = 1; i < n; i++)
+            for (int j = 1; j < m; j++)
+                array[i][j] = array[i - 1][j] + array[i][j - 1];
+
+        return array[n - 1][m - 1];
     }
 }
