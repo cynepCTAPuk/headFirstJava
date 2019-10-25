c4508b44ed9954c26705413f172893907f007d31
cynepCTAPuk <CTAPuk@gmail.com>
2019-10-25 19:21:44 +0300

    gradle-sprint-boot-project

--- gradle/spring-boot-project/src/main/java/hello/HelloGradleController.java
+++ gradle/spring-boot-project/src/main/java/hello/HelloGradleController.java
@@ -5,10 +5,8 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController("/")
 public class HelloGradleController {
-
     @GetMapping
     public String helloGradle() {
         return "Hello Gradle!";
     }
-
 }
