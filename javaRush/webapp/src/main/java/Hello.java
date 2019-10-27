b96457045cb519f2637e7d39032b8dbec77aa740
cynepCTAPuk <CTAPuk@gmail.com>
2019-10-27 14:39:43 +0300

    javaRush-webapp

--- javaRush/webapp/src/main/java/Hello.java
+++ javaRush/webapp/src/main/java/Hello.java
@@ -9,19 +9,17 @@ import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.io.PrintWriter;
 
-public class App extends HttpServlet {
+public class Hello extends HttpServlet {
     public String getGreeting() {
         return "Hello world.";
     }
 
     protected void doPost(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
-
     }
 
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
-        // https://www.oracle.com/technetwork/java/servlet-142430.html
         PrintWriter out = response.getWriter();
         out.println(this.getGreeting());
         out.close();
