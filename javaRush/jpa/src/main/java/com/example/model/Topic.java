129e7931da0dec203d23c596cece55170776d8fe
cynepCTAPuk <CTAPuk@gmail.com>
2019-10-28 16:40:11 +0300

    javaRush jpa

---
+++ javaRush/jpa/src/main/java/com/example/model/Topic.java
@@ -0,0 +1,13 @@
+package com.example.model;
+
+public class Topic {
+    private Category category;
+
+    public Category getCategory() {
+        return category;
+    }
+
+    public void setCategory(Category category) {
+        this.category = category;
+    }
+}
