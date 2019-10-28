1fe47424e30634bb39c728614541ed7b31ffa15b
cynepCTAPuk <CTAPuk@gmail.com>
2019-10-28 22:35:55 +0300

    javaRush jpa

--- javaRush/jpa/src/main/java/com/example/model/Category.java
+++ javaRush/jpa/src/main/java/com/example/model/Category.java
@@ -1,14 +1,38 @@
 package com.example.model;
 
+import javax.persistence.CascadeType;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.OneToMany;
+import java.util.HashSet;
+import java.util.Set;
+
+@Entity
 public class Category {
+    @Id
+    @GeneratedValue
     private Long id;
     private String title;
+
+    @OneToMany(cascade = CascadeType.ALL)
+    @JoinColumn(name = "topic_id")
+    private Set<Topic> topics = new HashSet<>();
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
 
     public String getTitle() {
         return title;
     }
 
-    public void setTitle(String title) {
-        this.title = title;
+    public Set<Topic> getTopics() {
+        return this.topics;
     }
 }
--- javaRush/jpa/src/main/java/com/example/model/Topic.java
+++ javaRush/jpa/src/main/java/com/example/model/Topic.java
@@ -1,13 +1,32 @@
 package com.example.model;
 
+import javax.persistence.*;
+
+@Entity
 public class Topic {
+    @Id
+    @GeneratedValue
+    private Long id;
+    private String title;
+
+    @ManyToOne
+    @JoinColumn(name = "category_id")
     private Category category;
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
 
     public Category getCategory() {
         return category;
     }
 
     public void setCategory(Category category) {
+        category.getTopics().add(this);
         this.category = category;
     }
 }
