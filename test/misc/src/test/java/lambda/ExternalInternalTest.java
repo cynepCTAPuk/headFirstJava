e4f4498d33dd39d40e9474de87b168562101b027
cynepCTAPuk <CTAPuk@gmail.com>
2019-11-05 16:16:04 +0300

    misc

--- test/misc/src/test/java/lambda/ExternalInternalTest.java
+++ test/misc/src/test/java/lambda/ExternalInternalTest.java
@@ -1,5 +1,27 @@
 package lambda;
 
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.util.Arrays;
+import java.util.List;
+
 public class ExternalInternalTest {
-    
+    @Test
+    public void testExternal() {
+        StringBuilder sb = new StringBuilder();
+        List<String> strings = Arrays.asList("Foo", "Bar", "Baz");
+        for (String s : strings) sb.append(s);
+        Assert.assertEquals("FooBarBaz", sb.toString());
+    }
+
+    @Test
+    public void testInternal() {
+        StringBuilder sb = new StringBuilder();
+        List<String> strings = Arrays.asList("Foo", "Bar", "Baz");
+//        strings.stream().filter((s) -> s.startsWith("F")).forEach(sb::append);
+        strings.parallelStream().filter((s) -> s.startsWith("F")).forEach(sb::append);
+        Assert.assertEquals("Foo", sb.toString());
+
+    }
 }
