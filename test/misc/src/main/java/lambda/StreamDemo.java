4318a3b37bfbd49eb69249a56caf1426298b5446
cynepCTAPuk <CTAPuk@gmail.com>
2019-11-06 14:28:04 +0300

    misc - stream

---
+++ test/misc/src/main/java/lambda/StreamDemo.java
@@ -0,0 +1,18 @@
+package lambda;
+
+import java.util.List;
+import java.util.concurrent.atomic.AtomicInteger;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+public class StreamDemo {
+    public static void main(String[] args) {
+        AtomicInteger aint = new AtomicInteger(0);
+        List<Integer> list = Stream
+//                .generate(aint::getAndIncrement)
+                .iterate(0, i -> i + 1)
+                .limit(10)
+                .collect(Collectors.toList());
+        System.out.println(list);
+    }
+}
