6eda4fc050439902efa5aa6e217eaa042521ea9f
cynepCTAPuk <CTAPuk@gmail.com>
2019-11-12 19:19:08 +0300

    gwt - first app

---
+++ gwt/MyWebApp/src/test/java/com/example/mywebapp/MyWebAppSuite.java
@@ -0,0 +1,14 @@
+package com.example.mywebapp;
+
+import com.example.mywebapp.client.MyWebAppTest;
+import com.google.gwt.junit.tools.GWTTestSuite;
+import junit.framework.Test;
+import junit.framework.TestSuite;
+
+public class MyWebAppSuite extends GWTTestSuite {
+  public static Test suite() {
+    TestSuite suite = new TestSuite("Tests for MyWebApp");
+    suite.addTestSuite(MyWebAppTest.class);
+    return suite;
+  }
+}
