052e420cdf777a47140b75d15a3e2fa326e38d4f
cynepCTAPuk <CTAPuk@gmail.com>
2019-11-20 22:05:15 +0300

    misc IO test

--- test/misc/src/main/java/inOut/InputStreamReaderTest2.java
+++ test/misc/src/main/java/inOut/InputStreamReaderTest2.java
@@ -4,7 +4,7 @@ import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
-public class InputStreamReaderTest {
+public class InputStreamReaderTest2 {
     public static void main(String[] args) throws IOException {
         InputStreamReader inputStreamReader = new InputStreamReader(
                 new FileInputStream("C:\\Users\\username\\Desktop\\testFile.txt"));
