d14f7a43c1c3143556f93dd18840aa744b089564
cynepCTAPuk <CTAPuk@gmail.com>
2019-11-21 23:11:39 +0300

    misc inOut

--- test/misc/src/main/java/inOut/BufferedReaderTest.java
+++ test/misc/src/main/java/inOut/BufferedReaderTest.java
@@ -1,10 +1,15 @@
 package inOut;
 
 import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
 import java.io.InputStreamReader;
 
 public class BufferedReaderTest {
-    public static void main(String[] args) {
-        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+    public static void main(String[] args) throws IOException {
+        InputStream inputStream = System.in;
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(inputStream));
+        String string = bufferedReader.readLine();
+        System.out.println(string);
     }
 }
