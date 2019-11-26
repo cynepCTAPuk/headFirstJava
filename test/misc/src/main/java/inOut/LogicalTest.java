0da8fd5c4a4a1297aa5a614ed59f3cfd5f72210f
cynepCTAPuk <CTAPuk@gmail.com>
2019-11-26 12:30:45 +0300

    misc

---
+++ test/misc/src/main/java/inOut/LogicalTest.java
@@ -0,0 +1,11 @@
+package inOut;
+
+public class LogicalTest {
+    public static void main(String[] args) {
+        if (false && true) {
+            System.out.println("hello");
+        } else {
+            System.out.println("world");
+        }
+    }
+}
