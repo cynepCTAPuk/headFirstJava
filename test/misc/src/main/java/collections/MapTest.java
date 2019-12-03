f0cc34b214cf5f640030edb05a8636a2ddb14899
cynepCTAPuk <CTAPuk@gmail.com>
2019-12-04 00:10:45 +0300

    -

--- test/misc/src/main/java/collections/MapTest.java
+++ test/misc/src/main/java/collections/MapTest.java
@@ -2,10 +2,12 @@ package collections;
 
 import java.util.HashMap;
 import java.util.Map;
+import java.util.TreeMap;
 
 public class MapTest {
     public static void main(String[] args) {
-        Map<Character, Integer> map = new HashMap<>();
+        Map<Character, Integer> hashMap = new HashMap<>();
+        Map<Character, Integer> treeMap = new TreeMap<>();
         
     }
 }
