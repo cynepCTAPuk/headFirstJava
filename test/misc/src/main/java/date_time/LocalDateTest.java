ce175d920856ce157003cf4aa434c871552509b3
cynepCTAPuk <CTAPuk@gmail.com>
2019-12-07 00:05:24 +0300

    -

--- test/misc/src/main/java/date_time/LocalDateTest.java
+++ test/misc/src/main/java/date_time/LocalDateTest.java
@@ -7,10 +7,14 @@ import java.util.Locale;
 
 public class LocalDateTest {
     public static void main(String[] args) {
-        Locale ruLocale = Locale.getDefault();
+        Locale ruLocale = new Locale("ru");
         LocalDate startJavaRush = LocalDate.of(2019, 11, 19);
         System.out.print(LocalDate.now().toEpochDay() - startJavaRush.toEpochDay() + 1
-                + "-й день с начала курса JavaRush и это было " + startJavaRush + " ");
+                + "-й день с начала курса JavaRush и это было "
+                + startJavaRush.getDayOfMonth() + "-го "
+                + startJavaRush.getMonth().getDisplayName(TextStyle.FULL, ruLocale) + " "
+                + startJavaRush.getYear() + "-го года "
+        );
         System.out.println(startJavaRush.getDayOfWeek().getDisplayName(TextStyle.FULL, ruLocale));
         System.out.print("Сегодня " + LocalDate.now() + " и это ");
         System.out.println(LocalDate.now().getDayOfWeek().getDisplayName(TextStyle.FULL, ruLocale));
