cab4135a671a81a704606347d78b5ff49cdaaff3
cynepCTAPuk <CTAPuk@gmail.com>
2019-12-17 00:29:25 +0300

    ---

--- test/misc/src/main/java/date_time/LocalDateTest.java
+++ test/misc/src/main/java/date_time/LocalDateTest.java
@@ -1,18 +1,29 @@
 package date_time;
 
+import java.time.Duration;
 import java.time.LocalDate;
+import java.time.LocalTime;
+import java.time.Period;
+import java.time.format.DateTimeFormatter;
 import java.time.format.TextStyle;
+import java.time.temporal.ChronoUnit;
 import java.util.Locale;
 
 public class LocalDateTest {
     public static void main(String[] args) {
         Locale ruLocale = new Locale("ru");
-        LocalDate startJavaRush = LocalDate.of(2019, 11, 19);
-        System.out.print("Сегодня " + LocalDate.now() + ", ");
-        System.out.println(LocalDate.now().getDayOfWeek().getDisplayName(TextStyle.FULL, ruLocale));
-
-        System.out.print(LocalDate.now().toEpochDay() - startJavaRush.toEpochDay() + 1
-                + "-й день с начала курса JavaRush, а это было "
+//        LocalDate startJavaRush = LocalDate.of(2019, 11, 19);
+//        LocalDate startJavaRush = LocalDate.parse("20191119", DateTimeFormatter.ofPattern("yyyyMMdd"));
+        LocalDate startJavaRush = LocalDate.parse("2019-11-19");
+        LocalDate now = LocalDate.now();
+        System.out.print("\n" + now + " " + LocalTime.now().format(DateTimeFormatter.ofPattern("HH:mm:ss")) + ", ");
+        System.out.println(now.getDayOfWeek().getDisplayName(TextStyle.FULL, ruLocale));
+//        System.out.print(Duration.between(startJavaRush.atStartOfDay(), now.atStartOfDay()).toDays() + 1);
+//        System.out.print(now.toEpochDay() - startJavaRush.toEpochDay() + 1);
+//        System.out.print(Period.between(startJavaRush, now).getDays() + 1);
+//        System.out.print(ChronoUnit.DAYS.between(startJavaRush, now) + 1);
+        System.out.print(startJavaRush.until(now, ChronoUnit.DAYS) + 1);
+        System.out.print("-й день с начала курса JavaRush, а это было "
                 + startJavaRush.getDayOfMonth() + "-го "
                 + startJavaRush.getMonth().getDisplayName(TextStyle.FULL, ruLocale) + " "
                 + startJavaRush.getYear() + "-го года, "
