a981e20e87092e82734c3305a61495f8ddc21747
cynepCTAPuk <CTAPuk@gmail.com>
2019-12-18 15:42:07 +0300

    ---

--- test/misc/src/main/java/inOut/FileCopy.java
+++ test/misc/src/main/java/inOut/FileCopy.java
@@ -8,9 +8,10 @@ import java.io.OutputStream;
 
 public class FileCopy {
     public static void main(String[] args) {
-        try (
-                InputStream fileInputStream = new FileInputStream("c:/000/data.txt");
-                OutputStream fileOutputStream = new FileOutputStream("c:/000/result1.txt")) {
+        String source = "c:/000/source.txt";
+        String destination = "c:/000/destination.txt";
+        try (InputStream fileInputStream = new FileInputStream(source);
+             OutputStream fileOutputStream = new FileOutputStream(destination)) {
             while (fileInputStream.available() > 0) {
                 int data = fileInputStream.read();
                 fileOutputStream.write(data);
--- test/misc/src/main/java/inOut/FileInputStreamTest.java
+++ test/misc/src/main/java/inOut/FileInputStreamTest.java
@@ -5,16 +5,20 @@ import java.io.FileInputStream;
 public class FileInputStreamTest {
     public static void main(String[] args) throws Exception {
         //создаем объект FileInputStream, привязанный к файлу «c:/data.txt».
-        FileInputStream inputStream = new FileInputStream("c:/000/1.txt");
+        FileInputStream fis = new FileInputStream("c:/000/1.txt");
         long sum = 0;
-
-        while (inputStream.available() > 0) {       //пока остались непрочитанные байты
-            int data = inputStream.read();      //прочитать очередной байт
+        int max = Integer.MIN_VALUE;
+        int min = Integer.MAX_VALUE;
+        while (fis.available() > 0) {       //пока остались непрочитанные байты
+            int data = fis.read();      //прочитать очередной байт
             System.out.print(data + " ");
             sum += data;                        //добавить его к общей сумме
+            if (data > max) max = data;
+            if (data < min) min = data;
         }
 
-        inputStream.close();                    // закрываем поток
-        System.out.println("\n" + sum);         //выводим сумму на экран.
+        fis.close();                    // закрываем поток
+        System.out.println("\n" + "sum: " + sum);         //выводим сумму на экран.
+        System.out.println("min: " + min + " max: " + max);
     }
 }
