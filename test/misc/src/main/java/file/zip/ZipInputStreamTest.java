2bf1372033568d334d0b728d5b9309b5f9bd85ee
cynepCTAPuk <CTAPuk@gmail.com>
2020-03-02 14:56:43 +0300

    ---

--- test/misc/src/main/java/file/zip/ZipInputStreamTest.java
+++ test/misc/src/main/java/file/zip/ZipInputStreamTest.java
@@ -8,10 +8,13 @@ import java.util.zip.ZipInputStream;
 
 public class ZipInputStreamTest {
     public static void main(String[] args) throws IOException {
-        File zipFile = new File("C:/000/zip.zip");
-
+//        File zipFile = new File("C:/000/zip.zip");
+        File zipFile = new File("C:/000/archive.zip");
         String fileSeparator = System.getProperty("file.separator");
         String absoluteZipFilePath = zipFile.getParent();
+        File unzipFolder = new File(absoluteZipFilePath + fileSeparator + "unzip");
+        if (!unzipFolder.exists()) unzipFolder.mkdir();
+
 
         ZipInputStream zis = new ZipInputStream(new FileInputStream(zipFile));
         ZipEntry entry;
@@ -19,17 +22,16 @@ public class ZipInputStreamTest {
         while ((entry = zis.getNextEntry()) != null) {
             if (!entry.isDirectory()) {
                 System.out.printf("%20s", entry);
-                System.out.printf("%,8d\t%-30s\n", entry.getSize(),
-                        new SimpleDateFormat("yyyy/MM/dd HH:mm")
-                                .format(new Date(entry.getTime())));
-            } else System.out.printf("%-20s\n", entry);
+                System.out.printf("%,8d\t%-30s\n"
+                        , entry.getSize()
+                        , new SimpleDateFormat("yyyy/MM/dd HH:mm").format(new Date(entry.getTime()))
+                );
+            } else
+                System.out.printf("%-20s\n", entry);
 
-            File file = new File(absoluteZipFilePath + "/unzip/" + entry);
-            System.out.println("file: " + file);
-            if (!file.exists()) {
-                System.out.println("create parent folder: "+ file.getParentFile());
-                file.getParentFile().mkdirs();
-            }
+            File file = new File(absoluteZipFilePath + "/unzip/" + entry.getName());
+            if (file.getParent() == null) file.getParentFile().mkdirs();
+
             FileOutputStream fos = new FileOutputStream(file);
             for (int data = zis.read(); data != -1; data = zis.read()) {
                 fos.write(data);
--- test/misc/src/main/java/file/zip/ZipInputStreamTest3.java
+++ test/misc/src/main/java/file/zip/ZipInputStreamTest3.java
@@ -1,9 +1,6 @@
 package file.zip;
 
-import java.io.FileInputStream;
-import java.io.FileOutputStream;
-import java.io.InputStream;
-import java.io.OutputStream;
+import java.io.*;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.nio.file.StandardCopyOption;
@@ -14,25 +11,38 @@ import java.util.zip.ZipInputStream;
 public class ZipInputStreamTest3 {
     public static void main(String args[]) throws Exception {
         String zipFile = "c:/000/archive.zip";
-//        String unzipDir = "c:/000/unzip";
-        String format = "Entry: %s len %d added %TD\n";
+        String unzipDir = "c:/000/unzip";
+        if (Files.notExists(Paths.get(unzipDir))) Files.createDirectory(Paths.get(unzipDir));
+        String format =
+                "name: %s\n" +
+                        "\tsize: %d\n" +
+                        "\tcompressed size: %d\n" +
+                        "\tmethod: %d\n" +
+                        "\tadded: %TD\n";
 
         byte[] buffer = new byte[2048];
 
-        InputStream fis = new FileInputStream(zipFile);
-        ZipInputStream zis = new ZipInputStream(fis);
+        try (ZipInputStream zipInputStream = new ZipInputStream(new FileInputStream(zipFile))) {
 
-        while (true) {
-            ZipEntry entry = zis.getNextEntry();
-            if (entry == null) break;
-            System.out.printf(format, entry.getName(), entry.getSize(), new Date(entry.getTime()));
-            String unzipFile = unzipDir + "/" + entry.getName();
-            OutputStream fos = new FileOutputStream(unzipFile);
-            int len = 0;
-            while ((len = zis.read(buffer)) > 0) fos.write(buffer, 0, len);
-            fos.close();
-            zis.closeEntry();
+            while (true) {
+                ZipEntry entry = zipInputStream.getNextEntry();
+                if (entry == null) break;
+                String unzipFile = unzipDir + File.separator + entry.getName();
+                OutputStream fileOutputStream = new FileOutputStream(unzipFile);
+                int len = 0;
+                while ((len = zipInputStream.read(buffer)) > 0)
+                    fileOutputStream.write(buffer, 0, len);
+
+                System.out.printf(format,
+                        entry.getName(),
+                        entry.getSize(),
+                        entry.getCompressedSize(),
+                        entry.getMethod(),
+                        new Date(entry.getTime()));
+
+                fileOutputStream.close();
+                zipInputStream.closeEntry();
+            }
         }
-        zis.close();
     }
 }
