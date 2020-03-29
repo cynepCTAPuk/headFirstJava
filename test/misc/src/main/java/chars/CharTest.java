55ee36dac84c31c4f0f422691e042342e65d9b50
cynepCTAPuk <CTAPuk@gmail.com>
2020-03-30 00:09:21 +0300

    ---

--- test/misc/src/main/java/chars/CharTest.java
+++ test/misc/src/main/java/chars/CharTest.java
@@ -5,6 +5,9 @@ public class CharTest {
         int i = 29;
         System.out.println(charExpression(i));
         System.out.println((int) '\\' + " " + (int) 'f');
+        char x = 50;
+        System.out.println(x);
+        System.out.println(((Object)x).getClass().getSimpleName());
     }
 
     public static char charExpression(int a) {
