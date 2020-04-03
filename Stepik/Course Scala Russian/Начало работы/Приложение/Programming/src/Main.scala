abb870e1aa321dba44cd011673eb525bddd2fc04
cynepCTAPuk <CTAPuk@gmail.com>
2020-04-03 17:31:11 +0300

    ---

--- Stepik/Course Scala Russian/Начало работы/Приложение/Programming/src/Main.scala
+++ Stepik/Course Scala Russian/Начало работы/Приложение/Programming/src/Main.scala
@@ -1,7 +1,9 @@
-import scala.io.StdIn
+import scala.io.StdIn.readLine
+
 object Main {
   def main(args: Array[String]): Unit = {
     // your code here
-
+    var line = readLine()
+    print("Hi, " + line + "!")
   }
 }
--- Stepik/Course Scala Russian/Начало работы/Приложение/Theory/src/Task.scala
+++ Stepik/Course Scala Russian/Начало работы/Приложение/Theory/src/Task.scala
@@ -1,8 +1,11 @@
 // You can experiment here, it won’t be checked
-
+import scala.io.StdIn.readLine
 object Task {
   def main(args: Array[String]): Unit = {
     // put your code here
-    println("Hello World!");
+    println("Hello, enter line")
+    val line = readLine()
+    print("you entered: ")
+    print(line)
   }
 }
