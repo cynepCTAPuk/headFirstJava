31e446835b55e562932fe74c864b8667332f330c
cynepCTAPuk <CTAPuk@gmail.com>
2020-04-05 23:55:46 +0300

    ---

---
+++ test/misc/src/main/java/in_out/FileRead.java
@@ -0,0 +1,30 @@
+package in_out;
+
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.List;
+
+public class FileRead {
+    public static void main(String[] args) throws IOException {
+        String FILE_NAME = "c:/000/2.txt";
+        try (BufferedReader reader = new BufferedReader(
+                new InputStreamReader(new FileInputStream(FILE_NAME), StandardCharsets.UTF_8))) {
+            String line;
+            while ((line = reader.readLine()) != null) {
+                System.out.println(line);
+            }
+        } catch (IOException e) {
+            // log error
+        }
+
+        List<String> lines = Files.readAllLines(Paths.get(FILE_NAME), StandardCharsets.UTF_8);
+        for (String line : lines) System.out.println(line);
+
+        Files.lines(Paths.get(FILE_NAME), StandardCharsets.UTF_8).forEach(System.out::println);
+    }
+}
--- test/misc/src/main/java/io/PrintStreamTest.java
+++
@@ -1,9 +0,0 @@
-package io;
-
-public class PrintStreamTest {
-    public static void main(String[] args) {
-        String name = "Kolan";
-        int age = 25;
-        System.out.format("My name is %s. My age is %d.", name, age);
-    }
-}
