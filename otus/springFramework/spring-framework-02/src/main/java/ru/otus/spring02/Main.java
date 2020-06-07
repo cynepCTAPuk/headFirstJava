4ca6d9c942fa5ece4bf4395afd4322eb93b0f4dd
cynepCTAPuk <CTAPuk@gmail.com>
2020-06-07 19:30:14 +0300

    ---

--- otus/springFramework/spring-framework-02/src/main/java/ru/otus/spring02/Main.java
+++ otus/springFramework/spring-framework-02/src/main/java/ru/otus/spring02/Main.java
@@ -1,5 +1,6 @@
 package ru.otus.spring02;
 
+import org.springframework.beans.factory.BeanFactory;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.annotation.ComponentScan;
 import ru.otus.spring02.domain.Person;
@@ -9,7 +10,8 @@ import ru.otus.spring02.service.PersonService;
 public class Main {
 
     public static void main(String[] args) {
-        AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext(Main.class);
+        BeanFactory context =
+                new AnnotationConfigApplicationContext(Main.class);
 
         PersonService service = context.getBean(PersonService.class);
 
