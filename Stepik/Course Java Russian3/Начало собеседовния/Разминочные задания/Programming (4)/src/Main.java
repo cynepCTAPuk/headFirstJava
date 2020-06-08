6c7a9517c66620294e287707c3e9dae268181a48
cynepCTAPuk <CTAPuk@gmail.com>
2020-06-08 12:13:08 +0300

    ---

--- Stepik/Course Java Russian3/Начало собеседовния/Разминочные задания/Programming (4)/src/Main.java
+++ Stepik/Course Java Russian3/Начало собеседовния/Разминочные задания/Programming (4)/src/Main.java
@@ -1,3 +1,13 @@
-void checkSortArrayLength(int[] array){
-    // enter your code
+void checkSortArrayLength(int[] a){
+    int size = a.length;
+    int[] d = new int[size];
+    for (int i = 0; i < size; i++) {
+        d[i] = 1;
+        for (int j = 0; j < i; j++)
+            if (a[i] > a[j])
+                d[i] = Math.max(d[j] + 1, d[i]);
+    }
+    int max = 0;
+    for (int i : d) if (max < i) max = i;
+    System.out.println(max);
 }
