6ac6973a131965d318e2757ef19d28f93393c23d
cynepCTAPuk <CTAPuk@gmail.com>
2020-06-11 18:06:36 +0300

    ---

---
+++ test/misc/src/main/java/primitive/Test.java
@@ -0,0 +1,11 @@
+package primitive;
+
+public class Test {
+    public static void main(String[] args) {
+        int i = 1;
+        i <<= 1;
+        System.out.println(i);
+        float f = 1;
+//        f <<= 1;
+    }
+}
