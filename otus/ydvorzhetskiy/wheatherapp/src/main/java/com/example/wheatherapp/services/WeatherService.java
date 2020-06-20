149980ae4e143d5908e393cee28326f87391f5c6
cynepCTAPuk <CTAPuk@gmail.com>
2020-06-20 16:57:31 +0300

    ---

---
+++ otus/ydvorzhetskiy/wheatherapp/src/main/java/com/example/wheatherapp/services/WeatherService.java
@@ -0,0 +1,9 @@
+package com.example.wheatherapp.services;
+
+import com.example.wheatherapp.model.Weather;
+
+import java.util.List;
+
+public interface WeatherService {
+    List<Weather> gWeather();
+}
---
+++ otus/ydvorzhetskiy/wheatherapp/src/test/java/com/example/wheatherapp/WheatherappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.wheatherapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WheatherappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
