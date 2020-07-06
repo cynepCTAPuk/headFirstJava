d85001268a7b14b578f7f5cdb89de1cbbaee5df6
cynepCTAPuk <CTAPuk@gmail.com>
2020-07-07 00:12:12 +0300

    JAXB - знакомство

---
+++ test/misc/src/main/java/jaxb/Test.java
@@ -0,0 +1,30 @@
+package jaxb;
+
+import javax.xml.bind.JAXBContext;
+import javax.xml.bind.JAXBException;
+import javax.xml.bind.Marshaller;
+import java.io.StringWriter;
+
+public class Test {
+    public static void main(String[] args) throws JAXBException {
+        //создание объекта для сериализации в XML
+        Cat cat = new Cat();
+        cat.name = "Murka";
+        cat.age = 5;
+        cat.weight = 4;
+
+        //писать результат сериализации будем в Writer(StringWriter)
+        StringWriter writer = new StringWriter();
+
+        //создание объекта Marshaller, который выполняет сериализацию
+        JAXBContext context = JAXBContext.newInstance(Cat.class);
+        Marshaller marshaller = context.createMarshaller();
+        marshaller.setProperty(Marshaller.JAXB_FORMATTED_OUTPUT, Boolean.TRUE);
+        // сама сериализация
+        marshaller.marshal(cat, writer);
+
+        //преобразовываем в строку все записанное в StringWriter
+        String result = writer.toString();
+        System.out.println(result);
+    }
+}
