aec71aeb1172555cddd478e9e73e0ee46ccb738d
cynepCTAPuk <CTAPuk@gmail.com>
2020-07-06 10:26:31 +0300

    ---

--- angularJs/monsterlessons.com/11/main.js
+++ angularJs/monsterlessons.com/11/main.js
@@ -1,12 +1,31 @@
 var app = angular.module('app', []);
 
+app.controller('mainCtrl', function ($scope) {
+    $scope.name = 'Bob';
+});
+/* app.directive('fooBar', function () {
+    return {
+        restrict: 'E',
+        transclude: true,
+        // template: 'This is my super directive! <div ng-transclude></dive>',
+        template: 'This is my super directive! <ng-transclude></ng-transclude>',
+        link: function (scope, element, attrs) {
+            console.log('This is my super directive!')
+        }
+    };
+}); */
 app.directive('fooBar', function () {
     return {
         restrict: 'E',
         transclude: true,
-        template: 'This is my super directive! <div ng-transclude></dive>',
-        link: function (scope, element, attrs) {
-            console.log('This is my super directive!')
+        // template: 'This is my super directive! <div ng-transclude></dive>',
+        template: 'This is my super directive!',
+        link: function (scope, element, attrs, ctrl, transclude) {
+            console.log('This is my super directive!');
+            transclude(scope, function (clone, scope) {
+                console.log(scope);
+                element.append(clone);
+            });
         }
     };
 });
