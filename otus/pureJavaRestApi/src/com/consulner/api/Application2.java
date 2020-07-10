8d4d6bdc864909790d2bc1919bb96fc08945d826
cynepCTAPuk <CTAPuk@gmail.com>
2020-07-10 15:46:12 +0300

    step-2

--- otus/pureJavaRestApi/src/com/consulner/api/Application2.java
+++ otus/pureJavaRestApi/src/com/consulner/api/Application2.java
@@ -6,21 +6,17 @@ import java.io.IOException;
 import java.io.OutputStream;
 import java.net.InetSocketAddress;
 
-class Application {
+class Application2 {
 
     public static void main(String[] args) throws IOException {
         int serverPort = 8000;
         HttpServer server = HttpServer.create(new InetSocketAddress(serverPort), 0);
         server.createContext("/api/hello", (exchange -> {
-            if ("GET".equals(exchange.getRequestMethod())) {
-                String respText = "Hello!";
-                exchange.sendResponseHeaders(200, respText.getBytes().length);
-                OutputStream output = exchange.getResponseBody();
-                output.write(respText.getBytes());
-                output.flush();
-            } else {
-                exchange.sendResponseHeaders(405, -1);// 405 Method Not Allowed
-            }
+            String respText = "Hello!";
+            exchange.sendResponseHeaders(200, respText.getBytes().length);
+            OutputStream output = exchange.getResponseBody();
+            output.write(respText.getBytes());
+            output.flush();
             exchange.close();
         }));
         server.setExecutor(null); // creates a default executor
