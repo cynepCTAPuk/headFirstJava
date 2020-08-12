acf091e783d6ba143d63a5943568bf9e73ccee2e
cynepCTAPuk <CTAPuk@gmail.com>
2020-08-12 20:22:56 +0300

    ---

--- test/misc/src/main/java/logical/OrderLogicalOps.java
+++ test/misc/src/main/java/logical/OrderLogicalOps.java
@@ -2,14 +2,14 @@ package logical;
 
 public class OrderLogicalOps {
     public static void main(String[] args) {
-        boolean[] booleans = new boolean[4];
+        boolean[] b = new boolean[4];
         for (int i = 0; i < 10; i++) {
-            for (int j = 0; j < booleans.length; j++) {
-                booleans[j] = !(Math.random() < 0.5);
+            for (int j = 0; j < b.length; j++) {
+                b[j] = !(Math.random() < 0.5);
             }
-            for(boolean b: booleans) System.out.print(b + " ");
+            for(boolean bool: b) System.out.print(bool + " ");
             System.out.println();
-            boolean result = booleans[0] && booleans[1] || booleans[2] && booleans[3];
+            boolean result = b[0] && b[1] || b[2] && b[3];
             System.out.println(i + ": " + result);
         }
     }
