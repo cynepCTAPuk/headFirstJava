2548d6d383d9f60b041ea40eb5556475f46279ae
cynepCTAPuk <CTAPuk@gmail.com>
2022-03-20 00:48:34 +0300

    Borisov Ripper 1

---
+++ EBorisovSpring/ripper1/src/main/java/com/example/demo/Main.java
@@ -0,0 +1,18 @@
+// https://www.youtube.com/watch?v=BmBr5diz8WA Евгений Борисов — Spring-потрошитель, часть 1
+package com.example.demo;
+
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class Main {
+    public static void main(String[] args) throws InterruptedException {
+        ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("context.xml");
+/*
+        while (true) {
+            Thread.sleep(1_000);
+            context.getBean(Quoter.class).sayQuote();
+        }
+*/
+        context.getBean(Quoter.class).sayQuote();
+
+    }
+}
---
+++ EBorisovSpring/ripper1/src/main/java/com/example/demo/ProfilingController.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+public class ProfilingController implements ProfilingControllerMBean {
+    private boolean enable = true;
+
+    public boolean isEnable() {
+        return enable;
+    }
+
+    public void setEnable(boolean enable) {
+        this.enable = enable;
+    }
+}
