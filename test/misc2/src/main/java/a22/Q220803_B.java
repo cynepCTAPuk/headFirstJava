898cb3ba9a4f270b2ec35185d9940548302658f2
cynepCTAPuk <CTAPuk@gmail.com>
2022-08-03 23:24:56 +0300

    --

---
+++ test/misc2/src/main/java/a22/Q220803_B.java
@@ -0,0 +1,20 @@
+package a22;
+
+abstract class Q220803_A {
+    int a = 8;
+    public Q220803_A() {
+        show();
+    }
+    abstract void show();
+}
+
+public class Q220803_B extends Q220803_A {
+    int a = 90;
+    void show() {
+        System.out.println(a);
+    }
+
+    public static void main(String[] args) {
+        new Q220803_B();
+    }
+}
