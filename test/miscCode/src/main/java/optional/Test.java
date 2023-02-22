55bf6d095b85debb60b1291b354bf33a21168072
cynepCTAPuk <CTAPuk@gmail.com>
2023-02-22 16:34:45 +0300

    Optional

--- test/miscCode/src/main/java/optional/Test.java
+++ test/miscCode/src/main/java/optional/Test.java
@@ -1,14 +1,23 @@
 package optional;
 
 import java.util.Optional;
+import java.util.function.Function;
 
 public class Test {
-    public static void main(String[] args) {
-        String hello = "Hello";
-        Optional<String> optional = Optional.of(hello);
-        System.out.println(optional.isPresent());
-        optional.ifPresent(s -> System.out.println(s.length()));
-        System.out.println(optional.get());
-        System.out.println(optional.orElse("oops..."));
-    }
+	public static void main(String[] args) throws Exception {
+		boolean isFooEqualsBar = Optional.of("foo").filter("bar"::equals).isPresent();
+		System.out.println(isFooEqualsBar);
+
+		String trimmed = Optional.of(" foo  ").map(String::trim).get();
+		System.out.println(trimmed);
+
+		Object num = Optional.of("1234").flatMap((Function<String, Optional<?>>) x -> Optional.of(Integer.parseInt(x))).orElse(-1);
+		System.out.println(num);
+
+		Double random = Optional.<Double>empty().orElseGet(Math::random);
+		System.out.println(random);
+
+		random = Optional.<Double>empty().orElseThrow(Exception::new);
+		System.out.println(random);
+	}
 }
