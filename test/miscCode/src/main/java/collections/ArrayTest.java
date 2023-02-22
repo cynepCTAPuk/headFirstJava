09c68d048a2a3cd3c9a99db0b96763d2913454bc
cynepCTAPuk <CTAPuk@gmail.com>
2023-02-22 16:34:10 +0300

    ArrayTest

--- test/miscCode/src/main/java/collections/ArrayTest.java
+++ test/miscCode/src/main/java/collections/ArrayTest.java
@@ -1,10 +1,21 @@
 package collections;
 
+import java.util.Arrays;
+
 class ArrayTest {
-    public static void main(String[] args) {
-        float[] f1, f2;
-        f1 = new float[10];
-        f2 = f1;
-        System.out.println("f2[0] = " + f2[0]);
-    }
+	public static void main(String[] args) {
+		int[] arr1 = {1, 2, 3};
+		int[][] a1 = {arr1};
+		System.out.println(Arrays.toString(arr1));
+		System.out.println(Arrays.deepToString(a1));
+		int[] arr2 = new int[]{1, 2, 3};
+		int[][] a2 = {arr2};
+		int[][] arr3 = {{1, 2, 3}, {1, 2, 3}, {1, 2, 3}};
+		System.out.println(Arrays.toString(arr3));
+		System.out.println(Arrays.deepToString(arr3));
+		int[][] arr4 = {{1, 2, 3}, {1, 2, 3}, {1, 2, 3}};
+
+		System.out.println(Arrays.deepEquals(a1, a2));
+		System.out.println(Arrays.deepEquals(arr3, arr4));
+	}
 }
