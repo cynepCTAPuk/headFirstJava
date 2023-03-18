8ac15c30ad5dd2b015f86e868e7044488b84d34e
cynepCTAPuk <CTAPuk@gmail.com>
2023-03-18 23:36:16 +0300

    text number to int

--- test/miscCode/src/main/java/primitive/Test.java
+++ test/miscCode/src/main/java/primitive/Test.java
@@ -1,13 +1,18 @@
 package primitive;
 
 public class Test {
-    public static void main(String[] args) {
-        String text = "0b11111100101";
-//        text = "03745";
-//        text = "0x7e5";
-        int value = text.toLowerCase().startsWith("0b")
-                ? Integer.parseInt(text.substring(2), 2)
-                : Integer.decode(text);
-        System.out.println(text + " = " + value);
-    }
+	public static void main(String[] args) {
+		String text = "0b11111100101";
+		System.out.println(text + " = " + decodeString(text));
+		text = "077";
+		System.out.println(text + " = " + decodeString(text));
+		text = "0xFF";
+		System.out.println(text + " = " + decodeString(text));
+	}
+
+	private static int decodeString(String text) {
+		return text.toLowerCase().startsWith("0b")
+				? Integer.parseInt(text.substring(2), 2)
+				: Integer.decode(text);
+	}
 }
