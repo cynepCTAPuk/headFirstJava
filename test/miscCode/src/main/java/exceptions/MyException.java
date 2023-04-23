eb02ff79d9a5e9ce53d40278d3612b5ec28a3e18
cynepCTAPuk <CTAPuk@gmail.com>
2023-04-23 21:08:34 +0300

    a bit log

--- test/miscCode/src/main/java/exceptions/MyException.java
+++ test/miscCode/src/main/java/exceptions/MyException.java
@@ -1,7 +1,14 @@
 package exceptions;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
 public class MyException extends RuntimeException {
+	private static final Logger log = LogManager.getLogger(MyException.class);
+
 	public MyException(String message) {
 		super(message);
+		log.error(message);
+//		System.out.println(message + " - MyException");
 	}
 }
--- test/miscCode/src/main/java/string/ReverseInteger.java
+++ test/miscCode/src/main/java/string/ReverseInteger.java
@@ -1,18 +1,23 @@
 package string;
 
 import exceptions.MyException;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
 public class ReverseInteger {
+	private static final Logger log = LogManager.getLogger(ReverseInteger.class);
+
 	public static void main(String[] args) {
 		String format = "%,20d%n";
-		int n = -1234567890;
+		int n = 1234567890;
 		System.out.printf(format, n);
-		System.out.printf(format, revers(n));
+		log.info(n);
+		System.out.printf(format, reverse(n));
 		System.out.printf(format, (Integer.MAX_VALUE));
-		System.out.printf(format, revers(Integer.MAX_VALUE));
+		System.out.printf(format, reverse(Integer.MAX_VALUE));
 	}
 
-	static long revers(long n) {
+	static long reverse(long n) {
 		if (n < 0) throw new MyException("Number must be positive");
 		StringBuilder stringBuilder = new StringBuilder(String.valueOf(n));
 		stringBuilder.reverse();
