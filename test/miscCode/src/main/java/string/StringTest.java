e7e007aac629f674372ea5092a605661136751c4
cynepCTAPuk <CTAPuk@gmail.com>
2023-08-04 19:37:11 +0300

    String UTF

--- test/miscCode/src/main/java/string/StringTest.java
+++ test/miscCode/src/main/java/string/StringTest.java
@@ -1,35 +1,19 @@
 package string;
 
 public class StringTest {
-    public static String toString(String string) {
-        System.out.println("toString");
-        return string;
-    }
-
     public static void main(String[] args) {
-/*
-        String string = "ПриветМир";
+        String string = "абвгдежзий";
         System.out.println(string.getBytes().length);
-        for (int i = 0; i < string.getBytes().length; i++) {
-            System.out.print(string.getBytes()[i]);
-        }
+        for (int i = 0; i < string.getBytes().length; i++) System.out.printf("%4s", string.getBytes()[i]);
         System.out.println();
-        System.out.println(string.length());
+
         String[] bytes = string.split("");
-        for (String s : bytes) {
-            System.out.format("%6s", s);
-        }
+        for (String s : bytes) System.out.format("%8s", s + "  ");
         System.out.println();
+
+        System.out.println(string.length());
         System.out.println("\uD83D\uDD0A");
         System.out.println("\u1148\u0097");
-*/
-/*
-        String s = "";
-        System.out.println(s.length());
-        System.out.println(s.isEmpty());
-*/
-
-        System.out.println(toString("main"));
     }
 
 }
